c9f1e9a34036d42e6de649bb7517bc1d00c3342c
Hans Pabst <hans.pabst@intel.com>
2014-04-04 11:11:29 +0200

    Another attempt to fix const-correctness.

--- include/micsmmmisc.h
+++ include/micsmmmisc.h
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #ifdef __MIC__
 __declspec( target (mic))
-inline __m512d   _MM512_LOADU_PD(const double* a) {
+inline __m512d   _MM512_LOADU_PD(double* a) {
   __m512d va= _mm512_setzero_pd();
   va=_mm512_loadunpacklo_pd(va, &a[0]);
   va=_mm512_loadunpackhi_pd(va, &a[8]);
@@ -11,7 +11,7 @@ inline __m512d   _MM512_LOADU_PD(const double* a) {
 
 
 __declspec( target (mic))
-inline void _MM512_STOREU_PD(const double* a,__m512d v) {
+inline void _MM512_STOREU_PD(double* a,__m512d v) {
   _mm512_packstorelo_pd(&a[0], v);
   _mm512_packstorehi_pd(&a[8], v);
 }
@@ -19,7 +19,7 @@ inline void _MM512_STOREU_PD(const double* a,__m512d v) {
 
 
 __declspec( target (mic))
-inline __m512d _MM512_MASK_LOADU_PD(const double* a, char mask) {
+inline __m512d _MM512_MASK_LOADU_PD(double* a, char mask) {
   __m512d va= _mm512_setzero_pd();
   va=_mm512_mask_loadunpacklo_pd(va, mask,&a[0]);
   va=_mm512_mask_loadunpackhi_pd(va, mask,&a[8]);
@@ -27,7 +27,7 @@ inline __m512d _MM512_MASK_LOADU_PD(const double* a, char mask) {
 }
 
 __declspec( target (mic))
-inline void _MM512_MASK_STOREU_PD(const double* a,__m512d v, char mask) {
+inline void _MM512_MASK_STOREU_PD(double* a,__m512d v, char mask) {
   _mm512_mask_packstorelo_pd(&a[0], mask,v);
   _mm512_mask_packstorehi_pd(&a[8],mask, v);
 }
