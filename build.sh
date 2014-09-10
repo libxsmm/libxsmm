72e82ba52d97d6ff35b15314446b8e0c0b4be0c0
Hans Pabst <hans.pabst@intel.com>
2014-09-10 09:33:35 +0200

    Adjusted repository layout. Cleanup.

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+DIRINC=include
+DIRSRC=src/mic
 SIZE=24
 
 if [ "$1" != "" ] ; then
@@ -7,34 +9,33 @@ if [ "$1" != "" ] ; then
 fi
 
 
-python scripts/generate_inc.py $SIZE $SIZE $SIZE  > include/micsmm.h
+python scripts/generate_inc.py $SIZE $SIZE $SIZE > $DIRINC/micsmm.h
 #python scripts/generate_lib.py 1 $SIZE > src/micsmm.c
 #python scripts/generate_ben.py 4 32 > src/micsmmbench.c
-rm src/gemms/*.cpp
-rm src/gemms/*.c
-rm src/gemms/*.o
-rm src/gemms/*.a
-#echo "#include <immintrin.h>" >> src/gemms/igemm.c
-#echo "#include <micsmmmisc.h>" >> src/gemms/igemm.c
-#echo "#include <mkl.h>" >> src/gemms/igemm.c
+rm $DIRSRC/*.cpp
+rm $DIRSRC/*.c
+rm $DIRSRC/*.o
+rm $DIRSRC/*.a
+#echo "#include <immintrin.h>" >> $DIRSRC/igemm.c
+#echo "#include <micsmmmisc.h>" >> $DIRSRC/igemm.c
+#echo "#include <mkl.h>" >> $DIRSRC/igemm.c
 
-for m in `seq 1 $SIZE`; do
-  for n in `seq 1 $SIZE`; do
-    for k in `seq 1 $SIZE`; do
+for m in $(seq 1 $SIZE); do
+  for n in $(seq 1 $SIZE); do
+    for k in $(seq 1 $SIZE); do
       echo $m $n $k
-      #python scripts/generate_singleigemm.py $m $k $n > src/gemms/igemm_"$m"_"$k"_"$n".c
-      python scripts/generate_singleigemm.py $m $n $k>> src/gemms/smm_dnn_"$m"_"$n"_"$k".c
+      python scripts/generate_singleigemm.py $m $n $k >> $DIRSRC/smm_dnn_"$m"_"$n"_"$k".c
     done
   done
 done
 
-python scripts/generate_switchingfunction.py $SIZE $SIZE $SIZE  >> src/gemms/smm.c
+python scripts/generate_switchingfunction.py $SIZE $SIZE $SIZE >> $DIRSRC/smm.c
 
-cd src/gemms
-#icc  -c -I ../include/ micsmm.c
+cd $DIRSRC
+#icc -c -I ../$DIRINC micsmm.c
 make -j 16
 cd ../..
 cd tests
-icc -offload-attribute-target=mic -mkl=sequential  -I ../include/  -c benchmark.cpp
-icc -offload-attribute-target=mic -mkl=sequential benchmark.o ../src/gemms/micsmm.a
+icc -offload-attribute-target=mic -mkl=sequential -I ../$DIRINC -c benchmark.cpp
+icc -offload-attribute-target=mic -mkl=sequential benchmark.o ../$DIRSRC/micsmm.a
 cd ..
