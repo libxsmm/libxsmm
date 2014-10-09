9d9bd4c19f54f8bc51ce7a8cbece5d4a19f154ef
Hans Pabst <hans.pabst@intel.com>
2014-10-09 10:28:11 +0200

    Allows to separate the dispatch query cost from the execution of multiple calls to dc_smm_dnn where M, N, and K does not change (homogeneous stack in the notion of CP2K). Documented the functions in xsmm_knc.h. The latter file is now assembled from xsmm_knc.begin, the series of generated prototype declarations (xsmm_knc_geninc.py) and xsmm_knc.end. Code cleanup.

--- scripts/xsmm_knc_genmain.py
+++ scripts/xsmm_knc_genmain.py
@@ -44,12 +44,12 @@ def create_symmetric_interface(dimsM,dimsN,dimsK,RowMajor):
     print "}"
     print
     print
-    print "void xsmm_dnn(int M, int N, int K, const double* a, const double* b, double* c)"
+    print "dc_smm_dnn_function_type dc_smm_dnn_function(int M, int N, int K)"
     print "{"
-    print "  static const int index_m[] = { "+str(dimsM).strip("[]")+" }, nm = sizeof(index_m) / sizeof(M);"
-    print "  static const int index_n[] = { "+str(dimsN).strip("[]")+" }, nn = sizeof(index_n) / sizeof(N);"
-    print "  static const int index_k[] = { "+str(dimsK).strip("[]")+" }, nk = sizeof(index_k) / sizeof(K);"
-    print "  static void (*ftable[])(const double*, const double*, double*) = {"
+    print "  static const int index_m[] = { "+str(dimsM).strip("[]")+" }, nm = sizeof(index_m) / sizeof(*index_m);"
+    print "  static const int index_n[] = { "+str(dimsN).strip("[]")+" }, nn = sizeof(index_n) / sizeof(*index_n);"
+    print "  static const int index_k[] = { "+str(dimsK).strip("[]")+" }, nk = sizeof(index_k) / sizeof(*index_k);"
+    print "  static const dc_smm_dnn_function_type functions[] = {"
     for m in dimsM:
         for n in dimsN:
            sys.stdout.write("    ")
@@ -59,13 +59,19 @@ def create_symmetric_interface(dimsM,dimsN,dimsK,RowMajor):
     print "  };"
     print
     print "  int m, n, k;"
-    print "  if ((m = (int*)bsearch(&M, index_m, nm, sizeof(M), compareints) - index_m) < nm"
-    print "   && (n = (int*)bsearch(&N, index_n, nn, sizeof(N), compareints) - index_n) < nn"
-    print "   && (k = (int*)bsearch(&K, index_k, nk, sizeof(K), compareints) - index_k) < nk)"
-    print "  {"
-    print "    const int i = nk * (m * nn + n) + k;"
-    print "    void (*f)(const double*, const double*, double*) = ftable[i];"
-    print "    (*f)(a, b, c);"
+    print "  return ((m = (int*)bsearch(&M, index_m, nm, sizeof(*index_m), compareints) - index_m) < nm"
+    print "       && (n = (int*)bsearch(&N, index_n, nn, sizeof(*index_n), compareints) - index_n) < nn"
+    print "       && (k = (int*)bsearch(&K, index_k, nk, sizeof(*index_k), compareints) - index_k) < nk)"
+    print "    ? functions[nk*(m*nn+n)+k]"
+    print "    : 0;"
+    print "  }"
+    print "}"
+    print
+    print
+    print "void xsmm_dnn(int M, int N, int K, const double* a, const double* b, double* c)"
+    print "{"
+    print "  if (const dc_smm_dnn_function_type function = dc_smm_dnn_function(M, N, K)) {"
+    print "    (*function)(a, b, c);"
     print "  }"
     print "  else {"
     if 0 != RowMajor:
