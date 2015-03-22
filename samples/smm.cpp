14c9a7a1da7455840c411d7a2ffc75f784ef4438
Hans Pabst <hans.pabst@intel.com>
2015-03-22 08:34:47 +0100

    Code cleanup.

--- samples/smm.cpp
+++ samples/smm.cpp
@@ -130,20 +130,20 @@ int main(int argc, char* argv[])
     const int n = 4 < argc ? std::atoi(argv[4]) : m;
     const int k = 5 < argc ? std::atoi(argv[5]) : m;
 
-#if (0 != LIBXSMM_ALIGNED_STORES)
-# if (0 != LIBXSMM_ROW_MAJOR)
+#if (0 != LIBXSMM_ROW_MAJOR)
+# if (0 < LIBXSMM_ALIGNED_STORES)
     const int ldc = LIBXSMM_ALIGN_VALUE(int, T, n, LIBXSMM_ALIGNED_STORES);
-    const int csize = m * ldc + (LIBXSMM_ALIGNED_STORES) / sizeof(T) - 1;
 # else
-    const int ldc = LIBXSMM_ALIGN_VALUE(int, T, m, LIBXSMM_ALIGNED_STORES);
-    const int csize = n * ldc + (LIBXSMM_ALIGNED_STORES) / sizeof(T) - 1;
+    const int ldc = n;
 # endif
+    const int csize = m * ldc;
 #else
-# if (0 != LIBXSMM_ROW_MAJOR)
-    const int ldc = n, csize = m * ldc;
+# if (0 < LIBXSMM_ALIGNED_STORES)
+    const int ldc = LIBXSMM_ALIGN_VALUE(int, T, m, LIBXSMM_ALIGNED_STORES);
 # else
-    const int ldc = m, csize = n * ldc;
+    const int ldc = m;
 # endif
+    const int csize = n * ldc;
 #endif
 
     const int asize = m * k, bsize = k * n;
