7b95ef58df57f87cc507c4dc827ad14a386f0e4d
Hans Pabst <hans.pabst@intel.com>
2015-03-25 09:10:19 +0100

    Rely on "bash" being available on Windows command line; regenerate the header/source code when starting VS IDE.

--- ide/_vs2013.bat
+++ ide/_vs2013.bat
@@ -1,6 +1,10 @@
 @ECHO OFF
 SETLOCAL
 
+cd ..
+bash -c "make realclean ; make M=\"1 2 3 4 5 23\" ALIGNED_STORES=1 header source main"
+cd ide
+
 SET LIBXSMMROOT=%~d0%~p0\..
 
 CALL %~d0"%~p0"_vs.bat vs2013
