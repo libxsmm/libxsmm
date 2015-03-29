93ce6eb0c8b69092e4d3ef423c488e895a6b0f42
Hans Pabst <hans.pabst@intel.com>
2015-03-29 18:45:41 +0200

    Extended the run wrapper script for MIC-hosted execution (via micnativeloadex).

--- samples/smm.sh
+++ samples/smm.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-env \
-  KMP_AFFINITY=scatter \
-  OFFLOAD_INIT=on_start \
-  MIC_ENV_PREFIX=MIC \
-  MIC_KMP_AFFINITY=scatter,granularity=fine \
-./smm $*
+if [[ "-mic" != "$1" ]] ; then
+  env \
+    KMP_AFFINITY=scatter \
+    OFFLOAD_INIT=on_start \
+    MIC_ENV_PREFIX=MIC \
+    MIC_KMP_AFFINITY=scatter,granularity=fine \
+  ./smm $*
+else
+  shift
+  env \
+    SINK_LD_LIBRARY_PATH=$MIC_LD_LIBRARY_PATH \
+  micnativeloadex \
+    ./smm -a "$*" \
+    -e "KMP_AFFINITY=scatter,granularity=fine"
+fi
+
