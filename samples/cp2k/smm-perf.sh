decd92305dfd44e0286fa58954ce4d832d80b3b4
Hans Pabst <hans.pabst@intel.com>
2015-04-20 12:22:25 +0200

    Improved script and cleanup.

--- samples/cp2k/smm-perf.sh
+++ samples/cp2k/smm-perf.sh
@@ -6,13 +6,12 @@ if ([ "" != "$1" ]) ; then
   VARIANT=$1
 fi
 
-PERF=$(grep -A1 -i "${VARIANT}" smm-test.txt | \
+HERE=$(cd $(dirname $0); pwd -P)
+PERF=$(grep -A1 -i "${VARIANT}" ${HERE}/smm-test.txt | \
   grep -e "performance" | \
   cut -d" " -f2 | \
   sort -n)
 
-#AVG=$((echo -n "scale=3;"; echo "${PERF}0" | tr "\n" "+") | bc)
-#echo ${AVG}
 NUM1=$(echo "${PERF}" | wc -l)
 NUM2=$((NUM1 / 2))
 MIN=$(echo ${PERF} | cut -d" " -f1)
