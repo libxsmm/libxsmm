869e9f8c15a65ebc61edaa35746aded88ce0fa2d
Hans Pabst <hans.pabst@intel.com>
2015-04-29 10:46:25 +0200

    Adjusted run-wrapper scripts.

--- samples/blas/blas.sh
+++ samples/blas/blas.sh
@@ -3,12 +3,20 @@
 HERE=$(cd $(dirname $0); pwd -P)
 NAME=$(basename ${HERE})
 
+MICINFO=$(which micinfo)
+if [[ "" != "${MICINFO}" ]] ; then
+  MICCORES=$(${MICINFO} | sed -n "0,/\s\+Total No of Active Cores :\s\+\([0-9]\+\)/s//\1/p")
+else
+  MICCORES=61
+fi
+
 if [[ "-mic" != "$1" ]] ; then
   env \
     KMP_AFFINITY=scatter,granularity=fine \
     OFFLOAD_INIT=on_start \
     MIC_ENV_PREFIX=MIC \
     MIC_KMP_AFFINITY=scatter,granularity=fine \
+    MIC_KMP_PLACE_THREADS=$((MICCORES-1))c3t \
   ${HERE}/${NAME} $*
 else
   shift
@@ -16,6 +24,7 @@ else
     SINK_LD_LIBRARY_PATH=$MIC_LD_LIBRARY_PATH \
   micnativeloadex \
     ${HERE}/${NAME} -a "$*" \
-    -e "KMP_AFFINITY=scatter,granularity=fine"
+    -e "KMP_AFFINITY=scatter,granularity=fine" \
+    -e "MIC_KMP_PLACE_THREADS=$((MICCORES-1))3t"
 fi
 
