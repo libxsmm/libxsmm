229b9987a1305f8caf4ec7dcfb1602fec4b1e137
Hans Pabst <hans.pabst@intel.com>
2015-06-26 11:13:56 +0200

    Generate M=N=K=23 MM since it is used in all code samples by default.

--- ide/_vs2013.bat
+++ ide/_vs2013.bat
@@ -2,7 +2,7 @@
 SETLOCAL
 
 cd ..
-bash -c "make realclean ; make M='1 2 3 4 5' header sources main"
+bash -c "make realclean ; make M='1 2 3 4 5 23' header sources main"
 cd ide
 
 SET LIBXSMMROOT=%~d0%~p0\..
