e0aa298c644f28b978439644d01016fc3494774f
Hans Pabst <hans.pabst@intel.com>
2015-06-26 23:04:09 +0200

    Fixed plot script after changing console output.

--- samples/smm/smm-plot.sh
+++ samples/smm/smm-plot.sh
@@ -32,14 +32,14 @@ SED=$(which sed)
 function capturedTxtToDataFile {
   ${GREP} -i -A2 \
     -e "^m=" -e "${VARIANT}" \
-    ${HERE}/$1.txt | \
-  ${SED} \
+    ${HERE}/$1.txt \
+  | ${SED} \
     -e "s/m=//" -e "s/n=//" -e "s/k=//" -e "s/ldc=//" -e "s/ (.\+) / /" \
     -e "s/size=//" -e "s/batch=//" -e "s/memory=//" -e "s/ GB\/s//" \
-    -e "/^.\+\.\.\./Id" -e "/^$/d" -e "/--/d" | \
-  ${SED} \
+    -e "/^.\+\.\.\./Id" -e "/^$/d" -e "/--/d" \
+  | ${SED} \
     -e "N;s/ MB\n\tperformance://g" \
-    -e "N;s/ GFLOPS\/s\n\tbandwidth://g" \
+    -e "N;s/ GFLOPS\/s\n\t.\+$//g" \
   > ${HERE}/$1.dat
 }
 
