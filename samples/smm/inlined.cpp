d4cd4925c1859b379437ca88e3d471a9606e87bc
Hans Pabst <hans.pabst@intel.com>
2015-07-21 20:16:19 +0200

    Missed to adjust memory consumption calculation (SMM/inlined).

--- samples/smm/inlined.cpp
+++ samples/smm/inlined.cpp
@@ -112,7 +112,7 @@ int main(int argc, char* argv[])
 #endif
       fprintf(stdout, "m=%i n=%i k=%i ldc=%i (%s) size=%i memory=%.f MB\n\n",
         m, n, k, ldc, 0 != (LIBXSMM_ROW_MAJOR) ? "row-major" : "column-major",
-        s, 1.0 * s * (asize + bsize + csize) * sizeof(T) / (1 << 20));
+        s, 1.0 * s * (asize + bsize) * sizeof(T) / (1 << 20));
 
       { // streaming
         fprintf(stdout, "Streamed...\n");
