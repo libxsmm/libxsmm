a480e2da323071d7bb7d13efc7cf1a26c9669642
Hans Pabst <hans.pabst@intel.com>
2015-08-09 14:27:47 +0200

    Fixed script syntax for launching VS IDE.

--- ide/_vs2013.bat
+++ ide/_vs2013.bat
@@ -5,11 +5,11 @@ cd ..
 bash -c "make realclean ; make M='1 2 3 4 5 23' header sources main"
 cd ide
 
-SET LIBXSMMROOT=%~d0%~p0\..
+SET LIBXSMMROOT=%~d0%~p0..
 
 CALL %~d0"%~p0"_vs.bat vs2013
 IF NOT "%VS_IDE%" == "" (
-  START "" "%VS_IDE%" %~d0"%~p0"_vs2013.sln
+  START "" "%VS_IDE%" "%~d0%~p0_vs2013.sln"
 ) ELSE (
   START %~d0"%~p0"_vs2013.sln
 )
--- ide/_vs2015.bat
+++ ide/_vs2015.bat
@@ -5,11 +5,11 @@ cd ..
 bash -c "make realclean ; make M='1 2 3 4 5 23' header sources main"
 cd ide
 
-SET LIBXSMMROOT=%~d0%~p0\..
+SET LIBXSMMROOT=%~d0%~p0..
 
 CALL %~d0"%~p0"_vs.bat vs2015
 IF NOT "%VS_IDE%" == "" (
-  START "" "%VS_IDE%" %~d0"%~p0"_vs2015.sln
+  START "" "%VS_IDE%" "%~d0%~p0_vs2015.sln"
 ) ELSE (
   START %~d0"%~p0"_vs2015.sln
 )
