a05d4043d913de8c0a7e3756108e3575d0b0a92f
Hans Pabst <hans.pabst@intel.com>
2015-10-12 10:46:52 +0200

    Fix warning about "format not a string literal and no format arguments" as unveiled by the Clang.

--- src/generator_common.c
+++ src/generator_common.c
@@ -660,7 +660,7 @@ void libxsmm_handle_error( libxsmm_generated_code* io_generated_code,
                            const unsigned int      i_error_code ) {
   io_generated_code->last_error = i_error_code;
 #ifndef NDEBUG
-  fprintf( stderr, libxsmm_strerror( i_error_code ) );
+  fprintf( stderr, "%s\n", libxsmm_strerror( i_error_code ) );
 #endif
 }
 
