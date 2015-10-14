d37cd12ae14d5c90e29f22a11ec7ee222a90a2fb
Hans Pabst <hans.pabst@intel.com>
2015-10-14 11:16:50 +0200

    Using a named critical section in case of building LIBXSMM with OMP=1; this is a playground for evaluating the performance of the lock-approach. By default, OMP=0 i.e. the lock-grading is in action (currently using 16 locks). The lock grading allows to distribute modifying accesses (of the function cache) across multiple locks. Also, ensuring all locks are created during libxsmm_build_static (statically initialized Pthread Mutexes may still be created at the time of acquiring a lock for the first time).

--- src/libxsmm_build.c
+++ src/libxsmm_build.c
@@ -52,7 +52,7 @@
 # pragma offload_attribute(pop)
 #endif
 
-#define LIBXSMM_BUILD_CACHESIZE (LIBXSMM_MAX_M) * (LIBXSMM_MAX_N) * (LIBXSMM_MAX_K) * 8
+#define LIBXSMM_BUILD_CACHESIZE ((LIBXSMM_MAX_M) * (LIBXSMM_MAX_N) * (LIBXSMM_MAX_K) * 8)
 #if !defined(_WIN32)
 #define LIBXSMM_BUILD_PAGESIZE sysconf(_SC_PAGESIZE)
 #else
@@ -63,24 +63,39 @@
 
 /** Filled with zeros due to C language rule. */
 LIBXSMM_RETARGETABLE libxsmm_function libxsmm_cache[2][(LIBXSMM_BUILD_CACHESIZE)];
-LIBXSMM_RETARGETABLE LIBXSMM_LOCK_TYPE libxsmm_locks[] = {
+
+#if !defined(_OPENMP)
+LIBXSMM_RETARGETABLE LIBXSMM_LOCK_TYPE libxsmm_build_lock[] = {
   LIBXSMM_LOCK_CONSTRUCT, LIBXSMM_LOCK_CONSTRUCT, LIBXSMM_LOCK_CONSTRUCT, LIBXSMM_LOCK_CONSTRUCT,
   LIBXSMM_LOCK_CONSTRUCT, LIBXSMM_LOCK_CONSTRUCT, LIBXSMM_LOCK_CONSTRUCT, LIBXSMM_LOCK_CONSTRUCT,
   LIBXSMM_LOCK_CONSTRUCT, LIBXSMM_LOCK_CONSTRUCT, LIBXSMM_LOCK_CONSTRUCT, LIBXSMM_LOCK_CONSTRUCT,
   LIBXSMM_LOCK_CONSTRUCT, LIBXSMM_LOCK_CONSTRUCT, LIBXSMM_LOCK_CONSTRUCT, LIBXSMM_LOCK_CONSTRUCT
 };
+#endif
 
 
 LIBXSMM_EXTERN_C LIBXSMM_RETARGETABLE void libxsmm_build_static()
 {
   static int init = 0;
+
   if (0 == init) {
-    LIBXSMM_LOCK_ACQUIRE(libxsmm_locks[0]);
+#if !defined(_OPENMP)
+    int i;
+    for (i = 0; i < 0; ++i) {
+      LIBXSMM_LOCK_ACQUIRE(libxsmm_build_lock[i]);
+    }
+#else
+#   pragma omp critical(libxsmm_build_lock)
+#endif
     if (0 == init) {
 #     include <libxsmm_build.h>
       init = 1;
     }
-    LIBXSMM_LOCK_RELEASE(libxsmm_locks[0]);
+#if !defined(_OPENMP)
+    for (i = 0; i < 0; ++i) {
+      LIBXSMM_LOCK_RELEASE(libxsmm_build_lock[i]);
+    }
+#endif
   }
 }
 
@@ -106,13 +121,18 @@ LIBXSMM_EXTERN_C LIBXSMM_RETARGETABLE libxsmm_function libxsmm_build_jit(int sin
 
   if (0 == result) {
 #if !defined(_WIN32)
-    const unsigned int lock = LIBXSMM_MOD2(indx, sizeof(libxsmm_locks) / sizeof(*libxsmm_locks));
-    LIBXSMM_LOCK_ACQUIRE(libxsmm_locks[lock]);
-    result = cache[indx];
+#if !defined(_OPENMP)
+    const unsigned int lock = LIBXSMM_MOD2(indx, sizeof(libxsmm_build_lock) / sizeof(*libxsmm_build_lock));
+    LIBXSMM_LOCK_ACQUIRE(libxsmm_build_lock[lock]);
+#else
+#   pragma omp critical(libxsmm_build_lock)
+#endif
+    {
+      result = cache[indx];
 
-    if (0 == result) {
-      /* set arch string */
-      char l_arch[14];
+      if (0 == result) {
+        /* set arch string */
+        char l_arch[14];
 #ifdef __SSE3__
 # ifndef __AVX__
 #   error "SSE3 instructions set extensions have no jitting support!"
@@ -122,92 +142,94 @@ LIBXSMM_EXTERN_C LIBXSMM_RETARGETABLE libxsmm_function libxsmm_build_jit(int sin
 # error "Xeon Phi coprocessors (IMCI architecture) have no jitting support!"
 #endif
 #ifdef __AVX__
-      strcpy(l_arch, "snb");
+        strcpy(l_arch, "snb");
 #endif
 #ifdef __AVX2__
-      strcpy(l_arch, "hsw");
+        strcpy(l_arch, "hsw");
 #endif
 #ifdef __AVX512F__
-      strcpy(l_arch, "knl");
+        strcpy(l_arch, "knl");
 #endif
-      /* allocate buffer for code */
-      unsigned char* l_gen_code = (unsigned char*) malloc(131072 * sizeof(unsigned char));
-      libxsmm_generated_code l_generated_code;
-      l_generated_code.generated_code = (void*)l_gen_code;
-      l_generated_code.buffer_size = 131072;
-      l_generated_code.code_size = 0;
-      l_generated_code.code_type = 2;
-      l_generated_code.last_error = 0;
+        /* allocate buffer for code */
+        unsigned char* l_gen_code = (unsigned char*) malloc(131072 * sizeof(unsigned char));
+        libxsmm_generated_code l_generated_code;
+        l_generated_code.generated_code = (void*)l_gen_code;
+        l_generated_code.buffer_size = 131072;
+        l_generated_code.code_size = 0;
+        l_generated_code.code_type = 2;
+        l_generated_code.last_error = 0;
 
-      /* generate kernel */
-      libxsmm_generator_dense_kernel(&l_generated_code, &l_xgemm_desc, l_arch);
+        /* generate kernel */
+        libxsmm_generator_dense_kernel(&l_generated_code, &l_xgemm_desc, l_arch);
 
-      /* handle an eventual error */
-      if (l_generated_code.last_error != 0) {
-        fprintf(stderr, "%s\n", libxsmm_strerror(l_generated_code.last_error));
-        exit(-1);
-      }
-
-      /* create executable buffer */
-      int l_code_pages = (((l_generated_code.code_size-1)*sizeof(unsigned char))/LIBXSMM_BUILD_PAGESIZE)+1;
-      int l_code_page_size = LIBXSMM_BUILD_PAGESIZE*l_code_pages;
-      int l_fd = open("/dev/zero", O_RDWR);
-      void* p = mmap(0, l_code_page_size, PROT_READ|PROT_WRITE, MAP_PRIVATE, l_fd, 0);
-      close(l_fd);
-      /* explicitly disable THP for this memory region, kernel 2.6.38 or higher!, remove -c89 when compiling LIBXSMM 
-      madvise(p, l_code_page_size, MADV_NOHUGEPAGE); */
-#if !defined(NDEBUG)
-      if (p == MAP_FAILED) {
-        fprintf(stderr, "LIBXSMM: something bad happend in mmap, couldn't allocate code buffer!\n");
-        exit(-1);
-      }
-#endif
-      unsigned char* l_code = (unsigned char*)p;
-      /*memset( l_code, 0, l_code_page_size );*/
-      memcpy( l_code, l_gen_code, l_generated_code.code_size );
-#if !defined(NDEBUG)
-      int error = 
-#endif
-      mprotect( (void*)l_code, l_code_page_size, PROT_EXEC | PROT_READ );
-#if !defined(NDEBUG)
-      if (error == -1) {
-        int errsv = errno;
-        if (errsv == EINVAL) {
-          fprintf(stderr, "LIBXSMM: mprotect failed: addr is not a valid pointer, or not a multiple of the system page size!\n");
-        } else if (errsv == ENOMEM) {
-          fprintf(stderr, "LIBXSMM: mprotect failed: Internal kernel structures could not be allocated!\n");
-        } else if (errsv == EACCES) {
-          fprintf(stderr, "LIBXSMM: mprotect failed: The memory cannot be given the specified access!\n");
-        } else {
-          fprintf(stderr, "LIBXSMM: mprotect failed: Unknown Error!\n");
+        /* handle an eventual error */
+        if (l_generated_code.last_error != 0) {
+          fprintf(stderr, "%s\n", libxsmm_strerror(l_generated_code.last_error));
+          exit(-1);
         }
-        exit(-1);
-      }
 
-      /* write buffer for manual decode as binary to a file */
-      char l_objdump_name[512];
-      sprintf( l_objdump_name, "kernel_prec%i_m%i_n%i_k%i_lda%i_ldb%i_ldc%i_a%i_b%i_ta%c_tb%c_pf%i.bin", 
-               l_xgemm_desc.single_precision, l_xgemm_desc.m, l_xgemm_desc.n, l_xgemm_desc.k,
-               l_xgemm_desc.lda, l_xgemm_desc.ldb, l_xgemm_desc.ldc, l_xgemm_desc.alpha, l_xgemm_desc.beta,
-               l_xgemm_desc.trans_a, l_xgemm_desc.trans_b, l_xgemm_desc.prefetch ); 
-      FILE *l_byte_code = fopen( l_objdump_name, "wb");
-      if ( l_byte_code != NULL ) {
-        fwrite( (const void*)l_gen_code, 1, l_generated_code.code_size, l_byte_code);
-        fclose( l_byte_code );
-      } else {
-        /* error */
-      }
+        /* create executable buffer */
+        int l_code_pages = (((l_generated_code.code_size-1)*sizeof(unsigned char))/LIBXSMM_BUILD_PAGESIZE)+1;
+        int l_code_page_size = LIBXSMM_BUILD_PAGESIZE*l_code_pages;
+        int l_fd = open("/dev/zero", O_RDWR);
+        void* p = mmap(0, l_code_page_size, PROT_READ|PROT_WRITE, MAP_PRIVATE, l_fd, 0);
+        close(l_fd);
+        /* explicitly disable THP for this memory region, kernel 2.6.38 or higher!, remove -c89 when compiling LIBXSMM 
+        madvise(p, l_code_page_size, MADV_NOHUGEPAGE); */
+#if !defined(NDEBUG)
+        if (p == MAP_FAILED) {
+          fprintf(stderr, "LIBXSMM: something bad happend in mmap, couldn't allocate code buffer!\n");
+          exit(-1);
+        }
 #endif
+        unsigned char* l_code = (unsigned char*)p;
+        /*memset( l_code, 0, l_code_page_size );*/
+        memcpy( l_code, l_gen_code, l_generated_code.code_size );
+#if !defined(NDEBUG)
+        int error = 
+#endif
+        mprotect( (void*)l_code, l_code_page_size, PROT_EXEC | PROT_READ );
+# if !defined(NDEBUG)
+        if (error == -1) {
+          int errsv = errno;
+          if (errsv == EINVAL) {
+            fprintf(stderr, "LIBXSMM: mprotect failed: addr is not a valid pointer, or not a multiple of the system page size!\n");
+          } else if (errsv == ENOMEM) {
+            fprintf(stderr, "LIBXSMM: mprotect failed: Internal kernel structures could not be allocated!\n");
+          } else if (errsv == EACCES) {
+            fprintf(stderr, "LIBXSMM: mprotect failed: The memory cannot be given the specified access!\n");
+          } else {
+            fprintf(stderr, "LIBXSMM: mprotect failed: Unknown Error!\n");
+          }
+          exit(-1);
+        }
 
-      /* free temporary buffer, and prepare return value */
-      free(l_gen_code);
-      result = (libxsmm_function)l_code;
+        /* write buffer for manual decode as binary to a file */
+        char l_objdump_name[512];
+        sprintf( l_objdump_name, "kernel_prec%i_m%i_n%i_k%i_lda%i_ldb%i_ldc%i_a%i_b%i_ta%c_tb%c_pf%i.bin", 
+                 l_xgemm_desc.single_precision, l_xgemm_desc.m, l_xgemm_desc.n, l_xgemm_desc.k,
+                 l_xgemm_desc.lda, l_xgemm_desc.ldb, l_xgemm_desc.ldc, l_xgemm_desc.alpha, l_xgemm_desc.beta,
+                 l_xgemm_desc.trans_a, l_xgemm_desc.trans_b, l_xgemm_desc.prefetch ); 
+        FILE *l_byte_code = fopen( l_objdump_name, "wb");
+        if ( l_byte_code != NULL ) {
+          fwrite( (const void*)l_gen_code, 1, l_generated_code.code_size, l_byte_code);
+          fclose( l_byte_code );
+        } else {
+          /* error */
+        }
+#endif
+        /* free temporary buffer, and prepare return value */
+        free(l_gen_code);
+        result = (libxsmm_function)l_code;
 
-      /* make function pointer available for dispatch */
-      cache[indx] = result;
+        /* make function pointer available for dispatch */
+        cache[indx] = result;
+      }
     }
 
-    LIBXSMM_LOCK_RELEASE(libxsmm_locks[lock]);
+#if !defined(_OPENMP)
+    LIBXSMM_LOCK_RELEASE(libxsmm_build_lock[lock]);
+#endif
 #else
     fprintf(stderr, "LIBXSMM ERROR: JITTING IS NOT SUPPORTED ON WINDOWS RIGHT NOW!\n");
 #endif /*_WIN32*/
