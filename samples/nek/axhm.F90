0560be86a5e0f81d647509aa46f4a9b547636c30
Alexander Heinecke <alexander.heinecke@intel.com>
2015-10-23 16:34:38 -0700

    fixed typo in lic header

--- samples/nek/axhm.F90
+++ samples/nek/axhm.F90
@@ -27,7 +27,7 @@
 !* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.              *!
 !*****************************************************************************!
 !* Hans Pabst (Intel Corp.), Alexander Heinecke (Intel Corp.), and           *! 
-!* Maxwell Hutchinson (University of Chicago                                 *!
+!* Maxwell Hutchinson (University of Chicago)                                *!
 !*****************************************************************************!
 
 
