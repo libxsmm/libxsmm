48e71ec8fc16824c56459e48f07b229a985a7f9e
Hans Pabst <hans.pabst@intel.com>
2015-10-24 15:21:55 +0200

    Attempt to have a failing test case whenever a test step fails; better distinct between what is considered a complete test case. Allow samples/cp2k/test.sh to execute in an out-of-tree fashion.

--- samples/cp2k/test.sh
+++ samples/cp2k/test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-if [ `grep "diff" cp2k-perf.txt | grep -v -c "diff=0.000"` -eq 0 ]; then
+HERE=$(cd $(dirname $0); pwd -P)
+
+if [ `grep "diff" ${HERE}/cp2k-perf.txt | grep -v -c "diff=0.000"` -eq 0 ]; then
   exit 0
 else
   exit 1
