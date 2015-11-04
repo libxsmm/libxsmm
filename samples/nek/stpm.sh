f7ee0418c77ebb8b0b6b21ede28cb42d958ac767
Hans Pabst <hans.pabst@intel.com>
2015-11-04 19:41:16 +0100

    Added run-wrapper scripts.

---
+++ samples/nek/stpm.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+HERE=$(cd $(dirname $0); pwd -P)
+NAME=axhm
+
+if [[ "-mic" != "$1" ]] ; then
+  if [[ "" != "$(ldd ${HERE}/${NAME} | grep libiomp5\.so)" ]] ; then
+    env OFFLOAD_INIT=on_start \
+      KMP_AFFINITY=scatter,granularity=fine,1 \
+      MIC_KMP_AFFINITY=scatter,granularity=fine \
+      MIC_ENV_PREFIX=MIC \
+    ${HERE}/${NAME} $*
+  else
+    env \
+      OMP_PROC_BIND=TRUE \
+    ${HERE}/${NAME} $*
+  fi
+else
+  shift
+  env \
+    SINK_LD_LIBRARY_PATH=$MIC_LD_LIBRARY_PATH \
+  micnativeloadex \
+    ${HERE}/${NAME} -a "$*" \
+    -e "KMP_AFFINITY=scatter,granularity=fine"
+fi
+
