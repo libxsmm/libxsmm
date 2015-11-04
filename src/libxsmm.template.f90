6c87037671c7b4422417914424b6fae4aeb61426
Hans Pabst <hans.pabst@intel.com>
2015-11-04 12:35:08 +0100

    Initial Fortran interface (issue #33).

--- src/libxsmm.template.f90
+++ src/libxsmm.template.f90
@@ -56,46 +56,74 @@ MODULE LIBXSMM
   INTEGER(LIBXSMM_INTEGER_TYPE), PARAMETER :: LIBXSMM_BETA            = $BETA
   INTEGER(LIBXSMM_INTEGER_TYPE), PARAMETER :: LIBXSMM_JIT             = $JIT
 
-  ! Overloaded BLAS routines (single/double precision)
+  ! Flag enumeration which can be IORed.
+  INTEGER(LIBXSMM_INTEGER_TYPE), PARAMETER :: LIBXSMM_GEMM_FLAG_TRANS_A = 1, &
+                                              LIBXSMM_GEMM_FLAG_TRANS_B = 2, &
+                                              LIBXSMM_GEMM_FLAG_ALIGN_A = 4, &
+                                              LIBXSMM_GEMM_FLAG_ALIGN_C = 8
+
+  INTEGER(LIBXSMM_INTEGER_TYPE), PARAMETER :: LIBXSMM_GEMM_FLAG_DEFAULT = IOR( &
+                                                MERGE(LIBXSMM_GEMM_FLAG_ALIGN_A, 0, 1.LT.LIBXSMM_ALIGNED_LOADS), &
+                                                MERGE(LIBXSMM_GEMM_FLAG_ALIGN_C, 0, 1.LT.LIBXSMM_ALIGNED_STORES))
+
+  ! Enumeration of the available prefetch strategies which can be IORed.
+  !   LIBXSMM_PREFETCH_NONE:      No prefetching and no prefetch fn. signature.
+  !   LIBXSMM_PREFETCH_SIGNATURE: Only function prefetch signature.
+  !   LIBXSMM_PREFETCH_AL2:       Prefetch PA using accesses to A.
+  !   LIBXSMM_PREFETCH_AL2_JPST:  Prefetch PA (aggressive).
+  !   LIBXSMM_PREFETCH_BL2_VIA_C: Prefetch PB using accesses to C.
+  !   LIBXSMM_PREFETCH_AL2_AHEAD: Prefetch A ahead.
+  INTEGER(LIBXSMM_INTEGER_TYPE), PARAMETER :: LIBXSMM_PREFETCH_NONE       = 0, &
+                                              LIBXSMM_PREFETCH_SIGNATURE  = 1, &
+                                              LIBXSMM_PREFETCH_AL2        = 2, &
+                                              LIBXSMM_PREFETCH_AL2_JPST   = 4, &
+                                              LIBXSMM_PREFETCH_BL2_VIA_C  = 8, &
+                                              LIBXSMM_PREFETCH_AL2_AHEAD  = 16, &
+                                              LIBXSMM_PREFETCH_AL2BL2_VIA_C       = IOR(LIBXSMM_PREFETCH_BL2_VIA_C, &
+                                                                                        LIBXSMM_PREFETCH_AL2), &
+                                              LIBXSMM_PREFETCH_AL2BL2_VIA_C_JPST  = IOR(LIBXSMM_PREFETCH_BL2_VIA_C, &
+                                                                                        LIBXSMM_PREFETCH_AL2_JPST), &
+                                              LIBXSMM_PREFETCH_AL2BL2_VIA_C_AHEAD = IOR(LIBXSMM_PREFETCH_BL2_VIA_C, &
+                                                                                        LIBXSMM_PREFETCH_AL2_AHEAD)
+
+  ! Overloaded dispatch/JIT routines (single/double precision).
+  INTERFACE libxsmm_mm_dispatch
+    MODULE PROCEDURE smm_dispatch, dmm_dispatch
+  END INTERFACE
+
+  ! Overloaded BLAS routines (single/double precision).
   INTERFACE libxsmm_blasmm
     MODULE PROCEDURE libxsmm_sblasmm, libxsmm_dblasmm
   END INTERFACE
 
-  ! Overloaded optimized routines (single/double precision)
+  ! Overloaded optimized routines (single/double precision).
   INTERFACE libxsmm_imm
     MODULE PROCEDURE libxsmm_simm, libxsmm_dimm
   END INTERFACE
 
-  ! Overloaded auto-dispatch routines (single/double precision)
+  ! Overloaded auto-dispatch routines (single/double precision).
   INTERFACE libxsmm_mm
     MODULE PROCEDURE libxsmm_smm, libxsmm_dmm
   END INTERFACE
 
-  ! Type of a function generated for a specific M, N, and K
+  ! Type of a function generated for a specific Alpha, Beta, and M, N, and K.
   ABSTRACT INTERFACE
-    ! This interface requires the use of C_LOC in the user code
-    ! on arrays which is not support in all FORTRAN compilers
-    PURE SUBROUTINE LIBXSMM_XMM_FUNCTION(a, b, c) BIND(C)
-      IMPORT :: C_PTR
-      TYPE(C_PTR), VALUE, INTENT(IN) :: a, b, c
-    END SUBROUTINE
-
-    PURE SUBROUTINE LIBXSMM_SMM_FUNCTION(a, b, c) BIND(C)
+    PURE SUBROUTINE LIBXSMM_SMM_FUNCTION(alpha, beta, a, b, c) BIND(C)
       IMPORT :: C_FLOAT
-      REAL(KIND=C_FLOAT), INTENT(IN) :: a(*), b(*)
-      REAL(KIND=C_FLOAT), INTENT(INOUT) :: c(*)
+      REAL(C_FLOAT), VALUE, INTENT(IN) :: alpha, beta
+      REAL(C_FLOAT), INTENT(IN) :: a(*), b(*)
+      REAL(C_FLOAT), INTENT(INOUT) :: c(*)
     END SUBROUTINE
-
-    PURE SUBROUTINE LIBXSMM_DMM_FUNCTION(a, b, c) BIND(C)
+    PURE SUBROUTINE LIBXSMM_DMM_FUNCTION(alpha, beta, a, b, c) BIND(C)
       IMPORT :: C_DOUBLE
-      REAL(KIND=C_DOUBLE), INTENT(IN) :: a(*), b(*)
-      REAL(KIND=C_DOUBLE), INTENT(INOUT) :: c(*)
+      REAL(C_DOUBLE), VALUE, INTENT(IN) :: alpha, beta
+      REAL(C_DOUBLE), INTENT(IN) :: a(*), b(*)
+      REAL(C_DOUBLE), INTENT(INOUT) :: c(*)
     END SUBROUTINE
   END INTERFACE
 
   !DIR$ ATTRIBUTES OFFLOAD:MIC :: sgemm, dgemm
-  !DIR$ ATTRIBUTES OFFLOAD:MIC :: libxsmm_init, libxsmm_build_jit
-  !DIR$ ATTRIBUTES OFFLOAD:MIC :: libxsmm_smm_dispatch, libxsmm_dmm_dispatch
+  !DIR$ ATTRIBUTES OFFLOAD:MIC :: libxsmm_init, libxsmm_smm_dispatch, libxsmm_dmm_dispatch
   !DIR$ ATTRIBUTES OFFLOAD:MIC :: libxsmm_timer_tick, libxsmm_timer_duration
   INTERFACE
     SUBROUTINE sgemm(transa, transb, m, n, k, alpha, a, lda, b, ldb, beta, c, ldc)
@@ -113,33 +141,21 @@ MODULE LIBXSMM
       REAL(LIBXSMM_DOUBLE_PRECISION), INTENT(INOUT) :: c(ldc,*)
     END SUBROUTINE
 
-    ! Initialize the library
-    PURE SUBROUTINE libxsmm_init() BIND(C)
+    ! Initialize the library; pay for setup cost at a specific point.
+    SUBROUTINE libxsmm_init() BIND(C)
     END SUBROUTINE
 
-    ! Build explicitly a kernel, do not rely on automatic JIT in dispatch
-    TYPE(C_FUNPTR) PURE FUNCTION libxsmm_build_jit(single_precision, m, n, k) BIND(C)
-      IMPORT :: C_FUNPTR, C_INT
-      INTEGER(C_INT), VALUE, INTENT(IN) :: single_precision, m, n, k
+    ! Query or JIT-generate a function; return zero if it does not exist or if JIT is not supported (single-precision).
+    TYPE(C_FUNPTR) PURE FUNCTION libxsmm_smm_dispatch(alpha, beta, m, n, k, lda, ldb, ldc, flags, prefetch) BIND(C)
+      IMPORT :: C_FUNPTR, C_FLOAT, C_INT
+      REAL(C_FLOAT), VALUE, INTENT(IN) :: alpha, beta
+      INTEGER(C_INT), VALUE, INTENT(IN) :: m, n, k, lda, ldb, ldc, flags, prefetch
     END FUNCTION
-
-    ! Build explicitly a kernel, do not rely on automatic JIT in dispatch, do not return the function pointer
-    ! However, the JIT function is available for dispatch
-    ! @TODO not all versions of gfortran seem to like this -> commented for now
-    !PURE SUBROUTINE libxsmm_build_jit_only(single_precision, m, n, k) BIND(C, name="libxsmm_build_jit")
-    !  IMPORT :: C_INT
-    !  INTEGER(C_INT), VALUE, INTENT(IN) :: single_precision, m, n, k
-    !END SUBROUTINE
-
-    ! Query the pointer of a generated function; zero if it does not exist, single-precision.
-    TYPE(C_FUNPTR) PURE FUNCTION libxsmm_smm_dispatch(m, n, k) BIND(C)
-      IMPORT :: C_FUNPTR, C_INT
-      INTEGER(C_INT), VALUE, INTENT(IN) :: m, n, k
-    END FUNCTION
-    ! Query the pointer of a generated function; zero if it does not exist; double-precision.
-    TYPE(C_FUNPTR) PURE FUNCTION libxsmm_dmm_dispatch(m, n, k) BIND(C)
-      IMPORT :: C_FUNPTR, C_INT
-      INTEGER(C_INT), VALUE, INTENT(IN) :: m, n, k
+    ! Query or JIT-generate a function; return zero if it does not exist or if JIT is not supported (double-precision).
+    TYPE(C_FUNPTR) PURE FUNCTION libxsmm_dmm_dispatch(alpha, beta, m, n, k, lda, ldb, ldc, flags, prefetch) BIND(C)
+      IMPORT :: C_FUNPTR, C_DOUBLE, C_INT
+      REAL(C_DOUBLE), VALUE, INTENT(IN) :: alpha, beta
+      INTEGER(C_INT), VALUE, INTENT(IN) :: m, n, k, lda, ldb, ldc, flags, prefetch
     END FUNCTION
 
     ! Non-pure function returning the current clock tick using a platform-specific resolution.
@@ -178,15 +194,14 @@ CONTAINS
     ld = MERGE(m, n, 0.NE.LIBXSMM_COL_MAJOR)
   END FUNCTION
 
-  ! Non-dispatched matrix-matrix multiplication using BLAS; single-precision.
+  ! Non-dispatched matrix-matrix multiplication using BLAS (single-precision).
   !DIR$ ATTRIBUTES OFFLOAD:MIC :: libxsmm_sblasmm
   !DIR$ ATTRIBUTES INLINE :: libxsmm_sblasmm
-  SUBROUTINE libxsmm_sblasmm(m, n, k, a, b, c)
+  SUBROUTINE libxsmm_sblasmm(alpha, beta, m, n, k, a, b, c)
     INTEGER(LIBXSMM_INTEGER_TYPE), PARAMETER :: T = LIBXSMM_SINGLE_PRECISION
     INTEGER(LIBXSMM_INTEGER_TYPE), INTENT(IN) :: m, n, k
-    REAL(T), INTENT(IN) :: a(:,:), b(:,:)
+    REAL(T), INTENT(IN) :: alpha, beta, a(:,:), b(:,:)
     REAL(T), INTENT(INOUT) :: c(:,:)
-    REAL(T), PARAMETER :: alpha = 1, beta = LIBXSMM_BETA
     IF (0.NE.LIBXSMM_COL_MAJOR) THEN
       CALL sgemm('N', 'N', m, n, k, alpha, a, m, b, k, beta, c, SIZE(c, 1))
     ELSE
@@ -194,15 +209,14 @@ CONTAINS
     ENDIF
   END SUBROUTINE
 
-  ! Non-dispatched matrix-matrix multiplication using BLAS; double-precision.
+  ! Non-dispatched matrix-matrix multiplication using BLAS (double-precision).
   !DIR$ ATTRIBUTES OFFLOAD:MIC :: libxsmm_dblasmm
   !DIR$ ATTRIBUTES INLINE :: libxsmm_dblasmm
-  SUBROUTINE libxsmm_dblasmm(m, n, k, a, b, c)
+  SUBROUTINE libxsmm_dblasmm(alpha, beta, m, n, k, a, b, c)
     INTEGER(LIBXSMM_INTEGER_TYPE), PARAMETER :: T = LIBXSMM_DOUBLE_PRECISION
     INTEGER(LIBXSMM_INTEGER_TYPE), INTENT(IN) :: m, n, k
-    REAL(T), INTENT(IN) :: a(:,:), b(:,:)
+    REAL(T), INTENT(IN) :: alpha, beta, a(:,:), b(:,:)
     REAL(T), INTENT(INOUT) :: c(:,:)
-    REAL(T), PARAMETER :: alpha = 1, beta = LIBXSMM_BETA
     IF (0.NE.LIBXSMM_COL_MAJOR) THEN
       CALL dgemm('N', 'N', m, n, k, alpha, a, m, b, k, beta, c, SIZE(c, 1))
     ELSE
@@ -210,40 +224,27 @@ CONTAINS
     ENDIF
   END SUBROUTINE
 
-  ! Non-dispatched matrix-matrix multiplication using optimized code; single-precision.
+  ! Non-dispatched matrix-matrix multiplication using optimized code (single-precision).
   !DIR$ ATTRIBUTES OFFLOAD:MIC :: libxsmm_simm
   !DIR$ ATTRIBUTES INLINE :: libxsmm_simm
-  SUBROUTINE libxsmm_simm(m, n, k, a, b, c)
+  SUBROUTINE libxsmm_simm(alpha, beta, m, n, k, a, b, c)
     INTEGER(LIBXSMM_INTEGER_TYPE), PARAMETER :: T = LIBXSMM_SINGLE_PRECISION
     INTEGER(LIBXSMM_INTEGER_TYPE), INTENT(IN) :: m, n, k
     INTEGER(LIBXSMM_INTEGER_TYPE) :: i, j
+    REAL(T), INTENT(IN) :: alpha, beta
     REAL(T), INTENT(IN), TARGET, CONTIGUOUS :: a(:,:), b(:,:)
     REAL(T), INTENT(INOUT) :: c($SHAPE_C1,$SHAPE_C2)
     REAL(T), POINTER :: x(:,:), y(:,:)
-    IF (0.NE.LIBXSMM_COL_MAJOR.AND.LIBXSMM_BETA.NE.0) THEN
+    REAL(T), PARAMETER :: Zero = 0, One = 1
+    REAL(T) :: xalpha, xbeta
+    xalpha = MERGE(alpha, One, 1.NE.LIBXSMM_COL_MAJOR)
+    xbeta = MERGE(beta, Zero, 0.NE.LIBXSMM_COL_MAJOR)
+    IF (0.NE.LIBXSMM_COL_MAJOR) THEN
       !DIR$ OMP SIMD COLLAPSE(2)
       DO j = LBOUND(b, 2), LBOUND(b, 2) + n - 1
         !DIR$ LOOP COUNT(1, LIBXSMM_MAX_M, LIBXSMM_AVG_M)
         DO i = LBOUND(a, 1), LBOUND(a, 1) + m - 1
-          c(i,j) = c(i,j) + DOT_PRODUCT(a(i,:), b(:,j))
-        END DO
-      END DO
-    ELSE IF (LIBXSMM_BETA.NE.0) THEN
-      x(1:$SHAPE_AT1,1:$SHAPE_AT2) => b(:,:)
-      y(1:$SHAPE_BT1,1:$SHAPE_BT2) => a(:,:)
-      !DIR$ OMP SIMD COLLAPSE(2)
-      DO j = 1, m
-        !DIR$ LOOP COUNT(1, LIBXSMM_MAX_N, LIBXSMM_AVG_N)
-        DO i = 1, n
-          c(i,j) = c(i,j) + DOT_PRODUCT(x(i,:), y(:,j))
-        END DO
-      END DO
-    ELSE IF (0.NE.LIBXSMM_COL_MAJOR) THEN
-      !DIR$ OMP SIMD COLLAPSE(2)
-      DO j = LBOUND(b, 2), LBOUND(b, 2) + n - 1
-        !DIR$ LOOP COUNT(1, LIBXSMM_MAX_M, LIBXSMM_AVG_M)
-        DO i = LBOUND(a, 1), LBOUND(a, 1) + m - 1
-          c(i,j) = DOT_PRODUCT(a(i,:), b(:,j))
+          c(i,j) = xbeta * c(i,j) + xalpha * DOT_PRODUCT(a(i,:), b(:,j))
         END DO
       END DO
     ELSE
@@ -253,46 +254,33 @@ CONTAINS
       DO j = 1, m
         !DIR$ LOOP COUNT(1, LIBXSMM_MAX_N, LIBXSMM_AVG_N)
         DO i = 1, n
-          c(i,j) = DOT_PRODUCT(x(i,:), y(:,j))
+          c(i,j) = xbeta * c(i,j) + xalpha * DOT_PRODUCT(x(i,:), y(:,j))
         END DO
       END DO
     ENDIF
   END SUBROUTINE
 
-  ! Non-dispatched matrix-matrix multiplication using optimized code; double-precision.
+  ! Non-dispatched matrix-matrix multiplication using optimized code (double-precision).
   !DIR$ ATTRIBUTES OFFLOAD:MIC :: libxsmm_dimm
   !DIR$ ATTRIBUTES INLINE :: libxsmm_dimm
-  SUBROUTINE libxsmm_dimm(m, n, k, a, b, c)
+  SUBROUTINE libxsmm_dimm(alpha, beta, m, n, k, a, b, c)
     INTEGER(LIBXSMM_INTEGER_TYPE), PARAMETER :: T = LIBXSMM_DOUBLE_PRECISION
     INTEGER(LIBXSMM_INTEGER_TYPE), INTENT(IN) :: m, n, k
     INTEGER(LIBXSMM_INTEGER_TYPE) :: i, j
+    REAL(T), INTENT(IN) :: alpha, beta
     REAL(T), INTENT(IN), TARGET, CONTIGUOUS :: a(:,:), b(:,:)
     REAL(T), INTENT(INOUT) :: c($SHAPE_C1,$SHAPE_C2)
     REAL(T), POINTER :: x(:,:), y(:,:)
-    IF (0.NE.LIBXSMM_COL_MAJOR.AND.LIBXSMM_BETA.NE.0) THEN
+    REAL(T), PARAMETER :: Zero = 0, One = 1
+    REAL(T) :: xalpha, xbeta
+    xalpha = MERGE(alpha, One, 1.NE.LIBXSMM_COL_MAJOR)
+    xbeta = MERGE(beta, Zero, 0.NE.LIBXSMM_COL_MAJOR)
+    IF (0.NE.LIBXSMM_COL_MAJOR) THEN
       !DIR$ OMP SIMD COLLAPSE(2)
       DO j = LBOUND(b, 2), LBOUND(b, 2) + n - 1
         !DIR$ LOOP COUNT(1, LIBXSMM_MAX_M, LIBXSMM_AVG_M)
         DO i = LBOUND(a, 1), LBOUND(a, 1) + m - 1
-          c(i,j) = c(i,j) + DOT_PRODUCT(a(i,:), b(:,j))
-        END DO
-      END DO
-    ELSE IF (LIBXSMM_BETA.NE.0) THEN
-      x(1:$SHAPE_AT1,1:$SHAPE_AT2) => b(:,:)
-      y(1:$SHAPE_BT1,1:$SHAPE_BT2) => a(:,:)
-      !DIR$ OMP SIMD COLLAPSE(2)
-      DO j = 1, m
-        !DIR$ LOOP COUNT(1, LIBXSMM_MAX_N, LIBXSMM_AVG_N)
-        DO i = 1, n
-          c(i,j) = c(i,j) + DOT_PRODUCT(x(i,:), y(:,j))
-        END DO
-      END DO
-    ELSE IF (0.NE.LIBXSMM_COL_MAJOR) THEN
-      !DIR$ OMP SIMD COLLAPSE(2)
-      DO j = LBOUND(b, 2), LBOUND(b, 2) + n - 1
-        !DIR$ LOOP COUNT(1, LIBXSMM_MAX_M, LIBXSMM_AVG_M)
-        DO i = LBOUND(a, 1), LBOUND(a, 1) + m - 1
-          c(i,j) = DOT_PRODUCT(a(i,:), b(:,j))
+          c(i,j) = xbeta * c(i,j) + xalpha * DOT_PRODUCT(a(i,:), b(:,j))
         END DO
       END DO
     ELSE
@@ -302,70 +290,89 @@ CONTAINS
       DO j = 1, m
         !DIR$ LOOP COUNT(1, LIBXSMM_MAX_N, LIBXSMM_AVG_N)
         DO i = 1, n
-          c(i,j) = DOT_PRODUCT(x(i,:), y(:,j))
+          c(i,j) = xbeta * c(i,j) + xalpha * DOT_PRODUCT(x(i,:), y(:,j))
         END DO
       END DO
     ENDIF
   END SUBROUTINE
 
-  ! Query the pointer of a generated function; zero if it does not exist.
-  !DIR$ ATTRIBUTES OFFLOAD:MIC :: libxsmm_mm_dispatch
-  !DIR$ ATTRIBUTES INLINE :: libxsmm_mm_dispatch
-  FUNCTION libxsmm_mm_dispatch(m, n, k, type) RESULT(f)
-    INTEGER(LIBXSMM_INTEGER_TYPE), INTENT(IN) :: m, n, k, type
-    !PROCEDURE(LIBXSMM_XMM_FUNCTION), POINTER :: f
-    TYPE(C_FUNPTR) :: f
-    f = MERGE( &
-      libxsmm_dmm_dispatch(m, n, k), &
-      libxsmm_smm_dispatch(m, n, k), &
-      LIBXSMM_DOUBLE_PRECISION.EQ.type)
-  END FUNCTION
-
-  ! Dispatched matrix-matrix multiplication; single-precision.
+  ! Dispatched matrix-matrix multiplication (single-precision).
   !DIR$ ATTRIBUTES OFFLOAD:MIC :: libxsmm_smm
   !DIR$ ATTRIBUTES INLINE :: libxsmm_smm
-  SUBROUTINE libxsmm_smm(m, n, k, a, b, c)
+  SUBROUTINE libxsmm_smm(alpha, beta, m, n, k, a, b, c)
     INTEGER(LIBXSMM_INTEGER_TYPE), PARAMETER :: T = LIBXSMM_SINGLE_PRECISION
     INTEGER(LIBXSMM_INTEGER_TYPE), INTENT(IN) :: m, n, k
-    REAL(T), TARGET, INTENT(IN) :: a(:,:), b(:,:)
-    REAL(T), TARGET, INTENT(INOUT) :: c(:,:)
+    REAL(T), INTENT(IN) :: alpha, beta, a(:,:), b(:,:)
+    REAL(T), INTENT(INOUT) :: c(:,:)
     !DIR$ ATTRIBUTES OFFLOAD:MIC :: smm
     PROCEDURE(LIBXSMM_SMM_FUNCTION), POINTER :: smm
     TYPE(C_FUNPTR) :: f
     IF (LIBXSMM_MAX_MNK.GE.(m * n * k)) THEN
-      f = libxsmm_smm_dispatch(m, n, k)
+      f = libxsmm_smm_dispatch(alpha, beta, m, n, k, libxsmm_ld(m, n), k, &
+            libxsmm_align_value(libxsmm_ld(m, n), T, LIBXSMM_ALIGNED_STORES), &
+            LIBXSMM_GEMM_FLAG_DEFAULT, LIBXSMM_PREFETCH)
       IF (C_ASSOCIATED(f)) THEN
         CALL C_F_PROCPOINTER(f, smm)
-        CALL smm(a, b, c)
+        CALL smm(alpha, beta, a, b, c)
       ELSE
-        CALL libxsmm_simm(m, n, k, a, b, c)
+        CALL libxsmm_simm(alpha, beta, m, n, k, a, b, c)
       ENDIF
     ELSE
-      CALL libxsmm_sblasmm(m, n, k, a, b, c)
+      CALL libxsmm_sblasmm(alpha, beta, m, n, k, a, b, c)
     ENDIF
   END SUBROUTINE
 
-  ! Dispatched matrix-matrix multiplication; double-precision.
+  ! Dispatched matrix-matrix multiplication (double-precision).
   !DIR$ ATTRIBUTES OFFLOAD:MIC :: libxsmm_dmm
   !DIR$ ATTRIBUTES INLINE :: libxsmm_dmm
-  SUBROUTINE libxsmm_dmm(m, n, k, a, b, c)
+  SUBROUTINE libxsmm_dmm(alpha, beta, m, n, k, a, b, c)
     INTEGER(LIBXSMM_INTEGER_TYPE), PARAMETER :: T = LIBXSMM_DOUBLE_PRECISION
     INTEGER(LIBXSMM_INTEGER_TYPE), INTENT(IN) :: m, n, k
-    REAL(T), TARGET, INTENT(IN) :: a(:,:), b(:,:)
-    REAL(T), TARGET, INTENT(INOUT) :: c(:,:)
+    REAL(T), INTENT(IN) :: alpha, beta, a(:,:), b(:,:)
+    REAL(T), INTENT(INOUT) :: c(:,:)
     !DIR$ ATTRIBUTES OFFLOAD:MIC :: dmm
     PROCEDURE(LIBXSMM_DMM_FUNCTION), POINTER :: dmm
     TYPE(C_FUNPTR) :: f
     IF (LIBXSMM_MAX_MNK.GE.(m * n * k)) THEN
-      f = libxsmm_dmm_dispatch(m, n, k)
+      f = libxsmm_dmm_dispatch(alpha, beta, m, n, k, libxsmm_ld(m, n), k, &
+            libxsmm_align_value(libxsmm_ld(m, n), T, LIBXSMM_ALIGNED_STORES), &
+            LIBXSMM_GEMM_FLAG_DEFAULT, LIBXSMM_PREFETCH)
       IF (C_ASSOCIATED(f)) THEN
         CALL C_F_PROCPOINTER(f, dmm)
-        CALL dmm(a, b, c)
+        CALL dmm(alpha, beta, a, b, c)
       ELSE
-        CALL libxsmm_dimm(m, n, k, a, b, c)
+        CALL libxsmm_dimm(alpha, beta, m, n, k, a, b, c)
       ENDIF
     ELSE
-      CALL libxsmm_dblasmm(m, n, k, a, b, c)
+      CALL libxsmm_dblasmm(alpha, beta, m, n, k, a, b, c)
     ENDIF
   END SUBROUTINE
+
+  !DIR$ ATTRIBUTES OFFLOAD:MIC :: smm_dispatch
+  !DIR$ ATTRIBUTES INLINE :: smm_dispatch
+  TYPE(C_FUNPTR) PURE FUNCTION smm_dispatch(alpha, beta, m, n, k, lda, ldb, ldc, flags, prefetch)
+    INTEGER(LIBXSMM_INTEGER_TYPE), PARAMETER :: T = LIBXSMM_SINGLE_PRECISION, Zero = 0
+    INTEGER(LIBXSMM_INTEGER_TYPE), OPTIONAL, INTENT(IN) :: lda, ldb, ldc, flags, prefetch
+    INTEGER(LIBXSMM_INTEGER_TYPE), INTENT(IN) :: m, n, k
+    REAL(T), INTENT(IN) :: alpha, beta
+    TYPE(C_FUNPTR) :: f
+    f = libxsmm_smm_dispatch(alpha, beta, m, n, k, &
+          MERGE(lda, Zero, PRESENT(lda)), MERGE(ldb, Zero, PRESENT(ldb)), MERGE(ldc, Zero, PRESENT(ldc)), &
+          MERGE(flags, LIBXSMM_GEMM_FLAG_DEFAULT, PRESENT(flags)), &
+          MERGE(prefetch, LIBXSMM_PREFETCH, PRESENT(prefetch)))
+  END FUNCTION
+
+  !DIR$ ATTRIBUTES OFFLOAD:MIC :: dmm_dispatch
+  !DIR$ ATTRIBUTES INLINE :: dmm_dispatch
+  TYPE(C_FUNPTR) PURE FUNCTION dmm_dispatch(alpha, beta, m, n, k, lda, ldb, ldc, flags, prefetch)
+    INTEGER(LIBXSMM_INTEGER_TYPE), PARAMETER :: T = LIBXSMM_DOUBLE_PRECISION, Zero = 0
+    INTEGER(LIBXSMM_INTEGER_TYPE), OPTIONAL, INTENT(IN) :: lda, ldb, ldc, flags, prefetch
+    INTEGER(LIBXSMM_INTEGER_TYPE), INTENT(IN) :: m, n, k
+    REAL(T), INTENT(IN) :: alpha, beta
+    TYPE(C_FUNPTR) :: f
+    f = libxsmm_dmm_dispatch(alpha, beta, m, n, k, &
+          MERGE(lda, Zero, PRESENT(lda)), MERGE(ldb, Zero, PRESENT(ldb)), MERGE(ldc, Zero, PRESENT(ldc)), &
+          MERGE(flags, LIBXSMM_GEMM_FLAG_DEFAULT, PRESENT(flags)), &
+          MERGE(prefetch, LIBXSMM_PREFETCH, PRESENT(prefetch)))
+  END FUNCTION
 END MODULE
