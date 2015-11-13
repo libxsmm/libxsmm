2ee12986e05feb1e3e9f2453504d098873b5538b
Alexander Heinecke <alexander.heinecke@intel.com>
2015-11-13 13:34:01 -0800

    added a routine to stream-store a componenten-wise vector product

--- samples/nek/stream_update_kernels.c
+++ samples/nek/stream_update_kernels.c
@@ -54,6 +54,61 @@ __attribute__((always_inline)) static inline void stream_init( int    i_length,
   *o_trip_stream = ((*o_trip_stream) > i_length) ? (*o_trip_prolog) : (*o_trip_stream);
 }
 
+void stream_vector_compscale( const double* i_a,
+                              const double* i_b,
+                              double*       io_c,
+                              const int     i_length) {
+  int l_n = 0;
+  int l_trip_prolog = 0;
+  int l_trip_stream = 0;
+  
+  /* init the trip counts */
+  stream_init( i_length, (size_t)io_c, &l_trip_prolog, &l_trip_stream );
+
+  /* run the prologue */
+  for ( ; l_n < l_trip_prolog;  l_n++ ) {
+    io_c[l_n] = i_a[l_n]*i_b[l_n];
+  }
+  /* run the bulk, hopefully using streaming stores */
+#if defined(__SSE3__) && defined(__AVX__) && !defined(__AVX512F__)
+  {
+    /* we need manual unrolling as the compiler otherwise generates 
+       too many dependencies */
+    for ( ; l_n < l_trip_stream;  l_n+=8 ) {
+      __m256d vec_a_1, vec_b_1;
+      __m256d vec_a_2, vec_b_2;
+
+      vec_a_1 = _mm256_loadu_pd(&(i_a[l_n]));
+      vec_a_2 = _mm256_loadu_pd(&(i_a[l_n+4]));
+      vec_b_1 = _mm256_loadu_pd(&(i_b[l_n]));
+      vec_b_2 = _mm256_loadu_pd(&(i_b[l_n+4]));
+
+      _mm256_stream_pd( &(io_c[l_n]),   _mm256_mul_pd( vec_a_1, vec_b_1 ) );
+      _mm256_stream_pd( &(io_c[l_n+4]), _mm256_mul_pd( vec_a_2, vec_b_2 ) );
+    }
+  }
+#elif defined(__SSE3__) && defined(__AVX__) && defined(__AVX512F__)
+  {
+    for ( ; l_n < l_trip_stream;  l_n+=8 ) {
+      __m512d vec_a, vec_b;
+
+      vec_a = _mm512_loadu_pd(&(i_a[l_n]));
+      vec_b = _mm512_loadu_pd(&(i_b[l_n]));
+
+      _mm512_stream_pd( &(io_c[l_n]), _mm512_mul_pd( vec_a, vec_b ) );
+    }
+  }
+#else
+  for ( ; l_n < l_trip_stream;  l_n++ ) {
+    io_c[l_n] = i_a[l_n]*i_b[l_n];
+  }
+#endif
+  /* run the epilogue */
+  for ( ; l_n < i_length;  l_n++ ) {
+    io_c[l_n] = i_a[l_n]*i_b[l_n];
+  }
+}
+
 void stream_update_helmholtz( const double* i_g1,
                               const double* i_g2,
                               const double* i_g3, 
