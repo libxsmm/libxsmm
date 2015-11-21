ec15d59c69c8be67b1531df8d1e7f1ddc408a448
Hans Pabst <hans.pabst@intel.com>
2015-11-21 22:40:47 +0100

    Testing hooks for issue #31.

--- .hooks/install.sh
+++ .hooks/install.sh
@@ -2,8 +2,11 @@
 
 HERE=$(cd $(dirname $0); pwd -P)
 GIT_DIR=${HERE}/../.git
+LOCKFILE=${GIT_DIR}/.commit
 CP=$(which cp)
+RM=$(which rm)
 
 if [[ -e ${GIT_DIR}/hooks ]] ; then
   ${CP} ${HERE}/pre-commit ${HERE}/post-commit ${GIT_DIR}/hooks
+  ${RM} -f ${LOCKFILE}
 fi
