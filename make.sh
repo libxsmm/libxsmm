b4dc7381aff6f40ac9aa7ab3eb98f4194e17405f
Hans Pabst <hans.pabst@intel.com>
2015-12-14 12:54:52 +0100

    Adjusted test coverage (PRECISION=2).

--- make.sh
+++ make.sh
@@ -2,7 +2,10 @@
 
 if [[ "-cp2k" == "$1" ]] ; then
   shift
-  make -e $* ALIGNED_LOADS=1 ALIGNED_STORES=1 MNK=" \
+  make -e $* \
+  ALIGNED_LOADS=1 ALIGNED_STORES=1 \
+  PRECISION=2 \
+  MNK=" \
     23, \
     6, \
     14 16 29, \
