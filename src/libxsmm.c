dd3065ad65da75d7bcb71549d3438f4b65f8e9e5
Hans Pabst <hans.pabst@intel.com>
2016-01-22 18:50:06 +0100

    Fixed the attempt mmap'ing the code buffer by resetting the result (clearing MAP_FAILED).

--- src/libxsmm.c
+++ src/libxsmm.c
@@ -484,6 +484,8 @@ LIBXSMM_INLINE LIBXSMM_RETARGETABLE void internal_build(const libxsmm_gemm_descr
         fprintf(stderr, "LIBXSMM: %s (mmap)!\n", strerror(errno));
 #endif
         free(generated_code.generated_code);
+        /* clear MAP_FAILED value */
+        *code = 0;
       }
     }
 #if !defined(NDEBUG)/* library code is expected to be mute */
