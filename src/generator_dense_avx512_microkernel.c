9f6ef66b99aaf2e209a68cfda7b86661e2c6c701
Hans Pabst <hans.pabst@intel.com>
2016-01-25 08:29:28 +0100

    Fixed compilation error happening without NDEBUG (DBG=1, SYM=1, or TRACE=1): remove referencing the former i_offset argument.

--- src/generator_dense_avx512_microkernel.c
+++ src/generator_dense_avx512_microkernel.c
@@ -40,7 +40,7 @@
 void libxsmm_generator_dense_avx512_microkernel( libxsmm_generated_code*             io_generated_code,
                                                  const libxsmm_gp_reg_mapping*       i_gp_reg_mapping,
                                                  const libxsmm_micro_kernel_config*  i_micro_kernel_config,
-                                                 const libxsmm_gemm_descriptor*     i_xgemm_desc,
+                                                 const libxsmm_gemm_descriptor*      i_xgemm_desc,
                                                  const unsigned int                  i_n_blocking,
                                                  const unsigned int                  i_k_blocking )
 {
@@ -59,9 +59,6 @@ void libxsmm_generator_dense_avx512_microkernel( libxsmm_generated_code*        
   if ( i_n_blocking > 30 ) {
     libxsmm_handle_error( io_generated_code, LIBXSMM_ERR_N_BLOCK );
     return;
-  }
-  if ( (i_offset >= 0) && (i_k_blocking != 1) ) {
-    fprintf(stderr, "LIBXSMM WARNING, libxsmm_generator_dense_avx512_microkernel: i_k_blocking is ignored as offset is >=0\n");
   }
 #endif
 
