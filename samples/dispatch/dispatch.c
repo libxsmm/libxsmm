a6ce6fb4f07bbe7ee98a5bb470c9677daf52b4a8
Hans Pabst <hans.pabst@intel.com>
2016-02-02 13:54:05 +0100

    Quantify dispatch cost relative to calling an empty (non-inline) function.

--- samples/dispatch/dispatch.c
+++ samples/dispatch/dispatch.c
@@ -14,7 +14,7 @@ int main(int argc, char* argv[])
 {
   const int size = LIBXSMM_DEFAULT(1 << 27, 1 < argc ? atoi(argv[1]) : 0);
   unsigned long long start;
-  double duration;
+  double dcall, ddisp;
   int i;
 
   fprintf(stdout, "Dispatching %i calls %s internal synchronization...\n", size,
@@ -33,10 +33,22 @@ int main(int argc, char* argv[])
   fprintf(stderr, "\tWarning: JIT support has been disabled at build time!\n");
 #endif
 
-  /* first invocation may actually generate code (which is here out of interest) */
+  /* first invocation may initialize some internals (libxsmm_init),
+   * or actually generate code (code gen. time is out of scope)
+   */
   libxsmm_dmmdispatch(LIBXSMM_AVG_M, LIBXSMM_AVG_N, LIBXSMM_AVG_K,
     NULL/*lda*/, NULL/*ldb*/, NULL/*ldc*/, NULL/*alpha*/, NULL/*beta*/,
     NULL/*flags*/, NULL/*prefetch*/);
+
+  /* run non-inline function to measure call overhead of an "empty" function */
+  start = libxsmm_timer_tick();
+#if defined(_OPENMP)
+# pragma omp parallel for
+#endif
+  for (i = 0; i < size; ++i) {
+    libxsmm_init(); /* subsequent calls are not doing any work */
+  }
+  dcall = libxsmm_timer_duration(start, libxsmm_timer_tick());
 
   start = libxsmm_timer_tick();
 #if defined(_OPENMP)
@@ -47,11 +59,12 @@ int main(int argc, char* argv[])
       NULL/*lda*/, NULL/*ldb*/, NULL/*ldc*/, NULL/*alpha*/, NULL/*beta*/,
       NULL/*flags*/, NULL/*prefetch*/);
   }
+  ddisp = libxsmm_timer_duration(start, libxsmm_timer_tick());
 
-  duration = libxsmm_timer_duration(start, libxsmm_timer_tick());
-  if (0 < duration) {
-    fprintf(stdout, "\tcalls/s: %.0f Hz\n", size / duration);
-    fprintf(stdout, "\tduration: %.0f ms\n", 1000.0 * duration);
+  if (0 < dcall && 0 < ddisp) {
+    fprintf(stdout, "\tdispatch calls/s: %.0f Hz\n", size / ddisp);
+    fprintf(stdout, "\tempty calls/s: %.0f Hz\n", size / dcall);
+    fprintf(stdout, "\toverhead: %.0fx\n", ddisp / dcall);
   }
   fprintf(stdout, "Finished\n");
 
