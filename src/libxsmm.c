836f5f30529c73b8681a28420b9c196c1fffc378
Hans Pabst <hans.pabst@intel.com>
2016-02-08 13:09:39 +0100

    Removed assertions due to warning "[...] will never be NULL".

--- src/libxsmm.c
+++ src/libxsmm.c
@@ -163,7 +163,6 @@ LIBXSMM_RETARGETABLE LIBXSMM_VISIBILITY_INTERNAL LIBXSMM_LOCK_TYPE internal_regl
   unsigned int hash, diff = 0, diff0 = 0, i, i0; \
   internal_code result; \
   INTERNAL_FIND_CODE_DECLARE(entry); \
-  assert(0 != (CRC32_FUNCTION) && 0 != (DIFF_FUNCTION)); \
   INTERNAL_FIND_CODE_INIT(entry); \
   /* check if the requested xGEMM is already JITted */ \
   LIBXSMM_PRAGMA_FORCEINLINE /* must precede a statement */ \
