f1afd0c28f5e2eef4e5144b92b1a55ae2edeffdb
Hans Pabst <hans.pabst@intel.com>
2016-02-12 20:27:04 +0100

    Completed test script for the link-time wrapper sample code.

--- samples/dgemm/test.sh
+++ samples/dgemm/test.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+LIBXSMM=../../lib/libxsmm.so
+
 HERE=$(cd $(dirname $0); pwd -P)
 ECHO=$(which echo)
 GREP=$(which grep)
@@ -8,18 +10,20 @@ ${ECHO} "============================="
 ${ECHO} "Running DGEMM (ORIGINAL BLAS)"
 ${ECHO} "============================="
 ( time ${HERE}/dgemm-blas.sh $*; ) 2>&1 | ${GREP} real
+${ECHO}
 
 if [ -e dgemm-wrap ]; then
   ${ECHO} "============================="
   ${ECHO} "Running DGEMM (STATIC WRAP)"
   ${ECHO} "============================="
   ( time ${HERE}/dgemm-wrap.sh $*; ) 2>&1 | ${GREP} real
+  ${ECHO}
 fi
 
-if [ -e ../../lib/libxsmm.so ]; then
+if [ -e ${LIBXSMM} ]; then
   ${ECHO} "============================="
   ${ECHO} "Running DGEMM (LD_PRELOAD)"
   ${ECHO} "============================="
-  ( LD_PRELOAD=xx time ${HERE}/dgemm-wrap.sh $*; ) 2>&1 | ${GREP} real
+  ( time LD_PRELOAD=${LIBXSMM} ${HERE}/dgemm-blas.sh $*; ) 2>&1 | ${GREP} real
 fi
 
