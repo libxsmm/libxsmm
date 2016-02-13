706e2bde889357985efad956c32efe4abb57b0d6
Hans Pabst <hans.pabst@intel.com>
2016-02-13 15:05:34 +0100

    Check that a "bigger" DGEMM (above LIBXSMM's threshold) does not enter a recursion if LIBXSMM is LD_PRELOADED or wrapped-in at link-time (via --wrap linker flag).

--- samples/dgemm/dgemm.c
+++ samples/dgemm/dgemm.c
@@ -1,14 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/** Problem size (sample) */
-#define M 23
-#define N M
-#define K M
-#define LDA M
-#define LDB K
-#define LDC M
-
 /** Construct symbol name from a given real type name (float or double). */
 #define GEMM(REAL) LIBXSMM_FSYMBOL(LIBXSMM_TPREFIX(REAL, gemm))
 
@@ -25,30 +17,40 @@ void dgemm_(const char*, const char*, const int*, const int*, const int*,
 int main(int argc, char* argv[])
 {
   const int size = 1 < argc ? atoi(argv[1]) : (1 << 22);
-  const int m = M, n = N, k = K, lda = LDA, ldb = LDB, ldc = LDC;
-  double a[LDA*K], b[LDB*N], c[LDC*N];
+  const int m = 23, n = m, k = m, lda = m, ldb = k, ldc = m, big = 512;
   const double alpha = 1, beta = 1;
   const char notrans = 'N';
+  double *a, *b, *c;
   int i, j;
 
-  for (i = 0; i < m; ++i) {
-    for (j = 0; j < k; ++j) {
-      const int index = i * lda + j;
+  a = (double*)malloc(big/*m*/ * big/*k*/ * sizeof(double));
+  b = (double*)malloc(big/*k*/ * big/*n*/ * sizeof(double));
+  c = (double*)malloc(big/*m*/ * big/*n*/ * sizeof(double));
+
+  for (i = 0; i < big/*m*/; ++i) {
+    for (j = 0; j < big/*k*/; ++j) {
+      const int index = i * big/*lda*/ + j;
       a[index] = 1.0 / (index + 1);
     }
   }
-  for (i = 0; i < k; ++i) {
-    for (j = 0; j < n; ++j) {
-      const int index = i * ldb + j;
+  for (i = 0; i < big/*k*/; ++i) {
+    for (j = 0; j < big/*n*/; ++j) {
+      const int index = i * big/*ldb*/ + j;
       b[index] = 2.0 / (index + 1);
     }
   }
-  for (i = 0; i < m; ++i) {
-    for (j = 0; j < n; ++j) {
-      const int index = i * ldc + j;
+  for (i = 0; i < big/*m*/; ++i) {
+    for (j = 0; j < big/*n*/; ++j) {
+      const int index = i * big/*ldc*/ + j;
       c[index] = -1.0;
     }
   }
+
+  /**
+   * warmup BLAS, and check that a "bigger" DGEMM (above LIBXSMM's threshold)
+   * does not enter a recursion if LIBXSMM is LD_PRELOADED or wrapped-in
+   */
+  dgemm_(&notrans, &notrans, &big, &big, &big, &alpha, a, &big, b, &big, &beta, c, &big);
 
 #if defined(_OPENMP)
 # pragma omp parallel for
@@ -56,6 +58,10 @@ int main(int argc, char* argv[])
   for (i = 0; i < size; ++i) {
     dgemm_(&notrans, &notrans, &m, &n, &k, &alpha, a, &lda, b, &ldb, &beta, c, &ldc);
   }
+
+  free(a);
+  free(b);
+  free(c);
 
   return EXIT_SUCCESS;
 }
