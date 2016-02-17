246266af44708a79b5b929f9cdd3f7bc480d3aef
Hans Pabst <hans.pabst@intel.com>
2016-02-17 12:31:57 +0100

    More portable test script (dgemm sample code).

--- samples/dgemm/dgemm-test.sh
+++ samples/dgemm/dgemm-test.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
-DEPDIR=../../lib
+DEPDIR=../..
 
 HERE=$(cd $(dirname $0); pwd -P)
+TMPF=$(mktemp /tmp/fileXXXXXX)
 UNAME=$(which uname)
 ECHO=$(which echo)
 GREP=$(which grep)
+SORT=$(which sort)
+RM=$(which rm)
 
 if [ "Darwin" != "$(${UNAME})" ]; then
-  export LD_LIBRARY_PATH=${DEPDIR}:${LD_LIBRARY_PATH}
   LIBEXT=so
 else
-  set env DYLD_LIBRARY_PATH ${DEPDIR}:${DYLD_LIBRARY_PATH}
   LIBEXT=dylib
 fi
 
@@ -19,44 +20,48 @@ if [ -e dgemm-blas ]; then
   ${ECHO} "============================="
   ${ECHO} "Running DGEMM (ORIGINAL BLAS)"
   ${ECHO} "============================="
-  ( time ERROR=$({ ${HERE}/dgemm-blas.sh $*; } 2>&1); ) 2>&1 | ${GREP} real
+  { time ${HERE}/dgemm-blas.sh $* 2>${TMPF}; } 2>&1 | ${GREP} real
   RESULT=$?
   if [ 0 != ${RESULT} ]; then
-    ${ECHO} "FAILED(${RESULT}) ${ERROR}"
+    ${ECHO} -n "FAILED(${RESULT}) "; ${SORT} -u ${TMPF}
   else
-    ${ECHO} "OK ${ERROR}"
+    ${ECHO} -n "OK "; ${SORT} -u ${TMPF}
   fi
   ${ECHO}
 
-  if [ -e ${DEPDIR}/libxsmmld.${LIBEXT} ]; then
+  if [ -e ${DEPDIR}/lib/libxsmmld.${LIBEXT} ]; then
+    ${ECHO}
     ${ECHO} "============================="
     ${ECHO} "Running DGEMM (LD_PRELOAD)"
     ${ECHO} "============================="
-    ( time ERROR=$({ \
-      LD_PRELOAD=${DEPDIR}/libxsmmld.${LIBEXT} \
-      DYLD_INSERT_LIBRARIES=${DEPDIR}/libxsmmld.${LIBEXT} \
-        ${HERE}/dgemm-blas.sh $*; } 2>&1); ) 2>&1 | ${GREP} real
+    { time \
+      LD_LIBRARY_PATH=${DEPDIR}/lib:${LD_LIBRARY_PATH} LD_PRELOAD=${DEPDIR}/lib/libxsmmld.${LIBEXT} \
+      DYLD_LIBRARY_PATH=${DEPDIR}/lib:${DYLD_LIBRARY_PATH} DYLD_INSERT_LIBRARIES=${DEPDIR}/lib/libxsmmld.${LIBEXT} \
+      ${HERE}/dgemm-blas.sh $* 2>${TMPF}; } 2>&1 | ${GREP} real
     RESULT=$?
     if [ 0 != ${RESULT} ]; then
-      ${ECHO} "FAILED(${RESULT}) ${ERROR}"
+      ${ECHO} -n "FAILED(${RESULT}) "; ${SORT} -u ${TMPF}
     else
-      ${ECHO} "OK ${ERROR}"
+      ${ECHO} -n "OK "; ${SORT} -u ${TMPF}
     fi
     ${ECHO}
   fi
 fi
 
 if [ -e dgemm-wrap ]; then
+  ${ECHO}
   ${ECHO} "============================="
   ${ECHO} "Running DGEMM (STATIC WRAP)"
   ${ECHO} "============================="
-  ( time ERROR=$({ ${HERE}/dgemm-wrap.sh $*; } 2>&1); ) 2>&1 | ${GREP} real
+  { time ${HERE}/dgemm-wrap.sh $* 2>${TMPF}; } 2>&1 | ${GREP} real
   RESULT=$?
   if [ 0 != ${RESULT} ]; then
-    ${ECHO} "FAILED(${RESULT}) ${ERROR}"
+    ${ECHO} -n "FAILED(${RESULT}) "; ${SORT} -u ${TMPF}
   else
-    ${ECHO} "OK ${ERROR}"
+    ${ECHO} -n "OK "; ${SORT} -u ${TMPF}
   fi
   ${ECHO}
 fi
+
+${RM} -f ${TMPF}
 
