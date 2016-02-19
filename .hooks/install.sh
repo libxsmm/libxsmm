de586e1c085edaa662572a95db0e25e7fc2902c9
Hans Pabst <hans.pabst@intel.com>
2016-02-19 16:14:03 +0100

    Adjusted Git hooks and hook-install script. Previously a user-defined version.txt was overwritten by the commit hooks.

--- .hooks/install.sh
+++ .hooks/install.sh
@@ -2,16 +2,15 @@
 
 HERE=$(cd $(dirname $0); pwd -P)
 GIT_DIR=${HERE}/../.git
-LOCKFILE=${GIT_DIR}/.commit
-
 CP=$(which cp)
 RM=$(which rm)
 
 if [ -e ${GIT_DIR}/hooks ]; then
-  ${CP} ${HERE}/version.sh ${GIT_DIR}/hooks
   ${CP} ${HERE}/pre-commit ${GIT_DIR}/hooks
   ${CP} ${HERE}/post-commit ${GIT_DIR}/hooks
   #${CP} ${HERE}/post-merge ${GIT_DIR}/hooks
+  #${CP} ${HERE}/version.sh ${GIT_DIR}/hooks
+  ${RM} -f ${GIT_DIR}/hooks/version.sh
   ${RM} -f ${GIT_DIR}/hooks/post-merge
-  ${RM} -f ${LOCKFILE}
+  ${RM} -f ${GIT_DIR}/.commit
 fi
