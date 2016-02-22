f81a040fa7a031d351a7bdbcb8fbd185b3d84e9d
Hans Pabst <hans.pabst@intel.com>
2016-02-22 14:35:14 +0100

    Re-enable LIBXSMM_GEMM_DIFF_SW in general.

--- src/libxsmm.c
+++ src/libxsmm.c
@@ -70,7 +70,7 @@
 #endif
 
 /* enable generic variant of libxsmm_gemm_diff */
-#if !defined(LIBXSMM_GEMM_DIFF_SW) && defined(__MIC__)
+#if !defined(LIBXSMM_GEMM_DIFF_SW) /*&& defined(__MIC__)*/
 # define LIBXSMM_GEMM_DIFF_SW
 #endif
 
