f256d1fe84a02894d8e239780d1fe3e26dbd81c0
Hans Pabst <hans.pabst@intel.com>
2016-03-06 16:33:10 -0800

    Issue #66 (Link behavior changed, cannot longer link to without OpenMP when library was built using OpenMP): remove dependency of the static link-time wrapper from "omps" routines by weakening the static wrapper symbol. Now enabling static wrap plus nested/multi-threaded "omps" routine requires linking libxsmmext.

--- src/libxsmm_gemm_extomp.c
+++ src/libxsmm_gemm_extomp.c
@@ -195,3 +195,69 @@ LIBXSMM_EXTERN_C LIBXSMM_RETARGETABLE void libxsmm_omps_dgemm(const char* transa
     c, *(ldc ? ldc : LIBXSMM_LD(m, n)));
 }
 
+
+#if defined(LIBXSMM_GEMM_EXTWRAP)
+
+LIBXSMM_EXTERN_C LIBXSMM_RETARGETABLE void LIBXSMM_GEMM_EXTWRAP_SGEMM(
+  const char* transa, const char* transb,
+  const libxsmm_blasint* m, const libxsmm_blasint* n, const libxsmm_blasint* k,
+  const float* alpha, const float* a, const libxsmm_blasint* lda,
+  const float* b, const libxsmm_blasint* ldb,
+  const float* beta, float* c, const libxsmm_blasint* ldc)
+{
+  assert(LIBXSMM_GEMM_EXTWRAP_SGEMM != libxsmm_internal_sgemm);
+  switch (libxsmm_internal_gemm) {
+    case 1: {
+      libxsmm_omps_sgemm(transa, transb, m, n, k, alpha, a, lda, b, ldb, beta, c, ldc);
+    } break;
+    case 2: {
+#if defined(_OPENMP)
+#     pragma omp parallel
+#     pragma omp single
+#endif
+      libxsmm_omps_sgemm(transa, transb, m, n, k, alpha, a, lda, b, ldb, beta, c, ldc);
+    } break;
+    default: {
+      LIBXSMM_GEMM_DECLARE_FLAGS(flags, transa, transb, m, n, k, a, b, c);
+      LIBXSMM_XGEMM(float, libxsmm_blasint, flags, *m, *n, *k,
+        0 != alpha ? *alpha : ((float)LIBXSMM_ALPHA),
+        a, *(lda ? lda : LIBXSMM_LD(m, k)), b, *(ldb ? ldb : LIBXSMM_LD(k, n)),
+        0 != beta ? *beta : ((float)LIBXSMM_BETA),
+        c, *(ldc ? ldc : LIBXSMM_LD(m, n)));
+    }
+  }
+}
+
+
+LIBXSMM_EXTERN_C LIBXSMM_RETARGETABLE void LIBXSMM_GEMM_EXTWRAP_DGEMM(
+  const char* transa, const char* transb,
+  const libxsmm_blasint* m, const libxsmm_blasint* n, const libxsmm_blasint* k,
+  const double* alpha, const double* a, const libxsmm_blasint* lda,
+  const double* b, const libxsmm_blasint* ldb,
+  const double* beta, double* c, const libxsmm_blasint* ldc)
+{
+  assert(LIBXSMM_GEMM_EXTWRAP_DGEMM != libxsmm_internal_dgemm);
+  switch (libxsmm_internal_gemm) {
+    case 1: {
+      libxsmm_omps_dgemm(transa, transb, m, n, k, alpha, a, lda, b, ldb, beta, c, ldc);
+    } break;
+    case 2: {
+#if defined(_OPENMP)
+#     pragma omp parallel
+#     pragma omp single
+#endif
+      libxsmm_omps_dgemm(transa, transb, m, n, k, alpha, a, lda, b, ldb, beta, c, ldc);
+    } break;
+    default: {
+      LIBXSMM_GEMM_DECLARE_FLAGS(flags, transa, transb, m, n, k, a, b, c);
+      LIBXSMM_XGEMM(double, libxsmm_blasint, flags, *m, *n, *k,
+        0 != alpha ? *alpha : ((double)LIBXSMM_ALPHA),
+        a, *(lda ? lda : LIBXSMM_LD(m, k)), b, *(ldb ? ldb : LIBXSMM_LD(k, n)),
+        0 != beta ? *beta : ((double)LIBXSMM_BETA),
+        c, *(ldc ? ldc : LIBXSMM_LD(m, n)));
+    }
+  }
+}
+
+#endif /*defined(LIBXSMM_GEMM_EXTWRAP)*/
+
