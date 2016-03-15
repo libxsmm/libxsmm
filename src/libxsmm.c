8d6e19574b5e17be64ae37478a8ba50546b48199
Hans Pabst <hans.pabst@intel.com>
2016-03-15 18:23:56 +0100

    Fixed string comparison.

--- src/libxsmm.c
+++ src/libxsmm.c
@@ -347,7 +347,7 @@ LIBXSMM_INLINE LIBXSMM_RETARGETABLE internal_regentry* internal_init(void)
       const char *const env_jit = getenv("LIBXSMM_JIT");
       if (env_jit && *env_jit) {
         const int jit = atoi(env_jit);
-        if (strcmp("0", env_jit)) { /* suppress running libxsmm_cpuid_x86 */
+        if (0 == strcmp("0", env_jit)) { /* suppress running libxsmm_cpuid_x86 */
           internal_target_archid = "generic";
         }
         else if (1 < jit) { /* suppress libxsmm_cpuid_x86 and override archid */
@@ -370,15 +370,15 @@ LIBXSMM_INLINE LIBXSMM_RETARGETABLE internal_regentry* internal_init(void)
             }
           }
         }
-        else if (strcmp("knl", env_jit) || strcmp("skx", env_jit)) {
+        else if (0 == strcmp("knl", env_jit) || 0 == strcmp("skx", env_jit)) {
           internal_target_arch = LIBXSMM_X86_AVX512;
           internal_target_archid = env_jit;
         }
-        else if (strcmp("hsw", env_jit)) {
+        else if (0 == strcmp("hsw", env_jit)) {
           internal_target_arch = LIBXSMM_X86_AVX2;
           internal_target_archid = env_jit;
         }
-        else if (strcmp("snb", env_jit)) {
+        else if (0 == strcmp("snb", env_jit)) {
           internal_target_arch = LIBXSMM_X86_AVX;
           internal_target_archid = env_jit;
         }
