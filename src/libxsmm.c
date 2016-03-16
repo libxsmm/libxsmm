33cc5875386ca6bfd6238da2cc0ba5c6dec99d57
Hans Pabst <hans.pabst@intel.com>
2016-03-16 15:20:15 +0100

    Adjusted termination message. [skip ci]

--- src/libxsmm.c
+++ src/libxsmm.c
@@ -573,11 +573,12 @@ LIBXSMM_RETARGETABLE void libxsmm_finalize(void)
             }
           }
 #if !defined(NDEBUG) /* library code is expected to be mute */
-          fprintf(stderr, "LIBXSMM_JIT=%s NJIT=%u NSTATIC=%u\n",
-            0 != internal_target_archid ? internal_target_archid : "0", njit, nstatic);
-
+          fprintf(stderr, "LIBXSMM_JIT=%s NJIT=%u NSTATIC=%u", 0 != internal_target_archid ? internal_target_archid : "0", njit, nstatic);
           if (0 != internal_ncollisions) {
             fprintf(stderr, ": %u hash key collisions handled!\n", internal_ncollisions);
+          }
+          else {
+            fprintf(stderr, "\n");
           }
 #endif
         }
