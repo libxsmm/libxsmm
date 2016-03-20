ca8a1509a862ecc068d799804a49abe1001b9272
Hans Pabst <hans.pabst@intel.com>
2016-03-20 22:45:41 +0100

    Fixed test case to account for compiler's padding.

--- tests/descriptor.c
+++ tests/descriptor.c
@@ -131,13 +131,13 @@ int main()
     descs[2] = b.descriptor; descs[3] = a.descriptor;
 
     if (0 != libxsmm_gemm_diffn(&a.descriptor, descs, 0/*hint*/,
-      sizeof(descs) / sizeof(*descs), LIBXSMM_GEMM_DESCRIPTOR_SIZE))
+      sizeof(descs) / sizeof(*descs), sizeof(libxsmm_gemm_descriptor)))
     {
       fprintf(stderr, "using dispatched diff-search\n");
       return 21;
     }
     else if (2 != libxsmm_gemm_diffn(&b.descriptor, descs, 0/*hint*/,
-      sizeof(descs) / sizeof(*descs), LIBXSMM_GEMM_DESCRIPTOR_SIZE))
+      sizeof(descs) / sizeof(*descs), sizeof(libxsmm_gemm_descriptor)))
     {
       fprintf(stderr, "using dispatched diff-search\n");
       return 22;
