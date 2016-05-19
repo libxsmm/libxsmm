fe5dcd492052c2a50613a83824489d367a7b725b
Hans Pabst <hans.pabst@intel.com>
2016-05-19 11:31:39 +0200

    Avoid warning about evaluation in an unspecified order.

--- src/libxsmm_gemm_diff.c
+++ src/libxsmm_gemm_diff.c
@@ -140,6 +140,7 @@ unsigned int libxsmm_gemm_diff_avx(const libxsmm_gemm_descriptor* reference, con
   assert(8 >= LIBXSMM_DIV2(LIBXSMM_GEMM_DESCRIPTOR_SIZE, sizeof(unsigned int)));
 # if (28 == LIBXSMM_GEMM_DESCRIPTOR_SIZE)
   {
+    int r0, r1;
     __m256i a256, b256;
 #   if defined(__CYGWIN__) && !defined(NDEBUG) /* Cygwin/GCC: _mm256_set_epi32 may cause an illegal instruction */
     const union { int32_t array[8]; __m256i i; } m256 = { /* use literal value rather than yes/no
@@ -158,7 +159,10 @@ unsigned int libxsmm_gemm_diff_avx(const libxsmm_gemm_descriptor* reference, con
     a256 = _mm256_loadu_si256((const __m256i*)reference);
 #   endif
     b256 = _mm256_castps_si256(_mm256_maskload_ps((const float*)desc, m256.i));
-    return _mm256_testnzc_si256(a256, b256) | _mm256_testnzc_si256(b256, a256);
+    /* avoid warning about eval. in unspecified order: r0, r1 */
+    r0 = _mm256_testnzc_si256(a256, b256);
+    r1 = _mm256_testnzc_si256(b256, a256);
+    return r0 | r1;
   }
 # elif (16 == LIBXSMM_GEMM_DESCRIPTOR_SIZE)
   {
@@ -169,7 +173,10 @@ unsigned int libxsmm_gemm_diff_avx(const libxsmm_gemm_descriptor* reference, con
     const __m256i a256 = _mm256_loadu_si256((const __m256i*)reference);
     const __m256i b256 = _mm256_loadu_si256((const __m256i*)desc);
 #   endif
-    return _mm256_testnzc_si256(a256, b256) | _mm256_testnzc_si256(b256, a256);
+    /* avoid warning about eval. in unspecified order: r0, r1 */
+    const int r0 = _mm256_testnzc_si256(a256, b256);
+    const int r1 = _mm256_testnzc_si256(b256, a256);
+    return r0 | r1;
   }
 # else
   return libxsmm_gemm_diff_sw(reference, desc);
@@ -206,7 +213,10 @@ unsigned int libxsmm_gemm_diff_avx2(const libxsmm_gemm_descriptor* reference, co
     const __m256i a256 = _mm256_loadu_si256((const __m256i*)reference);
 #   endif
     const __m256i b256 = _mm256_maskload_epi32((const void*)desc, m256);
-    return _mm256_testnzc_si256(a256, b256) | _mm256_testnzc_si256(b256, a256);
+    /* avoid warning about eval. in unspecified order: r0, r1 */
+    const int r0 = _mm256_testnzc_si256(a256, b256);
+    const int r1 = _mm256_testnzc_si256(b256, a256);
+    return r0 | r1;
   }
 # elif (16 == LIBXSMM_GEMM_DESCRIPTOR_SIZE)
   { /* no difference between AVX and AVX2 based implementation */
