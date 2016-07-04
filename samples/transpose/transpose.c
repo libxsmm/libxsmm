4dd5262c4dad398ea2b1bc1da4b283464cede622
Hans Pabst <hans.pabst@intel.com>
2016-07-04 22:53:47 +0200

    Prepared sample code for upcoming in-place transpose.

--- samples/transpose/transpose.c
+++ samples/transpose/transpose.c
@@ -26,35 +26,48 @@ LIBXSMM_INLINE REAL_TYPE initial_value(libxsmm_blasint i, libxsmm_blasint j, lib
 
 int main(int argc, char* argv[])
 {
-  const libxsmm_blasint m = 1 < argc ? atoi(argv[1]) : 4096;
-  const libxsmm_blasint n = 2 < argc ? atoi(argv[2]) : m;
-  const libxsmm_blasint lda = LIBXSMM_MAX(3 < argc ? atoi(argv[3]) : 0, m);
-  const libxsmm_blasint ldb = LIBXSMM_MAX(4 < argc ? atoi(argv[4]) : 0, n);
+  const char t = 1 < argc ? *argv[1] : 'o';
+  const libxsmm_blasint m = 2 < argc ? atoi(argv[2]) : 4096;
+  const libxsmm_blasint n = 3 < argc ? atoi(argv[3]) : m;
+  const libxsmm_blasint lda = LIBXSMM_MAX/*sanitize ld*/(4 < argc ? atoi(argv[4]) : 0, m);
+  const libxsmm_blasint ldb = LIBXSMM_MAX/*sanitize ld*/(5 < argc ? atoi(argv[5]) : 0, n);
 
-  REAL_TYPE *const a = (REAL_TYPE*)malloc(lda * n * sizeof(REAL_TYPE));
-  REAL_TYPE *const b = (REAL_TYPE*)malloc(ldb * m * sizeof(REAL_TYPE));
+  REAL_TYPE *const a = (REAL_TYPE*)malloc(lda * (('o' == t || 'O' == t) ? n : lda) * sizeof(REAL_TYPE));
+  REAL_TYPE *const b = (REAL_TYPE*)malloc(ldb * (('o' == t || 'O' == t) ? m : ldb) * sizeof(REAL_TYPE));
   const unsigned int size = m * n * sizeof(REAL_TYPE);
   unsigned long long start;
   libxsmm_blasint i, j;
   double duration;
 
-  fprintf(stdout, "m=%i n=%i lda=%i ldb=%i size=%.fMB (%s)\n", m, n, lda, ldb,
-    1.0 * size / (1 << 20), 8 == sizeof(REAL_TYPE) ? "DP" : "SP");
+  fprintf(stdout, "m=%i n=%i lda=%i ldb=%i size=%.fMB (%s, %s)\n", m, n, lda, ldb,
+    1.0 * size / (1 << 20), 8 == sizeof(REAL_TYPE) ? "DP" : "SP",
+    ('o' == t || 'O' == t) ? "out-of-place" : "in-place");
 
   for (i = 0; i < n; ++i) {
     for (j = 0; j < m; ++j) {
-      a[i*lda+j] = initial_value(i, j, lda);
+      a[i*lda+j] = initial_value(i, j, m);
     }
   }
 
-  start = libxsmm_timer_tick();
-  libxsmm_transpose_oop(b, a, sizeof(REAL_TYPE), m, n, lda, ldb);
-  libxsmm_transpose_oop(a, b, sizeof(REAL_TYPE), n, m, ldb, lda);
-  duration = libxsmm_timer_duration(start, libxsmm_timer_tick());
+  if (('o' == t || 'O' == t)) {
+    start = libxsmm_timer_tick();
+    libxsmm_transpose_oop(b, a, sizeof(REAL_TYPE), m, n, lda, ldb);
+    libxsmm_transpose_oop(a, b, sizeof(REAL_TYPE), n, m, ldb, lda);
+    duration = libxsmm_timer_duration(start, libxsmm_timer_tick());
+  }
+  else {
+    if (('i' != t && 'I' != t)) {
+      fprintf(stderr, "In-place transpose assumed!");
+    }
+    start = libxsmm_timer_tick();
+    /*libxsmm_transpose_inp(a, sizeof(REAL_TYPE), m, n, lda);
+    libxsmm_transpose_inp(a, sizeof(REAL_TYPE), n, m, lda);*/
+    duration = libxsmm_timer_duration(start, libxsmm_timer_tick());
+  }
 
   for (i = 0; i < n; ++i) {
     for (j = 0; j < m; ++j) {
-      if (0 < fabs(a[i*lda+j] - initial_value(i, j, lda))) {
+      if (initial_value(i, j, m) != a[i*lda+j]) {
         i = n + 1;
         break;
       }
@@ -74,10 +87,17 @@ int main(int argc, char* argv[])
 #if defined(__MKL) || defined(MKL_DIRECT_CALL_SEQ) || defined(MKL_DIRECT_CALL)
   {
     double mkl_duration;
-    start = libxsmm_timer_tick();
-    LIBXSMM_CONCATENATE(mkl_, LIBXSMM_TPREFIX(REAL_TYPE, omatcopy))('C', 'T', m, n, 1, a, lda, b, ldb);
-    LIBXSMM_CONCATENATE(mkl_, LIBXSMM_TPREFIX(REAL_TYPE, omatcopy))('C', 'T', n, m, 1, b, ldb, a, lda);
-    mkl_duration = libxsmm_timer_duration(start, libxsmm_timer_tick());
+    if (('o' == t || 'O' == t)) {
+      start = libxsmm_timer_tick();
+      LIBXSMM_CONCATENATE(mkl_, LIBXSMM_TPREFIX(REAL_TYPE, omatcopy))('C', 'T', m, n, 1, a, lda, b, ldb);
+      LIBXSMM_CONCATENATE(mkl_, LIBXSMM_TPREFIX(REAL_TYPE, omatcopy))('C', 'T', n, m, 1, b, ldb, a, lda);
+      mkl_duration = libxsmm_timer_duration(start, libxsmm_timer_tick());
+    }
+    else {
+      start = libxsmm_timer_tick();
+      /* TODO */
+      mkl_duration = libxsmm_timer_duration(start, libxsmm_timer_tick());
+    }
     if (0 < mkl_duration) {
       fprintf(stdout, "\tMKL: %.1fx\n", duration / mkl_duration);
     }
--- samples/transpose/transpose.f
+++ samples/transpose/transpose.f
@@ -42,58 +42,71 @@ PROGRAM transpose
   INTEGER(8) :: start
 
   CHARACTER(32) :: argv
+  CHARACTER :: trans
   INTEGER :: argc
 
   argc = COMMAND_ARGUMENT_COUNT()
   IF (1 <= argc) THEN
-    CALL GET_COMMAND_ARGUMENT(1, argv)
+    CALL GET_COMMAND_ARGUMENT(1, trans)
+  ELSE
+    trans = 'o'
+  END IF
+  IF (2 <= argc) THEN
+    CALL GET_COMMAND_ARGUMENT(2, argv)
     READ(argv, "(I32)") m
   ELSE
     m = 4096
   END IF
-  IF (2 <= argc) THEN
-    CALL GET_COMMAND_ARGUMENT(2, argv)
+  IF (3 <= argc) THEN
+    CALL GET_COMMAND_ARGUMENT(3, argv)
     READ(argv, "(I32)") n
   ELSE
     n = m
   END IF
-  IF (3 <= argc) THEN
-    CALL GET_COMMAND_ARGUMENT(3, argv)
+  IF (4 <= argc) THEN
+    CALL GET_COMMAND_ARGUMENT(4, argv)
     READ(argv, "(I32)") lda
   ELSE
     lda = m
   END IF
-  IF (4 <= argc) THEN
-    CALL GET_COMMAND_ARGUMENT(4, argv)
+  IF (5 <= argc) THEN
+    CALL GET_COMMAND_ARGUMENT(5, argv)
     READ(argv, "(I32)") ldb
   ELSE
     ldb = n
   END IF
 
   size = m * n * T ! size in Byte
-  WRITE(*, "(3(A,I0),A,I0,A,I0,A,I0)") "m=", m, " n=", n, " lda=", lda, " ldb=", ldb, &
-    " size=", (size / ISHFT(1, 20)), "MB"
+  WRITE(*, "(2(A,I0),2(A,I0),A,I0,A,2A,2A,1A)") "m=", m, " n=", n, " lda=", lda, " ldb=", ldb, &
+    " size=", (size / ISHFT(1, 20)), "MB (", MERGE("DP", "SP", 8.EQ.T), ", ", &
+    TRIM(MERGE("out-of-place", "in-place    ", 'o'.EQ.trans)), ")"
 
   ! Allocate matrices
-  ALLOCATE(a(lda,n))
-  ALLOCATE(b(ldb,m))
+  ALLOCATE(a(lda,MERGE(n,lda,('o'.EQ.trans).OR.('O'.EQ.trans))))
+  ALLOCATE(b(ldb,MERGE(m,ldb,('o'.EQ.trans).OR.('O'.EQ.trans))))
 
   DO j = 1, n
     DO i = 1, m
-      a(i,j) = initial_value(i - 1, j - 1, lda)
+      a(i,j) = initial_value(i - 1, j - 1, m)
     END DO
   END DO
 
-  start = libxsmm_timer_tick();
-  CALL libxsmm_transpose_oop(C_LOC(b), C_LOC(a), T, m, n, lda, ldb)
-  !CALL libxsmm_transpose_oop(C_LOC(a), C_LOC(b), T, n, m, ldb, lda)
-  CALL libxsmm_dtranspose_oop(a, b, n, m, ldb, lda)
-  duration = libxsmm_timer_duration(start, libxsmm_timer_tick());
+  IF (('o'.EQ.trans).OR.('O'.EQ.trans)) THEN
+    start = libxsmm_timer_tick();
+    CALL libxsmm_transpose_oop(C_LOC(b), C_LOC(a), T, m, n, lda, ldb)
+    !CALL libxsmm_transpose_oop(C_LOC(a), C_LOC(b), T, n, m, ldb, lda)
+    CALL libxsmm_dtranspose_oop(a, b, n, m, ldb, lda)
+    duration = libxsmm_timer_duration(start, libxsmm_timer_tick());
+  ELSE ! in-place
+    start = libxsmm_timer_tick();
+    ! TODO: in-place
+    duration = libxsmm_timer_duration(start, libxsmm_timer_tick());
+  END IF
 
   diff = 0
   DO j = 1, n
     DO i = 1, m
-      diff = MAX(diff, ABS(a(i,j) - initial_value(i - 1, j - 1, lda)))
+      diff = MAX(diff, ABS(a(i,j) - initial_value(i - 1, j - 1, m)))
     END DO
   END DO
 
