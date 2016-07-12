f3e1cb8f7012ef1e7afa4978f0d307c45d924b34
Hans Pabst <hans.pabst@intel.com>
2016-07-12 09:46:34 +0200

    Use virtual memory on a conditional basis (if certain buffer flags are requested); optionally allow virtual memory allocation function even for regular/default memory buffers. Incorporated THP madvise (TODO: request this in the first place i.e., when mmap is called). Extended libxsmm_alloc_info to rely on an internal variant supporting buffer flags on a conditional basis (compile-time). Incorporated error message at deallocation (debug code).

--- src/libxsmm_alloc.c
+++ src/libxsmm_alloc.c
@@ -29,6 +29,7 @@
 /* Hans Pabst (Intel Corp.)
 ******************************************************************************/
 #include "libxsmm_alloc.h"
+#include "libxsmm_sync.h"
 
 #if defined(LIBXSMM_OFFLOAD_TARGET)
 # pragma offload_attribute(push,target(LIBXSMM_OFFLOAD_TARGET))
@@ -36,29 +37,36 @@
 #include <stdlib.h>
 #if !defined(NDEBUG)
 # include <string.h>
+# include <stdio.h>
 #endif
 #if defined(_WIN32)
 # include <windows.h>
 #else
 # include <sys/mman.h>
+# include <errno.h>
 #endif
 #if defined(LIBXSMM_OFFLOAD_TARGET)
 # pragma offload_attribute(pop)
 #endif
 
-#if !defined(LIBXSMM_MAX_ALIGN)
-# define LIBXSMM_MAX_ALIGN (2 * 1024 *1024)
+#if !defined(LIBXSMM_ALLOC_ALIGNMAX)
+# define LIBXSMM_ALLOC_ALIGNMAX (2 * 1024 *1024)
 #endif
-
+#if !defined(LIBXSMM_ALLOC_ALIGNFCT)
+# define LIBXSMM_ALLOC_ALIGNFCT 2
+#endif
 #if !defined(LIBXSMM_ALLOC_MMAP)
 /*# define LIBXSMM_ALLOC_MMAP*/
 #endif
 
 
-typedef struct LIBXSMM_RETARGETABLE internal_alloc_info {
+typedef struct LIBXSMM_RETARGETABLE internal_alloc_info_type {
   void* pointer;
   unsigned int size;
-} internal_alloc_info;
+#if !defined(LIBXSMM_ALLOC_MMAP)
+  int flags;
+#endif
+} internal_alloc_info_type;
 
 
 LIBXSMM_EXTERN_C LIBXSMM_RETARGETABLE unsigned int libxsmm_gcd(unsigned int a, unsigned int b)
@@ -81,11 +89,11 @@ LIBXSMM_EXTERN_C LIBXSMM_RETARGETABLE unsigned int libxsmm_lcm(unsigned int a, u
 LIBXSMM_EXTERN_C LIBXSMM_RETARGETABLE unsigned int libxsmm_alignment(unsigned int size, unsigned int alignment)
 {
   unsigned int result = sizeof(void*);
-  if ((LIBXSMM_MAX_ALIGN) <= size) {
-    result = libxsmm_lcm(0 == alignment ? (LIBXSMM_ALIGNMENT) : libxsmm_lcm(alignment, LIBXSMM_ALIGNMENT), LIBXSMM_MAX_ALIGN);
+  if ((LIBXSMM_ALLOC_ALIGNFCT * LIBXSMM_ALLOC_ALIGNMAX) <= size) {
+    result = libxsmm_lcm(0 == alignment ? (LIBXSMM_ALIGNMENT) : libxsmm_lcm(alignment, LIBXSMM_ALIGNMENT), LIBXSMM_ALLOC_ALIGNMAX);
   }
   else {
-    if ((LIBXSMM_ALIGNMENT) <= size) {
+    if ((LIBXSMM_ALLOC_ALIGNFCT * LIBXSMM_ALIGNMENT) <= size) {
       result = (0 == alignment ? (LIBXSMM_ALIGNMENT) : libxsmm_lcm(alignment, LIBXSMM_ALIGNMENT));
     }
     else if (0 != alignment) {
@@ -96,20 +104,26 @@ LIBXSMM_EXTERN_C LIBXSMM_RETARGETABLE unsigned int libxsmm_alignment(unsigned in
 }
 
 
-LIBXSMM_EXTERN_C LIBXSMM_RETARGETABLE int libxsmm_alloc_info(const void* memory, unsigned int* size, void** extra)
+LIBXSMM_INLINE LIBXSMM_RETARGETABLE int internal_alloc_info(const void* memory, unsigned int* size, void** extra, int* flags)
 {
 #if !defined(NDEBUG)
   if (0 != size || 0 != extra)
 #endif
   {
     if (0 != memory) {
-      const internal_alloc_info *const info = (const internal_alloc_info*)(((const char*)memory) - sizeof(internal_alloc_info));
+      const internal_alloc_info_type *const info = (const internal_alloc_info_type*)(((const char*)memory) - sizeof(internal_alloc_info_type));
       if (size) *size = info->size;
       if (extra) *extra = info->pointer;
+#if defined(LIBXSMM_ALLOC_MMAP)
+      if (flags) *flags = 0;
+#else
+      if (flags) *flags = info->flags;
+#endif
     }
     else {
       if (size) *size = 0;
       if (extra) *extra = 0;
+      if (flags) *flags = 0;
     }
   }
 #if !defined(NDEBUG)
@@ -121,6 +135,12 @@ LIBXSMM_EXTERN_C LIBXSMM_RETARGETABLE int libxsmm_alloc_info(const void* memory,
 }
 
 
+LIBXSMM_EXTERN_C LIBXSMM_RETARGETABLE int libxsmm_alloc_info(const void* memory, unsigned int* size, void** extra)
+{
+  return internal_alloc_info(memory, size, extra, 0/*flags*/);
+}
+
+
 LIBXSMM_EXTERN_C LIBXSMM_RETARGETABLE int libxsmm_allocate(void** memory, unsigned int size, unsigned int alignment,
   int flags, const void* extra, unsigned int extra_size)
 {
@@ -128,7 +148,7 @@ LIBXSMM_EXTERN_C LIBXSMM_RETARGETABLE int libxsmm_allocate(void** memory, unsign
   if (memory) {
     if (0 < size) {
       const unsigned int auto_alignment = libxsmm_alignment(size, alignment);
-      const unsigned int alloc_size = size + extra_size + sizeof(internal_alloc_info) + auto_alignment - 1;
+      const unsigned int alloc_size = size + extra_size + sizeof(internal_alloc_info_type) + auto_alignment - 1;
       void* alloc_failed = 0;
       char* buffer = 0;
 #if !defined(LIBXSMM_ALLOC_MMAP)
@@ -162,6 +182,28 @@ LIBXSMM_EXTERN_C LIBXSMM_RETARGETABLE int libxsmm_allocate(void** memory, unsign
             MAP_ANONYMOUS | MAP_PRIVATE,
 # endif
             -1, 0);
+# if defined(MADV_NOHUGEPAGE)
+          /* disable THP for smaller allocations; req. Linux kernel 2.6.38 (or higher) */
+          if (LIBXSMM_ALLOC_ALIGNMAX > alloc_size) {
+#   if defined(NDEBUG)
+            /* proceed even in case of an error, we then just take what we got (THP) */
+            madvise(buffer, alloc_size, MADV_NOHUGEPAGE);
+#   else /* library code is expected to be mute */
+            if (0 != madvise(buffer, alloc_size, MADV_NOHUGEPAGE)) {
+              static LIBXSMM_TLS int madvise_error = 0;
+              if (0 == madvise_error) {
+                fprintf(stderr, "LIBXSMM: %s (madvise error #%i for range %p+%u)!\n",
+                  strerror(errno), errno, buffer, alloc_size);
+                madvise_error = 1;
+              }
+            }
+#   endif /*defined(NDEBUG)*/
+          }
+# elif !(defined(__APPLE__) && defined(__MACH__)) && !defined(__CYGWIN__)
+          LIBXSMM_MESSAGE("================================================================================")
+          LIBXSMM_MESSAGE("LIBXSMM: Adjusting THP is unavailable due to C89 or kernel older than 2.6.38!")
+          LIBXSMM_MESSAGE("================================================================================")
+# endif /*MADV_NOHUGEPAGE*/
         }
         else {
           buffer = alloc_failed;
@@ -169,8 +211,8 @@ LIBXSMM_EXTERN_C LIBXSMM_RETARGETABLE int libxsmm_allocate(void** memory, unsign
 #endif
       }
       if (alloc_failed != buffer) {
-        char *const aligned = LIBXSMM_ALIGN(buffer + extra_size + sizeof(internal_alloc_info), auto_alignment);
-        internal_alloc_info *const info = (internal_alloc_info*)(aligned - sizeof(internal_alloc_info));
+        char *const aligned = LIBXSMM_ALIGN(buffer + extra_size + sizeof(internal_alloc_info_type), auto_alignment);
+        internal_alloc_info_type *const info = (internal_alloc_info_type*)(aligned - sizeof(internal_alloc_info_type));
         assert((aligned + size) <= (buffer + alloc_size));
 #if !defined(NDEBUG)
         memset(buffer, 0, alloc_size);
@@ -190,6 +232,9 @@ LIBXSMM_EXTERN_C LIBXSMM_RETARGETABLE int libxsmm_allocate(void** memory, unsign
 #endif
         info->pointer = buffer;
         info->size = size;
+#if !defined(LIBXSMM_ALLOC_MMAP)
+        info->flags = 0 == flags ? LIBXSMM_ALLOC_FLAG_DEFAULT : flags;
+#endif
         *memory = aligned;
       }
       else {
@@ -214,20 +259,36 @@ LIBXSMM_EXTERN_C LIBXSMM_RETARGETABLE int libxsmm_deallocate(const void* memory)
 {
   int result = EXIT_SUCCESS;
   if (memory) {
-    void* buffer = 0;
-#if defined(LIBXSMM_ALLOC_MMAP)
-# if defined(_WIN32)
-    libxsmm_alloc_info(memory, 0, &buffer);
-    result = FALSE != VirtualFree(buffer, 0, MEM_RELEASE) ? EXIT_SUCCESS : EXIT_FAILURE;
-# else
     unsigned int size = 0;
-    libxsmm_alloc_info(memory, &size, &buffer);
-    result = 0 == munmap(buffer, size + (((const char*)memory) - ((char*)buffer))) ? EXIT_SUCCESS : EXIT_FAILURE;
-# endif
+    void* buffer = 0;
+#if !defined(LIBXSMM_ALLOC_MMAP)
+    int flags = 0;
+    internal_alloc_info(memory, &size, &buffer, &flags);
+    if (LIBXSMM_ALLOC_FLAG_DEFAULT == flags) {
+      free(buffer);
+    }
+    else
 #else
-    libxsmm_alloc_info(memory, 0, &buffer);
-    free(buffer);
+    internal_alloc_info(memory, &size, &buffer, 0/*flags*/);
 #endif
+    {
+#if defined(_WIN32)
+      result = FALSE != VirtualFree(buffer, 0, MEM_RELEASE) ? EXIT_SUCCESS : EXIT_FAILURE;
+#else
+      const unsigned int alloc_size = size + (((const char*)memory) - ((const char*)buffer));
+      if (0 != munmap(buffer, alloc_size)) {
+# if !defined(NDEBUG) /* library code is expected to be mute */
+        static LIBXSMM_TLS int munmap_error = 0;
+        if (0 == munmap_error) {
+          fprintf(stderr, "LIBXSMM: %s (munmap error #%i for range %p+%u)!\n",
+            strerror(errno), errno, buffer, alloc_size);
+          munmap_error = 1;
+        }
+# endif
+        result = EXIT_FAILURE;
+      }
+#endif
+    }
   }
   assert(EXIT_SUCCESS == result);
   return result;
