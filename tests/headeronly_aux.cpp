52a68fba6e3713d5cb680e86c9462137b31c2001
Hans Pabst <hans.pabst@intel.com>
2016-07-22 15:20:13 +0200

    Removed some debug code.

--- tests/headeronly_aux.cpp
+++ tests/headeronly_aux.cpp
@@ -3,7 +3,6 @@
 
 LIBXSMM_EXTERN libxsmm_dmmfunction dmmdispatch(int m, int n, int k)
 {
-  fprintf(stderr, "\nDEBUG: %p", internal_registry);
   return libxsmm_dmmdispatch(m, n, k,
     NULL/*lda*/, NULL/*ldb*/, NULL/*ldc*/,
     NULL/*alpha*/, NULL/*beta*/,
