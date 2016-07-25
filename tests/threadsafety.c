2deb0cee8eb4f8908b6642d6aa17cabb3374c8f6
Hans Pabst <hans.pabst@intel.com>
2016-07-25 22:31:49 +0200

    More extensive test.

--- tests/threadsafety.c
+++ tests/threadsafety.c
@@ -1,32 +1,51 @@
 #include <libxsmm.h>
 #include <stdlib.h>
 
+#if !defined(MAX_NKERNELS)
+# define MAX_NKERNELS 1000
+#endif
+
 
 int main()
 {
   /* we do not care about the initial values */
   /*const*/ float a[23*23], b[23*23];
+  libxsmm_smmfunction f[MAX_NKERNELS];
   int i;
 
 #if defined(_OPENMP)
 # pragma omp parallel for default(none) private(i)
 #endif
-  for (i = 0; i < 1000; ++i) {
+  for (i = 0; i < MAX_NKERNELS; ++i) {
     libxsmm_init();
   }
 
 #if defined(_OPENMP)
-# pragma omp parallel for default(none) private(i) shared(a, b)
+# pragma omp parallel for default(none) private(i) shared(a, b, f)
 #endif
-  for (i = 0; i < 1000; ++i) {
+  for (i = 0; i < MAX_NKERNELS; ++i) {
+    const libxsmm_blasint m = 23, n = 23, k = (i / 50) % 23 + 1;
+    /* playing ping-pong with fi's cache line is not the subject */
+    f[i] = libxsmm_smmdispatch(m, n, k,
+      NULL/*lda*/, NULL/*ldb*/, NULL/*ldc*/, NULL/*alpha*/, NULL/*beta*/,
+      NULL/*flags*/, NULL/*prefetch*/);
+  }
+
+  for (i = 0; i < MAX_NKERNELS; ++i) {
     float c[23/*m*/*23/*n*/];
     const libxsmm_blasint m = 23, n = 23, k = (i / 50) % 23 + 1;
-    const libxsmm_smmfunction f = libxsmm_smmdispatch(m, n, k,
+    const libxsmm_smmfunction fi = libxsmm_smmdispatch(m, n, k,
       NULL/*lda*/, NULL/*ldb*/, NULL/*ldc*/, NULL/*alpha*/, NULL/*beta*/,
       NULL/*flags*/, NULL/*prefetch*/);
 
-    if (NULL != f) {
-      LIBXSMM_MMCALL(f, a, b, c, m, n, k);
+    if (NULL != f[i]) {
+      if (fi == f[i]) {
+        LIBXSMM_MMCALL(f[i], a, b, c, m, n, k);
+      }
+      else {
+        assert(EXIT_SUCCESS != (i + 1));
+        return i + 1;
+      }
     }
     else {
       libxsmm_sgemm(NULL/*transa*/, NULL/*transb*/, &m, &n, &k,
@@ -38,7 +57,7 @@ int main()
 #if defined(_OPENMP)
 # pragma omp parallel for default(none) private(i)
 #endif
-  for (i = 0; i < 1000; ++i) {
+  for (i = 0; i < MAX_NKERNELS; ++i) {
     libxsmm_finalize();
   }
 
