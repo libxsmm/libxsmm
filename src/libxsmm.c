d12f4a350eb583944019d1aa0a72bc3aea27f49b
Hans Pabst <hans.pabst@intel.com>
2016-07-29 21:11:06 +0200

    More extensive preconditions in internal_build.

--- src/libxsmm.c
+++ src/libxsmm.c
@@ -994,9 +994,10 @@ LIBXSMM_INLINE LIBXSMM_RETARGETABLE void internal_build(const libxsmm_gemm_descr
 #if !defined(__MIC__) && (!defined(__CYGWIN__) || !defined(NDEBUG)/*code-coverage with Cygwin; fails@runtime!*/)
   const char *const target_arch = internal_get_target_arch(internal_target_archid);
   libxsmm_generated_code generated_code;
-  assert(0 != descriptor && 0 != code);
+  assert(0 != descriptor && 0 < descriptor->m && 0 < descriptor->n && 0 < descriptor->k);
+  assert(0 < descriptor->lda && 0 < descriptor->lda && 0 < descriptor->ldc);
   assert(0 != internal_target_archid);
-  assert(0 == code->pmm);
+  assert(0 != code && 0 == code->pmm);
 
   /* allocate temporary buffer which is large enough to cover the generated code */
   generated_code.generated_code = malloc(131072);
