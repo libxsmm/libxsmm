929195d1ea6d146f5ce74b04e3c7b5d5edeec650
Hans Pabst <hans.pabst@intel.com>
2016-08-22 10:16:15 +0200

    Assert against infinite recursion.

--- src/libxsmm_ext_gemm.c
+++ src/libxsmm_ext_gemm.c
@@ -246,6 +246,7 @@ LIBXSMM_EXTERN_C LIBXSMM_RETARGETABLE LIBXSMM_GEMM_WEAK void LIBXSMM_FSYMBOL(sge
   const float* b, const libxsmm_blasint* ldb,
   const float* beta, float* c, const libxsmm_blasint* ldc)
 {
+  assert(LIBXSMM_FSYMBOL(sgemm) != libxsmm_original_sgemm);
   libxsmm_sgemm_omp(transa, transb, m, n, k, alpha, a, lda, b, ldb, beta, c, ldc);
 }
 
@@ -257,6 +258,7 @@ LIBXSMM_EXTERN_C LIBXSMM_RETARGETABLE LIBXSMM_GEMM_WEAK void LIBXSMM_FSYMBOL(dge
   const double* b, const libxsmm_blasint* ldb,
   const double* beta, double* c, const libxsmm_blasint* ldc)
 {
+  assert(LIBXSMM_FSYMBOL(dgemm) != libxsmm_original_dgemm);
   libxsmm_dgemm_omp(transa, transb, m, n, k, alpha, a, lda, b, ldb, beta, c, ldc);
 }
 
