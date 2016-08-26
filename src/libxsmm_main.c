35a12ab9bc5a74fe986435ab86b24f3002b7cf79
Hans Pabst <hans.pabst@intel.com>
2016-08-26 20:22:15 +0200

    Fixed handling LIBXSMM_PREFETCH environment variable. [skip ci]

--- src/libxsmm_main.c
+++ src/libxsmm_main.c
@@ -599,7 +599,7 @@ LIBXSMM_INLINE LIBXSMM_RETARGETABLE libxsmm_code_pointer* internal_init(void)
       { /* select prefetch strategy for JIT */
         const char *const env = getenv("LIBXSMM_PREFETCH");
         internal_prefetch = (LIBXSMM_X86_AVX512_MIC != internal_target_archid ? INTERNAL_PREFETCH : LIBXSMM_PREFETCH_AL2BL2_VIA_C);
-        if (0 != env && 0 != *env) { /* user input is alwayd considered */
+        if (0 != env && 0 <= *env) { /* user input beyond auto-prefetch is always considered */
           switch (atoi(env)) {
             case 2:  internal_prefetch = LIBXSMM_PREFETCH_SIGONLY; break;
             case 3:  internal_prefetch = LIBXSMM_PREFETCH_BL2_VIA_C; break;
