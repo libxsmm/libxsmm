a1c6501c31f814a4f518c6d28318d15420047f36
Hans Pabst <hans.pabst@intel.com>
2016-09-05 15:47:46 +0200

    Print information about requested workload.

--- samples/xgemm/xgemm.c
+++ samples/xgemm/xgemm.c
@@ -66,6 +66,15 @@ int main(int argc, char* argv[])
     LIBXSMM_YGEMM_SYMBOL(REAL_TYPE)(&transa, &transb, &m, &n, &k, &alpha, a, &lda, b, &ldb, &beta, c, &ldc);
     LIBXSMM_XBLAS_SYMBOL(REAL_TYPE)(&transa, &transb, &m, &n, &k, &alpha, a, &lda, b, &ldb, &beta, d, &ldc);
 
+    fprintf(stdout, "%s('%c', '%c', %i/*m*/, %i/*n*/, %i/*k*/,\n"
+                    "      %g/*alpha*/, %p/*a*/, %i/*lda*/,\n"
+                    "                  %p/*b*/, %i/*ldb*/,\n"
+                    "       %g/*beta*/, %p/*c*/, %i/*ldc*/)\n",
+      LIBXSMM_STRINGIFY(LIBXSMM_TPREFIX(REAL_TYPE, gemm)),
+      transa, transb, m, n, k, alpha, (const void*)a, lda,
+                                      (const void*)b, ldb,
+                                beta, (const void*)c, ldc);
+
     { /* Tiled xGEMM */
       int i; double duration;
       unsigned long long start = libxsmm_timer_tick();
