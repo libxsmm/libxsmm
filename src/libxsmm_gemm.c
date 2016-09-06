75a4fa79d83ea2f3edc5c99f8ee86cd71cbc7899
Hans Pabst <hans.pabst@intel.com>
2016-09-06 09:08:27 +0200

    Fixed setting up libxsmm_gemm_prefetch. [skip ci]

--- src/libxsmm_gemm.c
+++ src/libxsmm_gemm.c
@@ -63,7 +63,7 @@ LIBXSMM_API_DEFINITION void libxsmm_gemm_init(int archid, int prefetch)
 {
   int config = 0;
   const char *const prefetch_env = getenv("LIBXSMM_GEMM_PREFETCH");
-  const int uid = (0 == prefetch_env || 0 == *prefetch_env) ? LIBXSMM_PREFETCH_AL2_AHEAD : atoi(prefetch_env);
+  const int uid = (0 == prefetch_env || 0 == *prefetch_env) ? 6/*LIBXSMM_PREFETCH_AL2_AHEAD*/ : atoi(prefetch_env);
   libxsmm_gemm_prefetch = 0 <= uid ? libxsmm_uid2prefetch2(uid) : prefetch;
 #if defined(__MIC__) || (LIBXSMM_X86_AVX512_MIC == LIBXSMM_STATIC_TARGET_ARCH)
   LIBXSMM_UNUSED(archid);
