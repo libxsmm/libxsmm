aba6efea57ad0c923d61d8c194603d072e9ab161
Hans Pabst <hans.pabst@intel.com>
2016-09-19 18:06:55 +0200

    Fixed assertion.

--- src/libxsmm_ext_gemm.c
+++ src/libxsmm_ext_gemm.c
@@ -108,11 +108,11 @@ LIBXSMM_API_DEFINITION void libxsmm_sgemm_omp(const char* transa, const char* tr
 {
   LIBXSMM_GEMM_DESCRIPTOR_DIM_TYPE tm, tn, tk;
   LIBXSMM_GEMM_DECLARE_FLAGS(flags, transa, transb, m, n, k, a, b, c);
-  assert(0 < tm && 0 < tn && 0 < tk && 0 < libxsmm_nt);
   LIBXSMM_INIT
   tm = libxsmm_gemm_tile[1/*SP*/][0/*M*/];
   tn = libxsmm_gemm_tile[1/*SP*/][1/*N*/];
   tk = libxsmm_gemm_tile[1/*SP*/][2/*K*/];
+  assert(0 < tm && 0 < tn && 0 < tk && 0 < libxsmm_nt);
 #if defined(_OPENMP)
   if (0 != libxsmm_mt) { /* enable OpenMP support */
     if (0 == LIBXSMM_MOD2(libxsmm_mt, 2)) { /* even: enable internal parallelization */
@@ -201,6 +201,7 @@ LIBXSMM_API_DEFINITION void libxsmm_dgemm_omp(const char* transa, const char* tr
   tm = libxsmm_gemm_tile[0/*DP*/][0/*M*/];
   tn = libxsmm_gemm_tile[0/*DP*/][1/*N*/];
   tk = libxsmm_gemm_tile[0/*DP*/][2/*K*/];
+  assert(0 < tm && 0 < tn && 0 < tk && 0 < libxsmm_nt);
 #if defined(_OPENMP)
   if (0 != libxsmm_mt) { /* enable OpenMP support */
     if (0 == LIBXSMM_MOD2(libxsmm_mt, 2)) { /* even: enable internal parallelization */
