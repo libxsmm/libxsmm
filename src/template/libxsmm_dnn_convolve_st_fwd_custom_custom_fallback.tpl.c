5b95ac177037da1042919363deb87c2a71f1b73a
Hans Pabst <hans.pabst@intel.com>
2016-09-20 16:57:02 +0200

    Fixed a typo artifact introduced during VLA rewrite.

--- src/template/libxsmm_dnn_convolve_st_fwd_custom_custom_fallback.tpl.c
+++ src/template/libxsmm_dnn_convolve_st_fwd_custom_custom_fallback.tpl.c
@@ -57,7 +57,7 @@ for (imgofm1 = thr_begin; imgofm1 < thr_end; ++imgofm1) {
           for (ki = 0; ki< handle->desc.S; ++ki) {
             for (ifm2 = 0; ifm2 < handle->ifmblock; ++ifm2) {
               for (ofm2 = 0; ofm2 < handle->ofmblock; ++ofm2) {
-                LIBXSMM_VLA_ACCESS(  5, output, img, ofm1, oj, oi, ofm2, handle->blocksofm, handle->ofhp, handle->ofwp, handle->ofmblock) *= (element_output_type)(
+                LIBXSMM_VLA_ACCESS(  5, output, img, ofm1, oj, oi, ofm2, handle->blocksofm, handle->ofhp, handle->ofwp, handle->ofmblock) += (element_output_type)(
                   LIBXSMM_VLA_ACCESS(5,  input, img, ifm1, ij + kj, ii + ki, ifm2, handle->blocksifm, handle->ifhp, handle->ifwp, handle->ifmblock)
                 * LIBXSMM_VLA_ACCESS(6, weight, ofm1, ifm1, kj, ki, ifm2, ofm2, handle->blocksifm, handle->desc.R, handle->desc.S, handle->ifmblock, handle->ofmblock));
               }
