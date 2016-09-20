675f02a8e4fa51cf11cf1e066c1b7c6d370c2224
Hans Pabst <hans.pabst@intel.com>
2016-09-20 10:10:20 +0200

    Introduced LIBXSMM_NO_OFFLOAD macro which (accidentally) mutes warnings when calling a non-decorated function in an offload-decorated function (missing target attribute).

--- src/libxsmm_main.c
+++ src/libxsmm_main.c
@@ -1025,7 +1025,7 @@ LIBXSMM_API_DEFINITION void libxsmm_build(const libxsmm_build_request* request, 
       {
         generated_code.generated_code = malloc(131072); /* large enough temporary buffer for generated code */
         generated_code.buffer_size = 0 != generated_code.generated_code ? 131072 : 0;
-        libxsmm_generator_gemm_kernel(&generated_code, request->descriptor.gemm, target_arch);
+        LIBXSMM_NO_OFFLOAD(libxsmm_generator_gemm_kernel, &generated_code, request->descriptor.gemm, target_arch);
 # if !defined(LIBXSMM_VTUNE)
         if (0 > internal_verbose_mode)
 # endif
@@ -1050,7 +1050,7 @@ LIBXSMM_API_DEFINITION void libxsmm_build(const libxsmm_build_request* request, 
       if (0 == (LIBXSMM_GEMM_FLAG_F32PREC & (request->descriptor.ssoa->gemm->flags))/*only double-precision*/) {
         generated_code.generated_code = malloc(131072); /* large enough temporary buffer for generated code */
         generated_code.buffer_size = 0 != generated_code.generated_code ? 131072 : 0;
-        libxsmm_generator_spgemm_csr_soa_kernel(&generated_code, request->descriptor.ssoa->gemm, target_arch,
+        LIBXSMM_NO_OFFLOAD(libxsmm_generator_spgemm_csr_soa_kernel, &generated_code, request->descriptor.ssoa->gemm, target_arch,
           request->descriptor.ssoa->row_ptr, request->descriptor.ssoa->column_idx,
           (const double*)request->descriptor.ssoa->values);
 # if !defined(LIBXSMM_VTUNE)
@@ -1078,7 +1078,7 @@ LIBXSMM_API_DEFINITION void libxsmm_build(const libxsmm_build_request* request, 
       {
         generated_code.generated_code = malloc(131072); /* large enough temporary buffer for generated code */
         generated_code.buffer_size = 0 != generated_code.generated_code ? 131072 : 0;
-        libxsmm_generator_convolution_forward_kernel(&generated_code, request->descriptor.cfwd, target_arch);
+        LIBXSMM_NO_OFFLOAD(libxsmm_generator_convolution_forward_kernel, &generated_code, request->descriptor.cfwd, target_arch);
 # if !defined(LIBXSMM_VTUNE)
         if (0 > internal_verbose_mode)
 # endif
@@ -1107,7 +1107,7 @@ LIBXSMM_API_DEFINITION void libxsmm_build(const libxsmm_build_request* request, 
       {
         generated_code.generated_code = malloc(131072); /* large enough temporary buffer for generated code */
         generated_code.buffer_size = 0 != generated_code.generated_code ? 131072 : 0;
-        libxsmm_generator_convolution_backward_kernel(&generated_code, request->descriptor.cbwd, target_arch);
+        LIBXSMM_NO_OFFLOAD(libxsmm_generator_convolution_backward_kernel, &generated_code, request->descriptor.cbwd, target_arch);
 # if !defined(LIBXSMM_VTUNE)
         if (0 > internal_verbose_mode)
 # endif
@@ -1137,7 +1137,7 @@ LIBXSMM_API_DEFINITION void libxsmm_build(const libxsmm_build_request* request, 
       {
         generated_code.generated_code = malloc(131072); /* large enough temporary buffer for generated code */
         generated_code.buffer_size = 0 != generated_code.generated_code ? 131072 : 0;
-        libxsmm_generator_convolution_weight_update_kernel(&generated_code, request->descriptor.cupd, target_arch);
+        LIBXSMM_NO_OFFLOAD(libxsmm_generator_convolution_weight_update_kernel, &generated_code, request->descriptor.cupd, target_arch);
 # if !defined(LIBXSMM_VTUNE)
         if (0 > internal_verbose_mode)
 # endif
@@ -1197,11 +1197,6 @@ LIBXSMM_API_DEFINITION void libxsmm_build(const libxsmm_build_request* request, 
 # endif
   free(generated_code.generated_code); /* free temporary/initial code buffer */
 #else /* unsupported platform */
-# if !defined(__MIC__)
-  LIBXSMM_MESSAGE("================================================================================")
-  LIBXSMM_MESSAGE("LIBXSMM: The JIT BACKEND is currently not supported under Microsoft Windows!")
-  LIBXSMM_MESSAGE("================================================================================")
-# endif
   LIBXSMM_UNUSED(request); LIBXSMM_UNUSED(regindex); LIBXSMM_UNUSED(code);
   /* libxsmm_get_target_arch also serves as a runtime check whether JIT is available or not */
   assert(LIBXSMM_X86_AVX > internal_target_archid);
