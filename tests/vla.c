f9b79d28e05ba71b3c70b37eb69c9aa53d1fe308
Hans Pabst <hans.pabst@intel.com>
2016-09-21 09:35:14 +0200

    Fixed compiler warning about loss of precision in implicit type-conversion.

--- tests/vla.c
+++ tests/vla.c
@@ -12,7 +12,7 @@ int main(/*int argc, char* argv[]*/)
   LIBXSMM_VLA_DECL(3, const ELEMENT_TYPE, in3, in1, nj, nk);
 
   assert(0 != in1);
-  for (i = 0; i < (ni * nj * nk); ++i) in1[i] = i;
+  for (i = 0; i < (ni * nj * nk); ++i) in1[i] = (ELEMENT_TYPE)i;
   for (i = 0; i < ni; ++i) {
     for (j = 0; j < nj; ++j) {
       for (k = 0; k < nk; ++k) {
