5fc0ce4377b309aee8d85b289faa7a2a14b8ecb5
Alexander Heinecke <alexander.heinecke@intel.com>
2016-09-23 09:40:16 -0700

    renamed functions to reflect avx512 code generation

--- src/generator_gemm_avx512_microkernel_nofsdbcst.h
+++ src/generator_gemm_avx512_microkernel_nofsdbcst.h
@@ -29,20 +29,20 @@
 /* Alexander Heinecke (Intel Corp.)
 ******************************************************************************/
 
-#ifndef GENERATOR_GEMM_AVX512_MICROKERNEL_NOBCST_H
-#define GENERATOR_GEMM_AVX512_MICROKERNEL_NOBCST_H
+#ifndef GENERATOR_GEMM_AVX512_MICROKERNEL_NOFSDBCST_H
+#define GENERATOR_GEMM_AVX512_MICROKERNEL_NOFSDBCST_H
 
 #include "generator_common.h"
 #include "generator_gemm_common.h"
 
 LIBXSMM_INTERNAL_API
-void libxsmm_generator_gemm_avx512_microkernel_nobcst( libxsmm_generated_code*            io_generated_code,
-                                                       const libxsmm_gp_reg_mapping*      i_gp_reg_mapping,
-                                                       const libxsmm_micro_kernel_config* i_micro_kernel_config,
-                                                       const libxsmm_gemm_descriptor*     i_xgemm_desc,
-                                                       const unsigned int                 i_m_blocking,
-                                                       const unsigned int                 i_n_blocking,
-                                                       const int                          i_offset );
+void libxsmm_generator_gemm_avx512_microkernel_nofsdbcst( libxsmm_generated_code*            io_generated_code,
+                                                          const libxsmm_gp_reg_mapping*      i_gp_reg_mapping,
+                                                          const libxsmm_micro_kernel_config* i_micro_kernel_config,
+                                                          const libxsmm_gemm_descriptor*     i_xgemm_desc,
+                                                          const unsigned int                 i_m_blocking,
+                                                          const unsigned int                 i_n_blocking,
+                                                          const int                          i_offset );
 
-#endif /* GENERATOR_GEMM_AVX512_MICROKERNEL_NOBCST_H */
+#endif /* GENERATOR_GEMM_AVX512_MICROKERNEL_NOFSDBCST_H */
 
