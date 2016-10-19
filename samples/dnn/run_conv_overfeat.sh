1f0c939a3b212d679e03444eda6953214af9a3ac
Alexander Heinecke <alexander.heinecke@intel.com>
2016-10-18 21:47:21 -0700

    cleanup help text of run scripts

--- samples/dnn/run_conv_overfeat.sh
+++ samples/dnn/run_conv_overfeat.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-if [ $# -ne 3 ]
+if [ $# -ne 4 ]
 then
-  echo "Usage: `basename $0` mb iters numa; using default values: 256 100 1"
+  echo "Usage: `basename $0` mb iters numa; using default values: 256 100 1 f32"
   MB=256
-  ITERS=100
+  ITERS=1000
   NUMA=1
+  BIN=f32
 else
   MB=$1
   ITERS=$2
   NUMA=$3
+  BIN=$4
 fi
 
 CPUFLAGS=$(if [ -e /proc/cpuinfo ]; then grep -m1 flags /proc/cpuinfo | cut -d: -f2-; fi)
@@ -19,8 +21,8 @@ if [ "" != "$(echo "${CPUFLAGS}" | grep -o avx512er)" ]; then
   fi
 fi
 
-${NUMACTL} ./layer_example_f32 ${ITERS} 231 231  ${MB}    3   96 11 11 0 4 1
-${NUMACTL} ./layer_example_f32 ${ITERS}  28  28  ${MB}   96  256  5  5 0 1 1
-${NUMACTL} ./layer_example_f32 ${ITERS}  14  14  ${MB}  256  512  3  3 1 1 1
-${NUMACTL} ./layer_example_f32 ${ITERS}  14  14  ${MB}  512 1024  3  3 1 1 1
-${NUMACTL} ./layer_example_f32 ${ITERS}  14  14  ${MB} 1024 1024  3  3 0 1 1
+${NUMACTL} ./layer_example_${BIN} ${ITERS} 231 231  ${MB}    3   96 11 11 0 4 1
+${NUMACTL} ./layer_example_${BIN} ${ITERS}  28  28  ${MB}   96  256  5  5 0 1 1
+${NUMACTL} ./layer_example_${BIN} ${ITERS}  14  14  ${MB}  256  512  3  3 1 1 1
+${NUMACTL} ./layer_example_${BIN} ${ITERS}  14  14  ${MB}  512 1024  3  3 1 1 1
+${NUMACTL} ./layer_example_${BIN} ${ITERS}  14  14  ${MB} 1024 1024  3  3 0 1 1
