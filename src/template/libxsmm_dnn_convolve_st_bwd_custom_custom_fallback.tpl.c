d0e9ef1f24580818f99748f7ce17f90cabee7ca8
Hans Pabst <hans.pabst@intel.com>
2016-11-03 14:45:41 +0100

    Adjusted code format.

--- src/template/libxsmm_dnn_convolve_st_bwd_custom_custom_fallback.tpl.c
+++ src/template/libxsmm_dnn_convolve_st_bwd_custom_custom_fallback.tpl.c
@@ -57,9 +57,9 @@ for (imgifm1 = thr_begin; imgifm1 < thr_end; ++imgifm1) {
           for (ki = 0; ki < handle->desc.S; ++ki) {
             for (ofm2 = 0; ofm2 < handle->ofmblock; ++ofm2) {
               for (ifm2 = 0; ifm2 < handle->ifmblock; ++ifm2) {
-                LIBXSMM_VLA_ACCESS(5,  input, img, ifm1, ij+kj, ii+ki, ifm2, handle->blocksifm, handle->ifhp, handle->ifwp, handle->ifmblock) +=  (element_input_type)(
-                                                                                                                                                                       LIBXSMM_VLA_ACCESS(5,  output, img, ofm1, oj, oi, ofm2, handle->blocksofm, handle->ofhp, handle->ofwp, handle->ofmblock)
-                                                                                                                                                                       * LIBXSMM_VLA_ACCESS(6, weight, ofm1, ifm1, kj, ki, ifm2, ofm2, handle->blocksifm, handle->desc.R, handle->desc.S, handle->ifmblock, handle->ofmblock));
+                LIBXSMM_VLA_ACCESS(5, input, img, ifm1, ij+kj, ii+ki, ifm2, handle->blocksifm, handle->ifhp, handle->ifwp, handle->ifmblock) += (element_input_type)(
+                  LIBXSMM_VLA_ACCESS(5, output, img, ofm1, oj, oi, ofm2, handle->blocksofm, handle->ofhp, handle->ofwp, handle->ofmblock)
+                * LIBXSMM_VLA_ACCESS(6, weight, ofm1, ifm1, kj, ki, ifm2, ofm2, handle->blocksifm, handle->desc.R, handle->desc.S, handle->ifmblock, handle->ofmblock));
               }
             }
           }
