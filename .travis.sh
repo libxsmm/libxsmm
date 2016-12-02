f357313d06fb7df714d729b0c243a8ddd5f24edb
Hans Pabst <hans.pabst@intel.com>
2016-12-02 16:20:05 +0100

    Command "source" might be not available with /bin/sh, hence request the Bash interpreter [skip ci].

--- .travis.sh
+++ .travis.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #############################################################################
 # Copyright (c) 2016, Intel Corporation                                     #
 # All rights reserved.                                                      #
