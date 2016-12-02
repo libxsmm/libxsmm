96441007ab628bf5a3db894adf81ffe3957632aa
Hans Pabst <hans.pabst@intel.com>
2016-12-02 13:53:59 +0100

    Improved interface documentation [skip ci].

--- src/libxsmm_main.h
+++ src/libxsmm_main.h
@@ -239,7 +239,7 @@ LIBXSMM_API int libxsmm_xmalloc(void** memory, size_t size, int alignment, int f
   const void* extra, size_t extra_size);
 LIBXSMM_API int libxsmm_xfree(const volatile void* memory);
 
-/** Attribute memory allocation and protect with only the necessary flags. */
+/** Attribute memory allocation and protect with only the necessary flags; may relocate the given memory. */
 LIBXSMM_API int libxsmm_malloc_attrib(void** memory, int flags,
   /** If a name is given, an executable buffer will be dumped into a file. */
   const char* name);
