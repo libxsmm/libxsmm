c4e6d99d21516ccd10d2b1d9a46f9b245350eb8a
Hans Pabst <hans.pabst@intel.com>
2017-01-02 20:21:16 +0100

    Adjusted workaround (Cygwin only).

--- src/libxsmm_sync.c
+++ src/libxsmm_sync.c
@@ -71,7 +71,7 @@
 #endif
 #if defined(__MIC__)
 # define LIBXSMM_SYNC_PAUSE(DELAY) _mm_delay_32(DELAY)
-#elif !defined(LIBXSMM_INTRINSICS_NONE)
+#elif !defined(LIBXSMM_INTRINSICS_NONE) && !defined(__CYGWIN__)
 # define LIBXSMM_SYNC_PAUSE(DELAY) _mm_pause()
 #else
 # define LIBXSMM_SYNC_PAUSE(DELAY)
@@ -216,12 +216,7 @@ LIBXSMM_API_DEFINITION void libxsmm_barrier_init(libxsmm_barrier* barrier, int t
 }
 
 
-LIBXSMM_API_DEFINITION
-#if defined(__GNUC__) && !defined(__clang__) && !defined(__INTEL_COMPILER)
-LIBXSMM_INTRINSICS(LIBXSMM_X86_AVX2) /* TODO: investigate issue */
-#else
-LIBXSMM_INTRINSICS(LIBXSMM_X86_GENERIC)
-#endif
+LIBXSMM_API_DEFINITION LIBXSMM_INTRINSICS(LIBXSMM_X86_GENERIC)
 void libxsmm_barrier_wait(libxsmm_barrier* barrier, int tid)
 {
 #if defined(_REENTRANT)
