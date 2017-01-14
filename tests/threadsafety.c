6e855948836c93ab7ae84a5c850991e5c53e1705
Hans Pabst <hans.pabst@intel.com>
2017-01-14 19:06:30 +0100

    Reduced test size to speedup execution.

--- tests/threadsafety.c
+++ tests/threadsafety.c
@@ -34,9 +34,9 @@
 
 #if !defined(MAX_NKERNELS)
 # if defined(LIBXSMM_REGSIZE)
-#   define MAX_NKERNELS ((LIBXSMM_REGSIZE) + 10)
+#   define MAX_NKERNELS ((LIBXSMM_REGSIZE) * 2)
 # else
-#   define MAX_NKERNELS 1000
+#   define MAX_NKERNELS 100
 # endif
 #endif
 #if !defined(USE_PARALLEL_JIT)
