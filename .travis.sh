485a79cbba761b1b13bd61613b974be25c45c6ee
Hans Pabst <hans.pabst@intel.com>
2017-01-15 22:42:31 +0100

    Adjusted status message of Travis CI wrapper script.

--- .travis.sh
+++ .travis.sh
@@ -120,8 +120,6 @@ if [ "" != "${MKTEMP}" ] && [ "" != "${CHMOD}" ] && [ "" != "${SED}" ] && [ "" !
     if [ "" = "$1" ] && [ "0" = "${RESULT}" ]; then
       TESTID=$((TESTID+1))
     else # finish
-      echo "--------------------------------------------------------------------------------"
-      echo "FAILURE"
       break
     fi
   done
@@ -129,6 +127,11 @@ if [ "" != "${MKTEMP}" ] && [ "" != "${CHMOD}" ] && [ "" != "${SED}" ] && [ "" !
   # remove temporary script (if it exists)
   if [ "" != "${TESTSCRIPT}" ] && [ -e ${TESTSCRIPT} ]; then
     ${RM} ${TESTSCRIPT}
+  fi
+
+  if [ "0" != "${RESULT}" ]; then
+    echo "--------------------------------------------------------------------------------"
+    echo "FAILURE"
   fi
 
   exit ${RESULT}
