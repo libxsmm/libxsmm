8f16daff03795aeef21a81b79a87417324acfbba
Hans Pabst <hans.pabst@intel.com>
2017-01-25 10:51:32 +0100

    Use libxsmm_aligned_scratch instead of libxsmm_aligned_malloc.

--- src/libxsmm_spmdm.c
+++ src/libxsmm_spmdm.c
@@ -118,7 +118,7 @@ LIBXSMM_INLINE LIBXSMM_RETARGETABLE void internal_spmdm_allocate_csr_a(libxsmm_s
   size_t sz_block = ((handle->bm + 1)*sizeof(uint16_t) + (handle->bm)*(handle->bk)*sizeof(uint16_t) + (handle->bm)*(handle->bk)*sizeof(float) + sizeof(libxsmm_CSR_sparseslice));
   size_t sz_all_blocks = sz_block * handle->mb * handle->kb;
 
-  char * memory_block = (char *)libxsmm_aligned_malloc(sz_all_blocks, 2097152);
+  char * memory_block = (char *)libxsmm_aligned_scratch(sz_all_blocks, 2097152);
   char * memory_head  = memory_block;
 
   libxsmm_CSR_sparseslice* libxsmm_output_csr_a = (libxsmm_CSR_sparseslice*)(memory_head);
@@ -147,7 +147,7 @@ LIBXSMM_INLINE LIBXSMM_RETARGETABLE void internal_spmdm_allocate_scratch(libxsmm
   sz_memory_for_scratch_per_thread = (sz_memory_for_scratch_per_thread + 4095)/4096 * 4096;
   sz_total_memory = sz_memory_for_scratch_per_thread * max_threads;
 
-  handle->base_ptr_scratch_B_scratch_C = (char *)libxsmm_aligned_malloc(sz_total_memory, 2097152);
+  handle->base_ptr_scratch_B_scratch_C = (char *)libxsmm_aligned_scratch(sz_total_memory, 2097152);
   handle->memory_for_scratch_per_thread = (int)sz_memory_for_scratch_per_thread;
 }
 
