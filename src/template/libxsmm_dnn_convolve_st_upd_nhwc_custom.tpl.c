cbc1b0fe7ddf65f9f70bcfda3f90a3abfcf21ab6
Evangelos Georganas <egeorgan@egeorgan-mac01.sc.intel.com>
2017-02-22 13:38:19 -0800

    Revert "dbg messg"
    This reverts commit 358df72bc9cd2440045dc9673cbe8ec3033a0bed.

--- src/template/libxsmm_dnn_convolve_st_upd_nhwc_custom.tpl.c
+++ src/template/libxsmm_dnn_convolve_st_upd_nhwc_custom.tpl.c
@@ -241,8 +241,11 @@ if ( libxsmm_target_archid == LIBXSMM_X86_AVX512_MIC ||
 {
   num_ofw_strips = handle->ofw/handle->upd_ofw_rb;
   num_ofh_strips = handle->ofh/handle->upd_ofh_rb;
+<<<<<<< HEAD
+=======
+
+>>>>>>> parent of 358df72... dbg messg
 if ((handle->blocksifm * handle->blocksofm) < (2*handle->desc.threads)) { /* special case for not enough parallelism */
-  printf("NEP: ifmblocks: %d, ofmblocks: %d\n", handle->blocksifm, handle->blocksofm);
 #ifdef LIBXSMM_WU_PER_THREAD_ALLOCATION
       /* lazy barrier init */
       if (handle->upd_use_external_reduce == 0) {
@@ -384,7 +387,6 @@ if ((handle->blocksifm * handle->blocksofm) < (2*handle->desc.threads)) { /* spe
       }
 #endif
     } else { /* enough parallelism available */
-      printf("EnoughP: ifmblocks: %d, ofmblocks: %d\n", handle->blocksifm, handle->blocksofm);
          for (ofm1ifm1 = thr_begin; ofm1ifm1 < thr_end; ++ofm1ifm1) {
            ofm1 = ofm1ifm1 / handle->blocksifm;
            ifm1 = ofm1ifm1 % handle->blocksifm;
