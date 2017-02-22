221f93749b0910b9b0b21270f42f9579c23984e8
Evangelos Georganas <egeorgan@egeorgan-mac01.sc.intel.com>
2017-02-22 11:07:03 -0800

    Minor fix

--- src/libxsmm_main.h
+++ src/libxsmm_main.h
@@ -78,7 +78,6 @@ typedef union LIBXSMM_RETARGETABLE libxsmm_code_pointer {
 #if defined(LIBXSMM_BUILD) || defined(LIBXSMM_DNN_INTERNAL_API)
   libxsmm_xconvfunction xconv;
 #endif
-  libxsmm_xmmfunction xmm;
   libxsmm_xmatcopyfunction xmatcopy;
 } libxsmm_code_pointer;
 
