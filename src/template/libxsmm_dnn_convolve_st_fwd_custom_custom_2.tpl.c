1d6d718a92f7d340c1e219f441fc725eaabfca5f
Evangelos Georganas <egeorgan@egeorgan-mac01.local>
2017-03-13 10:50:21 -0700

    Perfformnace debugging

--- src/template/libxsmm_dnn_convolve_st_fwd_custom_custom_2.tpl.c
+++ src/template/libxsmm_dnn_convolve_st_fwd_custom_custom_2.tpl.c
@@ -28,9 +28,6 @@
  ******************************************************************************/
 /* Alexander Heinecke, Evangelos Georganas, Hans Pabst (Intel Corp.)
  ******************************************************************************/
-#if defined(_OPENMP)
-# include <omp.h>
-#endif
 
 /* loop counters */
 int img1, ofm1, ifm1, oj, oi, ij, ii, kj, ki, i;
@@ -52,13 +49,10 @@ LIBXSMM_VLA_DECL(6, const element_input_type,  input_t, ((element_input_type*)ha
 LIBXSMM_VLA_DECL(6, const element_filter_type, filter_t, (element_filter_type*)handle->reg_filter->data, handle->blocksifm, handle->desc.R, handle->desc.S, handle->ifmblock, handle->ofmblock);
 libxsmm_mmfunction sixteen = (libxsmm_mmfunction) handle->code_fwd[0].smm;
 
-# pragma omp parallel for private(i, img1, ofm1, ifm1, oj, oi, ij, ii, kj, ki) num_threads(64)
+# pragma omp parallel for private(i, img1, ofm1, ifm1, oj, oi, ij, ii, kj, ki)
 for (i = 0; i < blocksofm * nBImg; i++) {
   img1 = i/blocksofm;
   ofm1 = i%blocksofm;
-  debug_tid = omp_get_thread_num();
-  if (i == debug_tid) printf("Thread %d came here....\n", debug_tid);
-
   for (oj = 0; oj < ofh; ++oj) {
     for (oi = 0; oi < ofw; ++oi) {
       ij = oj * u - pad_h;
