libxsmm/libxsmm

commit faed3937506670d040dcee67089f2d6475b27fcb

LIBXSMM intrinsically does not support 32-bit code (due to calling conventions and other issues), however fixed some (artificial) warnings related to 32/64-bit compatibility.

src/libxsmm_hash.c
@@ -356,7 +356,15 @@ LIBXSMM_RETARGETABLE LIBXSMM_VISIBILITY_INTERNAL const uint32_t internal_crc32_t
 #define LIBXSMM_HASH_CRC32_U8(SEED, N, VALUE) _mm_crc32_u8(SEED, VALUE)
 #define LIBXSMM_HASH_CRC32_U16(SEED, N, VALUE) _mm_crc32_u16(SEED, VALUE)
 #define LIBXSMM_HASH_CRC32_U32(SEED, N, VALUE) _mm_crc32_u32(SEED, VALUE)
-#define LIBXSMM_HASH_CRC32_U64(SEED, N, VALUE) _mm_crc32_u64(SEED, VALUE)
+
+#if !defined(_WIN32) || defined(_WIN64)
+# define LIBXSMM_HASH_CRC32_U64(SEED, N, VALUE) _mm_crc32_u64(SEED, VALUE)
+#else
+# define LIBXSMM_HASH_CRC32_U64(SEED, N, VALUE) LIBXSMM_HASH_CRC32_U32( \
+    LIBXSMM_HASH_CRC32_U32((uint32_t)(SEED), N, (uint32_t)(VALUE)), \
+    N, (uint32_t)((VALUE) << 32))
+#endif
+
 #define LIBXSMM_HASH_NGEN(SEED, NGEN, VALUE) ((((SEED) << 5) + (VALUE)) % (NGEN))
 #define LIBXSMM_HASH_NPOT(SEED, NPOT, VALUE) LIBXSMM_MOD2(((SEED) << 5) + (VALUE), NPOT)
 
@@ -416,10 +424,8 @@ LIBXSMM_INLINE LIBXSMM_RETARGETABLE unsigned int internal_crc32_u8(unsigned int 
 
 LIBXSMM_INLINE LIBXSMM_RETARGETABLE unsigned int internal_crc32_u16(unsigned int seed, unsigned int n, unsigned short value)
 {
-  union { uint16_t value; uint8_t half[2]; } split; split.value = value;
-  LIBXSMM_UNUSED(n);
-  seed = internal_crc32_u8(seed, n, split.half[0]);
-  seed = internal_crc32_u8(seed, n, split.half[1]);
+  seed = internal_crc32_u8(seed, n, (uint8_t)value);
+  seed = internal_crc32_u8(seed, n, (uint8_t)(value << 8));
   return seed;
 }
 
@@ -436,10 +442,8 @@ LIBXSMM_INLINE LIBXSMM_RETARGETABLE unsigned int internal_crc32_u32(unsigned int
 
 LIBXSMM_INLINE LIBXSMM_RETARGETABLE unsigned int internal_crc32_u64(unsigned int seed, unsigned int n, unsigned long long value)
 {
-  union { uint64_t value; uint32_t half[2]; } split; split.value = value;
-  LIBXSMM_UNUSED(n);
-  seed = internal_crc32_u32(seed, n, split.half[0]);
-  seed = internal_crc32_u32(seed, n, split.half[1]);
+  seed = internal_crc32_u32(seed, n, (uint32_t)value);
+  seed = internal_crc32_u32(seed, n, (uint32_t)(value << 32));
   return seed;
 }
 

commit 99e5fc96d3ba8b3315d62b4bbd426f1e4590f8db

Fixed incorrect CRC32/SW implementation/typo (introduced in faed3937506670d040dcee67089f2d6475b27fcb). This problem had no impact other not reproducing the same CRC32 value between SSE-4.2 and SW implementation (likely degraded CRC-quality of SW path).

src/libxsmm_hash.c
@@ -153,7 +153,7 @@ LIBXSMM_INLINE LIBXSMM_RETARGETABLE unsigned int internal_crc32_u16(
   unsigned int seed, unsigned int n, unsigned short value)
 {
   seed = internal_crc32_u8(seed, n, (uint8_t)value);
-  seed = internal_crc32_u8(seed, n, (uint8_t)(value << 8));
+  seed = internal_crc32_u8(seed, n, (uint8_t)(value >> 8));
   return seed;
 }
 
@@ -175,7 +175,7 @@ LIBXSMM_INLINE LIBXSMM_RETARGETABLE unsigned int internal_crc32_u64(
   unsigned int seed, unsigned int n, unsigned long long value)
 {
   seed = internal_crc32_u32(seed, n, (uint32_t)(value));
-  seed = internal_crc32_u32(seed, n, (uint32_t)(value << 32));
+  seed = internal_crc32_u32(seed, n, (uint32_t)(value >> 32));
   return seed;
 }
 