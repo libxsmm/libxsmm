3d86d37615560331594ff197460042429d490aa6
Hans Pabst <hans.pabst@intel.com>
2017-05-22 09:24:28 +0200

    Moved some test cases (independent of local memory) out of a condition checking for sufficient local memory. Updated definition of local size (MAX_SIZE).

--- samples/smm/specialized.cpp
+++ samples/smm/specialized.cpp
@@ -55,7 +55,7 @@
 #endif
 
 #if !defined(MAX_SIZE)
-# define MAX_SIZE (LIBXSMM_MAX_MNK / LIBXSMM_MAX_K)
+# define MAX_SIZE ((LIBXSMM_MAX_M) * (LIBXSMM_MAX_N))
 #endif
 
 
@@ -159,6 +159,62 @@ int main(int argc, char* argv[])
         fprintf(stdout, "\tduration: %.0f ms\n", 1000.0 * duration);
       }
 
+      { // streaming A and C
+        fprintf(stdout, "Streamed (A,C)...\n");
+        const unsigned long long start = libxsmm_timer_tick();
+        unsigned long long x = libxsmm_timer_xtick();
+#if defined(_OPENMP)
+#       pragma omp parallel for
+#endif
+        for (int i = 0; i < s; ++i) {
+          const T *const ai = a + i * asize;
+          T* ci = c + i * csize;
+#if (0 != LIBXSMM_PREFETCH)
+          xmm(ai, b, ci,
+            LIBXSMM_PREFETCH_A(ai + asize), LIBXSMM_PREFETCH_B(b),
+            LIBXSMM_PREFETCH_C(ci + csize));
+#else
+          xmm(ai, b, ci);
+#endif
+        }
+        x = std::max(libxsmm_timer_xtick(), x) - x;
+        const double duration = libxsmm_timer_duration(start, libxsmm_timer_tick());
+        if (0 < duration && 0 != x) {
+          fprintf(stdout, "\tpseudo-perf.: %.1f FLOPS/cycle\n", (s * (2.0 * m * n * k - m * n)) / x);
+          fprintf(stdout, "\tperformance: %.1f GFLOPS/s\n", gflops / duration);
+          fprintf(stdout, "\tbandwidth: %.1f GB/s\n", s * bwsize / (duration * (1 << 30)));
+        }
+        fprintf(stdout, "\tduration: %.0f ms\n", 1000.0 * duration);
+      }
+
+      { // streaming B and C
+        fprintf(stdout, "Streamed (B,C)...\n");
+        const unsigned long long start = libxsmm_timer_tick();
+        unsigned long long x = libxsmm_timer_xtick();
+#if defined(_OPENMP)
+#       pragma omp parallel for
+#endif
+        for (int i = 0; i < s; ++i) {
+          const T *const bi = b + i * bsize;
+          T* ci = c + i * csize;
+#if (0 != LIBXSMM_PREFETCH)
+          xmm(a, bi, ci,
+            LIBXSMM_PREFETCH_A(a), LIBXSMM_PREFETCH_B(bi + bsize),
+            LIBXSMM_PREFETCH_C(ci + csize));
+#else
+          xmm(a, bi, ci);
+#endif
+        }
+        x = std::max(libxsmm_timer_xtick(), x) - x;
+        const double duration = libxsmm_timer_duration(start, libxsmm_timer_tick());
+        if (0 < duration && 0 != x) {
+          fprintf(stdout, "\tpseudo-perf.: %.1f FLOPS/cycle\n", (s * (2.0 * m * n * k - m * n)) / x);
+          fprintf(stdout, "\tperformance: %.1f GFLOPS/s\n", gflops / duration);
+          fprintf(stdout, "\tbandwidth: %.1f GB/s\n", s * bwsize / (duration * (1 << 30)));
+        }
+        fprintf(stdout, "\tduration: %.0f ms\n", 1000.0 * duration);
+      }
+
       if ((MAX_SIZE) >= csize) {
         { // streaming A and B
           fprintf(stdout, "Streamed (A,B)...\n");
@@ -180,62 +236,6 @@ int main(int argc, char* argv[])
               LIBXSMM_PREFETCH_C(tmp));
 #else
             xmm(ai, bi, tmp);
-#endif
-          }
-          x = std::max(libxsmm_timer_xtick(), x) - x;
-          const double duration = libxsmm_timer_duration(start, libxsmm_timer_tick());
-          if (0 < duration && 0 != x) {
-            fprintf(stdout, "\tpseudo-perf.: %.1f FLOPS/cycle\n", (s * (2.0 * m * n * k - m * n)) / x);
-            fprintf(stdout, "\tperformance: %.1f GFLOPS/s\n", gflops / duration);
-            fprintf(stdout, "\tbandwidth: %.1f GB/s\n", s * bwsize / (duration * (1 << 30)));
-          }
-          fprintf(stdout, "\tduration: %.0f ms\n", 1000.0 * duration);
-        }
-
-        { // streaming A and C
-          fprintf(stdout, "Streamed (A,C)...\n");
-          const unsigned long long start = libxsmm_timer_tick();
-          unsigned long long x = libxsmm_timer_xtick();
-#if defined(_OPENMP)
-#         pragma omp parallel for
-#endif
-          for (int i = 0; i < s; ++i) {
-            const T *const ai = a + i * asize;
-            T* ci = c + i * csize;
-#if (0 != LIBXSMM_PREFETCH)
-            xmm(ai, b, ci,
-              LIBXSMM_PREFETCH_A(ai + asize), LIBXSMM_PREFETCH_B(b),
-              LIBXSMM_PREFETCH_C(ci + csize));
-#else
-            xmm(ai, b, ci);
-#endif
-          }
-          x = std::max(libxsmm_timer_xtick(), x) - x;
-          const double duration = libxsmm_timer_duration(start, libxsmm_timer_tick());
-          if (0 < duration && 0 != x) {
-            fprintf(stdout, "\tpseudo-perf.: %.1f FLOPS/cycle\n", (s * (2.0 * m * n * k - m * n)) / x);
-            fprintf(stdout, "\tperformance: %.1f GFLOPS/s\n", gflops / duration);
-            fprintf(stdout, "\tbandwidth: %.1f GB/s\n", s * bwsize / (duration * (1 << 30)));
-          }
-          fprintf(stdout, "\tduration: %.0f ms\n", 1000.0 * duration);
-        }
-
-        { // streaming B and C
-          fprintf(stdout, "Streamed (B,C)...\n");
-          const unsigned long long start = libxsmm_timer_tick();
-          unsigned long long x = libxsmm_timer_xtick();
-#if defined(_OPENMP)
-#         pragma omp parallel for
-#endif
-          for (int i = 0; i < s; ++i) {
-            const T *const bi = b + i * bsize;
-            T* ci = c + i * csize;
-#if (0 != LIBXSMM_PREFETCH)
-            xmm(a, bi, ci,
-              LIBXSMM_PREFETCH_A(a), LIBXSMM_PREFETCH_B(bi + bsize),
-              LIBXSMM_PREFETCH_C(ci + csize));
-#else
-            xmm(a, bi, ci);
 #endif
           }
           x = std::max(libxsmm_timer_xtick(), x) - x;
