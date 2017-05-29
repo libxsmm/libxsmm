feef81eae162f20ed02ca11721cf4668249b65ac
Hans Pabst <hans.pabst@intel.com>
2017-05-29 17:59:35 +0200

    Fixed the build (header-only implementation).

--- src/libxsmm_trace.c
+++ src/libxsmm_trace.c
@@ -460,8 +460,8 @@ LIBXSMM_API_DEFINITION void libxsmm_trace(FILE* stream, unsigned int depth, cons
 LIBXSMM_PRAGMA_OPTIMIZE_OFF
 #endif
 
-LIBXSMM_API_INTERN LIBXSMM_API_PRIVATE LIBXSMM_ATTRIBUTE(no_instrument_function) void __cyg_profile_func_enter(void*, void*);
-LIBXSMM_API_INTERN LIBXSMM_API_PRIVATE void __cyg_profile_func_enter(void* this_fn, void* call_site)
+LIBXSMM_API_PRIVATE LIBXSMM_ATTRIBUTE(no_instrument_function) void __cyg_profile_func_enter(void*, void*);
+LIBXSMM_API_PRIVATE void __cyg_profile_func_enter(void* this_fn, void* call_site)
 {
 #if defined(__TRACE)
 # if !defined(LIBXSMM_TRACE_DLINFO)
@@ -497,8 +497,8 @@ LIBXSMM_API_INTERN LIBXSMM_API_PRIVATE void __cyg_profile_func_enter(void* this_
 }
 
 
-LIBXSMM_API_INTERN LIBXSMM_API_PRIVATE LIBXSMM_ATTRIBUTE(no_instrument_function) void __cyg_profile_func_exit(void*, void*);
-LIBXSMM_API_INTERN LIBXSMM_API_PRIVATE void __cyg_profile_func_exit(void* this_fn, void* call_site)
+LIBXSMM_API_PRIVATE LIBXSMM_ATTRIBUTE(no_instrument_function) void __cyg_profile_func_exit(void*, void*);
+LIBXSMM_API_PRIVATE void __cyg_profile_func_exit(void* this_fn, void* call_site)
 {
   LIBXSMM_UNUSED(this_fn); LIBXSMM_UNUSED(call_site); /* suppress warning */
 }
