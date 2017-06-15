804e0549d66b6e4fa805a314a42d8bf47d3cc638
Hans Pabst <hans.pabst@intel.com>
2017-06-15 10:21:37 +0200

    Apparently scripts with ACL are not handled correctly by srun.

--- .travis.sh
+++ .travis.sh
@@ -31,6 +31,7 @@
 #############################################################################
 
 HERE=$(cd $(dirname $0); pwd -P)
+SETFACL=$(which setfacl 2> /dev/null)
 MKTEMP=$(which mktemp 2> /dev/null)
 MKDIR=$(which mkdir 2> /dev/null)
 CHMOD=$(which chmod 2> /dev/null)
@@ -94,6 +95,9 @@ then
     fi
     TESTSCRIPT=$(${MKTEMP} ${HERE}/.libxsmm_XXXXXX.sh)
     ${CHMOD} a+rwx ${TESTSCRIPT}
+    if [ "" != "${SETFACL}" ]; then
+      ${SETFACL} -b ${TESTSCRIPT}
+    fi
     LAUNCH="${SRUN} --ntasks=1 ${SRUN_CPUS_PER_TASK_FLAG} \
       --partition=\${PARTITION} --preserve-env --pty ${TESTSCRIPT}"
   else # avoid temporary script in case of non-batch execution
