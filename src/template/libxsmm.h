0a858787b31760e77d6fad10d9f6df9d5c71eae7
Hans Pabst <hans.pabst@intel.com>
2017-06-26 12:43:15 +0200

    Include libxsmm_blkgemm.h via libxsmm.h.

--- src/template/libxsmm.h
+++ src/template/libxsmm.h
@@ -57,13 +57,14 @@
 #include "libxsmm_typedefs.h"
 #include "libxsmm_generator.h"
 #include "libxsmm_frontend.h"
+#include "libxsmm_blkgemm.h"
+#include "libxsmm_fsspmdm.h"
 #include "libxsmm_malloc.h"
 #include "libxsmm_spmdm.h"
 #include "libxsmm_cpuid.h"
 #include "libxsmm_timer.h"
 #include "libxsmm_sync.h"
 #include "libxsmm_dnn.h"
-#include "libxsmm_fsspmdm.h"
 
 /** Integer type for LAPACK/BLAS (LP64: 32-bit, and ILP64: 64-bit). */
 typedef LIBXSMM_BLASINT libxsmm_blasint;
