2002472774de69bd80765b15c8b791113c01a6c2
Hans Pabst <hans.pabst@intel.com>
2017-07-22 13:47:26 +0200

    Renamed ELEMENT_TYPE to ELEM_TYPE since this name varies depending on the test settings.

--- tests/vla.c
+++ tests/vla.c
@@ -32,17 +32,17 @@
 #include <stdlib.h>
 #include <assert.h>
 
-#define ELEMENT_TYPE short
+#define ELEM_TYPE short
 
 
 int main(/*int argc, char* argv[]*/)
 {
   int ni = 9, nj = 7, nk = 3, i, j, k, linear = 0, result = EXIT_SUCCESS;
-  ELEMENT_TYPE* in1 = (ELEMENT_TYPE*)malloc(ni * nj * nk * sizeof(ELEMENT_TYPE));
-  LIBXSMM_VLA_DECL(3, const ELEMENT_TYPE, in3, in1, nj, nk);
+  ELEM_TYPE* in1 = (ELEM_TYPE*)malloc(ni * nj * nk * sizeof(ELEM_TYPE));
+  LIBXSMM_VLA_DECL(3, const ELEM_TYPE, in3, in1, nj, nk);
 
   assert(0 != in1);
-  for (i = 0; i < (ni * nj * nk); ++i) in1[i] = (ELEMENT_TYPE)i;
+  for (i = 0; i < (ni * nj * nk); ++i) in1[i] = (ELEM_TYPE)i;
   for (i = 0; i < ni; ++i) {
     for (j = 0; j < nj; ++j) {
       for (k = 0; k < nk; ++k) {
