0882b1b1c552e4b17d1072021e38ae78c6c47606
Kunal Banerjee <kunal.banerjee.cse@gmail.com>
2017-08-09 22:43:27 -0700

    Restoring winograd with default options

--- src/libxsmm_dnn_convolution_winograd_weight_update.c
+++ src/libxsmm_dnn_convolution_winograd_weight_update.c
@@ -203,22 +203,6 @@ LIBXSMM_API_DEFINITION libxsmm_dnn_err_t libxsmm_dnn_convolve_winograd_st_upd_cu
   }
   else {
     if (handle->datatype == LIBXSMM_DNN_DATATYPE_F32 && handle->datatype_itm == LIBXSMM_DNN_DATATYPE_F32) {
-#if 0
-      typedef float element_input_type;
-      typedef float element_output_type;
-      typedef float element_filter_type;
-      typedef libxsmm_sconvfunction libxsmm_convfunction;
-# include "template/libxsmm_dnn_convolve_winograd_st_upd_custom_custom.tpl.c"
-#endif
-
-      if (handle->flag_reuseInput == 1) {
-        if (handle->scratchTemp == 0) {
-          status = LIBXSMM_DNN_ERR_DATA_NOT_BOUND;
-          return status;
-        } else {
-        handle->scratch3 = handle->scratchTemp;
-        }
-      }
       if (handle->cwino_upd.alpha == 6  && libxsmm_target_archid == LIBXSMM_X86_AVX512_KNM && (handle->cwino_upd.itiles*handle->cwino_upd.jtiles*handle->cwino_upd.bimg % 4) == 0) {
         if (handle->scratchVk == 0) {
           status = LIBXSMM_DNN_ERR_DATA_NOT_BOUND;
@@ -293,22 +277,6 @@ LIBXSMM_API_DEFINITION libxsmm_dnn_err_t libxsmm_dnn_convolve_winograd_st_upd_nh
   }
   else {
     if (handle->datatype == LIBXSMM_DNN_DATATYPE_F32 && handle->datatype_itm == LIBXSMM_DNN_DATATYPE_F32) {
-#if 0
-      typedef float element_input_type;
-      typedef float element_output_type;
-      typedef float element_filter_type;
-      typedef libxsmm_sconvfunction libxsmm_convfunction;
-# include "template/libxsmm_dnn_convolve_winograd_st_upd_nhwc_custom.tpl.c"
-#endif
-
-      if (handle->flag_reuseInput == 1) {
-        if (handle->scratchTemp == 0) {
-          status = LIBXSMM_DNN_ERR_DATA_NOT_BOUND;
-          return status;
-        } else {
-        handle->scratch3 = handle->scratchTemp;
-        }
-      }
       if (handle->cwino_upd.alpha == 6 && libxsmm_target_archid == LIBXSMM_X86_AVX512_KNM && (handle->cwino_upd.itiles*handle->cwino_upd.jtiles*handle->cwino_upd.bimg % 4) == 0) {
         if (handle->scratchVk == 0) {
           status = LIBXSMM_DNN_ERR_DATA_NOT_BOUND;
