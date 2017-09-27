2c097455d257eaaa3b154111acfb317457f1c66a
Alexander Heinecke <alexander.heinecke@intel.com>
2017-09-26 21:06:10 -0700

    some attempt to fix UPD, didn't work

--- src/template/libxsmm_dnn_convolve_st_upd_custom_custom_stream_opt.tpl.c
+++ src/template/libxsmm_dnn_convolve_st_upd_custom_custom_stream_opt.tpl.c
@@ -151,10 +151,10 @@ if (handle->padding_flag == 1) {
 if (handle->padding_flag == 1) {
   if (handle->trans_ofw_ifm > 0) {
     input_base = &LIBXSMM_VLA_ACCESS(5, tr_input_padded, 0, 0, 0, 0, 0, handle->blocksifm, padded_h, handle->ifmblock, padded_w);
-    input_zero = &LIBXSMM_VLA_ACCESS(5, tr_input_padded, 0, 0, 0, 0, 0, handle->blocksifm, padded_h, handle->ifmblock, padded_w);
+    input_zero = &LIBXSMM_VLA_ACCESS(5, tr_input_padded, ltid, 0, 0, 0, 0, handle->blocksifm, padded_h, handle->ifmblock, padded_w);
   } else {
     input_base = &LIBXSMM_VLA_ACCESS(5, input_padded, 0, 0, 0, 0, 0, handle->blocksifm, padded_h, padded_w, handle->ifmblock);
-    input_zero = &LIBXSMM_VLA_ACCESS(5, input_padded, 0, 0, 0, 0, 0, handle->blocksifm, padded_h, padded_w, handle->ifmblock);
+    input_zero = &LIBXSMM_VLA_ACCESS(5, input_padded, ltid, 0, 0, 0, 0, handle->blocksifm, padded_h, padded_w, handle->ifmblock);
   }
   /* we need to set the scratch to zero */
   /* @TODO: we need to find a better/faster code here */
