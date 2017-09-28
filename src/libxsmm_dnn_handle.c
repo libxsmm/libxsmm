48e81c95375c54ccbcddc3158fc1877b169ddb41
egeorgan <evangelos.georganas@intel.com>
2017-09-28 13:56:00 -0700

    Fixed duality tuning...

--- src/libxsmm_dnn_handle.c
+++ src/libxsmm_dnn_handle.c
@@ -308,7 +308,7 @@ LIBXSMM_API_DEFINITION libxsmm_dnn_err_t libxsmm_dnn_internal_create_conv_handle
       handle->blocksofm_blocking = 1;
     }
 
-    if ( (handle->desc.C == 1024 && handle->desc.K == 256) || (handle->desc.C == 2048 && handle->desc.K == 512) ) {
+    if ( (handle->desc.C == 256 && handle->desc.K == 1024) || (handle->desc.C == 512 && handle->desc.K == 1024) ) {
       handle->blocksofm_blocking = 8;
     }
 
