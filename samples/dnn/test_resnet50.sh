cf7a1cc3d481d7b96c448de8ef6af7631e9f7c0d
Alexander Heinecke <alexander.heinecke@intel.com>
2017-09-28 15:40:34 -0700

    fixed typo in test resnet script

--- samples/dnn/test_resnet50.sh
+++ samples/dnn/test_resnet50.sh
@@ -16,6 +16,6 @@ srun ./run_resnet50_mock.sh 70 1000 1 f32 B L 1 | grep PERFDUMP
 srun ./run_resnet50_mock.sh 70 1000 1 f32 U L 1 | grep PERFDUMP
 srun ./run_resnet50_mock.sh 70 1000 1 f32 F L 0 | grep PERFDUMP
 srun ./run_resnet50_mock.sh 70 1000 1 f32 B L 0 | grep PERFDUMP
-srun ./run_resnet50-mock.sh 70 1000 1 f32 U L 0 | grep PERFDUMP
+srun ./run_resnet50_mock.sh 70 1000 1 f32 U L 0 | grep PERFDUMP
 
 set +x
