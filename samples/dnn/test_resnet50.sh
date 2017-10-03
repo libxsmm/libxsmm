ccf603dad617395e623a32651f3dd939e34ab067
Alexander Heinecke <alexander.heinecke@intel.com>
2017-10-03 15:36:10 -0700

    set number of test iterations to 1000 on test script

--- samples/dnn/test_resnet50.sh
+++ samples/dnn/test_resnet50.sh
@@ -4,7 +4,7 @@ set -x
 
 export OMP_NUM_THREADS=70
 export KMP_AFFINITY=granularity=fine,compact,1,2
-ITERS=1
+ITERS=1000
 
 srun ./run_resnet50.sh 70 ${ITERS} 1 f32 F L 1 | grep PERFDUMP
 srun ./run_resnet50.sh 70 ${ITERS} 1 f32 B L 1 | grep PERFDUMP
