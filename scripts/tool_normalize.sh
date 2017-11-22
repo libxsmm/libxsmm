73efdfe119aeabb9942bac9b652a8da9410e19e6
Hans Pabst <hans.pabst@intel.com>
2017-11-22 08:42:59 +0100

    Removed banned characters [skip ci].

--- scripts/tool_normalize.sh
+++ scripts/tool_normalize.sh
@@ -61,7 +61,7 @@ if [ "" != "${ICONV}" ] && [ "" != "${ECHO}" ] && [ "" != "${GIT}" ] && [ "" != 
         ${ECHO} "Warning: ${FILE} contains banned characters!"
       fi
       #${ICONV} -t ASCII ${FILE} | ${SED} -e "N;s/\r\n$/\n/" -e "N;s/\r$/\n/" | ${SED} -e "s/\s\s*$//" > ${TMPF}
-	  ${ICONV} -t ASCII ${FILE} | ${SED} -e "s/\s\s*$//" > ${TMPF}
+      ${ICONV} -t ASCII ${FILE} | ${SED} -e "s/\s\s*$//" > ${TMPF}
       ${CP} ${TMPF} ${FILE}
     done
   done
