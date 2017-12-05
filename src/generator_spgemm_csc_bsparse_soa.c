c60857669c69b7388cce40af032dd696ab3cbffd
Hans Pabst <hans.pabst@intel.com>
2017-12-05 20:59:12 +0100

    Removed trailing white spaces.

--- src/generator_spgemm_csc_bsparse_soa.c
+++ src/generator_spgemm_csc_bsparse_soa.c
@@ -207,8 +207,8 @@ void libxsmm_generator_spgemm_csc_bsparse_soa_avx256_512( libxsmm_generated_code
       l_found_mul = 0;
 
       /* let's figure out if we can apply qmadd when being sin F32 setting and on KNM */
-      if ( (l_k < ((unsigned int)i_xgemm_desc->k - 3))                       && 
-           (l_micro_kernel_config.instruction_set == LIBXSMM_X86_AVX512_KNM) && 
+      if ( (l_k < ((unsigned int)i_xgemm_desc->k - 3))                       &&
+           (l_micro_kernel_config.instruction_set == LIBXSMM_X86_AVX512_KNM) &&
            (LIBXSMM_GEMM_PRECISION_F32 == i_xgemm_desc->datatype)               ) {
         /* loop over the columns of B/C */
         for ( l_n = 0; l_n < l_n_limit - l_n_processed; l_n++ ) {
@@ -280,7 +280,7 @@ void libxsmm_generator_spgemm_csc_bsparse_soa_avx256_512( libxsmm_generated_code
                                             l_micro_kernel_config.vector_name,
                                             l_max_reg_block+l_lcl_k, 0, 0 );
         }
-        
+
         /* loop over the columns of B/C */
         for ( l_n = 0; l_n < l_n_limit - l_n_processed; l_n++ ) {
           /* issue a qmadd */
