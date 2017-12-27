c300b7af7ea0913eef6fe85f65f9121717d77f6a
Junyi Qiu <geoffrey0824@sjtu.edu.cn>
2017-12-27 14:59:00 -0800

    disabled all optimizations in csc soa generator due to performance issues

--- src/generator_spgemm_csc_bsparse_soa.c
+++ src/generator_spgemm_csc_bsparse_soa.c
@@ -199,6 +199,7 @@ void libxsmm_generator_spgemm_csc_bsparse_soa_avx256_512( libxsmm_generated_code
   /* unroll strategy */
   l_m_unroll_num = 1;
   l_m_peeling = 0;
+#if 0
   if ( strcmp(i_arch, "knl") == 0 ||
        strcmp(i_arch, "skx") == 0 ||
        ( strcmp(i_arch, "knm") == 0 && LIBXSMM_GEMM_PRECISION_F64 == i_xgemm_desc->datatype ) ) {
@@ -217,15 +218,18 @@ void libxsmm_generator_spgemm_csc_bsparse_soa_avx256_512( libxsmm_generated_code
       }
     }
   }
+#endif
 
   /* reorder strategy */
   l_reorder_enabled = 0;
+#if 0
   if ( ( LIBXSMM_GEMM_PRECISION_F64 == i_xgemm_desc->datatype ) &&
        ( strcmp(i_arch, "knl") == 0 ||
          strcmp(i_arch, "skx") == 0 ||
          strcmp(i_arch, "knm") == 0   )                            ) {
     l_reorder_enabled = 1;
   }
+#endif
 
   /* open asm */
   libxsmm_x86_instruction_open_stream( io_generated_code, &l_gp_reg_mapping, i_arch, i_xgemm_desc->prefetch );
