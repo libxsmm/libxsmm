08c578671c32583b2216159a897bdb6c496b19c2
egeorgan <evangelos.georganas@intel.com>
2018-01-11 19:55:38 -0800

    FInalized incomplete fix

--- src/libxsmm_dnn_handle.c
+++ src/libxsmm_dnn_handle.c
@@ -1835,7 +1835,7 @@ LIBXSMM_API_DEFINITION libxsmm_dnn_err_t libxsmm_dnn_internal_create_conv_handle
           handle->scratch4_size = handle->desc.threads * handle->blocksifm_lp * handle->ifmblock * handle->blocksofm * handle->ofmblock
             * handle->desc.R * handle->desc.S * handle->fm_lp_block * libxsmm_dnn_typesize(handle->datatype_out);
           handle->scratch4_size += handle->desc.threads *  handle->blocksofm *  handle->blocksifm * handle->desc.R
-            * handle->desc.S * handle->ifmblock * handle->ofmblock * handle->blocksifm_lp * libxsmm_dnn_typesize(handle->datatype_out);
+            * handle->desc.S * handle->ifmblock * handle->ofmblock * libxsmm_dnn_typesize(handle->datatype_out);
 
           /* enable external reduce of filter scratch */
           if ( (handle->options & LIBXSMM_DNN_CONV_OPTION_UPD_NO_FILTER_REDUCE) > 0 ) {
