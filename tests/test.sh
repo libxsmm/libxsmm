fceed51ef596b6f6336cf3ca264ba1470ad81190
Hans Pabst <hans.pabst@intel.com>
2018-01-30 19:46:36 +0100

    Adjust PATH for MinGW environment.

--- tests/test.sh
+++ tests/test.sh
@@ -16,7 +16,7 @@ if [ "Windows_NT" = "${OS}" ]; then
   # Cygwin's ldd hangs with dyn. linked executables or certain shared libraries
   LDD=$(which cygcheck)
   # Cygwin's "env" does not set PATH ("Files/Black: No such file or directory")
-  export PATH=${PATH}:${HERE}/../lib
+  export PATH=${PATH}:${HERE}/../lib:/usr/x86_64-w64-mingw32/sys-root/mingw/bin
 else
   if [ "" != "$(which ldd)" ]; then
     LDD=ldd
