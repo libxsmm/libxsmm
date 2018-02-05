437c0f1eea686ce5e22235f526a989bcf2f207b4
Hans Pabst <hans.pabst@intel.com>
2018-02-05 17:01:08 +0100

    Introduced LIBXSMM_INTRINSICS_MM512_PERMUTEVAR_EPI32 to fix-up uses of _mm512_permutevar_epi32 intrinsic.

--- include/libxsmm_intrinsics_x86.h
+++ include/libxsmm_intrinsics_x86.h
@@ -401,9 +401,11 @@
 # endif
 # if defined(__INTEL_COMPILER)
 #   define LIBXSMM_INTRINSICS_MM512_ABS_PS(A) _mm512_abs_ps(A)
+#   define LIBXSMM_INTRINSICS_MM512_PERMUTEVAR_EPI32(A, B) _mm512_permutevar_epi32(A, B)
 # else
 #   define LIBXSMM_INTRINSICS_MM512_ABS_PS(A) _mm512_castsi512_ps(_mm512_and_epi32( \
                              _mm512_castps_si512(A), _mm512_set1_epi32(0x7FFFFFFF)))
+#   define LIBXSMM_INTRINSICS_MM512_PERMUTEVAR_EPI32(A, B) _mm512_permutexvar_epi32(A, B)
 # endif
 
 #endif /*!defined(LIBXSMM_INTRINSICS_NONE)*/
