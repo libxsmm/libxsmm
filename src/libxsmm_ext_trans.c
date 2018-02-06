a378ca50e7feb9d16b505b6ce05d5da9311cf510
Hans Pabst <hans.pabst@intel.com>
2018-02-06 15:11:32 +0100

    Include missing header file (memset).

--- src/libxsmm_ext_trans.c
+++ src/libxsmm_ext_trans.c
@@ -35,6 +35,7 @@
 # pragma offload_attribute(push,target(LIBXSMM_OFFLOAD_TARGET))
 #endif
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #if defined(LIBXSMM_OFFLOAD_TARGET)
 # pragma offload_attribute(pop)
