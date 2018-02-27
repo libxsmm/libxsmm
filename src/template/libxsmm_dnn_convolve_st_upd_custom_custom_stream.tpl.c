47596f3651865f28f3eaebbebc2d05b09dff8ff2
Hans Pabst <hans.pabst@intel.com>
2018-02-27 15:53:49 +0100

    Removed trailing white spaces.

--- src/template/libxsmm_dnn_convolve_st_upd_custom_custom_stream.tpl.c
+++ src/template/libxsmm_dnn_convolve_st_upd_custom_custom_stream.tpl.c
@@ -268,7 +268,7 @@ if (handle->reduce_weights) {
     for ( i = 0; i < handle->weight_copies; i++ ) {
       weight_sum = _mm512_add_ps(weight_sum, _mm512_load_ps(&LIBXSMM_VLA_ACCESS(3, reduction_weight, j, i, 0, handle->weight_copies, 16)));
     }
-#ifndef __AVX512BW__  
+#ifndef __AVX512BW__
     _mm512_stream_ps(&weight_ptr[j*16], weight_sum);
 #else
     _mm512_store_ps(&weight_ptr[j*16], weight_sum);
--- src/template/libxsmm_dnn_convolve_st_upd_custom_custom_stream_opt.tpl.c
+++ src/template/libxsmm_dnn_convolve_st_upd_custom_custom_stream_opt.tpl.c
@@ -297,7 +297,7 @@ if (handle->upd_use_external_reduce == 0) {
       weight_sum = _mm512_add_ps(weight_sum, _mm512_load_ps(&LIBXSMM_VLA_ACCESS(3, reduction_weight, j, i, 0, handle->desc.threads, 16)));
     }
     if ( ((handle->options & LIBXSMM_DNN_CONV_OPTION_OVERWRITE) > 0) ) {
-#ifndef __AVX512BW__  
+#ifndef __AVX512BW__
       _mm512_stream_ps(&weight_ptr[j*16], weight_sum);
 #else
       _mm512_store_ps(&weight_ptr[j*16], weight_sum);
