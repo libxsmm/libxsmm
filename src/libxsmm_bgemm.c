8d9c535a1d46321b6fcfabf9021102018e4e0fa3
Hans Pabst <hans.pabst@intel.com>
2018-03-09 18:49:48 +0100

    Extended FE macros constructing type prefixes, type names, and dispatch function names for separate input and output types. Apply change to BGEMM to leverage WIMM and WSMM.

--- src/libxsmm_bgemm.c
+++ src/libxsmm_bgemm.c
@@ -443,13 +443,11 @@ LIBXSMM_API void libxsmm_bgemm(const libxsmm_bgemm_handle* handle,
 #         include "template/libxsmm_bgemm.tpl.c"
 #         undef LIBXSMM_BGEMM_TEMPLATE_REAL_TYPE_C
         }
-        else {
-#if 0
-/* @TODO fix the macros to support his flavor of bgemm */
+        else { /* WSMM */
+          LIBXSMM_ASSERT(LIBXSMM_GEMM_PRECISION_F32 == handle->oprec);
 #         define LIBXSMM_BGEMM_TEMPLATE_REAL_TYPE_C float
 #         include "template/libxsmm_bgemm.tpl.c"
 #         undef LIBXSMM_BGEMM_TEMPLATE_REAL_TYPE_C
-#endif
         }
 #       undef LIBXSMM_BGEMM_TEMPLATE_REAL_TYPE_AB
       } break;
