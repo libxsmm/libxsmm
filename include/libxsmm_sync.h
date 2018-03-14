b237ec4aa8a5c7831d79b823b17b09915686a179
Hans Pabst <hans.pabst@intel.com>
2018-03-14 13:13:46 +0100

    Attempt to fix broken build.

--- include/libxsmm_sync.h
+++ include/libxsmm_sync.h
@@ -64,10 +64,10 @@
 
 #if defined(__MIC__)
 # define LIBXSMM_SYNC_PAUSE _mm_delay_32(8/*delay*/)
-#elif !defined(LIBXSMM_INTRINSICS_NONE) && !defined(LIBXSMM_INTRINSICS_STATIC)
-# define LIBXSMM_SYNC_PAUSE _mm_pause()
 #elif defined(LIBXSMM_GCC_BASELINE) && !defined(__PGI)
 # define LIBXSMM_SYNC_PAUSE __builtin_ia32_pause()
+#elif !defined(LIBXSMM_INTRINSICS_NONE)
+# define LIBXSMM_SYNC_PAUSE _mm_pause()
 #else
 # define LIBXSMM_SYNC_PAUSE
 #endif
