63d448669afc7937fb3ab322bc4290e9889c434c
Hans Pabst <hans.pabst@intel.com>
2018-03-14 16:44:46 +0100

    Fixed compiler warnings and remarks.

--- src/generator_x86_instructions.c
+++ src/generator_x86_instructions.c
@@ -3501,12 +3501,12 @@ void libxsmm_x86_instruction_alu_imm_i64( libxsmm_generated_code* io_generated_c
 
     if ( i_alu_instr != LIBXSMM_X86_INSTR_MOVQ )
     {
-       fprintf(stderr,"How are you doing a 64-byte immediate on instruction: %d\n",i_alu_instr);
+       fprintf(stderr,"How are you doing a 64-byte immediate on instruction: %u\n",i_alu_instr);
        exit(-1);
     }
     if ( /*i_gp_reg_number < 0 ||*/ i_gp_reg_number > 15 )
     {
-       fprintf(stderr,"libxsmm_x86_instruction_alu_imm_i64 strange gp reg=%d\n",i_gp_reg_number);
+       fprintf(stderr,"libxsmm_x86_instruction_alu_imm_i64 strange gp reg=%u\n",i_gp_reg_number);
        exit(-1);
     }
     l_reg0 = i_gp_reg_number;
@@ -3542,7 +3542,7 @@ void libxsmm_x86_instruction_alu_imm_i64( libxsmm_generated_code* io_generated_c
       l_code_length = LIBXSMM_SNPRINTF(l_new_code, l_max_code_length, "                       \"%s $%llu, %%%%%s\\n\\t\"\n",
                                        l_instr_name, (unsigned long long)i_immediate, l_gp_reg_name );
     } else {
-      l_code_length = LIBXSMM_SNPRINTF(l_new_code, l_max_code_length, "                       %s $%lld, %%%s\n",
+      l_code_length = LIBXSMM_SNPRINTF(l_new_code, l_max_code_length, "                       %s $%llu, %%%s\n",
                                        l_instr_name, (unsigned long long)i_immediate, l_gp_reg_name );
     }
     libxsmm_append_code_as_string( io_generated_code, l_new_code, l_code_length );
