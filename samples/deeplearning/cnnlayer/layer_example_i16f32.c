39f48b2d9f90d2c906c9f559f93c979c13cb3fa8
Hans Pabst <hans.pabst@intel.com>
2018-05-11 18:19:09 +0200

    Adjusted built-in CHECK_SCALE (default).

--- samples/deeplearning/cnnlayer/layer_example_i16f32.c
+++ samples/deeplearning/cnnlayer/layer_example_i16f32.c
@@ -1084,7 +1084,7 @@ int main(int argc, char* argv[])
   libxsmm_free(dfilter_libxsmm);
 
   { const char *const env_check_scale = getenv("CHECK_SCALE");
-    const double check_scale = LIBXSMM_ABS(0 == env_check_scale ? 100.0 : atof(env_check_scale));
+    const double check_scale = LIBXSMM_ABS(0 == env_check_scale ? 50.0 : atof(env_check_scale));
     if (LIBXSMM_NEQ(0, check) && (check < 100.0 * check_scale * diff.normf_rel) && (global_status == LIBXSMM_DNN_SUCCESS)) {
       fprintf(stderr, "FAILED with an error of %f%%!\n", 100.0 * diff.normf_rel);
       exit(EXIT_FAILURE);
