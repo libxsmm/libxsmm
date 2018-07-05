565a44e494d6b7d5d4677177409355b97cfaedbe
Hans Pabst <hans.pabst@intel.com>
2018-07-05 11:18:08 +0200

    Adjusted two more assertions.

--- src/template/libxsmm_dnn_convolve_st_upd_nhwc_custom-rsck_generic.tpl.c
+++ src/template/libxsmm_dnn_convolve_st_upd_nhwc_custom-rsck_generic.tpl.c
@@ -80,7 +80,7 @@ LIBXSMM_VLA_DECL(3, element_input_type, input_padded, input_scratch, padded_w, h
 LIBXSMM_VLA_DECL(3, element_output_type, output_trans, output_scratch, handle->ofmblock, handle->ofwp);
 LIBXSMM_VLA_DECL(4, element_filter_type, weight_local, filter_scratch, handle->desc.S, handle->ofmblock, handle->ifmblock);
 
-LIBXSMM_ASSERT(scratch7_size * sizeof(element_input_type)  * handle->desc.threads <= handle->scratch7_size);
+LIBXSMM_ASSERT(scratch7_size * sizeof(element_input_type)  * handle->desc.threads <= handle->scratch7_size || 0 == handle->scratch7_size);
 LIBXSMM_ASSERT(scratch8_size * sizeof(element_output_type) * handle->desc.threads <= handle->scratch8_size);
 LIBXSMM_ASSERT(scratch9_size * sizeof(element_filter_type) * handle->desc.threads <= handle->scratch9_size);
 
