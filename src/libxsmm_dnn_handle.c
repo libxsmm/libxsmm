b189d384d649595d9a3ebfd3b37126a33d7038bc
Hans Pabst <hans.pabst@intel.com>
2018-07-05 12:50:30 +0200

    Fixed DNN regression tests.

--- src/libxsmm_dnn_handle.c
+++ src/libxsmm_dnn_handle.c
@@ -139,44 +139,42 @@ LIBXSMM_API_INTERN libxsmm_dnn_err_t libxsmm_dnn_internal_create_conv_handle_dir
     libxsmm_dnn_setup_scratch(handle);
   }
 
-  /* Generic code-path setup here... */
+  if (0 != noarch) { /* Setup generic code generation */
+    status = libxsmm_dnn_setup_generic(handle);
+  }
+
+  if (LIBXSMM_DNN_SUCCESS == status) {
 #if !defined(LIBXSMM_DNN_VLA_TLS1)
-  if (0 != noarch || 0 != handle->use_fwd_generic || 0 != handle->use_bwd_generic || 0 != handle->use_upd_generic)
-#else
-  if (0 != noarch)
+    handle->scratch7 = 0;
+    if (0 != handle->use_fwd_generic || 0 != handle->use_bwd_generic || 0 != handle->use_upd_generic) {
+      const int padded_h = handle->desc.H + (2 * handle->desc.pad_h);
+      const int padded_w = handle->desc.W + (2 * handle->desc.pad_w);
+      const size_t size7 = padded_h * padded_w * handle->ifmblock * libxsmm_dnn_typesize(handle->datatype_in);
+      handle->scratch7_size = LIBXSMM_UP2(size7, LIBXSMM_CACHELINE) * handle->desc.threads;
+    }
+    else
 #endif
-  {
-    /*Setup generic code generation here*/
-    const int handle_status = libxsmm_dnn_setup_generic(handle);
-    const int padded_h = handle->desc.H + (2 * handle->desc.pad_h);
-    const int padded_w = handle->desc.W + (2 * handle->desc.pad_w);
-    const size_t size7 = padded_h * padded_w * handle->ifmblock * libxsmm_dnn_typesize(handle->datatype_in);
-    handle->scratch7_size = LIBXSMM_UP2(size7, LIBXSMM_CACHELINE) * handle->desc.threads;
-    status = handle_status;
-  }
-  else {
-    handle->scratch7_size = 0;
-  }
-  if (0 != handle->use_upd_generic) {
-    const size_t output_typesize = libxsmm_dnn_typesize(handle->datatype_out);
-    /* FIXME: currently filter data-type is always smaller/equal output type */
-    const size_t filter_typesize = output_typesize;
-    const size_t size8 = handle->ofhp * handle->ofwp * handle->ofmblock * output_typesize;
-    const size_t size9 = handle->desc.R * handle->desc.S * handle->ifmblock * handle->ofmblock * filter_typesize;
-    handle->scratch8_size = LIBXSMM_UP2(size8, LIBXSMM_CACHELINE) * handle->desc.threads;
-    handle->scratch9_size = LIBXSMM_UP2(size9, LIBXSMM_CACHELINE) * handle->desc.threads;
-  }
-  else {
-    handle->scratch8_size = 0;
-    handle->scratch9_size = 0;
-  }
-#if !defined(LIBXSMM_DNN_VLA_TLS1)
-  handle->scratch7 = 0;
-#endif
+    {
+      handle->scratch7_size = 0;
+    }
 #if !defined(LIBXSMM_DNN_VLA_TLS2)
-  handle->scratch8 = 0;
-  handle->scratch9 = 0;
+    handle->scratch8 = handle->scratch9 = 0;
+    if (0 != handle->use_upd_generic) {
+      const size_t output_typesize = libxsmm_dnn_typesize(handle->datatype_out);
+      /* FIXME: currently filter data-type is always smaller/equal output type */
+      const size_t filter_typesize = output_typesize;
+      const size_t size8 = handle->ofhp * handle->ofwp * handle->ofmblock * output_typesize;
+      const size_t size9 = handle->desc.R * handle->desc.S * handle->ifmblock * handle->ofmblock * filter_typesize;
+      handle->scratch8_size = LIBXSMM_UP2(size8, LIBXSMM_CACHELINE) * handle->desc.threads;
+      handle->scratch9_size = LIBXSMM_UP2(size9, LIBXSMM_CACHELINE) * handle->desc.threads;
+    }
+    else
 #endif
+    {
+      handle->scratch8_size = handle->scratch9_size = 0;
+    }
+  }
+
   return status;
 }
 
