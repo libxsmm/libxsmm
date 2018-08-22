9558aa0c498b5ccc283c5b0ca5ce1af4aa66ec35
Hans Pabst <hans.pabst@intel.com>
2018-08-22 09:40:09 +0200

    Removed trailing white spaces [skip ci].

--- src/libxsmm_dnn_fusedbatchnorm_forward.c
+++ src/libxsmm_dnn_fusedbatchnorm_forward.c
@@ -64,17 +64,17 @@ libxsmm_dnn_err_t libxsmm_dnn_fusedbn_st_fwd_custom_f32_f32(libxsmm_dnn_fusedbn*
     } else if ( handle->desc.fuse_ops == LIBXSMM_DNN_FUSEDBN_OPS_BNSCALE_ELTWISE ) {
 # define LIBXSMM_DNN_FUSEDBN_FWD_ENABLE_ELTWISE
 # include "template/libxsmm_dnn_fusedbatchnorm_st_fwd_custom.tpl.c"
-# undef LIBXSMM_DNN_FUSEDBN_FWD_ENABLE_ELTWISE 
+# undef LIBXSMM_DNN_FUSEDBN_FWD_ENABLE_ELTWISE
     } else if ( handle->desc.fuse_ops == LIBXSMM_DNN_FUSEDBN_OPS_BNSCALE_RELU ) {
 # define LIBXSMM_DNN_FUSEDBN_FWD_ENABLE_RELU
 # include "template/libxsmm_dnn_fusedbatchnorm_st_fwd_custom.tpl.c"
-# undef LIBXSMM_DNN_FUSEDBN_FWD_ENABLE_RELU 
+# undef LIBXSMM_DNN_FUSEDBN_FWD_ENABLE_RELU
     } else if ( handle->desc.fuse_ops == LIBXSMM_DNN_FUSEDBN_OPS_BNSCALE_ELTWISE_RELU ) {
 # define LIBXSMM_DNN_FUSEDBN_FWD_ENABLE_ELTWISE
 # define LIBXSMM_DNN_FUSEDBN_FWD_ENABLE_RELU
 # include "template/libxsmm_dnn_fusedbatchnorm_st_fwd_custom.tpl.c"
 # undef LIBXSMM_DNN_FUSEDBN_FWD_ENABLE_RELU
-# undef LIBXSMM_DNN_FUSEDBN_FWD_ENABLE_ELTWISE 
+# undef LIBXSMM_DNN_FUSEDBN_FWD_ENABLE_ELTWISE
     } else {
       status = LIBXSMM_DNN_ERR_FUSEBN_UNSUPPORTED_FUSION;
     }
@@ -140,17 +140,17 @@ LIBXSMM_API_INTERN libxsmm_dnn_err_t libxsmm_dnn_convolve_st_fwd_custom(libxsmm_
         } else if ( handle->desc.fuse_ops == LIBXSMM_DNN_FUSEDBN_OPS_BNSCALE_ELTWISE ) {
 # define LIBXSMM_DNN_FUSEDBN_FWD_ENABLE_ELTWISE
 # include "template/libxsmm_dnn_fusedbatchnorm_st_fwd_custom.tpl.c"
-# undef LIBXSMM_DNN_FUSEDBN_FWD_ENABLE_ELTWISE 
+# undef LIBXSMM_DNN_FUSEDBN_FWD_ENABLE_ELTWISE
         } else if ( handle->desc.fuse_ops == LIBXSMM_DNN_FUSEDBN_OPS_BNSCALE_RELU ) {
 # define LIBXSMM_DNN_FUSEDBN_FWD_ENABLE_RELU
 # include "template/libxsmm_dnn_fusedbatchnorm_st_fwd_custom.tpl.c"
-# undef LIBXSMM_DNN_FUSEDBN_FWD_ENABLE_RELU 
+# undef LIBXSMM_DNN_FUSEDBN_FWD_ENABLE_RELU
         } else if ( handle->desc.fuse_ops == LIBXSMM_DNN_FUSEDBN_OPS_BNSCALE_ELTWISE_RELU ) {
 # define LIBXSMM_DNN_FUSEDBN_FWD_ENABLE_ELTWISE
 # define LIBXSMM_DNN_FUSEDBN_FWD_ENABLE_RELU
 # include "template/libxsmm_dnn_fusedbatchnorm_st_fwd_custom.tpl.c"
 # undef LIBXSMM_DNN_FUSEDBN_FWD_ENABLE_RELU
-# undef LIBXSMM_DNN_FUSEDBN_FWD_ENABLE_ELTWISE 
+# undef LIBXSMM_DNN_FUSEDBN_FWD_ENABLE_ELTWISE
         } else {
          status = LIBXSMM_DNN_ERR_FUSEBN_UNSUPPORTED_FUSION;
         }
