ec6f79ca0552e35ebb7734a5fd992c974b8bb800
Hans Pabst <hans.pabst@intel.com>
2018-09-08 15:46:46 +0200

    Included missing header file.

--- src/libxsmm_generator.c
+++ src/libxsmm_generator.c
@@ -30,6 +30,14 @@
 ******************************************************************************/
 #include "libxsmm_main.h"
 
+#if defined(LIBXSMM_OFFLOAD_TARGET)
+# pragma offload_attribute(push,target(LIBXSMM_OFFLOAD_TARGET))
+#endif
+#include <stdio.h>
+#if defined(LIBXSMM_OFFLOAD_TARGET)
+# pragma offload_attribute(pop)
+#endif
+
 
 LIBXSMM_API libxsmm_gemm_descriptor* libxsmm_dgemm_descriptor_init(libxsmm_descriptor_blob* blob,
   libxsmm_blasint m, libxsmm_blasint n, libxsmm_blasint k, libxsmm_blasint lda, libxsmm_blasint ldb, libxsmm_blasint ldc,
