377d85e12b113538e7c9838a160a3e0c51eb7752
Alexander Heinecke <alexander.heinecke@intel.com>
2018-09-10 19:31:19 -0700

    updated run script for pooling

--- samples/deeplearning/poolingdriver/run_resnet50.sh
+++ samples/deeplearning/poolingdriver/run_resnet50.sh
@@ -85,7 +85,7 @@ fi
 # ./layer_example_${BIN} iters inpWidth inpHeight nImg nIfm nOfm kw kh padw padh padw_in padh_in padw_out padw_in stride type pass
 
 #max pooling
-${NUMACTL} ./layer_example_${BIN} ${ITERS}  112  112  ${MB}  64 3 3 0 0 0 0 0 0 2 ${TYPE} ${PASS}
+${NUMACTL} ./layer_example_${BIN} ${ITERS}  112  112  ${MB}  64 3 3 1 1 0 0 0 0 2 ${TYPE} ${PASS}
 
 #avg pooling
 ${NUMACTL} ./layer_example_${BIN} ${ITERS}  7  7  ${MB}  2048 7 7 0 0 0 0 0 0 1 ${TYPE} ${PASS}
