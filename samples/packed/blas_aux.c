01301e283cf9872127a330d1c5b67f300c53ea3b
Hans Pabst <hans.pabst@intel.com>
2018-09-21 20:33:26 +0200

    Attempt to fix duplicate symbol error(s).

--- samples/packed/blas_aux.c
+++ samples/packed/blas_aux.c
@@ -1,20 +1,24 @@
+/* Optionally link-in the BLAS routines lsame_() and xerbla_() */
+#if defined(__MKL) || defined(__OPENBLAS)
+
 #include <stdio.h>
 
-/* Link in if we don't have the BLAS routines lsame_() and xerbla_() */
-int lsame_ ( char *ca, char *cb )
+int lsame_(const char* ca, const char* cb)
 {
-   if ( *ca == *cb ) return 1;
-   if ( (*cb >= 'a') && (*cb <= 'z') )
-   {
-      if ( *ca == *cb + 32 ) return 1;
-   } else if ( (*cb >= 'A') && (*cb <= 'Z') ) {
-      if ( *ca == *cb - 32 ) return 1;
-   }
-   return 0;
+  if ( *ca == *cb ) return 1;
+  if ( (*cb >= 'a') && (*cb <= 'z') ) {
+    if ( *ca == *cb + 32 ) return 1;
+  }
+  else if ( (*cb >= 'A') && (*cb <= 'Z') ) {
+    if ( *ca == *cb - 32 ) return 1;
+  }
+  return 0;
 }
 
-void xerbla_ ( char *c, int *info )
+void xerbla_(const char* c, const int* info)
 {
-   printf(" ** On entry to %s parameter number %02d had an illegal value\n",c,*info);
+  printf(" ** On entry to %s parameter number %02d had an illegal value\n", c, *info);
 }
+
+#endif /*defined(__MKL) || defined(__OPENBLAS)*/
 
