b5551d82f7bb8d07d7aa372e9aa64d8e187412d7
Hans Pabst <hans.pabst@intel.com>
2018-11-06 15:45:51 +0100

    Moderately touched up scripts (https://www.shellcheck.net/).

--- .state.sh
+++ .state.sh
@@ -33,10 +33,10 @@
 HERE=$(cd $(dirname $0); pwd -P)
 NAME=$(basename $0)
 
-TOUCH=$(which touch)
-ECHO=$(which echo)
-SED=$(which sed)
-TR=$(which tr)
+TOUCH=$(command -v touch)
+ECHO=$(command -v echo)
+SED=$(command -v sed)
+TR=$(command -v tr)
 
 DEST=$1
 if [ "$1" = "" ]; then
