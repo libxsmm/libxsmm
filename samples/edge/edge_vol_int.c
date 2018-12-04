8703ba0070656a4d18d8e0317a86c59d98a25ac7
Alexander Heinecke <alexander.heinecke@intel.com>
2018-12-04 14:14:26 -0800

    avoid false sharing in timing

--- samples/edge/edge_vol_int.c
+++ samples/edge/edge_vol_int.c
@@ -97,6 +97,7 @@ static double sec(struct timeval start, struct timeval end) {
   return ((double)(((end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec)))) / 1.0e6;
 }
 
+#if defined(__AVX512F__)
 static void matMulFusedAC(       unsigned short  i_r,
                                  unsigned int    i_m,
                                  unsigned int    i_n,
@@ -157,24 +158,24 @@ static void matMulFusedBC(        unsigned short  i_r,
     }
   }
 }
-
+#endif
 
 void amok_detect( const double* i_runtimes, size_t* io_amoks, const size_t i_workers ) {
   double time_avg;
   size_t i;
   time_avg = 0.0;
   for (i = 0; i < i_workers; i++) {
-    if ( io_amoks[i] == 0 ) {
-      time_avg += i_runtimes[i];
+    if ( io_amoks[8*i] == 0 ) {
+      time_avg += i_runtimes[8*i];
     }
   }
-  time_avg = time_avg/((double)(i_workers-io_amoks[i_workers]));
+  time_avg = time_avg/((double)(i_workers-io_amoks[8*i_workers]));
   /* let detect amoks */
   for (i = 0; i < i_workers; i++) {
-    if ( io_amoks[i] == 0 ) {
-      if ( i_runtimes[i] > time_avg*1.07 ) { /* this is the amok condition */
-        io_amoks[i_workers]++;
-        io_amoks[i] = 1;
+    if ( io_amoks[8*i] == 0 ) {
+      if ( i_runtimes[8*i] > time_avg*1.07 ) { /* this is the amok condition */
+        io_amoks[8*i_workers]++;
+        io_amoks[8*i] = 1;
       }
     }
   }
@@ -182,18 +183,18 @@ void amok_detect( const double* i_runtimes, size_t* io_amoks, const size_t i_wor
 
 void amok_balance( const size_t* i_amoks, const size_t i_workers, const size_t i_worksize, const size_t i_mytid, size_t* io_chunk, size_t* io_mystart, size_t* io_myend ) {
   size_t l_chunk, l_start, l_end;
-  size_t l_cur_amoks = i_amoks[i_workers];
+  size_t l_cur_amoks = i_amoks[8*i_workers];
   size_t l_non_amoks = i_workers - l_cur_amoks;
 
   l_chunk = (i_worksize % l_non_amoks == 0) ? (i_worksize / l_non_amoks) : ((i_worksize / l_non_amoks) + 1);
-  if (i_amoks[i_mytid] != 0) {
+  if (i_amoks[8*i_mytid] != 0) {
     l_start = 0;
     l_end = 0;
   } else {
     size_t l_tid_offset = 0;
     size_t l_z;
     for ( l_z = 0; l_z < i_mytid; l_z++) {
-      if ( i_amoks[l_z] != 0 ) {
+      if ( i_amoks[8*l_z] != 0 ) {
         l_tid_offset++;
       }
     }
@@ -297,10 +298,10 @@ int main(int argc, char* argv[])
 #else
   l_num_threads = 1;
 #endif
-  l_total_thread = (double*)malloc(l_num_threads*sizeof(double));
-  l_cur_thread_time = (double*)malloc(l_num_threads*sizeof(double));
-  amoks = (size_t*)malloc((l_num_threads+1)*sizeof(size_t));
-  for ( i = 0; i < l_num_threads+1; i++ ) {
+  l_total_thread = (double*)malloc(8*l_num_threads*sizeof(double));
+  l_cur_thread_time = (double*)malloc(8*l_num_threads*sizeof(double));
+  amoks = (size_t*)malloc(8*(l_num_threads+1)*sizeof(size_t));
+  for ( i = 0; i < 8*(l_num_threads+1); i++ ) {
     amoks[i] = 0;
   }
 
@@ -424,8 +425,8 @@ int main(int argc, char* argv[])
     for (i = 0; i < (int)num_reps; i++) {
 #if defined(HANDLE_AMOK)
       /* did we had an amok? */
-      if (cur_amoks != amoks[l_num_threads]) {
-        cur_amoks = amoks[l_num_threads];
+      if (cur_amoks != amoks[8*l_num_threads]) {
+        cur_amoks = amoks[8*l_num_threads];
         non_amoks = l_num_threads - cur_amoks;
         /* re-balance work */
         amok_balance( amoks, l_num_threads, num_elems, mytid, &l_el_chunk, &l_el_start, &l_el_end );
@@ -455,8 +456,8 @@ int main(int argc, char* argv[])
 #endif
       }
       gettimeofday(&myend, NULL);
-      l_cur_thread_time[mytid] = sec( mystart, myend );
-      l_total_thread[mytid] += sec( mystart, myend );
+      l_cur_thread_time[8*mytid] = sec( mystart, myend );
+      l_total_thread[8*mytid] += sec( mystart, myend );
 #if defined(_OPENMP)
       #pragma omp barrier
 #endif
@@ -480,21 +481,21 @@ int main(int argc, char* argv[])
   time_min = 80000000;
   time_avg = 0.0;
   for (i = 0; i < (int)l_num_threads; i++) {
-    if( amoks[i] == 0 ) { 
-      if( l_total_thread[i] > time_max) time_max = l_total_thread[i];
-      if( l_total_thread[i] < time_min) time_min = l_total_thread[i];
-      time_avg += l_total_thread[i];
+    if( amoks[8*i] == 0 ) { 
+      if( l_total_thread[8*i] > time_max) time_max = l_total_thread[8*i];
+      if( l_total_thread[8*i] < time_min) time_min = l_total_thread[8*i];
+      time_avg += l_total_thread[8*i];
     }
   }
-  time_avg = time_avg/((double)(l_num_threads-amoks[l_num_threads])); 
+  time_avg = time_avg/((double)(l_num_threads-amoks[8*l_num_threads])); 
 
   flops_vol  = (double)num_quants * (double)mat_a_nnz * (double)num_cfr * 2.0;
   flops_vol += (double)num_quants * (double)mat_b_nnz * (double)num_cfr * 2.0;
   flops_vol += (double)num_quants * (double)mat_c_nnz * (double)num_cfr * 2.0;
   flops_vol += (double)num_modes * (double)mat_st_nnz * (double)num_cfr * 6.0; /* 3 star matrix mul */
-  printf("%fs time for vol (asm), min %f, max %f, avg %f, #amoks %ld, amok-threads ", l_total, time_min, time_max, time_avg, amoks[l_num_threads]);
+  printf("%fs time for vol (asm), min %f, max %f, avg %f, #amoks %ld, amok-threads ", l_total, time_min, time_max, time_avg, amoks[8*l_num_threads]);
   for ( i = 0; i < l_num_threads; i++ ) {
-    if ( amoks[i] != 0 ) {
+    if ( amoks[8*i] != 0 ) {
       printf("%i,", i);
     }
   }
