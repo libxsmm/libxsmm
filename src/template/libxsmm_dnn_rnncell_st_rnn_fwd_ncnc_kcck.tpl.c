f814956c289b56ac774e975829b449480a6a21d6
egeorgan <evangelos.georganas@intel.com>
2018-12-07 19:04:39 -0800

    Cleaned up the fowd rnn cell implementation (now has a 2d decomp and a generic decomp)

--- src/template/libxsmm_dnn_rnncell_st_rnn_fwd_ncnc_kcck.tpl.c
+++ src/template/libxsmm_dnn_rnncell_st_rnn_fwd_ncnc_kcck.tpl.c
@@ -56,13 +56,7 @@ LIBXSMM_VLA_DECL(4, element_filter_type, w, wD, cBlocks, bc, bk);
 LIBXSMM_VLA_DECL(4, element_filter_type, r, rD, kBlocks, bk, bk);
 LIBXSMM_VLA_DECL(5, element_output_type, h, ht, nBlocks, kBlocks, bn, bk);
 LIBXSMM_VLA_DECL(5, element_output_type, z, zt, nBlocks, kBlocks, bn, bk);
-int prefetch_mode = atoi(getenv("PF")); //LIBXSMM_GEMM_PREFETCH_AL1_BL1;
-if (prefetch_mode) {
-  prefetch_mode = LIBXSMM_GEMM_PREFETCH_AL1_BL1;
-} else {
-  prefetch_mode = 0;
-}
-
+int prefetch_mode = LIBXSMM_GEMM_PREFETCH_NONE; //LIBXSMM_GEMM_PREFETCH_AL1_BL1;
 /* define gemm kernels */
 libxsmm_smmfunction_reducebatch batchreduce_kernela =  libxsmm_smmdispatch_reducebatch( bk, bn, bc, &bk, &bk, &bk, NULL, NULL, &prefetch_mode );
 libxsmm_smmfunction_reducebatch batchreduce_kernelb =  libxsmm_smmdispatch_reducebatch( bk, bn, bk, &bk, &bk, &bk, NULL, NULL, &prefetch_mode );
@@ -74,32 +68,35 @@ const libxsmm_blasint work = (N/bn) * (K/bk);
 /* compute chunk size */
 const libxsmm_blasint chunksize = (work % (libxsmm_blasint)handle->desc.threads == 0) ? (work / (libxsmm_blasint)handle->desc.threads) : ((work / (libxsmm_blasint)handle->desc.threads) + 1);
 /* compute thr_begin and thr_end */
-const libxsmm_blasint thr_begin = (ltid * chunksize < work) ? (ltid * chunksize) : work;
-const libxsmm_blasint thr_end = ((ltid + 1) * chunksize < work) ? ((ltid + 1) * chunksize) : work;
+libxsmm_blasint thr_begin = (ltid * chunksize < work) ? (ltid * chunksize) : work;
+libxsmm_blasint thr_end = ((ltid + 1) * chunksize < work) ? ((ltid + 1) * chunksize) : work;
 
-const int row_teams = atoi(getenv("R"));
-const int column_teams = atoi(getenv("C"));
-const int my_col_id = ltid % column_teams;
-const int my_row_id = ltid / column_teams;
-const int in_tasks = N/bn;
-const int ik_tasks = K/bk;
-const int in_tasks_per_thread = in_tasks/row_teams;
-const int ik_tasks_per_thread = ik_tasks/column_teams;
-
+/* The snippet below does a 2D domain decomposition of output IF the number of threads and the number of work items are compbatible */
+/* TODO: For now 2D decomposition targets single socket SKX */
+int row_teams = 7;
+int column_teams = 4;
+int my_col_id = ltid % column_teams;
+int my_row_id = ltid / column_teams;
+int in_tasks = N/bn;
+int ik_tasks = K/bk;
+int in_tasks_per_thread = in_tasks/row_teams;
+int ik_tasks_per_thread = ik_tasks/column_teams;
 int my_in_start = my_row_id * in_tasks_per_thread;
 int my_in_end = (my_row_id+1) * in_tasks_per_thread;
 int my_ik_start = my_col_id * ik_tasks_per_thread;
 int my_ik_end = (my_col_id+1) * ik_tasks_per_thread;
+int perform_2d_decomp = (in_tasks % row_teams == 0 && ik_tasks % column_teams == 0 && row_teams*column_teams == handle->desc.threads) ? 1 : 0;
 
-if (in_tasks % row_teams == 0) {
-  /* Auxuliary arrays for batch-reduce gemms  */
+if (perform_2d_decomp) {
+  /* Auxuliary arrays for batch-reduce gemms and potential prefetch */
   const element_input_type *A_array[ik_tasks_per_thread][in_tasks_per_thread][cBlocks];
   const element_input_type *B_array[ik_tasks_per_thread][in_tasks_per_thread][cBlocks];
   const element_input_type *A_array2[ik_tasks_per_thread][in_tasks_per_thread][kBlocks];
   const element_input_type *B_array2[ik_tasks_per_thread][in_tasks_per_thread][kBlocks];
-
-  const int pfa =  atoi(getenv("PFA"));
-  const int pfb =  atoi(getenv("PFB"));
+  const element_input_type *A_array_pf[ik_tasks_per_thread][in_tasks_per_thread][cBlocks];
+  const element_input_type *B_array_pf[ik_tasks_per_thread][in_tasks_per_thread][cBlocks];
+  const element_input_type *A_array2_pf[ik_tasks_per_thread][in_tasks_per_thread][kBlocks];
+  const element_input_type *B_array2_pf[ik_tasks_per_thread][in_tasks_per_thread][kBlocks];
   int ii, jj;
 
   /* lazy barrier init */
@@ -107,7 +104,7 @@ if (in_tasks % row_teams == 0) {
 
   /* All data is in column-major format */
   for (i = 0; i < t; ++i) {
-    /* Prepare arrays for the calls */
+    /* Prepare arrays for the batch-reduce calls */
     for (ik = my_ik_start, ii = 0; ik < my_ik_end; ++ik, ii++ ) {
       for (in = my_in_start, jj = 0; in < my_in_end; ++in, jj++ ) {
         /* Prepare arrays for the call */
@@ -133,17 +130,43 @@ if (in_tasks % row_teams == 0) {
       }
     }
 
+    if (prefetch_mode != LIBXSMM_GEMM_PREFETCH_NONE) {
+      /* Prepare addition prefetch arrays that are shifted images of regular ones when external prefetching is requested  */
+      int pf_dist_A = 2;
+      int pf_dist_B = 4;
+      int total_blocks = in_tasks_per_thread*ik_tasks_per_thread*cBlocks;
+      element_input_type *src_ptr = (element_input_type*) &A_array[0][0][0];
+      element_input_type *dst_ptr = (element_input_type*) &A_array_pf[0][0][0];
+      for (ii = 0 ; ii < total_blocks - pf_dist_A; ii++) {
+        dst_ptr[ii] = src_ptr[ii+pf_dist_A];
+      }
+      src_ptr = (element_input_type*) &B_array[0][0][0];
+      dst_ptr = (element_input_type*) &B_array_pf[0][0][0];
+      for (ii = 0 ; ii < total_blocks - pf_dist_B; ii++) {
+        dst_ptr[ii] = src_ptr[ii+pf_dist_B];
+      }
+      total_blocks = in_tasks_per_thread*ik_tasks_per_thread*kBlocks;
+      src_ptr = (element_input_type*) &A_array2[0][0][0];
+      dst_ptr = (element_input_type*) &A_array2_pf[0][0][0];
+      for (ii = 0 ; ii < total_blocks - pf_dist_A; ii++) {
+        dst_ptr[ii] = src_ptr[ii+pf_dist_A];
+      }
+      src_ptr = (element_input_type*) &B_array2[0][0][0];
+      dst_ptr = (element_input_type*) &B_array2_pf[0][0][0];
+      for (ii = 0 ; ii < total_blocks - pf_dist_B; ii++) {
+        dst_ptr[ii] = src_ptr[ii+pf_dist_B];
+      }
+    }
+
     /* let's run the cell in blocks for good locality */
     for (ik = my_ik_start, ii = 0; ik < my_ik_end; ++ik, ii++ ) {
       for (in = my_in_start, jj = 0; in < my_in_end; ++in, jj++ ) {
         /* z = per_col(b) */
         libxsmm_internal_matrix_bcst_colvector_ld( bk, bn, bk, &LIBXSMM_VLA_ACCESS(5, z, i, in, ik, 0, 0, nBlocks, kBlocks, bn, bk), &b[ik*bk]);
         /* z += W.x */
-        /* Reduce batch gemm call  */
-        batchreduce_kernela(&A_array[ii][jj][0], &B_array[ii][jj][0], &LIBXSMM_VLA_ACCESS(5, z, i, in, ik, 0, 0, nBlocks, kBlocks, bn, bk), &cBlocks, &A_array[ii][jj][pfa], &B_array[ii][jj][pfb]);
+        batchreduce_kernela(&A_array[ii][jj][0], &B_array[ii][jj][0], &LIBXSMM_VLA_ACCESS(5, z, i, in, ik, 0, 0, nBlocks, kBlocks, bn, bk), &cBlocks, &A_array_pf[ii][jj][0], &B_array_pf[ii][jj][0]);
         /* z += U.h */
-        /* Reduce batch gemm call  */
-        batchreduce_kernelb(&A_array2[ii][jj][0], &B_array2[ii][jj][0], &LIBXSMM_VLA_ACCESS(5, z, i, in, ik, 0, 0, nBlocks, kBlocks, bn, bk), &kBlocks, &A_array2[ii][jj][pfa], &B_array2[ii][jj][pfb]);
+        batchreduce_kernelb(&A_array2[ii][jj][0], &B_array2[ii][jj][0], &LIBXSMM_VLA_ACCESS(5, z, i, in, ik, 0, 0, nBlocks, kBlocks, bn, bk), &kBlocks, &A_array2_pf[ii][jj][0], &B_array2_pf[ii][jj][0]);
 
 #if defined(LIBXSMM_DNN_RNN_RELU_FWD)
         libxsmm_internal_matrix_relu_ld(    bk, bn, bk, &LIBXSMM_VLA_ACCESS(5, z, i, in, ik, 0, 0, nBlocks, kBlocks, bn, bk), &LIBXSMM_VLA_ACCESS(5, h, i, in, ik, 0, 0, nBlocks, kBlocks, bn, bk));
@@ -159,7 +182,6 @@ if (in_tasks % row_teams == 0) {
     libxsmm_barrier_wait(handle->barrier, (int)ltid);
   }
 } else {
-
   /* Auxuliary arrays for batch-reduce gemms  */
   const element_input_type *A_array[cBlocks];
   const element_input_type *B_array[cBlocks];
@@ -218,7 +240,6 @@ if (in_tasks % row_teams == 0) {
       libxsmm_internal_matrix_tanh_ld(    bk, bn, bk, &LIBXSMM_VLA_ACCESS(5, z, i, in, ik, 0, 0, nBlocks, kBlocks, bn, bk), &LIBXSMM_VLA_ACCESS(5, h, i, in, ik, 0, 0, nBlocks, kBlocks, bn, bk));
 #endif
     }
-
     libxsmm_barrier_wait(handle->barrier, (int)ltid);
   }
 }
