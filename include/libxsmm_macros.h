c1f03425a317254038dd58cd48f9b542ca017805
Hans Pabst <hans.pabst@intel.com>
2019-02-15 14:23:07 +0100

    Attempt to fix issue with GLIBC 2.27.

--- include/libxsmm_macros.h
+++ include/libxsmm_macros.h
@@ -649,6 +649,35 @@
       &&    (0 != ((LIBXSMM_INTEL_COMPILER) + (LIBXSMM_INTEL_COMPILER_UPDATE)))))))
 # define _Float128 __float128
 #endif
+#if !defined(_Float128X) && defined(__GNUC__) && !defined(__cplusplus) \
+  && (LIBXSMM_VERSION3(7, 0, 0) > LIBXSMM_VERSION3(__GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__) \
+  || (defined(LIBXSMM_INTEL_COMPILER) && defined(LIBXSMM_INTEL_COMPILER_UPDATE) && ( \
+        ((1800 <= ((LIBXSMM_INTEL_COMPILER) + (LIBXSMM_INTEL_COMPILER_UPDATE))) \
+      && (1801  > ((LIBXSMM_INTEL_COMPILER) + (LIBXSMM_INTEL_COMPILER_UPDATE)))) || \
+        ((1706  > ((LIBXSMM_INTEL_COMPILER) + (LIBXSMM_INTEL_COMPILER_UPDATE))) \
+      &&    (0 != ((LIBXSMM_INTEL_COMPILER) + (LIBXSMM_INTEL_COMPILER_UPDATE)))))))
+# define _Float128X __float128
+#endif
+#if !defined(_Float32) && defined(__GNUC__) && !defined(__cplusplus) \
+  && (LIBXSMM_VERSION3(7, 0, 0) > LIBXSMM_VERSION3(__GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__) \
+  || defined(LIBXSMM_INTEL_COMPILER)) /* TODO */
+# define _Float32 float
+#endif
+#if !defined(_Float32X) && defined(__GNUC__) && !defined(__cplusplus) \
+  && (LIBXSMM_VERSION3(7, 0, 0) > LIBXSMM_VERSION3(__GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__) \
+  || defined(LIBXSMM_INTEL_COMPILER)) /* TODO */
+# define _Float32X float
+#endif
+#if !defined(_Float64) && defined(__GNUC__) && !defined(__cplusplus) \
+  && (LIBXSMM_VERSION3(7, 0, 0) > LIBXSMM_VERSION3(__GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__) \
+  || defined(LIBXSMM_INTEL_COMPILER)) /* TODO */
+# define _Float32 double
+#endif
+#if !defined(_Float64X) && defined(__GNUC__) && !defined(__cplusplus) \
+  && (LIBXSMM_VERSION3(7, 0, 0) > LIBXSMM_VERSION3(__GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__) \
+  || defined(LIBXSMM_INTEL_COMPILER)) /* TODO */
+# define _Float64X double
+#endif
 
 #if defined(LIBXSMM_OFFLOAD_TARGET)
 # pragma offload_attribute(push,target(LIBXSMM_OFFLOAD_TARGET))
