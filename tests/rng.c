ed1c8d06fa4c47aa8f3ac58ac87cae792371eeb5
Hans Pabst <hans.pabst@intel.com>
2019-02-20 08:40:07 +0100

    Extended RNG-test.

--- tests/rng.c
+++ tests/rng.c
@@ -31,7 +31,7 @@
 #include <libxsmm.h>
 #include <math.h>
 
-#if !defined(USE_EXPECTED)
+#if !defined(USE_EXPECTED) && 0
 # define USE_EXPECTED
 #else
 # include <time.h>
@@ -46,8 +46,6 @@ int main(/*int argc, char* argv[]*/)
     0.438140392f, 0.284636021f, 0.808342457f, 0.140940785f, 0.740890265f, 0.0189954042f, 0.4811354880f, 0.616942167f,
     0.273835897f, 0.636928558f, 0.916998625f, 0.260923862f, 0.673431635f, 0.5160189870f, 0.0404732227f, 0.327739120f
   };
-#else
-  const unsigned int seed = (unsigned int)time(0);
 #endif
   libxsmm_blasint num_rngs = 1000, i;
   libxsmm_matdiff_info info;
@@ -59,12 +57,13 @@ int main(/*int argc, char* argv[]*/)
   /* mute warning about potentially uninitialized variable */
   libxsmm_matdiff_clear(&info);
 
+#if defined(USE_EXPECTED)
   /* setup reproducible sequence */
   libxsmm_rng_set_seed(seed);
 
   /* fill array with random floats */
   libxsmm_rng_f32_seq(rngs, num_rngs);
-#if defined(USE_EXPECTED)
+
   /* check expected value (depends on reproducible seed) */
   for (i = 0; i < 16; ++i) {
     if (rngs_expected[i] != rngs[i]) result = EXIT_FAILURE;
@@ -78,37 +77,50 @@ int main(/*int argc, char* argv[]*/)
   for (i = 0; i < 16; ++i) {
     if (rngs_expected[i] != rngs[i]) result = EXIT_FAILURE;
   }
-#endif
-  if (EXIT_SUCCESS == result) { /* calculate quality of random numbers */
-    result = libxsmm_matdiff(&info, LIBXSMM_DATATYPE_F32, 1/*m*/, num_rngs,
-      NULL/*ref*/, rngs/*tst*/, NULL/*ldref*/, NULL/*ldtst*/);
-  }
-
-  if (EXIT_SUCCESS == result) {
-    const double expected = 0.5 * num_rngs;
-    if (expected < 5 * LIBXSMM_DIFF(info.l1_tst, expected)) result = EXIT_FAILURE;
-  }
-
-  if (EXIT_SUCCESS == result) {
-    const double expected = 1.0 / 12.0;
-    if (expected < 5 * LIBXSMM_DIFF(info.var_tst, expected)) result = EXIT_FAILURE;
-  }
-
-  if (EXIT_SUCCESS == result) {
-    libxsmm_blasint num_odd = 0, num_even = 0;
-    const double scale = 0xFFFFFFFF;
-    for (i = 0; i < num_rngs; ++i) {
-      const unsigned int u = (unsigned int)LIBXSMM_ROUND(rngs[i] * scale);
-      if (u & 1) {
-        ++num_odd;
-      }
-      else {
-        ++num_even;
-      }
+#else
+  { int j; for (j = 0; j < 100; ++j) {
+    /* setup sequence */
+    libxsmm_rng_set_seed((unsigned int)time(0));
+    /* fill array with random floats */
+    switch (j % 2) {
+      case 1: {
+        for (i = 0; i < num_rngs; ++i) rngs[i] = (float)libxsmm_rng_f64();
+      } break;
+      default: libxsmm_rng_f32_seq(rngs, num_rngs);
     }
-    if (num_rngs < 4 * LIBXSMM_DIFF(num_odd, num_even)) result = EXIT_FAILURE;
-  }
+#endif
+    if (EXIT_SUCCESS == result) { /* calculate quality of random numbers */
+      result = libxsmm_matdiff(&info, LIBXSMM_DATATYPE_F32, 1/*m*/, num_rngs,
+        NULL/*ref*/, rngs/*tst*/, NULL/*ldref*/, NULL/*ldtst*/);
+    }
 
+    if (EXIT_SUCCESS == result) {
+      const double expected = 0.5 * num_rngs;
+      if (expected < 5 * LIBXSMM_DIFF(info.l1_tst, expected)) result = EXIT_FAILURE;
+    }
+
+    if (EXIT_SUCCESS == result) {
+      const double expected = 1.0 / 12.0;
+      if (expected < 5 * LIBXSMM_DIFF(info.var_tst, expected)) result = EXIT_FAILURE;
+    }
+
+    if (EXIT_SUCCESS == result) {
+      libxsmm_blasint num_odd = 0, num_even = 0;
+      const double scale = 0xFFFFFFFF;
+      for (i = 0; i < num_rngs; ++i) {
+        const unsigned int u = (unsigned int)LIBXSMM_ROUND(rngs[i] * scale);
+        if (u & 1) {
+          ++num_odd;
+        }
+        else {
+          ++num_even;
+        }
+      }
+      if (num_rngs < 4 * LIBXSMM_DIFF(num_odd, num_even)) result = EXIT_FAILURE;
+    }
+#if !defined(USE_EXPECTED)
+  }}
+#endif
   free(rngs);
 
   return result;
