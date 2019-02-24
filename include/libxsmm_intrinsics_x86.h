c25d1848732f2f8f3387db6835ba35f8bc367de2
Hans Pabst <hans.pabst@intel.com>
2019-02-24 19:11:23 +0100

    Make no attempt to edge-out older compilers (LIBXSMM_INTRINSICS_AVX512_NOREDUCTIONS).

--- include/libxsmm_intrinsics_x86.h
+++ include/libxsmm_intrinsics_x86.h
@@ -137,19 +137,6 @@
 #     define LIBXSMM_INTRINSICS(TARGET)/*no need for target flags*/
 #     define LIBXSMM_INTRINSICS_INCLUDE
 #     include <immintrin.h>
-#   elif (defined(__GNUC__) && LIBXSMM_VERSION3(5, 1, 0) <= LIBXSMM_VERSION3(__GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__)) \
-      && !defined(__PGI)
-      /* AVX-512 pseudo intrinsics are missing e.g., reductions */
-#     if !defined(LIBXSMM_INTRINSICS_AVX512_NOREDUCTIONS)
-#       define LIBXSMM_INTRINSICS_AVX512_NOREDUCTIONS
-#     endif
-#     if !defined(__CYGWIN__)
-#       define LIBXSMM_MAX_STATIC_TARGET_ARCH LIBXSMM_X86_AVX512_CORE
-#     else /* Error: invalid register for .seh_savexmm */
-#       define LIBXSMM_MAX_STATIC_TARGET_ARCH LIBXSMM_X86_AVX2
-#     endif
-#     define LIBXSMM_INTRINSICS_INCLUDE
-#     include <immintrin.h>
 #   elif (defined(__GNUC__) && LIBXSMM_VERSION3(4, 9, 0) <= LIBXSMM_VERSION3(__GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__)) \
       && !defined(__PGI)
       /* too many AVX-512 (pseudo-)intrinsics are missing e.g., reductions, or casts (_mm512_castps_si512) */
