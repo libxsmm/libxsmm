570b6b0fba7e0ae2a68f9e75691e70c99094a102
Hans Pabst <hans.pabst@intel.com>
2019-02-26 08:49:11 +0100

    Adjusted searching for dependencies (Makefile). Included Makefile (VC projects). Code cleanup.

--- samples/magazine/magazine_blas.c
+++ samples/magazine/magazine_blas.c
@@ -55,7 +55,7 @@ void sgemm_(const char*, const char*, const int*, const int*, const int*,
 # define CONCATENATE(A, B) CONCATENATE_AUX(A, B)
 # define GEMM CONCATENATE(GEMM_, TYPE)
 #endif
-#if !defined(NOFALLBACK)
+#if !defined(NOFALLBACK) && 0
 # define NOFALLBACK
 #endif
 
@@ -139,7 +139,7 @@ int main(int argc, char* argv[])
 # elif defined(__MKL) || defined(MKL_DIRECT_CALL_SEQ) || defined(MKL_DIRECT_CALL)
     duration = dsecnd() - duration;
 # endif
-}
+  }
   else
 # if defined(NOFALLBACK)
   if (0/*false*/)
