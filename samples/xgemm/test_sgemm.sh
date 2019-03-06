a10287934264f1b7101c55ecc8f674527b613880
Alexander Heinecke <alexander.heinecke@intel.com>
2019-03-05 20:42:09 -0800

    added test scripts for 100x100x100 GEMM kernel sweep

---
+++ samples/xgemm/test_sgemm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ITERS=10
+
+for m in `seq 1 1 100`
+do
+  for n in `seq 1 1 100`
+  do
+    for k in `seq 1 1 100`
+    do
+      taskset -c 4 ./kernel $m $n $k 100 100 100 1 1 0 0 0 0 nopf SP $ITERS
+      taskset -c 4 ./kernel $m $n $k 100 100 100 1 1 0 0 0 1 nopf SP $ITERS
+      taskset -c 4 ./kernel $m $n $k $m  $k  $m  1 1 0 0 0 0 nopf SP $ITERS
+      taskset -c 4 ./kernel $m $n $k $m  $n  $m  1 1 0 0 0 1 nopf SP $ITERS
+    done
+  done
+done
