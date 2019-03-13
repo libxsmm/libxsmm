d83f309987ff5e64b417139f17554790c9a94cca
Hans Pabst <hans.pabst@intel.com>
2019-03-13 16:49:13 +0100

    Updated run-wrapper script.

--- samples/deeplearning/grudriver/grudriver.sh
+++ samples/deeplearning/grudriver/grudriver.sh
@@ -94,35 +94,35 @@ export OMP_NUM_THREADS=56
 export KMP_AFFINITY=granularity=fine,compact,1,0
 
 
-echo "LSTM FWD for inference only"
-CHECK=${CHKVAL} ./lstmdriver  ${ITERS}  0  4096  1024  2048    8  1
+echo "GRU FWD for inference only"
+CHECK=${CHKVAL} ./grudriver  ${ITERS}  0  4096  1024  2048    8  1
 wait
-CHECK=${CHKVAL} ./lstmdriver  ${ITERS}  0  3072    32  1024  512  1
+CHECK=${CHKVAL} ./grudriver  ${ITERS}  0  3072    32  1024  512  1
 wait
-CHECK=${CHKVAL} ./lstmdriver  ${ITERS}  0  4096  1024  1024    8  1
+CHECK=${CHKVAL} ./grudriver  ${ITERS}  0  4096  1024  1024    8  1
 wait
-CHECK=${CHKVAL} ./lstmdriver  ${ITERS}  0  1024    32  1024  512  1
-wait
-
-echo "LSTM FWD for training"
-CHECK=${CHKVAL} ./lstmdriver  ${ITERS}  0  4096  1024  2048    8  0
-wait
-CHECK=${CHKVAL} ./lstmdriver  ${ITERS}  0  3072    32  1024  512  0
-wait
-CHECK=${CHKVAL} ./lstmdriver  ${ITERS}  0  4096  1024  1024    8  0
-wait
-CHECK=${CHKVAL} ./lstmdriver  ${ITERS}  0  1024    32  1024  512  0
+CHECK=${CHKVAL} ./grudriver  ${ITERS}  0  1024    32  1024  512  1
 wait
 
-echo "LSTM BWD+UPD for training"
-CHECK=${CHKVAL} ./lstmdriver  ${ITERS}  3  4096  1024  2048    8  0
+echo "GRU FWD for training"
+CHECK=${CHKVAL} ./grudriver  ${ITERS}  0  4096  1024  2048    8  0
 wait
-CHECK=${CHKVAL} ./lstmdriver  ${ITERS}  3  3072    32  1024  512  0
+CHECK=${CHKVAL} ./grudriver  ${ITERS}  0  3072    32  1024  512  0
 wait
-CHECK=${CHKVAL} ./lstmdriver  ${ITERS}  3  4096  1024  1024    8  0
+CHECK=${CHKVAL} ./grudriver  ${ITERS}  0  4096  1024  1024    8  0
 wait
-CHECK=${CHKVAL} ./lstmdriver  ${ITERS}  3  1024    32  1024  512  0
+CHECK=${CHKVAL} ./grudriver  ${ITERS}  0  1024    32  1024  512  0
 wait
 
-echo "LSTM performance done"
+echo "GRU BWD+UPD for training"
+CHECK=${CHKVAL} ./grudriver  ${ITERS}  3  4096  1024  2048    8  0
+wait
+CHECK=${CHKVAL} ./grudriver  ${ITERS}  3  3072    32  1024  512  0
+wait
+CHECK=${CHKVAL} ./grudriver  ${ITERS}  3  4096  1024  1024    8  0
+wait
+CHECK=${CHKVAL} ./grudriver  ${ITERS}  3  1024    32  1024  512  0
+wait
+
+echo "GRU performance done"
 echo ""
