f82f8b724863e59a7a1a16b24d86225116c78438
Hans Pabst <hans.pabst@intel.com>
2019-03-24 14:29:57 +0100

    Fixed a typo (code sample).  [skip ci]

--- samples/utilities/dispatch/dispatch.c
+++ samples/utilities/dispatch/dispatch.c
@@ -46,7 +46,7 @@
 # pragma offload_attribute(pop)
 #endif
 
-#if !defined(MKLJIT) && defined(mkl_jit_create_dgemm) && 0
+#if !defined(MKLJIT) && defined(mkl_jit_create_dgemm)
 # define MKLJIT
 #endif
 #if !defined(INTEL_MKL_VERSION) || (20190003 <= INTEL_MKL_VERSION)
