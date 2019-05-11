6059bf11b06fc528d5c23b89b64b2b27b89c76de
Alexander Heinecke <alexander.heinecke@intel.com>
2019-05-11 10:36:17 -0700

    fixed C89 conformence

--- src/template/libxsmm_dnn_tensor_buffer_copy_in_nchw.tpl.c
+++ src/template/libxsmm_dnn_tensor_buffer_copy_in_nchw.tpl.c
@@ -29,10 +29,8 @@
 /* Alexander Heinecke, Evangelos Georganas, Hans Pabst (Intel Corp.)
 ******************************************************************************/
 
-/* use for-loops to potentially leverage NUMA in the future */
-int i1, i2, i3, i4, i5, i6, lpb, bfm, W, H, fmb, N;
-
 if (tensor->layout->custom_format == LIBXSMM_DNN_TENSOR_FORMAT_LIBXSMM_1) {
+  int i1, i2, i3, i4, i5, i6, lpb, bfm, W, H, fmb, N, C;
   /* low precision formatting */
   if ( tensor->layout->num_dims == 6 ) {
     lpb = tensor->layout->dim_size[0];
@@ -49,21 +47,22 @@ if (tensor->layout->custom_format == LIBXSMM_DNN_TENSOR_FORMAT_LIBXSMM_1) {
     fmb = tensor->layout->dim_size[3];
     N = tensor->layout->dim_size[4];
   }
-  int C = fmb * bfm * lpb;
+  C = fmb * bfm * lpb;
 
   /*printf(" layout act copy in  N %i fmb %i H %i W %i bfm %i lpb %i \n", N, fmb, H, W, bfm, lpb);*/
+  {
+    LIBXSMM_VLA_DECL(6, element_type, handle_data_1, (element_type*)tensor->data, fmb, H, W, bfm, lpb);
+    LIBXSMM_VLA_DECL(4, const element_type, user_data, (const element_type*)data, C, H, W);
 
-  LIBXSMM_VLA_DECL(6, element_type, handle_data_1, (element_type*)tensor->data, fmb, H, W, bfm, lpb);
-  LIBXSMM_VLA_DECL(4, const element_type, user_data, (const element_type*)data, C, H, W);
-
-  for (i1 = 0; i1 < N; ++i1) {
-    for (i2 = 0; i2 < fmb; ++i2) {
-      for (i3 = 0; i3 < H; ++i3) {
-        for (i4 = 0; i4 < W; ++i4) {
-          for (i5 = 0; i5 < bfm; ++i5) {
-            for (i6 = 0; i6 < lpb; ++i6) {
-              LIBXSMM_VLA_ACCESS(6, handle_data_1, i1, i2, i3, i4, i5, i6, fmb, H, W, bfm, lpb) =
-                LIBXSMM_VLA_ACCESS(4, user_data, i1, ((size_t)i2*bfm*lpb) + ((size_t)i5*lpb) + i6, i3, i4, C, H, W);
+    for (i1 = 0; i1 < N; ++i1) {
+      for (i2 = 0; i2 < fmb; ++i2) {
+        for (i3 = 0; i3 < H; ++i3) {
+          for (i4 = 0; i4 < W; ++i4) {
+            for (i5 = 0; i5 < bfm; ++i5) {
+              for (i6 = 0; i6 < lpb; ++i6) {
+                LIBXSMM_VLA_ACCESS(6, handle_data_1, i1, i2, i3, i4, i5, i6, fmb, H, W, bfm, lpb) =
+                  LIBXSMM_VLA_ACCESS(4, user_data, i1, ((size_t)i2*bfm*lpb) + ((size_t)i5*lpb) + i6, i3, i4, C, H, W);
+              }
             }
           }
         }
--- src/template/libxsmm_dnn_tensor_buffer_copy_out_nchw.tpl.c
+++ src/template/libxsmm_dnn_tensor_buffer_copy_out_nchw.tpl.c
@@ -29,10 +29,8 @@
 /* Alexander Heinecke, Evangelos Georganas, Hans Pabst (Intel Corp.)
 ******************************************************************************/
 
-/* use for-loops to potentially leverage NUMA in the future */
-int i1, i2, i3, i4, i5, i6, lpb, bfm, W, H, fmb, N, C;
-
 if (tensor->layout->custom_format == LIBXSMM_DNN_TENSOR_FORMAT_LIBXSMM_1) {
+  int i1, i2, i3, i4, i5, i6, lpb, bfm, W, H, fmb, N, C;
   /* low precision formatting */
   if ( tensor->layout->num_dims == 6 ) {
     lpb = tensor->layout->dim_size[0];
@@ -51,19 +49,21 @@ if (tensor->layout->custom_format == LIBXSMM_DNN_TENSOR_FORMAT_LIBXSMM_1) {
 
   }
   C = fmb * bfm * lpb;
+
   /* printf(" layout act copy out  N %i fmb %i H %i W %i bfm %i lpb %i \n", N, fmb, H, W, bfm, lpb); */
+  {
+    LIBXSMM_VLA_DECL(6, const element_type, handle_data_1, (const element_type*)tensor->data, fmb, H, W, bfm, lpb);
+    LIBXSMM_VLA_DECL(4, element_type, user_data, (element_type*)data, C, H, W);
 
-  LIBXSMM_VLA_DECL(6, const element_type, handle_data_1, (const element_type*)tensor->data, fmb, H, W, bfm, lpb);
-  LIBXSMM_VLA_DECL(4, element_type, user_data, (element_type*)data, C, H, W);
-
-  for (i1 = 0; i1 < N; ++i1) {
-    for (i2 = 0; i2 < fmb; ++i2) {
-      for (i3 = 0; i3 < H; ++i3) {
-        for (i4 = 0; i4 < W; ++i4) {
-          for (i5 = 0; i5 < bfm; ++i5) {
-            for (i6 = 0; i6 < lpb; ++i6) {
-              LIBXSMM_VLA_ACCESS(4, user_data, i1, ((size_t)i2*bfm*lpb) + ((size_t)i5*lpb) + i6, i3, i4, C, H, W) =
-                LIBXSMM_VLA_ACCESS(6, handle_data_1, i1, i2, i3, i4, i5, i6, fmb, H, W, bfm, lpb);
+    for (i1 = 0; i1 < N; ++i1) {
+      for (i2 = 0; i2 < fmb; ++i2) {
+        for (i3 = 0; i3 < H; ++i3) {
+          for (i4 = 0; i4 < W; ++i4) {
+            for (i5 = 0; i5 < bfm; ++i5) {
+              for (i6 = 0; i6 < lpb; ++i6) {
+                LIBXSMM_VLA_ACCESS(4, user_data, i1, ((size_t)i2*bfm*lpb) + ((size_t)i5*lpb) + i6, i3, i4, C, H, W) =
+                  LIBXSMM_VLA_ACCESS(6, handle_data_1, i1, i2, i3, i4, i5, i6, fmb, H, W, bfm, lpb);
+              }
             }
           }
         }
