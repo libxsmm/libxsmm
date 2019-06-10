877fadf7f33ccc7f14fb21688c43cb2d2594c318
Hans Pabst <hans.pabst@intel.com>
2019-06-10 13:47:09 +0200

    Fixed unused variable warning. Fixed C89 conformance.

--- src/generator_x86_instructions.c
+++ src/generator_x86_instructions.c
@@ -4078,7 +4078,6 @@ void libxsmm_x86_instruction_alu_reg( libxsmm_generated_code* io_generated_code,
     /* unsigned int l_maxsize = io_generated_code->buffer_size;*/
     /* unsigned int l_maxsize = 1024; */
 
-    int l_first = 0;
     int l_second = 0;
     int l_third = 0;
     int l_extra_byte = 0;
@@ -4121,24 +4120,26 @@ void libxsmm_x86_instruction_alu_reg( libxsmm_generated_code* io_generated_code,
           fprintf(stderr, "libxsmm_instruction_alu_reg: Not sure what instruction you have in mind: %u\n",i_alu_instr);
           exit(-1);
     }
-    int l_regbas0 = l_reg0 % 8;
-    int l_gp8     = ((l_reg0 > 7)&&(l_reg0 <=15)?1:0);
-    int l_regnum  = l_reg1 % 8;
-    int l_nx8     = ((l_reg1 >7)&&(l_reg1<=15)?1:0);
+    {/* open new scope for additional variable declarations (C89) */
+      int l_regbas0 = l_reg0 % 8;
+      int l_gp8     = ((l_reg0 > 7)&&(l_reg0 <=15)?1:0);
+      int l_regnum  = l_reg1 % 8;
+      int l_nx8     = ((l_reg1 >7)&&(l_reg1<=15)?1:0);
 
-    if ( i_alu_instr == LIBXSMM_X86_INSTR_POPCNT ) {
-       buf[i++] = (unsigned char)(0xf3);
-    }
-    buf[i++] = (unsigned char)(0x48 + l_gp8 * 0x01 + l_nx8 * 0x04);
-    buf[i++] = (unsigned char)(0x01 + l_second);
-    if ( l_extra_byte )
-    {
-       buf[i++] = (unsigned char)(0x44 + l_third);
-    }
-    buf[i++] = (unsigned char)(0xc0 + l_regbas0 + 8*l_regnum);
+      if ( i_alu_instr == LIBXSMM_X86_INSTR_POPCNT ) {
+         buf[i++] = (unsigned char)(0xf3);
+      }
+      buf[i++] = (unsigned char)(0x48 + l_gp8 * 0x01 + l_nx8 * 0x04);
+      buf[i++] = (unsigned char)(0x01 + l_second);
+      if ( l_extra_byte )
+      {
+         buf[i++] = (unsigned char)(0x44 + l_third);
+      }
+      buf[i++] = (unsigned char)(0xc0 + l_regbas0 + 8*l_regnum);
 
-    io_generated_code->code_size = i;
-    /* *loc = i; */
+      io_generated_code->code_size = i;
+      /* *loc = i; */
+    }
   } else {
     char l_new_code[512];
     int l_max_code_length = 511;
