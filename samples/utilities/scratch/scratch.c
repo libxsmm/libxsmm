2a97b64fc7bfb82541eb7e4f0efc2edb7a1232ba
Hans Pabst <hans.pabst@intel.com>
2019-07-05 22:14:15 +0200

    Improved scratch code sample (microbenchmark).  [skip ci]

--- samples/utilities/scratch/scratch.c
+++ samples/utilities/scratch/scratch.c
@@ -56,7 +56,6 @@ void* malloc_offsite(size_t size);
 
 int main(int argc, char* argv[])
 {
-  const int ncalls = 1000000;
 #if defined(_OPENMP)
   const int max_nthreads = omp_get_max_threads();
 #else
@@ -65,15 +64,23 @@ int main(int argc, char* argv[])
   const int ncycles = LIBXSMM_MAX(1 < argc ? atoi(argv[1]) : 100, 1);
   const int max_nallocs = LIBXSMM_CLMP(2 < argc ? atoi(argv[2]) : 4, 1, MAX_MALLOC_N);
   const int nthreads = LIBXSMM_CLMP(3 < argc ? atoi(argv[3]) : 1, 1, max_nthreads);
-  unsigned int nallocs = 0, nerrors = 0;
+  unsigned int nallocs = 0, nerrors0 = 0, nerrors1 = 0;
   int r[MAX_MALLOC_N], i;
+  int max_size = 0;
 
   /* generate set of random number for parallel region */
   for (i = 0; i < (MAX_MALLOC_N); ++i) r[i] = rand();
 
   /* count number of calls according to randomized scheme */
   for (i = 0; i < ncycles; ++i) {
-    nallocs += r[i%(MAX_MALLOC_N)] % max_nallocs + 1;
+    const int count = r[i%(MAX_MALLOC_N)] % max_nallocs + 1;
+    int j;
+    for (j = 0; j < count; ++j) {
+      const int k = (i * count + j) % (MAX_MALLOC_N);
+      const int mbytes = (r[k] % (MAX_MALLOC_MB) + 1);
+      if (max_size < mbytes) max_size = mbytes;
+    }
+    nallocs += count;
   }
   assert(0 != nallocs);
 
@@ -85,20 +92,15 @@ int main(int argc, char* argv[])
 #endif
   {
     const char *const longlife_env = getenv("LONGLIFE");
-    const int enable_longlife = ((0 == longlife_env || 0 == *longlife_env) ? 0 : atoi(longlife_env));
-    void *const longlife = (0 == enable_longlife ? 0 : malloc_offsite((MAX_MALLOC_MB) << 20));
-    unsigned long long d0, d1 = 0;
+    const int enable_longlife = ((NULL == longlife_env || 0 == *longlife_env) ? 0 : atoi(longlife_env));
+    void* longlife = (0 == enable_longlife ? NULL : malloc_offsite((MAX_MALLOC_MB) << 20));
+    unsigned long long d0 = 0, d1 = 0;
     libxsmm_scratch_info info;
 
-    /* run non-inline function to measure call overhead of an "empty" function */
-    const unsigned long long t0 = libxsmm_timer_tick();
-    for (i = 0; i < ncalls; ++i) {
-      libxsmm_init(); /* subsequent calls are not doing any work */
-    }
-    d0 = libxsmm_timer_diff(t0, libxsmm_timer_tick());
+    libxsmm_init();
 
 #if defined(_OPENMP)
-#   pragma omp parallel for num_threads(nthreads) private(i) reduction(+:d1,nerrors)
+#   pragma omp parallel for num_threads(nthreads) private(i) reduction(+:d1,nerrors1)
 #endif
     for (i = 0; i < ncycles; ++i) {
       const int count = r[i%(MAX_MALLOC_N)] % max_nallocs + 1;
@@ -111,11 +113,11 @@ int main(int argc, char* argv[])
         const unsigned long long t1 = libxsmm_timer_tick();
         p[j] = libxsmm_aligned_scratch(nbytes, 0/*auto*/);
         d1 += libxsmm_timer_diff(t1, libxsmm_timer_tick());
-        if (0 != p[j]) {
+        if (NULL != p[j]) {
           memset(p[j], j, nbytes);
         }
         else {
-          ++nerrors;
+          ++nerrors1;
         }
       }
       for (j = 0; j < count; ++j) {
@@ -123,33 +125,58 @@ int main(int argc, char* argv[])
       }
     }
     libxsmm_free(longlife);
+    if (EXIT_SUCCESS == libxsmm_get_scratch_info(&info) && 0 < info.size) {
+      fprintf(stdout, "\nScratch: %.f MB (mallocs=%lu, pools=%u)\n",
+        1.0 * info.size / (1ULL << 20), (unsigned long int)info.nmallocs, info.npools);
+      libxsmm_release_scratch(); /* suppress LIBXSMM's termination message about scratch */
+    }
+
+    longlife = (0 == enable_longlife ? NULL : malloc((MAX_MALLOC_MB) << 20));
+#if defined(_OPENMP)
+#   pragma omp parallel for num_threads(nthreads) private(i) reduction(+:d0,nerrors0)
+#endif
+    for (i = 0; i < ncycles; ++i) {
+      const int count = r[i % (MAX_MALLOC_N)] % max_nallocs + 1;
+      void* p[MAX_MALLOC_N];
+      int j;
+      assert(count <= MAX_MALLOC_N);
+      for (j = 0; j < count; ++j) {
+        const int k = (i * count + j) % (MAX_MALLOC_N);
+        const size_t nbytes = ((size_t)r[k] % (MAX_MALLOC_MB) + 1) << 20;
+        const unsigned long long t1 = libxsmm_timer_tick();
+        p[j] = malloc(nbytes);
+        d0 += libxsmm_timer_diff(t1, libxsmm_timer_tick());
+        if (NULL != p[j]) {
+          memset(p[j], j, nbytes);
+        }
+        else {
+          ++nerrors0;
+        }
+      }
+      for (j = 0; j < count; ++j) {
+        free(p[j]);
+      }
+    }
+    free(longlife);
 
     if (0 != d0 && 0 != d1 && 0 < nallocs) {
       const double dcalls = libxsmm_timer_duration(0, d0);
       const double dalloc = libxsmm_timer_duration(0, d1);
-      const double alloc_freq = 1E-3 * nallocs / dalloc;
-      const double empty_freq = 1E-3 * ncalls / dcalls;
-      fprintf(stdout, "\tallocation+free calls/s: %.1f kHz\n", alloc_freq);
-      fprintf(stdout, "\tempty calls/s: %.1f MHz\n", 1E-3 * empty_freq);
-      fprintf(stdout, "\toverhead: %.1fx\n", empty_freq / alloc_freq);
-    }
-
-    if (EXIT_SUCCESS == libxsmm_get_scratch_info(&info) && 0 < info.size) {
-      fprintf(stdout, "\nScratch: %.f MB (mallocs=%lu, pools=%u",
-        1.0 * info.size / (1ULL << 20), (unsigned long int)info.nmallocs, info.npools);
-      if (1 < nthreads) fprintf(stdout, ", threads=%i)\n", nthreads); else fprintf(stdout, ")\n");
-      libxsmm_release_scratch(); /* suppress LIBXSMM's termination message about scratch */
+      const double scratch_freq = 1E-3 * nallocs / dalloc;
+      const double malloc_freq = 1E-3 * nallocs / dcalls;
+      fprintf(stdout, "\tlibxsmm scratch calls/s: %.1f kHz\n", scratch_freq);
+      fprintf(stdout, "Malloc: %i MB\n", max_size);
+      fprintf(stdout, "\tstd.malloc+free calls/s: %.1f kHz\n", malloc_freq);
+      fprintf(stdout, "Scratch Speedup: %.1fx\n", scratch_freq / malloc_freq);
     }
   }
 
-  if (0 == nerrors) {
-    fprintf(stdout, "Finished\n");
-    return EXIT_SUCCESS;
-  }
-  else {
-    fprintf(stdout, "FAILED (%u errors)\n", nerrors);
+  if (0 != nerrors0 || 0 != nerrors1) {
+    fprintf(stdout, "FAILED (errors: malloc=%u libxsmm=%u)\n", nerrors0, nerrors1);
     return EXIT_FAILURE;
   }
+
+  return EXIT_SUCCESS;
 }
 
 
