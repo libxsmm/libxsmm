5dc0b4c17d45d73c6abe7522d24c6b2c314bada9
Hans Pabst <hans.pabst@intel.com>
2019-09-11 15:35:50 +0200

    Fixed typo.

--- src/libxsmm_main.c
+++ src/libxsmm_main.c
@@ -633,7 +633,7 @@ LIBXSMM_API_INTERN void internal_init(void)
   if (NULL == internal_registry) { /* double-check after acquiring the lock(s) */
     void *new_registry = NULL, *new_keys = &internal_registry_keys;
 #if defined(LIBXSMM_CACHE_GLOBAL)
-    void* new_cache = &internal_cache_buffer
+    void* new_cache = &internal_cache_buffer;
 #endif
     /* setup verbosity as early as possible since below code may rely on verbose output */
     const char *const env_verbose = getenv("LIBXSMM_VERBOSE");
