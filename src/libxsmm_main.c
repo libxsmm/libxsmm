319bb5da784fc284588058d9acd8fe8bf81e0f22
Hans Pabst <hans.pabst@intel.com>
2019-09-19 20:20:57 +0200

    Moved memory allocation setup up despite of intercepting internal allocations (reduce risk of asymmetric allocation/deallocation or need to detect foreign pointers).

--- src/libxsmm_main.c
+++ src/libxsmm_main.c
@@ -706,6 +706,21 @@ LIBXSMM_API_INTERN void internal_init(void)
     }
     libxsmm_set_scratch_limit(internal_parse_nbytes(getenv("LIBXSMM_SCRATCH_LIMIT"), LIBXSMM_SCRATCH_DEFAULT));
 #endif /*defined(LIBXSMM_MALLOC_SCRATCH_MAX_NPOOLS) && (0 < (LIBXSMM_MALLOC_SCRATCH_MAX_NPOOLS))*/
+    { /* setup malloc-interception after internal allocations */
+      const libxsmm_malloc_function null_malloc_fn = { 0 };
+      const libxsmm_free_function null_free_fn = { 0 };
+      const char *const env_k = getenv("LIBXSMM_MALLOC");
+      char *const env_t = getenv("LIBXSMM_MALLOC_LIMIT");
+      const char* env_i = (NULL != env_t ? strtok(env_t, INTERNAL_DELIMS) : NULL);
+      const size_t malloc_lo = internal_parse_nbytes(env_i, LIBXSMM_MALLOC_LIMIT);
+      const size_t malloc_hi = (NULL != env_i ? internal_parse_nbytes(
+        strtok(NULL, INTERNAL_DELIMS), LIBXSMM_SCRATCH_UNLIMITED) : LIBXSMM_SCRATCH_UNLIMITED);
+      const int malloc_kind = ((NULL == env_k || 0 == *env_k) ? 0/*disabled*/ : atoi(env_k));
+      libxsmm_set_malloc(malloc_kind, &malloc_lo, &malloc_hi);
+      libxsmm_xset_default_allocator(NULL/*lock*/, NULL/*context*/, null_malloc_fn, null_free_fn);
+      libxsmm_xset_scratch_allocator(NULL/*lock*/, NULL/*context*/, null_malloc_fn, null_free_fn);
+      libxsmm_malloc_init();
+    }
 #if defined(LIBXSMM_MAXTARGET)
     libxsmm_set_target_arch(LIBXSMM_STRINGIFY(LIBXSMM_MAXTARGET));
 #else /* attempt to set libxsmm_target_archid per environment variable */
@@ -788,21 +803,6 @@ LIBXSMM_API_INTERN void internal_init(void)
         }
       }
 #endif
-      { /* setup malloc-interception after internal allocations */
-        const libxsmm_malloc_function null_malloc_fn = { 0 };
-        const libxsmm_free_function null_free_fn = { 0 };
-        const char *const env_k = getenv("LIBXSMM_MALLOC");
-        char *const env_t = getenv("LIBXSMM_MALLOC_LIMIT");
-        const char* env_i = (NULL != env_t ? strtok(env_t, INTERNAL_DELIMS) : NULL);
-        const size_t malloc_lo = internal_parse_nbytes(env_i, LIBXSMM_MALLOC_LIMIT);
-        const size_t malloc_hi = (NULL != env_i ? internal_parse_nbytes(
-          strtok(NULL, INTERNAL_DELIMS), LIBXSMM_SCRATCH_UNLIMITED) : LIBXSMM_SCRATCH_UNLIMITED);
-        const int malloc_kind = ((NULL == env_k || 0 == *env_k) ? 0/*disabled*/ : atoi(env_k));
-        libxsmm_set_malloc(malloc_kind, &malloc_lo, &malloc_hi);
-        libxsmm_xset_default_allocator(NULL/*lock*/, NULL/*context*/, null_malloc_fn, null_free_fn);
-        libxsmm_xset_scratch_allocator(NULL/*lock*/, NULL/*context*/, null_malloc_fn, null_free_fn);
-        libxsmm_malloc_init();
-      }
       { /* commit the registry buffer and enable global visibility */
         void *const pv_registry = &internal_registry;
         LIBXSMM_ATOMIC(LIBXSMM_ATOMIC_STORE, LIBXSMM_BITS)((void**)pv_registry, (void*)new_registry, LIBXSMM_ATOMIC_SEQ_CST);
