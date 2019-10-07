b56dcecb84b834665bd2bb40463bce86f65920e5
Hans Pabst <hans.pabst@intel.com>
2019-10-07 16:01:45 +0200

    Partially reverted previous change (Fortran interface).

--- src/template/libxsmm.f
+++ src/template/libxsmm.f
@@ -743,17 +743,23 @@
         !DIR$ ATTRIBUTES OFFLOAD:MIC :: libxsmm_ptr_z1
         FUNCTION libxsmm_ptr_z1(a)
           COMPLEX(C_DOUBLE), INTENT(IN) :: a(:)
-          COMPLEX(C_DOUBLE), POINTER :: fptr(:)
           TYPE(C_PTR) :: libxsmm_ptr_z1
-          fptr => a; libxsmm_ptr_z1 = C_LOC(fptr)
+          IF (0.LT.SIZE(a)) THEN
+            libxsmm_ptr_z1 = libxsmm_ptr_z0(a(LBOUND(a,1)))
+          ELSE
+            libxsmm_ptr_z1 = C_NULL_PTR
+          END IF
         END FUNCTION
 
         !DIR$ ATTRIBUTES OFFLOAD:MIC :: libxsmm_ptr_z2
         FUNCTION libxsmm_ptr_z2(a)
           COMPLEX(C_DOUBLE), INTENT(IN) :: a(:,:)
-          COMPLEX(C_DOUBLE), POINTER :: fptr(:,;)
           TYPE(C_PTR) :: libxsmm_ptr_z2
-          fptr => a; libxsmm_ptr_z2 = C_LOC(fptr)
+          IF (ALL(0.LT.SHAPE(a))) THEN
+            libxsmm_ptr_z2 = libxsmm_ptr_z0(a(LBOUND(a,1),LBOUND(a,2)))
+          ELSE
+            libxsmm_ptr_z2 = C_NULL_PTR
+          END IF
         END FUNCTION
 
         !DIR$ ATTRIBUTES OFFLOAD:MIC :: libxsmm_ptr_c0
@@ -767,17 +773,23 @@
         !DIR$ ATTRIBUTES OFFLOAD:MIC :: libxsmm_ptr_c1
         FUNCTION libxsmm_ptr_c1(a)
           COMPLEX(C_FLOAT), INTENT(IN) :: a(:)
-          COMPLEX(C_FLOAT), POINTER :: fptr(:)
           TYPE(C_PTR) :: libxsmm_ptr_c1
-          fptr => a; libxsmm_ptr_c1 = C_LOC(fptr)
+          IF (0.LT.SIZE(a)) THEN
+            libxsmm_ptr_c1 = libxsmm_ptr_c0(a(LBOUND(a,1)))
+          ELSE
+            libxsmm_ptr_c1 = C_NULL_PTR
+          END IF
         END FUNCTION
 
         !DIR$ ATTRIBUTES OFFLOAD:MIC :: libxsmm_ptr_c2
         FUNCTION libxsmm_ptr_c2(a)
           COMPLEX(C_FLOAT), INTENT(IN) :: a(:,:)
-          COMPLEX(C_FLOAT), POINTER :: fptr(:,:)
           TYPE(C_PTR) :: libxsmm_ptr_c2
-          fptr => a; libxsmm_ptr_c2 = C_LOC(fptr)
+          IF (ALL(0.LT.SHAPE(a))) THEN
+            libxsmm_ptr_c2 = libxsmm_ptr_c0(a(LBOUND(a,1),LBOUND(a,2)))
+          ELSE
+            libxsmm_ptr_c2 = C_NULL_PTR
+          END IF
         END FUNCTION
 
         !DIR$ ATTRIBUTES OFFLOAD:MIC :: libxsmm_ptr_d0
@@ -791,17 +803,23 @@
         !DIR$ ATTRIBUTES OFFLOAD:MIC :: libxsmm_ptr_d1
         FUNCTION libxsmm_ptr_d1(a)
           REAL(C_DOUBLE), INTENT(IN) :: a(:)
-          REAL(C_DOUBLE), POINTER :: fptr(:)
           TYPE(C_PTR) :: libxsmm_ptr_d1
-          fptr => a; libxsmm_ptr_d1 = C_LOC(fptr)
+          IF (0.LT.SIZE(a)) THEN
+            libxsmm_ptr_d1 = libxsmm_ptr_d0(a(LBOUND(a,1)))
+          ELSE
+            libxsmm_ptr_d1 = C_NULL_PTR
+          END IF
         END FUNCTION
 
         !DIR$ ATTRIBUTES OFFLOAD:MIC :: libxsmm_ptr_d2
         FUNCTION libxsmm_ptr_d2(a)
           REAL(C_DOUBLE), INTENT(IN) :: a(:,:)
-          REAL(C_DOUBLE), POINTER :: fptr(:,:)
           TYPE(C_PTR) :: libxsmm_ptr_d2
-          fptr => a; libxsmm_ptr_d2 = C_LOC(fptr)
+          IF (ALL(0.LT.SHAPE(a))) THEN
+            libxsmm_ptr_d2 = libxsmm_ptr_d0(a(LBOUND(a,1),LBOUND(a,2)))
+          ELSE
+            libxsmm_ptr_d2 = C_NULL_PTR
+          END IF
         END FUNCTION
 
         !DIR$ ATTRIBUTES OFFLOAD:MIC :: libxsmm_ptr_s0
@@ -815,17 +833,23 @@
         !DIR$ ATTRIBUTES OFFLOAD:MIC :: libxsmm_ptr_s1
         FUNCTION libxsmm_ptr_s1(a)
           REAL(C_FLOAT), INTENT(IN) :: a(:)
-          REAL(C_FLOAT), POINTER :: fptr(:)
           TYPE(C_PTR) :: libxsmm_ptr_s1
-          fptr => a; libxsmm_ptr_s1 = C_LOC(fptr)
+          IF (0.LT.SIZE(a)) THEN
+            libxsmm_ptr_s1 = libxsmm_ptr_s0(a(LBOUND(a,1)))
+          ELSE
+            libxsmm_ptr_s1 = C_NULL_PTR
+          END IF
         END FUNCTION
 
         !DIR$ ATTRIBUTES OFFLOAD:MIC :: libxsmm_ptr_s2
         FUNCTION libxsmm_ptr_s2(a)
           REAL(C_FLOAT), INTENT(IN) :: a(:,:)
-          REAL(C_FLOAT), POINTER :: fptr(:,:)
           TYPE(C_PTR) :: libxsmm_ptr_s2
-          fptr => a; libxsmm_ptr_s2 = C_LOC(fptr)
+          IF (ALL(0.LT.SHAPE(a))) THEN
+            libxsmm_ptr_s2 = libxsmm_ptr_s0(a(LBOUND(a,1),LBOUND(a,2)))
+          ELSE
+            libxsmm_ptr_s2 = C_NULL_PTR
+          END IF
         END FUNCTION
 
         !DIR$ ATTRIBUTES OFFLOAD:MIC :: libxsmm_ptr_i0
@@ -839,17 +863,23 @@
         !DIR$ ATTRIBUTES OFFLOAD:MIC :: libxsmm_ptr_i1
         FUNCTION libxsmm_ptr_i1(a)
           INTEGER(C_INT), INTENT(IN) :: a(:)
-          INTEGER(C_INT), POINTER :: fptr(:)
           TYPE(C_PTR) :: libxsmm_ptr_i1
-          fptr => a; libxsmm_ptr_i1 = C_LOC(fptr)
+          IF (0.LT.SIZE(a)) THEN
+            libxsmm_ptr_i1 = libxsmm_ptr_i0(a(LBOUND(a,1)))
+          ELSE
+            libxsmm_ptr_i1 = C_NULL_PTR
+          END IF
         END FUNCTION
 
         !DIR$ ATTRIBUTES OFFLOAD:MIC :: libxsmm_ptr_i2
         FUNCTION libxsmm_ptr_i2(a)
           INTEGER(C_INT), INTENT(IN) :: a(:,:)
-          INTEGER(C_INT), POINTER :: fptr(:,:)
           TYPE(C_PTR) :: libxsmm_ptr_i2
-          fptr => a; libxsmm_ptr_i2 = C_LOC(fptr)
+          IF (ALL(0.LT.SHAPE(a))) THEN
+            libxsmm_ptr_i2 = libxsmm_ptr_i0(a(LBOUND(a,1),LBOUND(a,2)))
+          ELSE
+            libxsmm_ptr_i2 = C_NULL_PTR
+          END IF
         END FUNCTION
 
         !DIR$ ATTRIBUTES OFFLOAD:MIC :: libxsmm_ptr_w0
@@ -863,17 +893,23 @@
         !DIR$ ATTRIBUTES OFFLOAD:MIC :: libxsmm_ptr_w1
         FUNCTION libxsmm_ptr_w1(a)
           INTEGER(C_SHORT), INTENT(IN) :: a(:)
-          INTEGER(C_SHORT), POINTER :: fptr(:)
           TYPE(C_PTR) :: libxsmm_ptr_w1
-          fptr => a; libxsmm_ptr_w1 = C_LOC(fptr)
+          IF (0.LT.SIZE(a)) THEN
+            libxsmm_ptr_w1 = libxsmm_ptr_w0(a(LBOUND(a,1)))
+          ELSE
+            libxsmm_ptr_w1 = C_NULL_PTR
+          END IF
         END FUNCTION
 
         !DIR$ ATTRIBUTES OFFLOAD:MIC :: libxsmm_ptr_w2
         FUNCTION libxsmm_ptr_w2(a)
           INTEGER(C_SHORT), INTENT(IN) :: a(:,:)
-          INTEGER(C_SHORT), POINTER :: fptr(:,:)
           TYPE(C_PTR) :: libxsmm_ptr_w2
-          fptr => a; libxsmm_ptr_w2 = C_LOC(fptr)
+          IF (ALL(0.LT.SHAPE(a))) THEN
+            libxsmm_ptr_w2 = libxsmm_ptr_w0(a(LBOUND(a,1),LBOUND(a,2)))
+          ELSE
+            libxsmm_ptr_w2 = C_NULL_PTR
+          END IF
         END FUNCTION
 
         !DIR$ ATTRIBUTES OFFLOAD:MIC :: libxsmm_ptr_j0
@@ -887,17 +923,23 @@
         !DIR$ ATTRIBUTES OFFLOAD:MIC :: libxsmm_ptr_j1
         FUNCTION libxsmm_ptr_j1(a)
           INTEGER(C_INT8_T), INTENT(IN) :: a(:)
-          INTEGER(C_INT8_T), POINTER :: fptr(:)
           TYPE(C_PTR) :: libxsmm_ptr_j1
-          fptr => a; libxsmm_ptr_j1 = C_LOC(fptr)
+          IF (0.LT.SIZE(a)) THEN
+            libxsmm_ptr_j1 = libxsmm_ptr_j0(a(LBOUND(a,1)))
+          ELSE
+            libxsmm_ptr_j1 = C_NULL_PTR
+          END IF
         END FUNCTION
 
         !DIR$ ATTRIBUTES OFFLOAD:MIC :: libxsmm_ptr_j2
         FUNCTION libxsmm_ptr_j2(a)
           INTEGER(C_INT8_T), INTENT(IN) :: a(:,:)
-          INTEGER(C_INT8_T), POINTER :: fptr(:,:)
           TYPE(C_PTR) :: libxsmm_ptr_j2
-          fptr => a; libxsmm_ptr_j2 = C_LOC(fptr)
+          IF (ALL(0.LT.SHAPE(a))) THEN
+            libxsmm_ptr_j2 = libxsmm_ptr_j0(a(LBOUND(a,1),LBOUND(a,2)))
+          ELSE
+            libxsmm_ptr_j2 = C_NULL_PTR
+          END IF
         END FUNCTION
 
         !DIR$ ATTRIBUTES OFFLOAD:MIC :: libxsmm_ptr_b0
@@ -911,17 +953,23 @@
         !DIR$ ATTRIBUTES OFFLOAD:MIC :: libxsmm_ptr_b1
         FUNCTION libxsmm_ptr_b1(a)
           CHARACTER(C_CHAR), INTENT(IN) :: a(:)
-          CHARACTER(C_CHAR), POINTER :: fptr(:)
           TYPE(C_PTR) :: libxsmm_ptr_b1
-          fptr => a; libxsmm_ptr_b1 = C_LOC(fptr)
+          IF (0.LT.SIZE(a)) THEN
+            libxsmm_ptr_b1 = libxsmm_ptr_b0(a(LBOUND(a,1)))
+          ELSE
+            libxsmm_ptr_b1 = C_NULL_PTR
+          END IF
         END FUNCTION
 
         !DIR$ ATTRIBUTES OFFLOAD:MIC :: libxsmm_ptr_b2
         FUNCTION libxsmm_ptr_b2(a)
           CHARACTER(C_CHAR), INTENT(IN) :: a(:,:)
-          CHARACTER(C_CHAR), POINTER :: fptr(:,:)
           TYPE(C_PTR) :: libxsmm_ptr_b2
-          fptr => a; libxsmm_ptr_b2 = C_LOC(fptr)
+          IF (ALL(0.LT.SHAPE(a))) THEN
+            libxsmm_ptr_b2 = libxsmm_ptr_b0(a(LBOUND(a,1),LBOUND(a,2)))
+          ELSE
+            libxsmm_ptr_b2 = C_NULL_PTR
+          END IF
         END FUNCTION
 
         !DIR$ ATTRIBUTES OFFLOAD:MIC :: libxsmm_ptr_l0
@@ -935,17 +983,23 @@
         !DIR$ ATTRIBUTES OFFLOAD:MIC :: libxsmm_ptr_l1
         FUNCTION libxsmm_ptr_l1(a)
           INTEGER(C_LONG_LONG), INTENT(IN) :: a(:)
-          INTEGER(C_LONG_LONG), POINTER :: fptr(:)
           TYPE(C_PTR) :: libxsmm_ptr_l1
-          fptr => a; libxsmm_ptr_l1 = C_LOC(fptr)
+          IF (0.LT.SIZE(a)) THEN
+            libxsmm_ptr_l1 = libxsmm_ptr_l0(a(LBOUND(a,1)))
+          ELSE
+            libxsmm_ptr_l1 = C_NULL_PTR
+          END IF
         END FUNCTION
 
         !DIR$ ATTRIBUTES OFFLOAD:MIC :: libxsmm_ptr_l2
         FUNCTION libxsmm_ptr_l2(a)
           INTEGER(C_LONG_LONG), INTENT(IN) :: a(:,:)
-          INTEGER(C_LONG_LONG), POINTER :: fptr(:,:)
           TYPE(C_PTR) :: libxsmm_ptr_l2
-          fptr => a; libxsmm_ptr_l2 = C_LOC(fptr)
+          IF (ALL(0.LT.SHAPE(a))) THEN
+            libxsmm_ptr_l2 = libxsmm_ptr_l0(a(LBOUND(a,1),LBOUND(a,2)))
+          ELSE
+            libxsmm_ptr_l2 = C_NULL_PTR
+          END IF
         END FUNCTION
 
         !DIR$ ATTRIBUTES OFFLOAD:MIC :: libxsmm_release_dmmkernel
