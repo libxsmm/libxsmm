66f24c8e9b846fefc54b0c67b28cabee015fa62b
Hans Pabst <hans.pabst@intel.com>
2019-11-28 10:12:43 +0100

    Issue #343: Rely on user-defined macros instead of directly using LIBXSMM_VLA_DECL/LIBXSMM_VLA_ACCESS. This way, a layout transformation can be implemented at the definition of the macro (expressions using it can remain unchanged except for changing the macro name of course). The DIM-argument could be omitted for derived macros, but it is kept in order to keep the target-expression unchanged (which supplied the dimension). A limitation of such a data layout transformation is that the prime index must stay in front and cannot be involved into index permutation.

--- tests/vla.c
+++ tests/vla.c
@@ -35,26 +35,35 @@
 # define ELEM_TYPE short
 #endif
 
+#define VLA_IJK_DECL(DIM, TYPE, ARRAY, DATA, S1, S2) LIBXSMM_VLA_DECL(  DIM, TYPE, ARRAY, DATA, S1, S2)
+#define VLA_IJK_INDX(DIM, ARRAY, I0, I1, I2, S1, S2) LIBXSMM_VLA_ACCESS(DIM, ARRAY, I0, I1, I2, S1, S2)
+
+#define VLA_IKJ_DECL(DIM, TYPE, ARRAY, DATA, S1, S2) LIBXSMM_VLA_DECL(  DIM, TYPE, ARRAY, DATA, S2, S1)
+#define VLA_IKJ_INDX(DIM, ARRAY, I0, I1, I2, S1, S2) LIBXSMM_VLA_ACCESS(DIM, ARRAY, I0, I2, I1, S2, S1)
+
 
 int main(/*int argc, char* argv[]*/)
 {
   int ni = 9, nj = 7, nk = 3, i, j, k, linear = 0, result = EXIT_SUCCESS;
   ELEM_TYPE *const input = (ELEM_TYPE*)malloc(sizeof(ELEM_TYPE) * ni * nj * nk);
   LIBXSMM_VLA_DECL(1, const ELEM_TYPE, in1, input);
-  LIBXSMM_VLA_DECL(3, const ELEM_TYPE, in3, input, nj, nk);
+  VLA_IJK_DECL(3, const ELEM_TYPE, jk3, input, nj, nk);
+  VLA_IKJ_DECL(3, const ELEM_TYPE, kj3, input, nj, nk);
 
   LIBXSMM_ASSERT(NULL != input);
   for (i = 0; i < (ni * nj * nk); ++i) input[i] = (ELEM_TYPE)i;
-  for (i = 0; i < ni; ++i) {
+  for (i = 0; i < ni && EXIT_SUCCESS == result; ++i) {
     for (j = 0; j < nj; ++j) {
       for (k = 0; k < nk; ++k) {
-        const ELEM_TYPE gold = input[linear];
-        if (gold != LIBXSMM_VLA_ACCESS(3, in3, i, j, k, nj, nk) ||
-            gold != LIBXSMM_VLA_ACCESS(1, in1, linear))
+        const ELEM_TYPE gold0 = input[linear];
+        const ELEM_TYPE test0 = VLA_IJK_INDX(3, jk3, i, j, k, nj, nk);
+        const ELEM_TYPE gold1 = VLA_IJK_INDX(3, jk3, i, k, j, nk, nj);
+        const ELEM_TYPE test1 = VLA_IKJ_INDX(3, kj3, i, j, k, nj, nk);
+        if (gold0 != LIBXSMM_VLA_ACCESS(1, in1, linear) ||
+            gold0 != test0 || gold1 != test1)
         {
           result = EXIT_FAILURE;
-          i = ni; j = nj;
-          break;
+          j = nj; break;
         }
         ++linear;
       }
@@ -64,3 +73,4 @@ int main(/*int argc, char* argv[]*/)
   free(input);
   return result;
 }
+
