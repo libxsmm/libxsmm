f71835f4624cc17635baa2127546b9d664711f15
Hans Pabst <hans.pabst@intel.com>
2020-01-13 08:05:26 +0100

    Added tool to make GitHub forks available as Git-remotes [skip ci].

---
+++ scripts/tool_addforks.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+###############################################################################
+# Copyright (c) Intel Corporation - All rights reserved.                      #
+# This file is part of the LIBXSMM library.                                   #
+#                                                                             #
+# For information on the license, see the LICENSE file.                       #
+# Further information: https://github.com/hfp/libxsmm/                        #
+# SPDX-License-Identifier: BSD-3-Clause                                       #
+###############################################################################
+# Hans Pabst (Intel Corp.)
+###############################################################################
+
+CURL=$(command -v curl)
+GREP=$(command -v grep)
+CUT=$(command -v cut)
+GIT=$(command -v git)
+
+if [ "" != "${CURL}" ] && [ "" != "${GIT}" ] && \
+   [ "" != "${GREP}" ] && [ "" != "${CUT}" ];
+then
+  for FORK in $(${CURL} -s https://api.github.com/repos/hfp/libxsmm/forks \
+  | ${GREP} "\"html_url\"" | ${GREP} "libxsmm" | ${CUT} -d/ -f4);
+  do
+    ${GIT} remote add ${FORK} https://github.com/${FORK}/recoll-webui.git
+    ${GIT} fetch ${FORK}
+  done
+else
+  echo "Error: missing prerequisites!"
+  exit 1
+fi
+
