2d21a4cdbc9d589ebcfaba5282454b72710b735a
egeorgan <evangelos.georganas@intel.com>
2020-01-21 11:29:03 -0800

    Fixing more travis issues

--- src/libxsmm_dnn_fullyconnected_forward.c
+++ src/libxsmm_dnn_fullyconnected_forward.c
@@ -167,7 +167,7 @@ libxsmm_dnn_err_t libxsmm_dnn_fullyconnected_st_fwd_ncnc_kcck_bf16_bf16(libxsmm_
 }
 #else
 LIBXSMM_API_INTERN LIBXSMM_INTRINSICS(LIBXSMM_X86_AVX512_CORE)
-libxsmm_dnn_err_t libxsmm_dnn_fullyconnected_st_fwd_ncnc_kcck_bf16_bf16(libxsmm_dnn_layer* handle, int start_thread, int tid)
+libxsmm_dnn_err_t libxsmm_dnn_fullyconnected_st_fwd_ncnc_kcck_bf16_bf16(libxsmm_dnn_fullyconnected* handle, int start_thread, int tid)
 {
   return libxsmm_dnn_fullyconnected_st_fwd_ncnc_kcck_bf16_bf16_emu( handle, start_thread, tid );
 }
