f9723493a64397f2fc2dfa51dfee4c26d2fc65f4
egeorgan <evangelos.georganas@intel.com>
2020-01-27 15:47:39 -0800

    Enhanced tuning script

--- samples/deeplearning/fullyconnecteddriver/fc_bf16_tuning_job.sh
+++ samples/deeplearning/fullyconnecteddriver/fc_bf16_tuning_job.sh
@@ -7,7 +7,7 @@
 export OMP_NUM_THREADS=28
 export KMP_AFFINITY=granularity=fine,compact,1,0
 export CHECK=1
-ITERS=1000
+ITERS=10
 
 # Initialize Env vars
 export FWD_BF=1
@@ -30,30 +30,83 @@ MB=2016
 BFN=48
 IFM=1024
 OFM=1024
-rm -f FWD_TUNING_${MB}_${IFM}_${OFM}
-touch FWD_TUNING_${MB}_${IFM}_${OFM}
 
-THREADS=28
-export OMP_NUM_THREADS=${THREADS}
-export FWD_2D_BLOCKING=0
-for BFM in 32 64; do
-  for BFACC in 1 2 4 8; do
-    export FWD_BF=${BFACC}
-    ./layer_example_bf16 ${ITERS} ${MB} ${IFM} ${OFM} 0 F B ${BFN} ${BFM} ${BFM} >> FWD_TUNING_${MB}_${IFM}_${OFM}
-  done
-done
+for PASS in 'FWD' 'BWD' 'UPD'; do
+  if [ $PASS == "FWD" ]
+  then
+    PASS_ARG='F'
+  fi
+  if [ $PASS == "BWD" ]
+  then
+    PASS_ARG='B'
+  fi
+  if [ $PASS == "UPD" ]
+  then
+    PASS_ARG='U'
+  fi
 
-export FWD_2D_BLOCKING=1
-for ROWS in 7 14; do
-  export FWD_ROW_TEAMS=${ROWS}
-  COLUMNS=$((THREADS / ROWS))
-  export FWD_COLUMN_TEAMS=${COLUMNS}
-  for BFM in 32 64; do
-    for BFACC in 1 2 4 8; do
-      export FWD_BF=${BFACC}
-      ./layer_example_bf16 ${ITERS} ${MB} ${IFM} ${OFM} 0 F B ${BFN} ${BFM} ${BFM} >> FWD_TUNING_${MB}_${IFM}_${OFM}
+  echo ${PASS_ARG}
+
+  rm -f ${PASS}_TUNING_${MB}_${IFM}_${OFM}
+  touch ${PASS}_TUNING_${MB}_${IFM}_${OFM}
+
+
+  THREADS=28
+  export OMP_NUM_THREADS=${THREADS}
+  export ${PASS}_2D_BLOCKING=0
+
+  if [ $PASS == "UPD" ]
+  then
+    for IFMSUBTASKS in 1 2; do
+      export IFM_SUBTASKS=${IFMSUBTASKS}
+      for OFMSUBTASKS in 1 2; do
+        export OFM_SUBTASKS=${OFMSUBTASKS}
+        for BFM in 32 64; do
+          for BFACC in 1 2; do
+            export ${PASS}_BF=${BFACC}
+            ./layer_example_bf16 ${ITERS} ${MB} ${IFM} ${OFM} 0 ${PASS_ARG} B ${BFN} ${BFM} ${BFM} >> ${PASS}_TUNING_${MB}_${IFM}_${OFM}
+          done
+        done
+      done
     done
-  done
+  else
+    for BFM in 32 64; do
+      for BFACC in 1 2 4 8; do
+        export ${PASS}_BF=${BFACC}
+        ./layer_example_bf16 ${ITERS} ${MB} ${IFM} ${OFM} 0 ${PASS_ARG} B ${BFN} ${BFM} ${BFM} >> ${PASS}_TUNING_${MB}_${IFM}_${OFM}
+      done
+    done
+  fi
+
+  export ${PASS}_2D_BLOCKING=1
+  export IFM_SUBTASKS=1
+  export OFM_SUBTASKS=1
+  if [ $PASS == "UPD" ]
+  then
+    for ROWS in 7 14; do
+      export ${PASS}_ROW_TEAMS=${ROWS}
+      COLUMNS=$((THREADS / ROWS))
+      export ${PASS}_COLUMN_TEAMS=${COLUMNS}
+      for BFM in 32 64; do
+        for BFACC in 1 2; do
+          export ${PASS}_BF=${BFACC}
+          ./layer_example_bf16 ${ITERS} ${MB} ${IFM} ${OFM} 0 ${PASS_ARG} B ${BFN} ${BFM} ${BFM} >> ${PASS}_TUNING_${MB}_${IFM}_${OFM}
+        done
+      done
+    done
+  else
+    for ROWS in 7 14; do
+      export ${PASS}_ROW_TEAMS=${ROWS}
+      COLUMNS=$((THREADS / ROWS))
+      export ${PASS}_COLUMN_TEAMS=${COLUMNS}
+      for BFM in 32 64; do
+        for BFACC in 1 2 4 8; do
+          export ${PASS}_BF=${BFACC}
+          ./layer_example_bf16 ${ITERS} ${MB} ${IFM} ${OFM} 0 ${PASS_ARG} B ${BFN} ${BFM} ${BFM} >> ${PASS}_TUNING_${MB}_${IFM}_${OFM}
+        done
+      done
+    done
+  fi
 done
 
 
