d44ba2d8cca131c9ce5ff3aca7aefa115ddcb1bf
egeorgan <evangelos.georganas@intel.com>
2020-01-28 14:16:08 -0800

    Added tuning for all sweeps

--- samples/deeplearning/fullyconnecteddriver/fc_bf16_tuning_job.sh
+++ samples/deeplearning/fullyconnecteddriver/fc_bf16_tuning_job.sh
@@ -25,93 +25,246 @@ export UPD_COLUMN_TEAMS=1
 export IFM_SUBTASKS=1
 export OFM_SUBTASKS=1
 
-# Tune FWD
-MB=2016
+MB=2160
 BFN=24
-IFM=1024
-OFM=1024
 
-for PASS in 'FWD' 'BWD' 'UPD'; do
-  if [ $PASS == "FWD" ]
-  then
-    PASS_ARG='F'
-  fi
-  if [ $PASS == "BWD" ]
-  then
-    PASS_ARG='B'
-  fi
-  if [ $PASS == "UPD" ]
-  then
-    PASS_ARG='U'
-  fi
+# Tune layers (1024,1024) and (512,512)
+for THREADS in 20 24; do
+  for OFM in 512 1024; do
+    IFM=${OFM}
+    for PASS in 'FWD' 'BWD' 'UPD'; do
+      if [ $PASS == "FWD" ]
+      then
+        PASS_ARG='F'
+      fi
+      if [ $PASS == "BWD" ]
+      then
+        PASS_ARG='B'
+      fi
+      if [ $PASS == "UPD" ]
+      then
+        PASS_ARG='U'
+      fi
 
-  echo ${PASS_ARG}
+      rm -f ${PASS}_TUNING_${MB}_${IFM}_${OFM}_threads_${THREADS}
+      touch ${PASS}_TUNING_${MB}_${IFM}_${OFM}_threads_${THREADS}
 
-  rm -f ${PASS}_TUNING_${MB}_${IFM}_${OFM}
-  touch ${PASS}_TUNING_${MB}_${IFM}_${OFM}
+      export OMP_NUM_THREADS=${THREADS}
+      export ${PASS}_2D_BLOCKING=0
 
-
-  THREADS=28
-  export OMP_NUM_THREADS=${THREADS}
-  export ${PASS}_2D_BLOCKING=0
-
-  if [ $PASS == "UPD" ]
-  then
-    for IFMSUBTASKS in 1 2; do
-      export IFM_SUBTASKS=${IFMSUBTASKS}
-      for OFMSUBTASKS in 1 2; do
-        export OFM_SUBTASKS=${OFMSUBTASKS}
+      if [ $PASS == "UPD" ]
+      then
+        for IFMSUBTASKS in 1 2; do
+          export IFM_SUBTASKS=${IFMSUBTASKS}
+          for OFMSUBTASKS in 1 2; do
+            export OFM_SUBTASKS=${OFMSUBTASKS}
+            for BFM in 32 64; do
+              for BFACC in 1 2 3 6 9 10 15 30 45 90; do
+                export ${PASS}_BF=${BFACC}
+                ./layer_example_bf16 ${ITERS} ${MB} ${IFM} ${OFM} 0 ${PASS_ARG} B ${BFN} ${BFM} ${BFM} >> ${PASS}_TUNING_${MB}_${IFM}_${OFM}_threads_${THREADS}
+              done
+            done
+          done
+        done
+      else
         for BFM in 32 64; do
-          for BFACC in 1 2 4 7 14 21 42 84; do
+          for BFACC in 1 2 4 8; do
             export ${PASS}_BF=${BFACC}
-            ./layer_example_bf16 ${ITERS} ${MB} ${IFM} ${OFM} 0 ${PASS_ARG} B ${BFN} ${BFM} ${BFM} >> ${PASS}_TUNING_${MB}_${IFM}_${OFM}
+            ./layer_example_bf16 ${ITERS} ${MB} ${IFM} ${OFM} 0 ${PASS_ARG} B ${BFN} ${BFM} ${BFM} >> ${PASS}_TUNING_${MB}_${IFM}_${OFM}_threads_${THREADS}
+          done
+        done
+      fi
+
+      export ${PASS}_2D_BLOCKING=1
+      export IFM_SUBTASKS=1
+      export OFM_SUBTASKS=1
+      if [ $PASS == "UPD" ]
+      then
+        for COLUMNS in 2 4; do
+          export ${PASS}_COLUMN_TEAMS=${COLUMNS}
+          ROWS=$((THREADS / COLUMNS))
+          export ${PASS}_ROW_TEAMS=${ROWS}
+          for BFM in 32 64; do
+            for BFACC in 1 2 3 6 9 10 15 30 45 90; do
+              export ${PASS}_BF=${BFACC}
+              ./layer_example_bf16 ${ITERS} ${MB} ${IFM} ${OFM} 0 ${PASS_ARG} B ${BFN} ${BFM} ${BFM} >> ${PASS}_TUNING_${MB}_${IFM}_${OFM}_threads_${THREADS}
+            done
+          done
+        done
+      else
+        for COLUMNS in 2 4; do
+          export ${PASS}_COLUMN_TEAMS=${COLUMNS}
+          ROWS=$((THREADS / COLUMNS))
+          export ${PASS}_ROW_TEAMS=${ROWS}
+          for BFM in 32 64; do
+            for BFACC in 1 2 4 8; do
+              export ${PASS}_BF=${BFACC}
+              ./layer_example_bf16 ${ITERS} ${MB} ${IFM} ${OFM} 0 ${PASS_ARG} B ${BFN} ${BFM} ${BFM} >> ${PASS}_TUNING_${MB}_${IFM}_${OFM}_threads_${THREADS}
+            done
+          done
+        done
+      fi
+    done
+  done
+done
+
+# Tune layer (100,1024)
+IMF=100
+OFM=1024
+for THREADS in 20 24; do
+  for PASS in 'FWD' 'BWD' 'UPD'; do
+    if [ $PASS == "FWD" ]
+    then
+      PASS_ARG='F'
+    fi
+    if [ $PASS == "BWD" ]
+    then
+      PASS_ARG='B'
+    fi
+    if [ $PASS == "UPD" ]
+    then
+      PASS_ARG='U'
+    fi
+
+    rm -f ${PASS}_TUNING_${MB}_${IFM}_${OFM}_threads_${THREADS}
+    touch ${PASS}_TUNING_${MB}_${IFM}_${OFM}_threads_${THREADS}
+
+    export OMP_NUM_THREADS=${THREADS}
+    export ${PASS}_2D_BLOCKING=0
+
+    if [ $PASS == "UPD" ]
+    then
+      for IFMSUBTASKS in 1 2; do
+        export IFM_SUBTASKS=${IFMSUBTASKS}
+        for OFMSUBTASKS in 1 2; do
+          export OFM_SUBTASKS=${OFMSUBTASKS}
+          for BFM in 32 64; do
+            for BFACC in 1 2 3 6 9 10 15 30 45 90; do
+              export ${PASS}_BF=${BFACC}
+              ./layer_example_bf16 ${ITERS} ${MB} ${IFM} ${OFM} 0 ${PASS_ARG} B ${BFN} ${BFM} 50 >> ${PASS}_TUNING_${MB}_${IFM}_${OFM}_threads_${THREADS}
+              ./layer_example_bf16 ${ITERS} ${MB} ${IFM} ${OFM} 0 ${PASS_ARG} B ${BFN} ${BFM} 100 >> ${PASS}_TUNING_${MB}_${IFM}_${OFM}_threads_${THREADS}
+            done
           done
         done
       done
-    done
-  else
-    for BFM in 32 64; do
-      for BFACC in 1 2 4 8; do
-        export ${PASS}_BF=${BFACC}
-        ./layer_example_bf16 ${ITERS} ${MB} ${IFM} ${OFM} 0 ${PASS_ARG} B ${BFN} ${BFM} ${BFM} >> ${PASS}_TUNING_${MB}_${IFM}_${OFM}
-      done
-    done
-  fi
-
-  export ${PASS}_2D_BLOCKING=1
-  export IFM_SUBTASKS=1
-  export OFM_SUBTASKS=1
-  if [ $PASS == "UPD" ]
-  then
-    for ROWS in 7 14; do
-      export ${PASS}_ROW_TEAMS=${ROWS}
-      COLUMNS=$((THREADS / ROWS))
-      export ${PASS}_COLUMN_TEAMS=${COLUMNS}
-      for BFM in 32 64; do
-        for BFACC in 1 2 4 7 14 21 42 84; do
-          export ${PASS}_BF=${BFACC}
-          ./layer_example_bf16 ${ITERS} ${MB} ${IFM} ${OFM} 0 ${PASS_ARG} B ${BFN} ${BFM} ${BFM} >> ${PASS}_TUNING_${MB}_${IFM}_${OFM}
-        done
-      done
-    done
-  else
-    for ROWS in 7 14; do
-      export ${PASS}_ROW_TEAMS=${ROWS}
-      COLUMNS=$((THREADS / ROWS))
-      export ${PASS}_COLUMN_TEAMS=${COLUMNS}
+    else
       for BFM in 32 64; do
         for BFACC in 1 2 4 8; do
           export ${PASS}_BF=${BFACC}
-          ./layer_example_bf16 ${ITERS} ${MB} ${IFM} ${OFM} 0 ${PASS_ARG} B ${BFN} ${BFM} ${BFM} >> ${PASS}_TUNING_${MB}_${IFM}_${OFM}
+          ./layer_example_bf16 ${ITERS} ${MB} ${IFM} ${OFM} 0 ${PASS_ARG} B ${BFN} ${BFM} ${BFM} >> ${PASS}_TUNING_${MB}_${IFM}_${OFM}_threads_${THREADS}
         done
       done
-    done
-  fi
+    fi
+
+    export ${PASS}_2D_BLOCKING=1
+    export IFM_SUBTASKS=1
+    export OFM_SUBTASKS=1
+    if [ $PASS == "UPD" ]
+    then
+      for COLUMNS in 2 4; do
+        export ${PASS}_COLUMN_TEAMS=${COLUMNS}
+        ROWS=$((THREADS / COLUMNS))
+        export ${PASS}_ROW_TEAMS=${ROWS}
+        for BFM in 32 64; do
+          for BFACC in 1 2 3 6 9 10 15 30 45 90; do
+            export ${PASS}_BF=${BFACC}
+            ./layer_example_bf16 ${ITERS} ${MB} ${IFM} ${OFM} 0 ${PASS_ARG} B ${BFN} ${BFM} 50 >> ${PASS}_TUNING_${MB}_${IFM}_${OFM}_threads_${THREADS}
+            ./layer_example_bf16 ${ITERS} ${MB} ${IFM} ${OFM} 0 ${PASS_ARG} B ${BFN} ${BFM} 100 >> ${PASS}_TUNING_${MB}_${IFM}_${OFM}_threads_${THREADS}
+          done
+        done
+      done
+    else
+      for COLUMNS in 2 4; do
+        export ${PASS}_COLUMN_TEAMS=${COLUMNS}
+        ROWS=$((THREADS / COLUMNS))
+        export ${PASS}_ROW_TEAMS=${ROWS}
+        for BFM in 32 64; do
+          for BFACC in 1 2 4 8; do
+            export ${PASS}_BF=${BFACC}
+            ./layer_example_bf16 ${ITERS} ${MB} ${IFM} ${OFM} 0 ${PASS_ARG} B ${BFN} ${BFM} ${BFM} >> ${PASS}_TUNING_${MB}_${IFM}_${OFM}_threads_${THREADS}
+          done
+        done
+      done
+    fi
+  done
 done
 
+# Tune layer (1024,1)
+IMF=1024
+OFM=1
+for THREADS in 20 24; do
+  for PASS in 'FWD' 'BWD' 'UPD'; do
+    if [ $PASS == "FWD" ]
+    then
+      PASS_ARG='F'
+    fi
+    if [ $PASS == "BWD" ]
+    then
+      PASS_ARG='B'
+    fi
+    if [ $PASS == "UPD" ]
+    then
+      PASS_ARG='U'
+    fi
 
-#srun -n 1 ./layer_example_bf16 ${ITERS} ${MB} 512 512 0 F B 32 32 32
-#srun -n 1 ./layer_example_bf16 ${ITERS} ${MB} 100 1024 0 F B 32 32 50
-#srun -n 1 ./layer_example_bf16 ${ITERS} ${MB} 1024 1 0 F B 32 32 32
+    rm -f ${PASS}_TUNING_${MB}_${IFM}_${OFM}_threads_${THREADS}
+    touch ${PASS}_TUNING_${MB}_${IFM}_${OFM}_threads_${THREADS}
 
+    export OMP_NUM_THREADS=${THREADS}
+    export ${PASS}_2D_BLOCKING=0
+
+    if [ $PASS == "UPD" ]
+    then
+      for IFMSUBTASKS in 1 2; do
+        export IFM_SUBTASKS=${IFMSUBTASKS}
+        for OFMSUBTASKS in 1 2; do
+          export OFM_SUBTASKS=${OFMSUBTASKS}
+          for BFM in 16 32 64; do
+            for BFACC in 1 2 3 6 9 10 15 30 45 90; do
+              export ${PASS}_BF=${BFACC}
+              ./layer_example_bf16 ${ITERS} ${MB} ${IFM} ${OFM} 0 ${PASS_ARG} B ${BFN} 1 ${BFM} >> ${PASS}_TUNING_${MB}_${IFM}_${OFM}_threads_${THREADS}
+            done
+          done
+        done
+      done
+    else
+      for BFM in 16 32 64; do
+        for BFACC in 1 2 4 8; do
+          export ${PASS}_BF=${BFACC}
+          ./layer_example_bf16 ${ITERS} ${MB} ${IFM} ${OFM} 0 ${PASS_ARG} B ${BFN} 1 ${BFM} >> ${PASS}_TUNING_${MB}_${IFM}_${OFM}_threads_${THREADS}
+        done
+      done
+    fi
+
+    export ${PASS}_2D_BLOCKING=1
+    export IFM_SUBTASKS=1
+    export OFM_SUBTASKS=1
+    if [ $PASS == "UPD" ]
+    then
+      for COLUMNS in 2 4; do
+        export ${PASS}_COLUMN_TEAMS=${COLUMNS}
+        ROWS=$((THREADS / COLUMNS))
+        export ${PASS}_ROW_TEAMS=${ROWS}
+        for BFM in 16 32 64; do
+          for BFACC in 1 2 3 6 9 10 15 30 45 90; do
+            export ${PASS}_BF=${BFACC}
+            ./layer_example_bf16 ${ITERS} ${MB} ${IFM} ${OFM} 0 ${PASS_ARG} B ${BFN} 1 ${BFM} >> ${PASS}_TUNING_${MB}_${IFM}_${OFM}_threads_${THREADS}
+          done
+        done
+      done
+    else
+      for COLUMNS in 2 4; do
+        export ${PASS}_COLUMN_TEAMS=${COLUMNS}
+        ROWS=$((THREADS / COLUMNS))
+        export ${PASS}_ROW_TEAMS=${ROWS}
+        for BFM in 16 32 64; do
+          for BFACC in 1 2 4 8; do
+            export ${PASS}_BF=${BFACC}
+            ./layer_example_bf16 ${ITERS} ${MB} ${IFM} ${OFM} 0 ${PASS_ARG} B ${BFN} 1 ${BFM} >> ${PASS}_TUNING_${MB}_${IFM}_${OFM}_threads_${THREADS}
+          done
+        done
+      done
+    fi
+  done
+done
 
