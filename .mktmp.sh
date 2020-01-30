0aae4cb588e5bb2f98a104810c51144e217084c8
Hans Pabst <hf.pabst@gmail.com>
2020-01-30 20:48:06 +0100

    Script cleanup [skip ci].

--- .mktmp.sh
+++ .mktmp.sh
@@ -11,16 +11,15 @@
 ###############################################################################
 
 MKTEMP=$(command -v mktemp)
-ECHO=$(command -v echo)
 MV=$(command -v mv)
 
-if [ "" != "${MKTEMP}" ] && [ "" != "${ECHO}" ] && [ "" != "${MV}" ]; then
+if [ "" != "${MKTEMP}" ] && [ "" != "${MV}" ]; then
   TEMPLATE=${1/XXXXXX/}.XXXXXX
   TMPFILE=$(${MKTEMP} ${TEMPLATE})
   EXTFILE=${TMPFILE: -6}
   NEWFILE=${1/XXXXXX/${EXTFILE}}
   ${MV} ${TMPFILE} ${NEWFILE}
-  ${ECHO} "${NEWFILE}"
+  echo "${NEWFILE}"
 else
   touch $1
 fi
