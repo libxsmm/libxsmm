ae63684d8452527c0f3f593f1c8da7abe470c739
Alexander Heinecke <alexander.heinecke@intel.com>
2020-02-03 06:15:15 -0800

    reworked modularity for new sparse = dense_soa * dense_soa generator to allow for future register blocking

--- src/generator_spgemm_csc_csparse_soa.c
+++ src/generator_spgemm_csc_csparse_soa.c
@@ -11,7 +11,6 @@
 #include "generator_spgemm_csc_csparse_soa.h"
 #include "generator_gemm_common.h"
 #include "generator_x86_instructions.h"
-#include "generator_common.h"
 #include "libxsmm_main.h"
 
 #if defined(LIBXSMM_OFFLOAD_TARGET)
@@ -23,6 +22,150 @@
 #if defined(LIBXSMM_OFFLOAD_TARGET)
 # pragma offload_attribute(pop)
 #endif
+
+LIBXSMM_API_INTERN
+void libxsmm_genertaor_spgemm_csc_csparse_soa_axv256_512_single( libxsmm_generated_code*            io_generated_code,
+                                                                 libxsmm_loop_label_tracker*        io_loop_label_tracker,
+                                                                 const libxsmm_gp_reg_mapping*      i_gp_reg_mapping,
+                                                                 const libxsmm_micro_kernel_config* i_micro_kernel_config,
+                                                                 const libxsmm_gemm_descriptor*     i_xgemm_desc,
+                                                                 const unsigned int*                i_row_idx,
+                                                                 const unsigned int*                i_column_idx,
+                                                                 const unsigned int                 i_soa_width,
+                                                                 const unsigned int                 i_n,
+                                                                 const unsigned int                 i_m ) {
+  /* set c accumulator to 0 */
+  libxsmm_x86_instruction_vec_compute_reg( io_generated_code,
+                                           i_micro_kernel_config->instruction_set,
+                                           i_micro_kernel_config->vxor_instruction,
+                                           i_micro_kernel_config->vector_name,
+                                           31, 31, 31 );
+
+  /* k loop */
+  libxsmm_x86_instruction_alu_imm( io_generated_code, i_micro_kernel_config->alu_mov_instruction, i_gp_reg_mapping->gp_reg_kloop, 0 );
+  libxsmm_x86_instruction_register_jump_back_label( io_generated_code, io_loop_label_tracker );
+  libxsmm_x86_instruction_alu_imm( io_generated_code, i_micro_kernel_config->alu_add_instruction, i_gp_reg_mapping->gp_reg_kloop, 1 );
+
+  /* load a */
+  libxsmm_x86_instruction_vec_move( io_generated_code,
+                                    i_micro_kernel_config->instruction_set,
+                                    LIBXSMM_X86_INSTR_VMOVUPS,
+                                    i_gp_reg_mapping->gp_reg_a,
+                                    LIBXSMM_X86_GP_REG_UNDEF, 0,
+                                    i_micro_kernel_config->datatype_size*i_soa_width*i_row_idx[i_column_idx[i_n]+i_m],
+                                    i_micro_kernel_config->vector_name,
+                                    0, 0, 1, 0 );
+
+  /* load b */
+  libxsmm_x86_instruction_vec_move( io_generated_code,
+                                    i_micro_kernel_config->instruction_set,
+                                    LIBXSMM_X86_INSTR_VMOVUPS,
+                                    i_gp_reg_mapping->gp_reg_b,
+                                    LIBXSMM_X86_GP_REG_UNDEF, 0,
+                                    i_micro_kernel_config->datatype_size*i_soa_width*i_n,
+                                    i_micro_kernel_config->vector_name,
+                                    1, 0, 1, 0 );
+
+  /* FMA */
+  libxsmm_x86_instruction_vec_compute_reg( io_generated_code,
+                                           i_micro_kernel_config->instruction_set,
+                                           LIBXSMM_X86_INSTR_VFMADD231PS,
+                                           i_micro_kernel_config->vector_name,
+                                           0, 1, 31 );
+
+  /* advance a and b pointer */
+  libxsmm_x86_instruction_alu_imm( io_generated_code, i_micro_kernel_config->alu_add_instruction, i_gp_reg_mapping->gp_reg_a, i_micro_kernel_config->datatype_size*i_soa_width*i_xgemm_desc->lda );
+  libxsmm_x86_instruction_alu_imm( io_generated_code, i_micro_kernel_config->alu_add_instruction, i_gp_reg_mapping->gp_reg_b, i_micro_kernel_config->datatype_size*i_soa_width*i_xgemm_desc->ldb );
+
+  /* close k loop */
+  libxsmm_x86_instruction_alu_imm( io_generated_code, i_micro_kernel_config->alu_cmp_instruction, i_gp_reg_mapping->gp_reg_kloop, i_xgemm_desc->k );
+  libxsmm_x86_instruction_jump_back_to_label( io_generated_code, i_micro_kernel_config->alu_jmp_instruction, io_loop_label_tracker );
+
+  /* re-set a and b pointer */
+  libxsmm_x86_instruction_alu_imm( io_generated_code, i_micro_kernel_config->alu_sub_instruction, i_gp_reg_mapping->gp_reg_a, i_xgemm_desc->k*i_micro_kernel_config->datatype_size*i_soa_width*i_xgemm_desc->lda );
+  libxsmm_x86_instruction_alu_imm( io_generated_code, i_micro_kernel_config->alu_sub_instruction, i_gp_reg_mapping->gp_reg_b, i_xgemm_desc->k*i_micro_kernel_config->datatype_size*i_soa_width*i_xgemm_desc->ldb );
+
+  /* reduce C */
+  /* zmm31; 0000 0000 0000 0000 -> ---- ---- 0000 0000 */
+  libxsmm_x86_instruction_vec_shuffle_reg( io_generated_code,
+                                           i_micro_kernel_config->instruction_set,
+                                           LIBXSMM_X86_INSTR_VSHUFF64X2,
+                                           i_micro_kernel_config->vector_name,
+                                           31, 31, 0, 0x4e );
+
+  libxsmm_x86_instruction_vec_compute_reg( io_generated_code,
+                                           i_micro_kernel_config->instruction_set,
+                                           LIBXSMM_X86_INSTR_VADDPS,
+                                           i_micro_kernel_config->vector_name,
+                                           31, 0, 31 );
+
+  /* zmm31: ---- ---- 0000 0000 -> ---- ---- ---- 0000 */
+  libxsmm_x86_instruction_vec_shuffle_reg( io_generated_code,
+                                           i_micro_kernel_config->instruction_set,
+                                           LIBXSMM_X86_INSTR_VSHUFF64X2,
+                                           i_micro_kernel_config->vector_name,
+                                           31, 31, 0, 0xb1 );
+
+  libxsmm_x86_instruction_vec_compute_reg( io_generated_code,
+                                           i_micro_kernel_config->instruction_set,
+                                           LIBXSMM_X86_INSTR_VADDPS,
+                                           i_micro_kernel_config->vector_name,
+                                           31, 0, 15 );
+
+  /* ymm15;           ---- 0000 ->           ---- --00 */
+  libxsmm_x86_instruction_vec_shuffle_reg( io_generated_code,
+                                           i_micro_kernel_config->instruction_set,
+                                           LIBXSMM_X86_INSTR_VSHUFPS,
+                                           'y',
+                                           15, 15, 0, 0x4e );
+
+  libxsmm_x86_instruction_vec_compute_reg( io_generated_code,
+                                           i_micro_kernel_config->instruction_set,
+                                           LIBXSMM_X86_INSTR_VADDPS,
+                                           'y',
+                                           15, 0, 15 );
+
+  /* ymm15;           ---- --00 ->           ---- ---0 */
+  libxsmm_x86_instruction_vec_shuffle_reg( io_generated_code,
+                                           i_micro_kernel_config->instruction_set,
+                                           LIBXSMM_X86_INSTR_VSHUFPS,
+                                           'y',
+                                           15, 15, 0, 0x1 );
+
+  libxsmm_x86_instruction_vec_compute_reg( io_generated_code,
+                                           i_micro_kernel_config->instruction_set,
+                                           LIBXSMM_X86_INSTR_VADDPS,
+                                           'y',
+                                           15, 0, 15 );
+
+  /* update sparse C */
+  if ( 0 == (LIBXSMM_GEMM_FLAG_BETA_0 & i_xgemm_desc->flags) ) {
+    libxsmm_x86_instruction_vec_move( io_generated_code,
+                                      i_micro_kernel_config->instruction_set,
+                                      LIBXSMM_X86_INSTR_VMOVSS,
+                                      i_gp_reg_mapping->gp_reg_c,
+                                      LIBXSMM_X86_GP_REG_UNDEF, 0,
+                                      i_micro_kernel_config->datatype_size*(i_column_idx[i_n]+i_m),
+                                      'x',
+                                      0, 0, 1, 0 );
+
+    libxsmm_x86_instruction_vec_compute_reg( io_generated_code,
+                                             i_micro_kernel_config->instruction_set,
+                                             LIBXSMM_X86_INSTR_VADDSS,
+                                             'x',
+                                             15, 0, 15 );
+  }
+
+  libxsmm_x86_instruction_vec_move( io_generated_code,
+                                    i_micro_kernel_config->instruction_set,
+                                    LIBXSMM_X86_INSTR_VMOVSS,
+                                    i_gp_reg_mapping->gp_reg_c,
+                                    LIBXSMM_X86_GP_REG_UNDEF, 0,
+                                    i_micro_kernel_config->datatype_size*(i_column_idx[i_n]+i_m),
+                                    'x',
+                                    15, 0, 1, 1 );
+}
+
 
 LIBXSMM_API_INTERN
 void libxsmm_generator_spgemm_csc_csparse_soa( libxsmm_generated_code*         io_generated_code,
@@ -60,6 +203,7 @@ void libxsmm_generator_spgemm_csc_csparse_soa( libxsmm_generated_code*         i
     exit(-1);
   }
 }
+
 
 LIBXSMM_API_INTERN
 void libxsmm_generator_spgemm_csc_csparse_soa_avx256_512( libxsmm_generated_code*         io_generated_code,
@@ -137,137 +281,10 @@ void libxsmm_generator_spgemm_csc_csparse_soa_avx256_512( libxsmm_generated_code
   /* loop over the sparse elements of C */
   for ( l_n = 0; l_n < (unsigned int)i_xgemm_desc->n; l_n++ ) {
     unsigned int l_col_elements = i_column_idx[l_n+1] - i_column_idx[l_n];
+
     for ( l_m = 0; l_m < l_col_elements; ++l_m ) {
-      /* set c accumulator to 0 */
-      libxsmm_x86_instruction_vec_compute_reg( io_generated_code,
-                                               l_micro_kernel_config.instruction_set,
-                                               l_micro_kernel_config.vxor_instruction,
-                                               l_micro_kernel_config.vector_name,
-                                               31, 31, 31 );
-
-      /* k loop */
-      libxsmm_x86_instruction_alu_imm( io_generated_code, l_micro_kernel_config.alu_mov_instruction, l_gp_reg_mapping.gp_reg_kloop, 0 );
-      libxsmm_x86_instruction_register_jump_back_label( io_generated_code, &l_loop_label_tracker );
-      libxsmm_x86_instruction_alu_imm( io_generated_code, l_micro_kernel_config.alu_add_instruction, l_gp_reg_mapping.gp_reg_kloop, 1 );
-
-      /* load a */
-      libxsmm_x86_instruction_vec_move( io_generated_code,
-                                        l_micro_kernel_config.instruction_set,
-                                        LIBXSMM_X86_INSTR_VMOVUPS,
-                                        l_gp_reg_mapping.gp_reg_a,
-                                        LIBXSMM_X86_GP_REG_UNDEF, 0,
-                                        l_micro_kernel_config.datatype_size*l_soa_width*i_row_idx[i_column_idx[l_n]+l_m],
-                                        l_micro_kernel_config.vector_name,
-                                        0, 0, 1, 0 );
-
-      /* load b */
-      libxsmm_x86_instruction_vec_move( io_generated_code,
-                                        l_micro_kernel_config.instruction_set,
-                                        LIBXSMM_X86_INSTR_VMOVUPS,
-                                        l_gp_reg_mapping.gp_reg_b,
-                                        LIBXSMM_X86_GP_REG_UNDEF, 0,
-                                        l_micro_kernel_config.datatype_size*l_soa_width*l_n,
-                                        l_micro_kernel_config.vector_name,
-                                        1, 0, 1, 0 );
-
-      /* FMA */
-      libxsmm_x86_instruction_vec_compute_reg( io_generated_code,
-                                               l_micro_kernel_config.instruction_set,
-                                               LIBXSMM_X86_INSTR_VFMADD231PS,
-                                               l_micro_kernel_config.vector_name,
-                                               0, 1, 31 );
-
-      /* advance a and b pointer */
-      libxsmm_x86_instruction_alu_imm( io_generated_code, l_micro_kernel_config.alu_add_instruction, l_gp_reg_mapping.gp_reg_a, l_micro_kernel_config.datatype_size*l_soa_width*i_xgemm_desc->lda );
-      libxsmm_x86_instruction_alu_imm( io_generated_code, l_micro_kernel_config.alu_add_instruction, l_gp_reg_mapping.gp_reg_b, l_micro_kernel_config.datatype_size*l_soa_width*i_xgemm_desc->ldb );
-
-      /* close k loop */
-      libxsmm_x86_instruction_alu_imm( io_generated_code, l_micro_kernel_config.alu_cmp_instruction, l_gp_reg_mapping.gp_reg_kloop, i_xgemm_desc->k );
-      libxsmm_x86_instruction_jump_back_to_label( io_generated_code, l_micro_kernel_config.alu_jmp_instruction, &l_loop_label_tracker );
-
-      /* re-set a and b pointer */
-      libxsmm_x86_instruction_alu_imm( io_generated_code, l_micro_kernel_config.alu_sub_instruction, l_gp_reg_mapping.gp_reg_a, i_xgemm_desc->k*l_micro_kernel_config.datatype_size*l_soa_width*i_xgemm_desc->lda );
-      libxsmm_x86_instruction_alu_imm( io_generated_code, l_micro_kernel_config.alu_sub_instruction, l_gp_reg_mapping.gp_reg_b, i_xgemm_desc->k*l_micro_kernel_config.datatype_size*l_soa_width*i_xgemm_desc->ldb );
-
-      /* reduce C */
-      /* zmm31; 0000 0000 0000 0000 -> ---- ---- 0000 0000 */
-      libxsmm_x86_instruction_vec_shuffle_reg( io_generated_code,
-                                               l_micro_kernel_config.instruction_set,
-                                               LIBXSMM_X86_INSTR_VSHUFF64X2,
-                                               l_micro_kernel_config.vector_name,
-                                               31, 31, 0, 0x4e );
-
-      libxsmm_x86_instruction_vec_compute_reg( io_generated_code,
-                                               l_micro_kernel_config.instruction_set,
-                                               LIBXSMM_X86_INSTR_VADDPS,
-                                               l_micro_kernel_config.vector_name,
-                                               31, 0, 31 );
-
-      /* zmm31: ---- ---- 0000 0000 -> ---- ---- ---- 0000 */
-      libxsmm_x86_instruction_vec_shuffle_reg( io_generated_code,
-                                               l_micro_kernel_config.instruction_set,
-                                               LIBXSMM_X86_INSTR_VSHUFF64X2,
-                                               l_micro_kernel_config.vector_name,
-                                               31, 31, 0, 0xb1 );
-
-      libxsmm_x86_instruction_vec_compute_reg( io_generated_code,
-                                               l_micro_kernel_config.instruction_set,
-                                               LIBXSMM_X86_INSTR_VADDPS,
-                                               l_micro_kernel_config.vector_name,
-                                               31, 0, 15 );
-
-      /* ymm15;           ---- 0000 ->           ---- --00 */
-      libxsmm_x86_instruction_vec_shuffle_reg( io_generated_code,
-                                               l_micro_kernel_config.instruction_set,
-                                               LIBXSMM_X86_INSTR_VSHUFPS,
-                                               'y',
-                                               15, 15, 0, 0x4e );
-
-      libxsmm_x86_instruction_vec_compute_reg( io_generated_code,
-                                               l_micro_kernel_config.instruction_set,
-                                               LIBXSMM_X86_INSTR_VADDPS,
-                                               'y',
-                                               15, 0, 15 );
-
-      /* ymm15;           ---- --00 ->           ---- ---0 */
-      libxsmm_x86_instruction_vec_shuffle_reg( io_generated_code,
-                                               l_micro_kernel_config.instruction_set,
-                                               LIBXSMM_X86_INSTR_VSHUFPS,
-                                               'y',
-                                               15, 15, 0, 0x1 );
-
-      libxsmm_x86_instruction_vec_compute_reg( io_generated_code,
-                                               l_micro_kernel_config.instruction_set,
-                                               LIBXSMM_X86_INSTR_VADDPS,
-                                               'y',
-                                               15, 0, 15 );
-
-      /* update sparse C */
-      if ( 0 == (LIBXSMM_GEMM_FLAG_BETA_0 & i_xgemm_desc->flags) ) {
-        libxsmm_x86_instruction_vec_move( io_generated_code,
-                                          l_micro_kernel_config.instruction_set,
-                                          LIBXSMM_X86_INSTR_VMOVSS,
-                                          l_gp_reg_mapping.gp_reg_c,
-                                          LIBXSMM_X86_GP_REG_UNDEF, 0,
-                                          l_micro_kernel_config.datatype_size*(i_column_idx[l_n]+l_m),
-                                          'x',
-                                          0, 0, 1, 0 );
-
-        libxsmm_x86_instruction_vec_compute_reg( io_generated_code,
-                                                 l_micro_kernel_config.instruction_set,
-                                                 LIBXSMM_X86_INSTR_VADDSS,
-                                                 'x',
-                                                 15, 0, 15 );
-      }
-
-      libxsmm_x86_instruction_vec_move( io_generated_code,
-                                        l_micro_kernel_config.instruction_set,
-                                        LIBXSMM_X86_INSTR_VMOVSS,
-                                        l_gp_reg_mapping.gp_reg_c,
-                                        LIBXSMM_X86_GP_REG_UNDEF, 0,
-                                        l_micro_kernel_config.datatype_size*(i_column_idx[l_n]+l_m),
-                                        'x',
-                                        15, 0, 1, 1 );
+      libxsmm_genertaor_spgemm_csc_csparse_soa_axv256_512_single( io_generated_code, &l_loop_label_tracker, &l_gp_reg_mapping, &l_micro_kernel_config, i_xgemm_desc,
+                                                                    i_row_idx, i_column_idx, l_soa_width, l_n, l_m );
     }
   }
 
