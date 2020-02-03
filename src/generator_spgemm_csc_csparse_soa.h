75197a6df7e082741feed5480d39946593f326c2
Alexander Heinecke <alexander.heinecke@intel.com>
2020-02-03 09:54:45 -0800

    added 16blocked version

--- src/generator_spgemm_csc_csparse_soa.h
+++ src/generator_spgemm_csc_csparse_soa.h
@@ -31,7 +31,6 @@ void libxsmm_generator_spgemm_csc_csparse_soa_avx256_512( libxsmm_generated_code
                                                           const unsigned int*             i_column_idx,
                                                           const void*                     i_values );
 
-
 LIBXSMM_API_INTERN
 void libxsmm_genertaor_spgemm_csc_csparse_soa_axv256_512_single( libxsmm_generated_code*            io_generated_code,
                                                                  libxsmm_loop_label_tracker*        io_loop_label_tracker,
@@ -43,5 +42,19 @@ void libxsmm_genertaor_spgemm_csc_csparse_soa_axv256_512_single( libxsmm_generat
                                                                  const unsigned int                 i_soa_width,
                                                                  const unsigned int                 i_n,
                                                                  const unsigned int                 i_m );
+
+LIBXSMM_API_INTERN
+void libxsmm_genertaor_spgemm_csc_csparse_soa_axv256_512_16accs( libxsmm_generated_code*            io_generated_code,
+                                                                 libxsmm_loop_label_tracker*        io_loop_label_tracker,
+                                                                 const libxsmm_gp_reg_mapping*      i_gp_reg_mapping,
+                                                                 const libxsmm_micro_kernel_config* i_micro_kernel_config,
+                                                                 const libxsmm_gemm_descriptor*     i_xgemm_desc,
+                                                                 const unsigned int*                i_row_idx,
+                                                                 const unsigned int*                i_column_idx,
+                                                                 const unsigned int                 i_soa_width,
+                                                                 const unsigned int                 i_n,
+                                                                 const unsigned int                 i_m,
+                                                                 const unsigned int                 i_m_blocking );
+
 #endif /* GENERATOR_SPGEMM_CSC_CSPARSE_SOA_H */
 
