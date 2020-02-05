c682642976abf5ce5b39132cd9acf0626f299a8c
Hans Pabst <hans.pabst@intel.com>
2020-02-05 14:33:01 +0100

    Some preparation to eventually use Sphinx [skip ci].

--- documentation/conf.py
+++ documentation/conf.py
@@ -1,59 +1,43 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
-# -- Project information -----------------------------------------------------
+###############################################################################
+# Copyright (c) Intel Corporation - All rights reserved.                      #
+# This file is part of the LIBXSMM library.                                   #
+#                                                                             #
+# For information on the license, see the LICENSE file.                       #
+# Further information: https://github.com/hfp/libxsmm/                        #
+# SPDX-License-Identifier: BSD-3-Clause                                       #
+###############################################################################
+# Hans Pabst (Intel Corp.)
+###############################################################################
+import sphinx_rtd_theme
 
 project = 'LIBXSMM'
 copyright = '2009-2020, Intel Corporation.'
 author = 'Intel Corporation'
 
-
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
+# m2r implies recommonmark
 extensions = [
-    'recommonmark'
+    "m2r"
 ]
 
-source_suffix = ['.md']
-master_doc = 'index'
+master_doc = "index"
+source_suffix = [
+    ".rst", ".md"
+]
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+exclude_patterns = [
+    "Thumbs.db",
+    ".DS_Store",
+    "_build"
+]
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'sphinx_rtd_theme'
-
+html_theme = "sphinx_rtd_theme"
 html_theme_options = {
-    'navigation_depth': 1
+    "navigation_depth": 2
 }
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_static_path = ["../.theme"]
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['../.theme']
+templates_path = ["_templates"]
+pygments_style = "sphinx"
+
+language = None
---
+++ documentation/index.rst
@@ -0,0 +1,11 @@
+.. toctree::
+   :hidden:
+
+   MM: Matrix Multiplication <libxsmm_mm.md>
+   DNN: Deep Neural Networks <libxsmm_dl.md>
+   AUX: Service Functions <libxsmm_aux.md>
+   PROF: Performance Profiling <libxsmm_prof.md>
+   TUNE: Customization <libxsmm_tune.md>
+   BE: Backend <libxsmm_be.md>
+
+.. mdinclude:: index.md
