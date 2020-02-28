ccb788fabcf62f3814a774cdbdde37aeebc0993a
Hans Pabst <hans.pabst@intel.com>
2020-02-28 17:59:42 +0100

    Moved implementation of internal function libxsmm_dnn_get_feature_map_blocks (from libxsmm_main.c to libxsmm_dnn.c).

--- src/libxsmm_main.c
+++ src/libxsmm_main.c
@@ -1431,61 +1431,6 @@ LIBXSMM_API_INLINE const char* internal_get_typesize_string(size_t typesize)
 }
 
 
-LIBXSMM_API_INTERN libxsmm_dnn_err_t libxsmm_dnn_get_feature_map_blocks( int C, int K, int* C_block, int* K_block, int* fm_lp_block, libxsmm_dnn_datatype datatype_in, libxsmm_dnn_datatype datatype_out ) {
-  libxsmm_dnn_err_t status = LIBXSMM_DNN_SUCCESS;
-  int ifmblock = 0;
-  int ofmblock = 0;
-  int lp_block = 0;
-  int tmp_max_c_block = 32;
-  int tmp_max_k_block = 32;
-  int tmp_block = 0;
-
-  /* C */
-  if (libxsmm_target_archid >= LIBXSMM_X86_AVX512_CORE) {
-    tmp_max_c_block = 64;
-  }
-  if ( C < tmp_max_c_block ) {
-    ifmblock = C;
-  } else {
-    for ( tmp_block = 1; tmp_block <= tmp_max_c_block; tmp_block *= 2 ) {
-      if ( C % tmp_block == 0 ) ifmblock = tmp_block;
-    }
-  }
-
-  /* K */
-  if (libxsmm_target_archid >= LIBXSMM_X86_AVX512_CORE) {
-    tmp_max_k_block = 64;
-  }
-  if ( K < tmp_max_k_block ) {
-    ofmblock = K;
-  } else {
-    for ( tmp_block = 1; tmp_block <= tmp_max_k_block; tmp_block *= 2 ) {
-      if ( K % tmp_block == 0 ) ofmblock = tmp_block;
-    }
-  }
-
-  /* when do we need VNNI format? */
-  if ( (datatype_in == LIBXSMM_DNN_DATATYPE_F32) && (datatype_out == LIBXSMM_DNN_DATATYPE_F32) ) {
-    lp_block = 1;
-  } else if ( (datatype_in == LIBXSMM_DNN_DATATYPE_BF16) && (datatype_out == LIBXSMM_DNN_DATATYPE_BF16) ) {
-    lp_block = 2;
-  } else if ( (datatype_in == LIBXSMM_DNN_DATATYPE_I16) && ((datatype_out == LIBXSMM_DNN_DATATYPE_I32) || (datatype_out == LIBXSMM_DNN_DATATYPE_F32)) ) {
-    lp_block = 2;
-  } else if (datatype_in == LIBXSMM_DNN_DATATYPE_I8) {
-    lp_block = 4;
-  } else {
-    status = LIBXSMM_DNN_ERR_UNSUPPORTED_DATATYPE;
-    return status;
-  }
-
-  *C_block = ifmblock;
-  *K_block = ofmblock;
-  *fm_lp_block = lp_block;
-
-  return status;
-}
-
-
 LIBXSMM_API_INTERN int libxsmm_build(const libxsmm_build_request* request, unsigned int regindex, libxsmm_code_pointer* code)
 {
   int result = EXIT_SUCCESS;
