c93653270b9855cc61a931788f2e7ac585dd69ef
Hans Pabst <hf.pabst@gmail.com>
2020-04-26 11:47:05 +0200

    Improved test case (matcopy).

--- tests/matcopy.c
+++ tests/matcopy.c
@@ -13,6 +13,15 @@
 #if !defined(ELEM_TYPE)
 # define ELEM_TYPE float
 #endif
+#if !defined(TEST_MZERO)
+# define TEST_MZERO
+#endif
+#if !defined(TEST_MCOPY)
+# define TEST_MCOPY
+#endif
+#if !defined(TEST_JIT)
+# define TEST_JIT
+#endif
 
 #if LIBXSMM_EQUAL(ELEM_TYPE, float) || LIBXSMM_EQUAL(ELEM_TYPE, double)
 # if defined(__MKL) || defined(MKL_DIRECT_CALL_SEQ) || defined(MKL_DIRECT_CALL)
@@ -43,7 +52,7 @@ int main(void)
   const libxsmm_blasint ldo[] = { 0, 1, 1, 1, 1, 1, 1, 8, 2,  2, 3, 4, 6, 6, 8, 8, 9, 9,  9,  16, 16, 64, 512,  16, 3072 };
   const int prefetch[]        = { 0, 0, 1, 1, 1, 0, 1, 0, 1,  0, 1, 0, 1, 0, 1, 0, 0, 0,  0,   0,  1,  0,   1,   0,    1 };
   const int start = 0, ntests = sizeof(m) / sizeof(*m);
-  libxsmm_blasint max_size_a = 0, max_size_b = 0;
+  libxsmm_blasint max_size_a = 0, max_size_b = 0, i, j;
   unsigned int nerrors = 0;
   ELEM_TYPE *a = 0, *b = 0;
 #if defined(MATCOPY_GOLD)
@@ -62,14 +71,12 @@ int main(void)
   }
   a = (ELEM_TYPE*)libxsmm_malloc((size_t)(max_size_a * sizeof(ELEM_TYPE)));
   b = (ELEM_TYPE*)libxsmm_malloc((size_t)(max_size_b * sizeof(ELEM_TYPE)));
-  assert(0 != a && 0 != b);
+  assert(NULL != a && NULL != b);
 
   LIBXSMM_MATINIT_OMP(ELEM_TYPE, 42, a, max_size_a, 1, max_size_a, 1.0);
-  LIBXSMM_MATINIT_OMP(ELEM_TYPE,  0, b, max_size_b, 1, max_size_b, 1.0);
 #if defined(MATCOPY_GOLD)
   c = (ELEM_TYPE*)libxsmm_malloc((size_t)(max_size_b * sizeof(ELEM_TYPE)));
-  assert(0 != c);
-  LIBXSMM_MATINIT_OMP(ELEM_TYPE, 0, c, max_size_b, 1, max_size_b, 1.0);
+  assert(NULL != c);
 #endif
 
   for (fun = 0; fun < 2; ++fun) {
@@ -77,76 +84,88 @@ int main(void)
       ELEM_TYPE pattern;
       memset(b, -1, (size_t)(max_size_b * sizeof(ELEM_TYPE)));
       pattern = b[0]; /* -NaN */
+#if defined(TEST_MZERO)
       matcopy[fun](b, NULL, sizeof(ELEM_TYPE), m[test], n[test], ldi[test], ldo[test]);
-      { /* validation */
-        unsigned int testerrors = 0;
-        libxsmm_blasint i, j;
+      for (i = 0; i < n[test]; ++i) {
+        for (j = 0; j < m[test]; ++j) {
+          const ELEM_TYPE u = 0;
+          const ELEM_TYPE v = b[i*ldo[test]+j];
+          if (LIBXSMM_NEQ(u, v)) {
+            ++nerrors;
+          }
+        }
+        for (j = m[test]; j < ldo[test]; ++j) {
+          if (0 != memcmp(&pattern, b + (size_t)i * ldo[test] + j, sizeof(ELEM_TYPE))) {
+            ++nerrors;
+          }
+        }
+      }
+      if (0 != nerrors) { /* break-out */
+        fun = test = INT_MAX; break;
+      }
+#endif
+#if defined(TEST_MCOPY)
+      matcopy[fun](b, a, sizeof(ELEM_TYPE), m[test], n[test], ldi[test], ldo[test]);
+      for (i = 0; i < n[test]; ++i) {
+        for (j = 0; j < m[test]; ++j) {
+          const ELEM_TYPE u = a[i*ldi[test]+j];
+          const ELEM_TYPE v = b[i*ldo[test]+j];
+          if (LIBXSMM_NEQ(u, v)) {
+            ++nerrors;
+          }
+        }
+        for (j = m[test]; j < ldo[test]; ++j) {
+          if (0 != memcmp(&pattern, b + (size_t)i * ldo[test] + j, sizeof(ELEM_TYPE))) {
+            ++nerrors;
+          }
+        }
+      }
+      if (0 != nerrors) { /* break-out */
+        fun = test = INT_MAX; break;
+      }
+#endif
+#if defined(MATCOPY_GOLD)
+      if (0 == fun) {
+        MATCOPY_GOLD(m + test, n + test, a, ldi + test, c, ldo + test);
         for (i = 0; i < n[test]; ++i) {
           for (j = 0; j < m[test]; ++j) {
-            const ELEM_TYPE u = 0;
-            const ELEM_TYPE v = b[i*ldo[test]+j];
+            const ELEM_TYPE u = b[i*ldo[test]+j];
+            const ELEM_TYPE v = c[i*ldo[test]+j];
             if (LIBXSMM_NEQ(u, v)) {
-              ++testerrors;
+              ++nerrors;
             }
           }
           for (j = m[test]; j < ldo[test]; ++j) {
             if (0 != memcmp(&pattern, b + (size_t)i * ldo[test] + j, sizeof(ELEM_TYPE))) {
-              ++testerrors;
+              ++nerrors;
             }
           }
+        }
+        if (0 != nerrors) { /* break-out */
+          fun = test = INT_MAX; break;
         }
       }
-      matcopy[fun](b, a, sizeof(ELEM_TYPE), m[test], n[test], ldi[test], ldo[test]);
-      { /* validation */
-        unsigned int testerrors = 0;
-        libxsmm_blasint i, j;
-        for (i = 0; i < n[test]; ++i) {
-          for (j = 0; j < m[test]; ++j) {
-            const ELEM_TYPE u = a[i*ldi[test]+j];
-            const ELEM_TYPE v = b[i*ldo[test]+j];
-            if (LIBXSMM_NEQ(u, v)) {
-              ++testerrors;
-            }
-          }
-        }
-#if defined(MATCOPY_GOLD)
-        if (0 == testerrors) {
-          MATCOPY_GOLD(m + test, n + test, a, ldi + test, c, ldo + test);
-          for (i = 0; i < n[test]; ++i) {
-            for (j = 0; j < m[test]; ++j) {
-              const ELEM_TYPE u = b[i*ldo[test]+j];
-              const ELEM_TYPE v = c[i*ldo[test]+j];
-              if (LIBXSMM_NEQ(u, v)) {
-                ++testerrors;
-              }
-            }
-          }
-        }
 #endif
-#if (0 != LIBXSMM_JIT) /* dispatch kernel and check that it is available */
+#if defined(TEST_JIT) && (0 != LIBXSMM_JIT) /* dispatch kernel and check that it is available */
 # if 0  /* Issue #354 */
-        if (LIBXSMM_X86_AVX <= libxsmm_get_target_archid())
+      if (0 == fun && LIBXSMM_X86_AVX <= libxsmm_get_target_archid())
 # else
-        if (LIBXSMM_X86_AVX512 <= libxsmm_get_target_archid())
+      if (0 == fun && LIBXSMM_X86_AVX512 <= libxsmm_get_target_archid())
 # endif
-        {
-          libxsmm_descriptor_blob blob;
-          const libxsmm_mcopy_descriptor *const desc = libxsmm_mcopy_descriptor_init(&blob, sizeof(ELEM_TYPE),
-            (unsigned int)m[test], (unsigned int)n[test], (unsigned int)ldo[test], (unsigned int)ldi[test],
-            LIBXSMM_MATCOPY_FLAG_DEFAULT, prefetch[test], NULL/*unroll*/);
-          const libxsmm_xmcopyfunction kernel = libxsmm_dispatch_mcopy(desc);
-          if (NULL == kernel) {
+      {
+        libxsmm_descriptor_blob blob;
+        const libxsmm_mcopy_descriptor *const desc = libxsmm_mcopy_descriptor_init(&blob, sizeof(ELEM_TYPE),
+          (unsigned int)m[test], (unsigned int)n[test], (unsigned int)ldo[test], (unsigned int)ldi[test],
+          LIBXSMM_MATCOPY_FLAG_DEFAULT, prefetch[test], NULL/*unroll*/);
+        const libxsmm_xmcopyfunction kernel = libxsmm_dispatch_mcopy(desc);
+        if (NULL == kernel) {
 # if defined(_DEBUG)
-            fprintf(stderr, "\nERROR: kernel %i.%i not generated!\n", fun + 1, test + 1);
+          fprintf(stderr, "\nERROR: kernel %i.%i not generated!\n", fun + 1, test + 1);
 # endif
-            ++testerrors;
-          }
-        }
-#endif
-        if (nerrors < testerrors) {
-          nerrors = testerrors;
+          ++nerrors; fun = test = INT_MAX; break; /* break-out */
         }
       }
+#endif
     }
   }
 
