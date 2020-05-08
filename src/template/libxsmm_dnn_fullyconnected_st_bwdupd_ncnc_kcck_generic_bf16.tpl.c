89a63d69b3989b547b8f83336cb6ad93111aa14a
Alexander Heinecke <alexander.heinecke@intel.com>
2020-05-07 19:37:38 -0700

    fixed defines

--- src/template/libxsmm_dnn_fullyconnected_st_bwdupd_ncnc_kcck_generic_bf16.tpl.c
+++ src/template/libxsmm_dnn_fullyconnected_st_bwdupd_ncnc_kcck_generic_bf16.tpl.c
@@ -423,8 +423,9 @@ if ( (kind == LIBXSMM_DNN_COMPUTE_KIND_BWD) || (kind == LIBXSMM_DNN_COMPUTE_KIND
 
 #undef LIBXSMM_DNN_FC_BWD_CONVERT_F32_BF16
 
-#define _mm512_cvt2_fp32_bf16(A, B) LIBXSMM_INTRINSICS_MM512_CVT2_FP32_BF16(A, B)
-#if defined(LIBXSMM_DNN_FC_UPD_AVX512_CPX)
+#define _mm512_cvt2_fp32_bf16(A, B) LIBXSMM_INTRINSISCS_MM512_CVTNE2PS_PBH(A, B)
+
+#if defined(LIBXSMM_DNN_FC_BWD_AVX512_CPX)
 #define LIBXSMM_DNN_FC_UPD_CONVERT_F32_BF16(in, out, length) do { \
   unsigned int full_chunks = length / 32; \
   unsigned int remainder = length % 32; \
