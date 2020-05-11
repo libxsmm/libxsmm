6d8032ff835d436662f4b66400baafda4386f096
Hans Pabst <hans.pabst@intel.com>
2020-05-11 09:47:31 +0200

    Revised/deprecated batch/startup script which runs static configuration. [skip ci]

--- ide/_vs2019-configure.bat
+++ ide/_vs2019-configure.bat
@@ -4,6 +4,10 @@ SETLOCAL
 ECHO ================================================================================
 ECHO One-time configuration (Cygwin w/ GNU GCC, GNU Make, and Python needed in PATH)
 ECHO When configured, it is sufficient to start _vs2019.bat or _vs2019.sln
+ECHO IMPORTANT: due to zero-config, configuration is not necessary anymore!
+ECHO            One may terminate this configuration (CTRL-C)
+ECHO            and simply start _vs2019.bat or _vs2019.sln.
+PAUSE
 cd ..
 bash -c "make realclean ; make headers sources"
 cd ide
