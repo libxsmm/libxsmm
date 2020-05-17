c4a96c05d3e890e7a8f0f2828320f3a2aaa3184f
Alexander Heinecke <alexander.heinecke@intel.com>
2020-05-16 18:16:42 -0700

    algined rnn-driver with lstmdriver

---
+++ samples/deeplearning/rnndriver/run_rnncell.sh
@@ -0,0 +1,94 @@
+#!/usr/bin/env sh
+###############################################################################
+# Copyright (c) Intel Corporation - All rights reserved.                      #
+# This file is part of the LIBXSMM library.                                   #
+#                                                                             #
+# For information on the license, see the LICENSE file.                       #
+# Further information: https://github.com/hfp/libxsmm/                        #
+# SPDX-License-Identifier: BSD-3-Clause                                       #
+###############################################################################
+# Hans Pabst (Intel Corp.), Kunal Banerjee (Intel Corp.)
+###############################################################################
+set -eo pipefail
+
+UNAME=$(command -v uname)
+SORT=$(command -v sort)
+GREP=$(command -v grep)
+CUT=$(command -v cut)
+WC=$(command -v wc)
+TR=$(command -v tr)
+
+if [ "" = "${CHECK}" ] || [ "0" = "${CHECK}" ]; then
+  if [ "" = "${CHECK_DNN_MB}" ]; then CHECK_DNN_MB=256; fi
+  if [ "" = "${CHECK_DNN_ITERS}" ]; then CHECK_DNN_ITERS=1000; fi
+else # check
+  if [ "" = "${CHECK_DNN_MB}" ]; then CHECK_DNN_MB=256; fi
+  if [ "" = "${CHECK_DNN_ITERS}" ]; then CHECK_DNN_ITERS=1; fi
+fi
+
+if [ $# -ne 8 ]
+then
+  echo "Usage: $(basename $0) format=(nc_ck, ${FORMAT}${SUFIXBIN}) bin=(f32, bf16) iters type=(0-fwd, 1-bwd, 2-upd, 3-bwdupd) act={1-relu, 2-sigmoid, 3-tanh} MB bn bc bk"
+  FORMAT=nc_ck
+  BIN=f32
+  ITERS=${CHECK_DNN_ITERS}
+  TYPE=0
+  ACT=1
+  MB=${CHECK_DNN_MB}
+  BN=32
+  BC=32
+  BK=32
+else
+  FORMAT=$1
+  BIN=$2
+  ITERS=$3
+  TYPE=$4
+  ACT=$5
+  MB=$6
+  BN=$7
+  BC=$8
+  BK=$9
+fi
+
+if [ "" != "${GREP}" ] && [ "" != "${CUT}" ] && [ "" != "${SORT}" ] && [ "" != "${WC}" ] && [ -e /proc/cpuinfo ]; then
+  export NS=$(${GREP} "physical id" /proc/cpuinfo | ${SORT} -u | ${WC} -l | ${TR} -d " ")
+  export NC=$((NS*$(${GREP} -m1 "cpu cores" /proc/cpuinfo | ${TR} -d " " | ${CUT} -d: -f2)))
+  export NT=$(${GREP} "core id" /proc/cpuinfo | ${WC} -l | ${TR} -d " ")
+elif [ "" != "${UNAME}" ] && [ "" != "${CUT}" ] && [ "Darwin" = "$(${UNAME})" ]; then
+  export NS=$(sysctl hw.packages | ${CUT} -d: -f2 | tr -d " ")
+  export NC=$(sysctl hw.physicalcpu | ${CUT} -d: -f2 | tr -d " ")
+  export NT=$(sysctl hw.logicalcpu | ${CUT} -d: -f2 | tr -d " ")
+fi
+if [ "" != "${NC}" ] && [ "" != "${NT}" ]; then
+  export HT=$((NT/(NC)))
+else
+  export NS=1 NC=1 NT=1 HT=1
+fi
+if [ "" != "${GREP}" ] && [ "" != "${CUT}" ] && [ "" != "$(command -v numactl)" ]; then
+  export NN=$(numactl -H | ${GREP} available: | ${CUT} -d' ' -f2)
+else
+  export NN=${NS}
+fi
+
+if [ "" = "${OMP_NUM_THREADS}" ] || [ "0" = "${OMP_NUM_THREADS}" ]; then
+  if [ "" = "${KMP_AFFINITY}" ]; then
+    export KMP_AFFINITY=compact,granularity=fine KMP_HW_SUBSET=1T
+  fi
+  export OMP_NUM_THREADS=$((NC))
+fi
+
+if [ "" = "${MB}" ] || [ "0" = "${MB}" ]; then
+  MB=${OMP_NUM_THREADS}
+fi
+
+if [ "" = "${LIBXSMM_TARGET_HIDDEN}" ] || [ "0" = "${LIBXSMM_TARGET_HIDDEN}" ]; then
+  echo "OMP_NUM_THREADS=${OMP_NUM_THREADS} NUMACTL=\"${NUMACTL}\""
+  echo
+fi
+
+./rnndriver_${FORMAT}_${BIN} ${ITERS} ${TYPE} ${ACT} ${MB}  256   256  5 ${BN} ${BC} ${BK}
+./rnndriver_${FORMAT}_${BIN} ${ITERS} ${TYPE} ${ACT} ${MB}  512   512  5 ${BN} ${BC} ${BK}
+./rnndriver_${FORMAT}_${BIN} ${ITERS} ${TYPE} ${ACT} ${MB} 1024  1024  5 ${BN} ${BC} ${BK}
+./rnndriver_${FORMAT}_${BIN} ${ITERS} ${TYPE} ${ACT} ${MB} 2048  2048  5 ${BN} ${BC} ${BK}
+./rnndriver_${FORMAT}_${BIN} ${ITERS} ${TYPE} ${ACT} ${MB} 4096  4096  5 ${BN} ${BC} ${BK}
+
