eb92a1bfdcb9d64a96c0668e4e11c6ece635eff3
Evangelos Georganas <egeor@eecs.berkeley.edu>
2020-05-27 13:30:25 -0700

    Started JITing in the mateltwise kernel

--- src/generator_mateltwise.c
+++ src/generator_mateltwise.c
@@ -6,21 +6,20 @@
 * Further information: https://github.com/hfp/libxsmm/                        *
 * SPDX-License-Identifier: BSD-3-Clause                                       *
 ******************************************************************************/
-/* Alexander Heinecke (Intel Corp.)
+/* Evangelos Georganas, Alexander Heinecke (Intel Corp.)
 ******************************************************************************/
 #include <libxsmm_generator.h>
 #include "generator_common.h"
+#include "generator_mateltwise_avx_avx512.h"
 
 LIBXSMM_API
 void libxsmm_generator_mateltwise_kernel( libxsmm_generated_code*          io_generated_code,
                                           const libxsmm_meltw_descriptor*  i_mateltw_desc ) {
   /* generate kernel */
-  if ( io_generated_code->arch > LIBXSMM_X86_SSE4  ) {
-    LIBXSMM_UNUSED( io_generated_code );
-    LIBXSMM_UNUSED( i_mateltw_desc );
-
-  } else {
-    /* TODO fix this error */
+  if ( io_generated_code->arch >= LIBXSMM_X86_AVX512_CORE  ) {
+    libxsmm_generator_mateltwise_avx_avx512_kernel( io_generated_code, i_mateltw_desc );
+   } else {
+    /* TODO fix this errori and support for more architectures */
     LIBXSMM_HANDLE_ERROR( io_generated_code, LIBXSMM_ERR_ARCH );
     return;
   }
