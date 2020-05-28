915f677024caba59950db289095f8f86f8f09e29
Hans Pabst <hans.pabst@intel.com>
2020-05-28 08:43:51 +0200

    Improved script and show message in case of error.

--- scripts/tool_addforks.sh
+++ scripts/tool_addforks.sh
@@ -15,16 +15,26 @@ GREP=$(command -v grep)
 CUT=$(command -v cut)
 GIT=$(command -v git)
 
+PRJ=libxsmm
+URL="https://api.github.com/repos/hfp/${PRJ}/forks"
+
 if [ "" != "${CURL}" ] && [ "" != "${GIT}" ] && \
    [ "" != "${GREP}" ] && [ "" != "${CUT}" ];
 then
-  for FORK in $(${CURL} -s https://api.github.com/repos/hfp/libxsmm/forks \
-  | ${GREP} "\"html_url\"" | ${GREP} "libxsmm" | ${CUT} -d/ -f4);
+  N=0
+  for FORK in $(${CURL} -s ${URL} \
+  | ${GREP} "\"html_url\"" | ${GREP} "${PRJ}" | ${CUT} -d/ -f4);
   do
     echo "Adding fork ${FORK}..."
-    ${GIT} remote add ${FORK} https://github.com/${FORK}/libxsmm.git
+    ${GIT} remote add ${FORK} https://github.com/${FORK}/${PRJ}.git
     ${GIT} fetch ${FORK}
+    N=$((N+1))
   done
+  if [ "0" != "${N}" ]; then
+    echo "Processed number of forks: ${N}"
+  else
+    ${CURL} ${URL}
+  fi
 else
   echo "Error: missing prerequisites!"
   exit 1
