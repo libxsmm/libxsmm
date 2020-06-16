ca6836214b9f01e1e005c8e7df3f560a6799fd2c
Hans Pabst <hans.pabst@intel.com>
2020-06-16 08:42:11 +0200

    (Re-)Enabled policy hook. Incorporated check for C++ comments. Improved policy script/hook (cleanup).

--- scripts/tool_normalize.sh
+++ scripts/tool_normalize.sh
@@ -72,7 +72,7 @@ then
     if [[ "${DIR}" && (${FILE} != "${DIR}/"*) ]]; then continue; fi
     echo -n "${FILE}"
     #
-    # Reformat code (fallback: check for banned characters).
+    # Reformat code (fallback: check for banned characters, etc.).
     #
     REFORMAT=0
     if [[ (${FILE} = *".c"*) || (${FILE} = *".h"*) ]]; then
