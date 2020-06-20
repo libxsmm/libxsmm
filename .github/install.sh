d906b847a84eba1021cb12de523fb078bd3bb04d
Hans Pabst <hans.pabst@intel.com>
2020-06-20 19:37:14 +0200

    Cleanup (install.sh).

--- .github/install.sh
+++ .github/install.sh
@@ -21,7 +21,7 @@ CP=$(command -v cp)
 RM=$(command -v rm)
 
 if [ -e "${GIT_DIR}/hooks" ] && [ "${GIT}" ] && \
-   [ "${CHMOD}" ] && [ "${CP}" ] && [ "${RM}" ]; \
+   [ "${CHMOD}" ] && [ "${CP}" ] && [ "${RM}" ];
 then
   # make sure the path to .gitconfig is a relative path
   ${GIT} config --local include.path ../.gitconfig 2>/dev/null
