6be60ba01a66182606688853b97c3c40c40f004a
Hans Pabst <hans.pabst@intel.com>
2020-08-05 10:18:05 +0200

    Added helper script. Renamed script.

---
+++ scripts/tool_gitprune.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+###############################################################################
+# Copyright (c) Intel Corporation - All rights reserved.                      #
+# This file is part of the LIBXSMM library.                                   #
+#                                                                             #
+# For information on the license, see the LICENSE file.                       #
+# Further information: https://github.com/hfp/libxsmm/                        #
+# SPDX-License-Identifier: BSD-3-Clause                                       #
+###############################################################################
+# Hans Pabst (Intel Corp.)
+###############################################################################
+
+GIT=$(command -v git)
+
+if [ "" != "${GIT}" ]; then
+  ${GIT} reflog expire --expire=now --all
+  ${GIT} gc --prune=now
+else
+  echo "Error: missing prerequisites!"
+  exit 1
+fi
+
