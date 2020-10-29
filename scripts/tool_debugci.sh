4d57dffea2013c08bf5a97fbe1406faa05773602
Hans Pabst <hans.pabst@intel.com>
2020-10-29 11:11:15 +0100

    Adjusted references after migration to Travis-CI.com (org->com). Regenerated documentation.

--- scripts/tool_debugci.sh
+++ scripts/tool_debugci.sh
@@ -11,7 +11,7 @@
 ###############################################################################
 
 CURL=$(command -v curl)
-DOMAIN=org
+DOMAIN=com
 TOKEN=$1
 JOBID=$2
 
@@ -22,7 +22,7 @@ fi
 
 if [ ! "${JOBID}" ] || [ ! "${TOKEN}" ]; then
   echo "Usage: $0 <token> <jobid>"
-  echo "       token: API authentication; see https://travis-ci.org/account/preferences"
+  echo "       token: API authentication; see https://travis-ci.${DOMAIN}/account/preferences"
   echo "       jobid: not the build-id; expand \"Build system information\" (job's log)"
   echo "See also https://docs.travis-ci.com/user/running-build-in-debug-mode/"
   exit 1
