3c033ac46fb1211f97d776a10569c91bc3aa0672
egeorgan <evangelos.georganas@intel.com>
2021-01-11 14:50:02 -0800

    Refactored scale meltw

--- include/libxsmm_source.h
+++ include/libxsmm_source.h
@@ -54,6 +54,7 @@
 #include "../src/generator_mateltwise_dropout_avx_avx512.c"
 #include "../src/generator_mateltwise_reduce_avx_avx512.c"
 #include "../src/generator_mateltwise_relu_avx_avx512.c"
+#include "../src/generator_mateltwise_scale_avx_avx512.c"
 #include "../src/generator_mateltwise_transform_avx_avx512.c"
 #include "../src/generator_mateltwise_unary_avx_avx512.c"
 #include "../src/generator_packed.c"
