9f399d3d1411c11378adc8b4fffe6a07614cd249
Hans Pabst <hans.pabst@intel.com>
2021-02-22 14:55:39 +0100

    Avoid greedy match, write (optional) envfile to be source'd, and fixed issues pointed out by Shellcheck (scripts/tool_envrestore.sh).

--- scripts/tool_envrestore.sh
+++ scripts/tool_envrestore.sh
@@ -22,16 +22,24 @@ if [ "" != "${DIFF}" ] && \
    [ "" != "${SED}" ];
 then
   ENVFILE=$1
-  shift
   if [ -e "${ENVFILE}" ]; then
+    shift
+    ENVSRCF=$2
+    if [ "${ENVSRCF}" ]; then
+      if [ -e "${ENVSRCF}" ]; then truncate -s0 "${ENVSRCF}"; fi
+      shift
+    fi
     # no need to have unique values in ENVDIFF aka "sort -u"
-    ENVDIFF=$(declare -px | ${DIFF} ${ENVFILE} - | ${SED} -n 's/[<>] \(..*\)/\1/p' | ${SED} -n 's/declare -x \(..*\)=..*/\1/p')
+    ENVDIFF=$(declare -px | ${DIFF} "${ENVFILE}" - | ${SED} -n 's/[<>] \(..*\)/\1/p' | ${SED} -n 's/declare -x \(.[^=]*\)=..*/\1/p')
     for ENV in ${ENVDIFF}; do # restore environment
-      ENVVAR=$(${SED} -n "/declare \-x ${ENV}=/p" ${ENVFILE})
-      if [ "" != "${ENVVAR}" ]; then
-        eval ${ENVVAR}
+      ENVDEF=$(${SED} -n "/declare \-x ${ENV}=/p" "${ENVFILE}")
+      if [ "" != "${ENVDEF}" ]; then
+        if [ "${ENVSRCF}" ]; then
+          echo "${ENVDEF}" | ${SED} "s/declare -x //" >>"${ENVSRCF}"
+        fi
+        eval "${ENVDEF}"
       else
-        unset ${ENV}
+        unset "${ENV}"
       fi
     done
   else
