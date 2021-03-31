6ca10e6f784e2d1bd92faaab98d0fbafa37b015c
egeorgan <evangelos.georganas@intel.com>
2021-03-30 19:09:18 -0700

    Ammend

--- samples/deeplearning/fullyconnected_tpp/layer_example_f32.c
+++ samples/deeplearning/fullyconnected_tpp/layer_example_f32.c
@@ -155,8 +155,8 @@ my_fc_fwd_config setup_my_fc_fwd(libxsmm_blasint N, libxsmm_blasint C, libxsmm_b
   if (res.C == 1024 && res.K == 1024 && res.threads == 28) {
     res.fwd_bf = 1/*((res.C/res.bc) % 1 == 0) ? 1 : 1*/;
     res.fwd_2d_blocking = 1;
-    res.fwd_col_teams = 4;
-    res.fwd_row_teams = 7;
+    res.fwd_col_teams = 7;
+    res.fwd_row_teams = 4;
   }
 
   if (res.C == 512 && res.K == 512 && res.threads == 28) {
