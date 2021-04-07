c032fa4938f2e27fabd848d9d088d01d296afc3b
Hans Pabst <hans.pabst@intel.com>
2021-04-07 17:17:25 +0200

    Fixed a typo.

--- src/libxsmm_main.c
+++ src/libxsmm_main.c
@@ -1583,7 +1583,7 @@ LIBXSMM_API void libxsmm_set_target_arch(const char* arch)
       target_archid = LIBXSMM_X86_GENERIC;
     }
 #elif defined(LIBXSMM_PLATFORM_AARCH64)
-    else if (arch == libxsmm_stristr(arch, "arm") || arch == libxsmm_stristr(arch, "arm64"))
+    else if (arch == libxsmm_stristr(arch, "arm") || arch == libxsmm_stristr(arch, "arm64")
           || arch == libxsmm_stristr(arch, "arm_v81")
           || arch == libxsmm_stristr(arch, "aarch64")
     {
