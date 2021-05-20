7b6f2c794090d5e2a2afafd7cc3421f521870bf2
Ishwar Bhati <ishwar.s.bhati@intel.com>
2021-05-20 06:20:27 -0700

    use lock free for update (default), moved JIT initialization in the constructor

--- samples/deeplearning/embbag_distri/EmbeddingBag.h
+++ samples/deeplearning/embbag_distri/EmbeddingBag.h
@@ -8,10 +8,7 @@
 ******************************************************************************/
 /* Dhiraj Kalamkar, Evangelos Georganas (Intel Corp.)
 ******************************************************************************/
-#define JIT_REDUCE_COLS_IDX
-#define JIT_REPLICATE_COLS_VAR
-#define JIT_SCALE
-#if defined( JIT_REDUCE_COLS_IDX) || defined(JIT_REPLICATE_COLS_VAR) || defined(JIT_SCALE)
+#if defined(USE_LIBXSMM_JIT)
 #include <libxsmm.h>
 #endif
 #include "utils.h"
@@ -24,6 +21,18 @@ public:
   EmbeddingBagImpl(int M, int E) : M(M), E(E)
   {
     weight_ = (T*)my_malloc((size_t)M * E * sizeof(T), alignment);
+
+#ifdef USE_LIBXSMM_JIT
+    _ld = E;
+    if (sizeof(T) == 4) {
+      kernel = libxsmm_dispatch_meltw_reduce_cols_idx(E, &_ld, &_ld, LIBXSMM_DATATYPE_F32, LIBXSMM_DATATYPE_F32, (sizeof(long) == 8) ? LIBXSMM_DATATYPE_I64 : LIBXSMM_DATATYPE_I32);
+    } else {
+      kernel = libxsmm_dispatch_meltw_reduce_cols_idx(E, &_ld, &_ld, LIBXSMM_DATATYPE_F16, LIBXSMM_DATATYPE_F16, (sizeof(long) == 8) ? LIBXSMM_DATATYPE_I64 : LIBXSMM_DATATYPE_I32);
+    }
+    kernel1 = libxsmm_dispatch_meltw_unary(E, 0, &_ld, &_ld, LIBXSMM_DATATYPE_F32, LIBXSMM_DATATYPE_F32, LIBXSMM_DATATYPE_F32, LIBXSMM_MELTW_FLAG_UNARY_NONE, LIBXSMM_MELTW_TYPE_UNARY_REPLICATE_COL_VAR);
+    kernel2 = libxsmm_dispatch_meltw_binary(E, 1, &_ld, &_ld, &_ld, LIBXSMM_DATATYPE_F32, LIBXSMM_DATATYPE_F32, LIBXSMM_DATATYPE_F32, LIBXSMM_MELTW_FLAG_BINARY_BCAST_SCALAR_IN_0, LIBXSMM_MELTW_TYPE_BINARY_MULADD);
+#endif
+
   }
 
   ~EmbeddingBagImpl()
@@ -37,19 +46,13 @@ public:
     init_random(M * E, weight_, low, high);
   }
 
-#ifdef JIT_REDUCE_COLS_IDX
+#ifdef USE_LIBXSMM_JIT
   void forward(int N, int NS, const long *offsets, const long *indices, T *output_)
   {
     T(*__restrict weight)[E] = (T(*)[*])weight_;
     T(*__restrict output)[E] = (T(*)[*])output_;
-    libxsmm_meltwfunction_reduce_cols_idx kernel;
-    int _ld = E;
-    if (sizeof(T) == 4) {
-      kernel = libxsmm_dispatch_meltw_reduce_cols_idx(E, &_ld, &_ld, LIBXSMM_DATATYPE_F32, LIBXSMM_DATATYPE_F32, (sizeof(long) == 8) ? LIBXSMM_DATATYPE_I64 : LIBXSMM_DATATYPE_I32);
-    } else {
-      kernel = libxsmm_dispatch_meltw_reduce_cols_idx(E, &_ld, &_ld, LIBXSMM_DATATYPE_F16, LIBXSMM_DATATYPE_F16, (sizeof(long) == 8) ? LIBXSMM_DATATYPE_I64 : LIBXSMM_DATATYPE_I32);
-    }
-#pragma omp parallel for
+
+    #pragma omp parallel for
     for (int n = 0; n < N; n++)
     {
       libxsmm_meltw_reduce_cols_idx_param params;
@@ -89,13 +92,12 @@ public:
   }
 #endif
 
-#ifdef JIT_REPLICATE_COLS_VAR
+#ifdef USE_LIBXSMM_JIT
   void backward(int N, int NS, const T *gradout_, const long *offsets, const long *indices, T *values_)
   {
     T(*__restrict gradout)[E] = (T(*)[*])gradout_;
     T(*__restrict values)[E] = (T(*)[*])values_;
     int _ld = E;
-    libxsmm_meltwfunction_unary kernel = libxsmm_dispatch_meltw_unary(E, 0, &_ld, &_ld, LIBXSMM_DATATYPE_F32, LIBXSMM_DATATYPE_F32, LIBXSMM_DATATYPE_F32, LIBXSMM_MELTW_FLAG_UNARY_NONE, LIBXSMM_MELTW_TYPE_UNARY_REPLICATE_COL_VAR);
 #pragma omp parallel for
     for (int n = 0; n < N; n++)
     {
@@ -108,7 +110,7 @@ public:
       unary_param.out.primary   = (void*)&values[start][0];
       unary_param.out.secondary = (void*)&_N;
 
-      kernel(&unary_param);
+      kernel1(&unary_param);
     }
   }
 #else
@@ -135,45 +137,89 @@ public:
   }
 #endif
 
-#ifdef JIT_SCALE
-  void update(int NS, const T *grads_, const long *indices, float lr)
+#ifdef USE_LIBXSMM_JIT
+  void update(int NS, const T *grads_, const long *indices, float lr, int M, int use_rtm)
   {
+    int use_lock_free = use_rtm == 0 ? 1: 0;
     T(*__restrict weight)[E] = (T(*)[*])weight_;
     T(*__restrict grads)[E] = (T(*)[*])grads_;
     int _ld = E;
-    libxsmm_meltwfunction_binary kernel = libxsmm_dispatch_meltw_binary(E, 1, &_ld, &_ld, &_ld, LIBXSMM_DATATYPE_F32, LIBXSMM_DATATYPE_F32, LIBXSMM_DATATYPE_F32, LIBXSMM_MELTW_FLAG_BINARY_BCAST_SCALAR_IN_0, LIBXSMM_MELTW_TYPE_BINARY_MULADD);
-
-    SimpleSpinLock fallBackLock;
-#pragma omp parallel for
-    for (long i = 0; i < NS; i++)
-    {
-      libxsmm_meltw_binary_param binary_param;
-      long ind = indices[i];
-      binary_param.in0.primary  = (void*)&lr;
-      binary_param.in1.primary  = (void*)&grads[i][0];
-      binary_param.out.primary  = (void*)&weight[ind][0];
+    if(use_lock_free) {
+      //printf("Using lock free update\n");
+      int max_thr = omp_get_max_threads();
+      if(M < max_thr) max_thr = M;
+#pragma omp parallel num_threads(max_thr)
       {
-        TransactionScope guard(fallBackLock, 100, 0);
-        kernel(&binary_param);
+        int tid = omp_get_thread_num();
+        long j_begin = (tid * M) / max_thr;
+        long j_end = ((tid+1) * M) / max_thr;
+        for(long i = 0; i < NS; i++) {
+          auto ind = indices[i];
+          if(ind >= j_begin && ind < j_end) {
+            libxsmm_meltw_binary_param binary_param;
+            binary_param.in0.primary  = (void*)&lr;
+            binary_param.in1.primary  = (void*)&grads[i][0];
+            binary_param.out.primary  = (void*)&weight[ind][0];
+            {
+              kernel2(&binary_param);
+            }
+          }
+        }
+      }
+    } else {
+      SimpleSpinLock fallBackLock;
+#pragma omp parallel for
+      for (long i = 0; i < NS; i++)
+      {
+        libxsmm_meltw_binary_param binary_param;
+        long ind = indices[i];
+        binary_param.in0.primary  = (void*)&lr;
+        binary_param.in1.primary  = (void*)&grads[i][0];
+        binary_param.out.primary  = (void*)&weight[ind][0];
+        {
+          TransactionScope guard(fallBackLock, 100, 0);
+          kernel2(&binary_param);
+        }
       }
     }
   }
 #else
-  void update(int NS, const T *grads_, const long *indices, float lr)
+  void update(int NS, const T *grads_, const long *indices, float lr, int M, int use_rtm)
   {
     T(*__restrict weight)[E] = (T(*)[*])weight_;
     T(*__restrict grads)[E] = (T(*)[*])grads_;
 
-    SimpleSpinLock fallBackLock;
-#pragma omp parallel for
-    for (long i = 0; i < NS; i++)
-    {
-      long ind = indices[i];
+    int use_lock_free = use_rtm == 0 ? 1: 0;
+
+    if(use_lock_free) {
+      int max_thr = omp_get_max_threads();
+      if(M < max_thr) max_thr = M;
+#pragma omp parallel num_threads(max_thr)
       {
-        TransactionScope guard(fallBackLock, 100, 0);
+        int tid = omp_get_thread_num();
+        long j_begin = (tid * M) / max_thr;
+        long j_end = ((tid+1) * M) / max_thr;
+        for(long i = 0; i < NS; i++) {
+          auto ind = indices[i];
+          if(ind >= j_begin && ind < j_end) {
 #pragma omp simd
-        for (long v = 0; v < E; v++)
-          weight[ind][v] += lr * grads[i][v];
+            for (long v = 0; v < E; v++)
+              weight[ind][v] += lr * grads[i][v];
+          }
+        }
+      }
+    } else {
+      SimpleSpinLock fallBackLock;
+#pragma omp parallel for
+      for (long i = 0; i < NS; i++)
+      {
+        long ind = indices[i];
+        {
+          TransactionScope guard(fallBackLock, 100, 0);
+#pragma omp simd
+          for (long v = 0; v < E; v++)
+            weight[ind][v] += lr * grads[i][v];
+        }
       }
     }
   }
@@ -182,5 +228,12 @@ public:
   T *weight_;
   int M;
   int E;
+
+#ifdef USE_LIBXSMM_JIT
+  int _ld;
+  libxsmm_meltwfunction_reduce_cols_idx kernel;
+  libxsmm_meltwfunction_unary kernel1;
+  libxsmm_meltwfunction_binary kernel2;
+#endif
 };
 
