b4dd5279e8f2f5a891687ae4542dac10baea5819
Alexander Heinecke <heinecke.alex@gmail.com>
2021-05-29 23:43:49 -0700

    renamed scripts

--- samples/eltwise/kernel_test/create_new_unary_simple_test.sh
+++ samples/eltwise/kernel_test/create_new_unary_simple_test.sh
@@ -8,12 +8,12 @@ else
   NUMBER=9999
 fi
 
-cp unary_copy_32b_eqld.slurm unary_${NAME}_32b_eqld.slurm
-cp unary_copy_mixed_eqld.slurm unary_${NAME}_mixed_eqld.slurm
-cp unary_copy_32b_gtld.slurm unary_${NAME}_32b_gtld.slurm
-cp unary_copy_mixed_gtld.slurm unary_${NAME}_mixed_gtld.slurm
+cp unary_copy_32b_eqld.sh unary_${NAME}_32b_eqld.sh
+cp unary_copy_mixed_eqld.sh unary_${NAME}_mixed_eqld.sh
+cp unary_copy_32b_gtld.sh unary_${NAME}_32b_gtld.sh
+cp unary_copy_mixed_gtld.sh unary_${NAME}_mixed_gtld.sh
 
-sed "s/UNARY_OP=1/UNARY_OP=${NUMBER}/g" -i unary_${NAME}_32b_eqld.slurm
-sed "s/UNARY_OP=1/UNARY_OP=${NUMBER}/g" -i unary_${NAME}_mixed_eqld.slurm
-sed "s/UNARY_OP=1/UNARY_OP=${NUMBER}/g" -i unary_${NAME}_32b_gtld.slurm
-sed "s/UNARY_OP=1/UNARY_OP=${NUMBER}/g" -i unary_${NAME}_mixed_gtld.slurm
+sed "s/UNARY_OP=1/UNARY_OP=${NUMBER}/g" -i unary_${NAME}_32b_eqld.sh
+sed "s/UNARY_OP=1/UNARY_OP=${NUMBER}/g" -i unary_${NAME}_mixed_eqld.sh
+sed "s/UNARY_OP=1/UNARY_OP=${NUMBER}/g" -i unary_${NAME}_32b_gtld.sh
+sed "s/UNARY_OP=1/UNARY_OP=${NUMBER}/g" -i unary_${NAME}_mixed_gtld.sh
