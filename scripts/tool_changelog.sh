dcec331b0731ab1540bddab48b1bb593e7217332
Hans Pabst <hans.pabst@intel.com>
2021-08-02 09:47:36 +0200

    Resolved issues pointed out by Shellcheck.

--- scripts/tool_changelog.sh
+++ scripts/tool_changelog.sh
@@ -17,7 +17,7 @@ GIT=$(command -v git)
 if [ "${GREP}"   ] && [ "${UNIQ}"  ] && [ "${GIT}" ];
 then
   LASTRELEASE=$(${GIT} describe --match "[0-9]*" --abbrev=0)
-  ${GIT} shortlog ${LASTRELEASE}..HEAD \
+  ${GIT} shortlog "${LASTRELEASE}..HEAD" \
   | ${GREP} -v "Merge branch " \
   | ${GREP} -v "Merge pull request " \
   | ${UNIQ}
