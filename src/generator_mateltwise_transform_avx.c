2af3567a99b3f44604a88ea6ecb8bd0643f57187
Alexander Heinecke <heinecke.alex@gmail.com>
2021-09-18 09:28:12 -0700

    added support for PADN%2, PADM%2, PADNM%2 transforms for AVX and SSE

--- src/generator_mateltwise_transform_avx.c
+++ src/generator_mateltwise_transform_avx.c
@@ -499,6 +499,20 @@ void libxsmm_generator_transform_vnni_to_vnnit_16bit_avx_microkernel( libxsmm_ge
 }
 
 LIBXSMM_API_INTERN
+void libxsmm_generator_transform_norm_padnm_mod2_16bit_avx_microkernel( libxsmm_generated_code*                 io_generated_code,
+                                                                        libxsmm_loop_label_tracker*             io_loop_label_tracker,
+                                                                        const unsigned int                      i_gp_reg_in,
+                                                                        const unsigned int                      i_gp_reg_out,
+                                                                        const unsigned int                      i_gp_reg_m_loop,
+                                                                        const unsigned int                      i_gp_reg_n_loop,
+                                                                        const libxsmm_mateltwise_kernel_config* i_micro_kernel_config,
+                                                                        const libxsmm_meltw_descriptor*         i_mateltwise_desc ) {
+  libxsmm_generator_transform_norm_padnm_mod2_mbit_scalar_sse_microkernel( io_generated_code, io_loop_label_tracker,
+                                                                           i_gp_reg_in, i_gp_reg_out, i_gp_reg_m_loop, i_gp_reg_n_loop,
+                                                                           i_micro_kernel_config, i_mateltwise_desc );
+}
+
+LIBXSMM_API_INTERN
 void libxsmm_generator_transform_avx_microkernel( libxsmm_generated_code*                        io_generated_code,
                                                   libxsmm_loop_label_tracker*                    io_loop_label_tracker,
                                                   libxsmm_mateltwise_gp_reg_mapping*             i_gp_reg_mapping,
@@ -533,14 +547,25 @@ void libxsmm_generator_transform_avx_microkernel( libxsmm_generated_code*       
       return;
     }
   } else if ( (i_mateltwise_desc->param == LIBXSMM_MELTW_TYPE_UNARY_TRANSFORM_NORM_TO_VNNI)     ||
-              (i_mateltwise_desc->param == LIBXSMM_MELTW_TYPE_UNARY_TRANSFORM_NORM_TO_VNNI_PAD)    ) {
+              (i_mateltwise_desc->param == LIBXSMM_MELTW_TYPE_UNARY_TRANSFORM_NORM_TO_VNNI_PAD) ||
+              (i_mateltwise_desc->param == LIBXSMM_MELTW_TYPE_UNARY_TRANSFORM_PADN_MOD2)        ||
+              (i_mateltwise_desc->param == LIBXSMM_MELTW_TYPE_UNARY_TRANSFORM_PADM_MOD2)        ||
+              (i_mateltwise_desc->param == LIBXSMM_MELTW_TYPE_UNARY_TRANSFORM_PADNM_MOD2)          ) {
     if ( (i_mateltwise_desc->m > i_mateltwise_desc->ldi) ) {
       LIBXSMM_HANDLE_ERROR( io_generated_code, LIBXSMM_ERR_LDA );
       return;
     }
-    if ( (i_mateltwise_desc->m > i_mateltwise_desc->ldo) ) {
-      LIBXSMM_HANDLE_ERROR( io_generated_code, LIBXSMM_ERR_LDB );
-      return;
+    if ( (i_mateltwise_desc->param == LIBXSMM_MELTW_TYPE_UNARY_TRANSFORM_PADM_MOD2)  ||
+         (i_mateltwise_desc->param == LIBXSMM_MELTW_TYPE_UNARY_TRANSFORM_PADNM_MOD2)    ) {
+      if ( (i_mateltwise_desc->m + i_mateltwise_desc->m%2) > i_mateltwise_desc->ldo ) {
+        LIBXSMM_HANDLE_ERROR( io_generated_code, LIBXSMM_ERR_LDB );
+        return;
+      }
+    } else {
+      if ( (i_mateltwise_desc->m > i_mateltwise_desc->ldo) ) {
+        LIBXSMM_HANDLE_ERROR( io_generated_code, LIBXSMM_ERR_LDB );
+        return;
+      }
     }
   } else {
     /* should not happen */
@@ -588,6 +613,12 @@ void libxsmm_generator_transform_avx_microkernel( libxsmm_generated_code*       
       libxsmm_generator_transform_norm_to_vnni_16bit_avx_microkernel( io_generated_code, io_loop_label_tracker,
                                                                       l_gp_reg_in, l_gp_reg_out, l_gp_reg_mloop, l_gp_reg_nloop,
                                                                       i_micro_kernel_config, i_mateltwise_desc, 1 );
+    } else if ( (i_mateltwise_desc->param == LIBXSMM_MELTW_TYPE_UNARY_TRANSFORM_PADN_MOD2)  ||
+                (i_mateltwise_desc->param == LIBXSMM_MELTW_TYPE_UNARY_TRANSFORM_PADM_MOD2)  ||
+                (i_mateltwise_desc->param == LIBXSMM_MELTW_TYPE_UNARY_TRANSFORM_PADNM_MOD2)    ) {
+      libxsmm_generator_transform_norm_padnm_mod2_16bit_avx_microkernel( io_generated_code, io_loop_label_tracker,
+                                                                         l_gp_reg_in, l_gp_reg_out, l_gp_reg_mloop, l_gp_reg_nloop,
+                                                                         i_micro_kernel_config, i_mateltwise_desc );
     } else {
       LIBXSMM_HANDLE_ERROR( io_generated_code, LIBXSMM_ERR_GENERAL );
       return;
