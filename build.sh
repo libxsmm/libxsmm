1e827fd037bedf5a553de32a78a782f654b7f5c7
Deepti Aggarwal <deepti.aggarwal@intel.com>
2021-09-24 11:02:14 -0700

    M and N loop fix

---
+++ build.sh
@@ -0,0 +1,8 @@
+ make realclean && make DBG=1 TRACE=1 -j 6
+ cd ./samples/xgemm/
+ make realclean && make DBG=1 TRACE=1 -j 6
+ cd ./../eltwise/
+ make realclean && make DBG=1 TRACE=1 -j 6
+ cd ./../deeplearning/cnnlayer
+ make realclean && make DBG=1 TRACE=1 -j 6
+
