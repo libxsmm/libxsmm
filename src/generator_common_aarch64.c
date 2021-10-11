7e5dd9ba860fc4f6b147a867bfac1460e90f74d7
Alexander Heinecke <heinecke.alex@gmail.com>
2021-10-11 00:07:50 -0700

    initial adjustments to enable eltwise kernels on aarch64 for arbitrary dimensions

--- src/generator_common_aarch64.c
+++ src/generator_common_aarch64.c
@@ -85,6 +85,57 @@ void libxsmm_generator_loop_header_gp_reg_bound_aarch64( libxsmm_generated_code*
 }
 
 LIBXSMM_API_INTERN
+void libxsmm_generator_load_store_partial_vreg_advgp_aarch64_asimd( libxsmm_generated_code* io_generated_code,
+                                                                    const unsigned int      i_gp_reg_addr,
+                                                                    const unsigned int      i_gp_reg_scratch,
+                                                                    const unsigned int      i_vec_reg,
+                                                                    const unsigned int      i_datatype_size,
+                                                                    const unsigned int      i_masked_elems,
+                                                                    const unsigned int      i_is_store ) {
+  unsigned int l_move_instr = ( i_is_store == 0 ) ? LIBXSMM_AARCH64_INSTR_ASIMD_LDR_I_POST : LIBXSMM_AARCH64_INSTR_ASIMD_STR_I_POST;
+
+  if ( i_masked_elems == 1 ) {
+    if ( i_is_store == 0 ) {
+      libxsmm_aarch64_instruction_asimd_compute( io_generated_code, LIBXSMM_AARCH64_INSTR_ASIMD_EOR_V, i_vec_reg, i_vec_reg, 0, i_vec_reg, LIBXSMM_AARCH64_ASIMD_TUPLETYPE_16B );
+    }
+    libxsmm_aarch64_instruction_asimd_move( io_generated_code, l_move_instr,
+                                            i_gp_reg_addr, LIBXSMM_AARCH64_GP_REG_UNDEF, i_datatype_size, i_vec_reg,
+                                            (i_datatype_size == 4) ? LIBXSMM_AARCH64_ASIMD_WIDTH_S : LIBXSMM_AARCH64_ASIMD_WIDTH_D );
+  } else if ( i_masked_elems == 2 ) {
+    if ( i_is_store == 0 ) {
+      libxsmm_aarch64_instruction_asimd_compute( io_generated_code, LIBXSMM_AARCH64_INSTR_ASIMD_EOR_V, i_vec_reg, i_vec_reg, 0, i_vec_reg, LIBXSMM_AARCH64_ASIMD_TUPLETYPE_16B );
+    }
+    libxsmm_aarch64_instruction_asimd_move( io_generated_code, l_move_instr,
+                                            i_gp_reg_addr, LIBXSMM_AARCH64_GP_REG_UNDEF, 8, i_vec_reg,
+                                            LIBXSMM_AARCH64_ASIMD_WIDTH_D );
+  } else if ( i_masked_elems == 3 ) {
+    if ( i_is_store == 0 ) {
+      libxsmm_aarch64_instruction_asimd_compute( io_generated_code, LIBXSMM_AARCH64_INSTR_ASIMD_EOR_V, i_vec_reg, i_vec_reg, 0, i_vec_reg, LIBXSMM_AARCH64_ASIMD_TUPLETYPE_16B );
+    }
+    libxsmm_aarch64_instruction_asimd_move( io_generated_code, l_move_instr,
+                                            i_gp_reg_addr, LIBXSMM_AARCH64_GP_REG_UNDEF, 8, i_vec_reg,
+                                            LIBXSMM_AARCH64_ASIMD_WIDTH_D );
+    if ( i_is_store == 0 ) {
+      libxsmm_aarch64_instruction_alu_move( io_generated_code, LIBXSMM_AARCH64_INSTR_GP_LDR_I_POST,
+                                            i_gp_reg_addr, LIBXSMM_AARCH64_GP_REG_UNDEF, 4,
+                                            0x1f & i_gp_reg_scratch );
+      libxsmm_aarch64_instruction_asimd_gpr_move( io_generated_code, LIBXSMM_AARCH64_INSTR_ASIMD_MOV_G_V,
+                                                  i_gp_reg_scratch, i_vec_reg, 3, LIBXSMM_AARCH64_ASIMD_WIDTH_S );
+    } else {
+      libxsmm_aarch64_instruction_asimd_gpr_move( io_generated_code, LIBXSMM_AARCH64_INSTR_ASIMD_UMOV_V_G,
+                                                  i_gp_reg_scratch, i_vec_reg, 3, LIBXSMM_AARCH64_ASIMD_WIDTH_S );
+      libxsmm_aarch64_instruction_alu_move( io_generated_code, LIBXSMM_AARCH64_INSTR_GP_STR_I_POST,
+                                            i_gp_reg_addr, LIBXSMM_AARCH64_GP_REG_UNDEF, 4,
+                                            0x1f & i_gp_reg_scratch );
+    }
+  } else {
+    libxsmm_aarch64_instruction_asimd_move( io_generated_code, l_move_instr,
+                                            i_gp_reg_addr, LIBXSMM_AARCH64_GP_REG_UNDEF, 16, i_vec_reg,
+                                            LIBXSMM_AARCH64_ASIMD_WIDTH_Q );
+  }
+}
+
+LIBXSMM_API_INTERN
 void libxsmm_generator_load_2dregblock_aarch64_asimd( libxsmm_generated_code* io_generated_code,
                                                       const unsigned int      i_gp_reg_addr,
                                                       const unsigned int      i_gp_reg_scratch,
