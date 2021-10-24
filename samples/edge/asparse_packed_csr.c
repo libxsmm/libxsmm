c92f04400a7d7f908243b987ffcc3f7eaeaa95d8
Adrian Krauß <adrian.krauss@tum.de>
2021-10-24 16:20:19 +0200

    fixed usage of wrong variables in samples/edge/asparse_packed_csr.c

--- samples/edge/asparse_packed_csr.c
+++ samples/edge/asparse_packed_csr.c
@@ -9,31 +9,29 @@
 /* Alexander Heinecke (Intel Corp.)
 ******************************************************************************/
 #include <libxsmm.h>
-
 #include "common_edge_proxy.h"
 
-
 int main(int argc, char* argv[]) {
-  int M = ( argc == 7 ) ? atoi(argv[1]) : 9;
-  int N = ( argc == 7 ) ? atoi(argv[2]) : 10;
-  int K = ( argc == 7 ) ? atoi(argv[3]) : 9;
-  unsigned int N_CRUNS = ( argc == 7 ) ? atoi(argv[4]) : 8;
-  unsigned int REPS =    ( argc == 7 ) ? atoi(argv[5]) : 1;
-  char* l_csr_file =     ( argc == 7 ) ?      argv[6]  : "file.csr";
+  int M = atoi(argv[1]);
+  int N = atoi(argv[2]);
+  int K = atoi(argv[3]);
+  unsigned int N_CRUNS = atoi(argv[4]);
+  unsigned int REPS =    atoi(argv[5]);
+  char* l_csr_file =     argv[6];
 
   const libxsmm_gemm_prefetch_type prefetch = LIBXSMM_GEMM_PREFETCH_NONE;
   const int flags = LIBXSMM_GEMM_FLAGS('N', 'N');
   const REALTYPE alpha = 1, beta = 1;
 
-  REALTYPE* l_a_de = (REALTYPE*)libxsmm_aligned_malloc(sizeof(REALTYPE) * K * K, 64);
+  REALTYPE* l_a_de = (REALTYPE*)libxsmm_aligned_malloc(sizeof(REALTYPE) * M * K, 64);
   REALTYPE* l_a_sp = NULL;
   REALTYPE* l_b = (REALTYPE*)libxsmm_aligned_malloc(sizeof(REALTYPE) * K * N * N_CRUNS, 64);
   unsigned int* l_rowptr = NULL;
   unsigned int* l_colidx = NULL;
   unsigned int l_rowcount, l_colcount, l_elements;
-  REALTYPE* l_c = (REALTYPE*)libxsmm_aligned_malloc(sizeof(REALTYPE) * K * N * N_CRUNS, 64);
-  REALTYPE* l_c_gold = (REALTYPE*)libxsmm_aligned_malloc(sizeof(REALTYPE) * K * N * N_CRUNS, 64);
-  REALTYPE* l_c_asm = (REALTYPE*)libxsmm_aligned_malloc(sizeof(REALTYPE) * K * N * N_CRUNS, 64);
+  REALTYPE* l_c = (REALTYPE*)libxsmm_aligned_malloc(sizeof(REALTYPE) * M * N * N_CRUNS, 64);
+  REALTYPE* l_c_gold = (REALTYPE*)libxsmm_aligned_malloc(sizeof(REALTYPE) * M * N * N_CRUNS, 64);
+  REALTYPE* l_c_asm = (REALTYPE*)libxsmm_aligned_malloc(sizeof(REALTYPE) * M * N * N_CRUNS, 64);
   REALTYPE l_max_error = 0.0;
   unsigned int l_k, l_n;
   int l_i, l_j, l_jj;
@@ -66,7 +64,7 @@ int main(int argc, char* argv[]) {
   }
 
   /* touch C */
-  for ( l_i = 0; l_i < K; l_i++) {
+  for ( l_i = 0; l_i < M; l_i++) {
     for ( l_j = 0; l_j < N; l_j++) {
       for ( l_k = 0; l_k < N_CRUNS; l_k++ ) {
         LIBXSMM_VLA_ACCESS(3, l_p_c_gold, l_i, l_j, l_k, N, N_CRUNS) = (REALTYPE)0.0;
@@ -86,11 +84,11 @@ int main(int argc, char* argv[]) {
   printf("CSR matrix data structure we just read:\n");
   printf("rows: %u, columns: %u, elements: %u\n", l_rowcount, l_colcount, l_elements);
 
-  for ( l_n = 0; l_n < (((unsigned int)K) * K); l_n++) {
+  for ( l_n = 0; l_n < (((unsigned int)M) * K); l_n++) {
     l_a_de[l_n] = 0.0;
   }
 
-  for ( l_n = 0; l_n < (unsigned int)K; l_n++) {
+  for ( l_n = 0; l_n < (unsigned int)M; l_n++) {
     const unsigned int l_rowelems = l_rowptr[l_n+1] - l_rowptr[l_n];
     assert(l_rowptr[l_n+1] >= l_rowptr[l_n]);
 
@@ -103,7 +101,7 @@ int main(int argc, char* argv[]) {
   l_start = libxsmm_timer_tick();
 #if 1
   for ( l_n = 0; l_n < REPS; l_n++) {
-    for ( l_i = 0; l_i < K; l_i++) {
+    for ( l_i = 0; l_i < M; l_i++) {
       for ( l_j = 0; l_j < N; l_j++) {
         for ( l_jj = 0; l_jj < K; l_jj++) {
           LIBXSMM_PRAGMA_SIMD
@@ -120,10 +118,10 @@ int main(int argc, char* argv[]) {
   l_end = libxsmm_timer_tick();
   l_total = libxsmm_timer_duration(l_start, l_end);
   printf("%fs for dense\n", l_total);
-  printf("%f GFLOPS for dense\n", ((double)((double)REPS * (double)K * (double)K * (double)N * (double)N_CRUNS) * 2.0) / (l_total * 1.0e9));
+  printf("%f GFLOPS for dense\n", ((double)((double)REPS * (double)M * (double)K * (double)N * (double)N_CRUNS) * 2.0) / (l_total * 1.0e9));
 
   l_xgemm_desc = libxsmm_gemm_descriptor_dinit(&l_xgemm_blob, LIBXSMM_GEMM_PRECISION(REALTYPE),
-    K, N, K, 0, N, N, alpha, beta, flags, prefetch);
+    M, N, K, 0, N, N, alpha, beta, flags, prefetch);
 
   /* sparse routine */
 #if defined(__EDGE_EXECUTE_F32__)
@@ -131,7 +129,7 @@ int main(int argc, char* argv[]) {
 #else
   mykernel = libxsmm_create_packed_spxgemm_csr( l_xgemm_desc, N_CRUNS, l_rowptr, l_colidx, (const void*)l_a_sp ).dmm;
 #endif
-
+  
   l_start = libxsmm_timer_tick();
   for ( l_n = 0; l_n < REPS; l_n++) {
     mykernel( l_a_sp, l_b, l_c_asm );
@@ -146,7 +144,7 @@ int main(int argc, char* argv[]) {
 
   /* check for errors */
   l_max_error = (REALTYPE)0.0;
-  for ( l_i = 0; l_i < K; l_i++) {
+  for ( l_i = 0; l_i < M; l_i++) {
     for ( l_j = 0; l_j < N; l_j++) {
       for ( l_k = 0; l_k < N_CRUNS; l_k++ ) {
         if (fabs( LIBXSMM_VLA_ACCESS(3, l_p_c_gold, l_i, l_j, l_k, N, N_CRUNS)
@@ -157,10 +155,11 @@ int main(int argc, char* argv[]) {
       }
     }
   }
+
   printf("max error: %f\n", l_max_error);
 
-  printf("PERFDUMP,%s,%u,%i,%i,%i,%u,%u,%f,%f,%f\n", l_csr_file, REPS, M, N, K, l_elements, K * l_elements * N_CRUNS * 2, l_max_error, l_total, ((double)((double)REPS * (double)K * (double)l_elements * (double)N_CRUNS) * 2.0) / (l_total * 1.0e9) );
-
+  printf("PERFDUMP,%s,%u,%i,%i,%i,%u,%u,%f,%f,%f\n", l_csr_file, REPS, M, N, K, l_elements, N * l_elements * N_CRUNS * 2, l_max_error, l_total, ((double)((double)REPS * (double)N * (double)l_elements * (double)N_CRUNS) * 2.0) / (l_total * 1.0e9) );
+  
   /* free */
   libxsmm_free( l_a_de );
   libxsmm_free( l_b );
@@ -174,4 +173,3 @@ int main(int argc, char* argv[]) {
 
   return 0;
 }
-
