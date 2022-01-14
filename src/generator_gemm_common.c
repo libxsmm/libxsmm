89c293a6584ba3b86a040eea0729dea56963e7f6
Voronin Kirill <kirill.voronin@intel.com>
2022-01-14 10:25:33 -0800

    Removed an unnecessary check for EXT_ABI in stack_allocate_scratch for the transpose

--- src/generator_gemm_common.c
+++ src/generator_gemm_common.c
@@ -806,8 +806,7 @@ void libxsmm_generator_gemm_setup_stack_frame_allocate_scratch( libxsmm_generate
     }
   }
 
-  if ( ((LIBXSMM_GEMM_FLAG_USE_XGEMM_EXT_ABI & i_xgemm_desc->flags) == LIBXSMM_GEMM_FLAG_USE_XGEMM_EXT_ABI) &&
-       ((LIBXSMM_GEMM_FLAG_TRANS_A & i_xgemm_desc->flags) > 0)  ) {
+  if ( (LIBXSMM_GEMM_FLAG_TRANS_A & i_xgemm_desc->flags) > 0 ) {
     transpose_scratch_size = i_xgemm_desc->m * i_xgemm_desc->k * LIBXSMM_TYPESIZE(LIBXSMM_GETENUM_OUT(i_xgemm_desc->datatype));
     transpose_pad_size  = (transpose_scratch_size % 64 == 0) ? 0 : ((transpose_scratch_size + 63)/64) * 64 - transpose_scratch_size;
     transpose_scratch_size += transpose_pad_size;
