672e150c261b01e7d935aa68b826794bd6184b54
Alexander Heinecke <heinecke.alex@gmail.com>
2022-01-24 22:24:59 -0800

    updated pooling_tpp

--- samples/deeplearning/op_lib_tpp/pooling_tpp.h
+++ samples/deeplearning/op_lib_tpp/pooling_tpp.h
@@ -27,10 +27,14 @@ typedef struct my_pooling_fwd_config {
   libxsmm_blasint  C;
   libxsmm_blasint  H;
   libxsmm_blasint  W;
+  libxsmm_blasint  R;
+  libxsmm_blasint  S;
   libxsmm_blasint  bc;
   libxsmm_blasint  Bc;
   libxsmm_blasint  ofh;
   libxsmm_blasint  ofw;
+  libxsmm_blasint  u;
+  libxsmm_blasint  v;
   libxsmm_blasint  pad_h;
   libxsmm_blasint  pad_w;
   libxsmm_blasint  pad_h_in;
@@ -49,10 +53,14 @@ typedef struct my_pooling_bwd_config {
   libxsmm_blasint  C;
   libxsmm_blasint  H;
   libxsmm_blasint  W;
+  libxsmm_blasint  R;
+  libxsmm_blasint  S;
   libxsmm_blasint  bc;
   libxsmm_blasint  Bc;
   libxsmm_blasint  ofh;
   libxsmm_blasint  ofw;
+  libxsmm_blasint  u;
+  libxsmm_blasint  v;
   libxsmm_blasint  pad_h;
   libxsmm_blasint  pad_w;
   libxsmm_blasint  pad_h_in;
@@ -67,6 +75,8 @@ typedef struct my_pooling_bwd_config {
 } my_pooling_bwd_config;
 
 my_pooling_fwd_config setup_my_pooling_fwd( libxsmm_blasint N, libxsmm_blasint C, libxsmm_blasint H, libxsmm_blasint W,
+                                            libxsmm_blasint R, libxsmm_blasint S,
+                                            libxsmm_blasint stride_h, libxsmm_blasint stride_w,
                                             libxsmm_blasint pad_h, libxsmm_blasint pad_w,
                                             libxsmm_blasint pad_h_in, libxsmm_blasint pad_w_in,
                                             libxsmm_blasint pad_h_out, libxsmm_blasint pad_w_out,
@@ -78,10 +88,14 @@ my_pooling_fwd_config setup_my_pooling_fwd( libxsmm_blasint N, libxsmm_blasint C
   res.C = C;
   res.H = H;
   res.W = W;
+  res.R = R;
+  res.S = S;
   res.bc = bc;
   res.Bc = C / bc;
   res.pool_type = pool_type;
   res.pass_type = MY_POOLING_PASS_FWD;
+  res.u = stride_h;
+  res.v = stride_w;
   res.pad_h = pad_h;
   res.pad_w = pad_w;
   res.pad_h_in = pad_h_in;
@@ -90,8 +104,8 @@ my_pooling_fwd_config setup_my_pooling_fwd( libxsmm_blasint N, libxsmm_blasint C
   res.pad_w_out = pad_w_out;
 
   /* setting ofh and ofw */
-  res.ofh = (handle->desc.H + 2 * handle->desc.pad_h - handle->desc.R) / handle->desc.u + 1;
-  res.ofw = (handle->desc.W + 2 * handle->desc.pad_w - handle->desc.S) / handle->desc.v + 1;
+  res.ofh = (H + 2 * pad_h - R) / u + 1;
+  res.ofw = (W + 2 * pad_w - S) / v + 1;
    /* create barrier */
   res.barrier = libxsmm_barrier_create(threads, 1);
   /* calculate scratch size for local pooling copies of one feature map block per thread */
@@ -103,6 +117,8 @@ my_pooling_fwd_config setup_my_pooling_fwd( libxsmm_blasint N, libxsmm_blasint C
 }
 
 my_pooling_bwd_config setup_my_pooling_bwd( libxsmm_blasint N, libxsmm_blasint C, libxsmm_blasint H, libxsmm_blasint W,
+                                            libxsmm_blasint R, libxsmm_blasint S,
+                                            libxsmm_blasint stride_h, libxsmm_blasint stride_w,
                                             libxsmm_blasint pad_h, libxsmm_blasint pad_w,
                                             libxsmm_blasint pad_h_in, libxsmm_blasint pad_w_in,
                                             libxsmm_blasint pad_h_out, libxsmm_blasint pad_w_out,
@@ -114,10 +130,14 @@ my_pooling_bwd_config setup_my_pooling_bwd( libxsmm_blasint N, libxsmm_blasint C
   res.C = C;
   res.H = H;
   res.W = W;
+  res.R = R;
+  res.S = S;
   res.bc = bc;
   res.Bc = C / bc;
   res.pool_type = pool_type;
   res.pass_type = MY_POOLING_PASS_FWD;
+  res.u = stride_h;
+  res.v = stride_w;
   res.pad_h = pad_h;
   res.pad_w = pad_w;
   res.pad_h_in = pad_h_in;
@@ -126,8 +146,8 @@ my_pooling_bwd_config setup_my_pooling_bwd( libxsmm_blasint N, libxsmm_blasint C
   res.pad_w_out = pad_w_out;
 
   /* setting ofh and ofw */
-  res.ofh = (handle->desc.H + 2 * handle->desc.pad_h - handle->desc.R) / handle->desc.u + 1;
-  res.ofw = (handle->desc.W + 2 * handle->desc.pad_w - handle->desc.S) / handle->desc.v + 1;
+  res.ofh = (H + 2 * pad_h - R) / u + 1;
+  res.ofw = (W + 2 * pad_w - S) / v + 1;
    /* create barrier */
   res.barrier = libxsmm_barrier_create(threads, 1);
   /* calculate scratch size for local pooling copies of one feature map block per thread */
@@ -141,31 +161,17 @@ my_pooling_bwd_config setup_my_pooling_bwd( libxsmm_blasint N, libxsmm_blasint C
 void my_pooling_fwd_exec_f32( my_pooling_fwd_config cfg, const float* in_act_ptr, float* out_act_ptr, libxsmm_blasint* mask_ptr,
                               libxsmm_blasint start_tid, libxsmm_blasint my_tid, void* scratch ) {
   /* size variables, all const */
-  const int nImg = handle->desc.N;
-  const int ifh = handle->desc.H;
-  const int ifw = handle->desc.W;
-  const int sh = handle->desc.u;
-  const int sw = handle->desc.v;
-  const int ofh = handle->ofh;
-const int ofw = handle->ofw;
-const int iph = handle->desc.pad_h_in;
-const int ipw = handle->desc.pad_w_in;
-const int oph = handle->desc.pad_h_out;
-const int opw = handle->desc.pad_w_out;
-const int ofhp = ofh + 2*oph;
-const int ofwp = ofw + 2*opw;
-const int ifhp = ifh + 2*iph;
-const int ifwp = ifw + 2*ipw;
-/* here we assume that input and output blocking is similar */
-const int nBlocksFm = handle->blocksifm;
-const int nFmBlock = handle->ifmblock;
+  const libxsmm_blasint ofhp = cfg.ofh + 2*cfg.pad_h_out;
+  const libxsmm_blasint ofwp = cfg.ofw + 2*cfg.pad_w_out;
+  const libxsmm_blasint ifhp = cfg.H   + 2*cfg.pad_h_in;
+  const libxsmm_blasint ifwp = cfg.W   + 2*cfg.pad_w_in;
 
   /* computing first logical thread */
   const libxsmm_blasint ltid = tid - start_thread;
   /* number of tasks that could be run in parallel */
   const libxsmm_blasint work = cfg.N * cfg.Bc;
   /* compute chunk size */
-  const libxsmm_blasint chunksize = (work % handle->desc.threads == 0) ? (work / handle->desc.threads) : ((work / handle->desc.threads) + 1);
+  const libxsmm_blasint chunksize = (work % cfg.threads == 0) ? (work / cfg.threads) : ((work / cfg.threads) + 1);
   /* compute thr_begin and thr_end */
   const libxsmm_blasint thr_begin = (ltid * chunksize < work) ? (ltid * chunksize) : work;
   const libxsmm_blasint thr_end = ((ltid + 1) * chunksize < work) ? ((ltid + 1) * chunksize) : work;
@@ -183,24 +189,24 @@ const int nFmBlock = handle->ifmblock;
   libxsmm_blasint v = 0;
 
   /* only for average pooling */
-  float recp_pool_size = 1.0f/((float)handle->desc.R*(float)handle->desc.S);
+  float recp_pool_size = 1.0f/((float)cfg.R*(float)cfg.S);
 
   /* multi-dim arrays declaration */
-  float *const lcl_buffer_ptr = (float*)handle->scratch + (size_t)ofh*ofw*nFmBlock*ltid;
-  LIBXSMM_VLA_DECL(3,                 float, lcl_output, lcl_buffer_ptr, ofw, nFmBlock);
-  LIBXSMM_VLA_DECL(5, const float,             input,  in_act_ptr, nBlocksFm, ifhp, ifwp, nFmBlock);
-  LIBXSMM_VLA_DECL(5,       float,            output, out_act_ptr, nBlocksFm, ofhp, ofwp, nFmBlock);
-  LIBXSMM_VLA_DECL(5,       libxsmm_blasint,    mask,    mask_ptr, nBlocksFm,  ofh,  ofw, nFmBlock);
+  float *const lcl_buffer_ptr = (float*)scratch + (size_t)cfg.ofh*cfg.ofw*cfg.bc*ltid;
+  LIBXSMM_VLA_DECL(3,                 float, lcl_output, lcl_buffer_ptr, ofw, cfg.bc;
+  LIBXSMM_VLA_DECL(5, const float,             input,  in_act_ptr, cfg.Bc,    ifhp,    ifwp, cfg.bc);
+  LIBXSMM_VLA_DECL(5,       float,            output, out_act_ptr, cfg.Bc,    ofhp,    ofwp, cfg.bc);
+  LIBXSMM_VLA_DECL(5,       libxsmm_blasint,    mask,    mask_ptr, cfg.Bc, cfg.ofh, cfg.ofw, cfg.bc);
 
   /* lazy barrier init */
   libxsmm_barrier_init(handle->barrier, ltid);
 
   for (imgfm = thr_begin; imgfm < thr_end; ++imgfm) {
-    img = imgfm / nBlocksFm;
-    fm = imgfm % nBlocksFm;
+    img = imgfm / cfg.Bc;
+    fm = imgfm % cfg.Bc;
 
     LIBXSMM_PRAGMA_SIMD
-    for ( v = 0; v < ofh*ofw*nFmBlock; v++ ) {
+    for ( v = 0; v < cfg.ofh*cfg.ofw*cg.bc; v++ ) {
       if ( cfg.pool_type == MY_POOLINT_TYPE_MAX ) {
         lcl_buffer_ptr[v] = -FLT_MAX;
       } else if ( cfg.pool_type == MY_POOLINT_TYPE_AVG ) {
@@ -208,20 +214,20 @@ const int nFmBlock = handle->ifmblock;
       }
     }
 
-    for ( ho = oph; ho < (ofh+oph); ho++ ) {
-      hi = ((ho-oph) * sh) - handle->desc.pad_h;
-      for ( wo = opw; wo < (ofw+opw); wo++ ) {
-        wi = ((wo-opw) * sw) - handle->desc.pad_w;
-        for ( kh = 0; kh < handle->desc.R; kh++ ) {
+    for ( ho = cfg.pad_h_out; ho < (cfg.ofh+cfg.pad_h_out); ho++ ) {
+      hi = ((ho-cfg.pad_h_out) * cfg.u) - cfg.pad_h;
+      for ( wo = cfg.pad_w_out; wo < (cfg.ofw+cfg.pad_w_out); wo++ ) {
+        wi = ((wo-cfg.pad_w_out) * cfg.v) - cfg.pad_w;
+        for ( kh = 0; kh < cfg.R; kh++ ) {
           if (hi+kh < 0 || hi+kh >= ifh) continue;
-          for ( kw = 0; kw < handle->desc.S; kw++ ) {
+          for ( kw = 0; kw < cfg.S; kw++ ) {
             if (wi+kw < 0 || wi+kw >= ifw) {
               continue;
             } else {
-              const float*          input_ptr = &LIBXSMM_VLA_ACCESS(5, input,      img, fm, hi+kh+iph, wi+kw+ipw, 0, nBlocksFm, ifhp, ifwp, nFmBlock);
-                    float*     lcl_output_ptr = &LIBXSMM_VLA_ACCESS(3, lcl_output,             ho-oph,    wo-opw, 0,                   ofw, nFmBlock);
-              const libxsmm_blasint       idx = (hi+kh)*ifw*nFmBlock + (wi+kw)*nFmBlock;
-                    libxsmm_blasint* mask_ptr = &LIBXSMM_VLA_ACCESS(5, mask,       img, fm,    ho-oph,    wo-opw, 0, nBlocksFm,  ofh,  ofw, nFmBlock);
+              const float*          input_ptr = &LIBXSMM_VLA_ACCESS(5, input,      img, fm,  hi+kh+cfg.pad_h_in,  wi+kw+cfg.pad_w_in, 0, cfg.Bc,    ifhp,    ifwp, cfg.bc);
+                    float*     lcl_output_ptr = &LIBXSMM_VLA_ACCESS(3, lcl_output,             ho-cfg.pad_h_out,    wo-cfg.pad_w_out, 0,                  cfg.ofw, cfg.bc);
+              const libxsmm_blasint       idx = (hi+kh)*ifw*cfg.bc + (wi+kw)*cfg.bc;
+                    libxsmm_blasint* mask_ptr = &LIBXSMM_VLA_ACCESS(5, mask,       img, fm,    ho-cfg.pad_h_out,    wo-cfg.pad_w_out, 0, cfg.Bc, cfg.ofh, cfg.ofw, cfg.bc);
               LIBXSMM_PRAGMA_SIMD
               for ( v = 0; v < nFmBlock; v++ ) {
                 if ( cfg.pool_type == MY_POOLINT_TYPE_MAX ) {
@@ -240,10 +246,10 @@ const int nFmBlock = handle->ifmblock;
     }
 
     /* copy the local buffer into output activations */
-    for ( ho = oph; ho < (ofh+oph); ho++ ) {
-      for ( wo = opw; wo < (ofw+opw); wo++ ) {
-        float*     output_ptr = &LIBXSMM_VLA_ACCESS(5, output,     img, fm,        ho,        wo, 0, nBlocksFm, ofhp, ofwp, nFmBlock);
-        float* lcl_output_ptr = &LIBXSMM_VLA_ACCESS(3, lcl_output,             ho-oph,    wo-opw, 0,                   ofw, nFmBlock);
+    for ( ho = cfg.pad_h_out; ho < (cfg.ofh+cfg.pad_h_out); ho++ ) {
+      for ( wo = cfg.pad_w_out; wo < (cfg.ofw+cfg.pad_w_out); wo++ ) {
+        float*     output_ptr = &LIBXSMM_VLA_ACCESS(5, output,     img, fm,        ho,        wo, 0, cfg.Bc, ofhp, ofwp, cfg.bc);
+        float* lcl_output_ptr = &LIBXSMM_VLA_ACCESS(3, lcl_output,             ho-cfg.pad_h_out,    wo-cfg.pad_w_out, 0,                   cfg.ofw, cfg.bc);
 
         LIBXSMM_PRAGMA_SIMD
         for ( v = 0; v < nFmBlock; v++ ) {
