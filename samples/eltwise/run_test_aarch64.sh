78105b0ca8dc1cf445bb199c1a40297b8ae8c079
Alexander Heinecke <heinecke.alex@gmail.com>
2022-02-08 23:16:13 -0800

    added parallelism to aarch64 test script

--- samples/eltwise/run_test_aarch64.sh
+++ samples/eltwise/run_test_aarch64.sh
@@ -1,64 +1,69 @@
 #!/usr/bin/env bash
 
-. ./kernel_test/unary_trans_08b_eqld.sh
-. ./kernel_test/unary_trans_08b_gtld.sh
-. ./kernel_test/unary_trans_16b_eqld.sh
-. ./kernel_test/unary_trans_16b_gtld.sh
-. ./kernel_test/unary_trans_32b_eqld.sh
-. ./kernel_test/unary_trans_32b_gtld.sh
-. ./kernel_test/unary_trans_64b_eqld.sh
-. ./kernel_test/unary_trans_64b_gtld.sh
-. ./kernel_test/unary_trans_padm_mod2_16b_eqld.sh
-. ./kernel_test/unary_trans_padm_mod2_16b_gtld.sh
-. ./kernel_test/unary_trans_padnm_mod2_16b_eqld.sh
-. ./kernel_test/unary_trans_padnm_mod2_16b_gtld.sh
-. ./kernel_test/unary_trans_padn_mod2_16b_eqld.sh
-. ./kernel_test/unary_trans_padn_mod2_16b_gtld.sh
-. ./kernel_test/unary_vnni_16b_eqld.sh
-. ./kernel_test/unary_vnni_16b_gtld.sh
-. ./kernel_test/unary_vnnitrans_16b_eqld.sh
-. ./kernel_test/unary_vnnitrans_16b_gtld.sh
-. ./kernel_test/unary_copy_32b_eqld.sh
-. ./kernel_test/unary_copy_32b_gtld.sh
-. ./kernel_test/unary_xor_32b_eqld.sh
-. ./kernel_test/unary_xor_32b_gtld.sh
-. ./kernel_test/unary_sqrt_32b_eqld.sh
-. ./kernel_test/unary_sqrt_32b_gtld.sh
-. ./kernel_test/unary_x2_32b_eqld.sh
-. ./kernel_test/unary_x2_32b_gtld.sh
-. ./kernel_test/unary_negate_32b_eqld.sh
-. ./kernel_test/unary_negate_32b_gtld.sh
-. ./kernel_test/unary_exp_32b_eqld.sh
-. ./kernel_test/unary_exp_32b_gtld.sh
-. ./kernel_test/unary_inc_32b_eqld.sh
-. ./kernel_test/unary_inc_32b_gtld.sh
-. ./kernel_test/unary_rcp_32b_eqld.sh
-. ./kernel_test/unary_rcp_32b_gtld.sh
-. ./kernel_test/unary_rcp_sqrt_32b_eqld.sh
-. ./kernel_test/unary_rcp_sqrt_32b_gtld.sh
-. ./kernel_test/unary_relu_32b_eqld.sh
-. ./kernel_test/unary_relu_32b_gtld.sh
-. ./kernel_test/unary_dropout_32b_eqld.sh
-. ./kernel_test/unary_dropout_32b_gtld.sh
-. ./kernel_test/binary_add_32b_eqld.sh
-. ./kernel_test/binary_add_32b_gtld.sh
-. ./kernel_test/binary_mul_32b_eqld.sh
-. ./kernel_test/binary_mul_32b_gtld.sh
-. ./kernel_test/binary_sub_32b_eqld.sh
-. ./kernel_test/binary_sub_32b_gtld.sh
-. ./kernel_test/binary_div_32b_gtld.sh
-. ./kernel_test/binary_div_32b_eqld.sh
-. ./kernel_test/binary_muladd_32b_gtld.sh
-. ./kernel_test/binary_muladd_32b_eqld.sh
-. ./kernel_test/unary_gelu_32b_eqld.sh
-. ./kernel_test/unary_gelu_32b_gtld.sh
-. ./kernel_test/unary_gelu_inv_32b_eqld.sh
-. ./kernel_test/unary_gelu_inv_32b_gtld.sh
-. ./kernel_test/unary_tanh_32b_eqld.sh
-. ./kernel_test/unary_tanh_32b_gtld.sh
-. ./kernel_test/unary_tanh_inv_32b_eqld.sh
-. ./kernel_test/unary_tanh_inv_32b_gtld.sh
-. ./kernel_test/unary_sigmoid_32b_eqld.sh
-. ./kernel_test/unary_sigmoid_32b_gtld.sh
-. ./kernel_test/unary_sigmoid_inv_32b_eqld.sh
-. ./kernel_test/unary_sigmoid_inv_32b_gtld.sh
+cat <<EOM | ../../scripts/tool_pexec.sh
+./kernel_test/unary_trans_08b_eqld.sh
+./kernel_test/unary_trans_08b_gtld.sh
+./kernel_test/unary_trans_16b_eqld.sh
+./kernel_test/unary_trans_16b_gtld.sh
+./kernel_test/unary_trans_32b_eqld.sh
+./kernel_test/unary_trans_32b_gtld.sh
+./kernel_test/unary_trans_64b_eqld.sh
+./kernel_test/unary_trans_64b_gtld.sh
+./kernel_test/unary_trans_padm_mod2_16b_eqld.sh
+./kernel_test/unary_trans_padm_mod2_16b_gtld.sh
+./kernel_test/unary_trans_padnm_mod2_16b_eqld.sh
+./kernel_test/unary_trans_padnm_mod2_16b_gtld.sh
+./kernel_test/unary_trans_padn_mod2_16b_eqld.sh
+./kernel_test/unary_trans_padn_mod2_16b_gtld.sh
+./kernel_test/unary_vnni_16b_eqld.sh
+./kernel_test/unary_vnni_16b_gtld.sh
+./kernel_test/unary_vnnitrans_16b_eqld.sh
+./kernel_test/unary_vnnitrans_16b_gtld.sh
+./kernel_test/unary_copy_32b_eqld.sh
+./kernel_test/unary_copy_32b_gtld.sh
+./kernel_test/unary_xor_32b_eqld.sh
+./kernel_test/unary_xor_32b_gtld.sh
+./kernel_test/unary_sqrt_32b_eqld.sh
+./kernel_test/unary_sqrt_32b_gtld.sh
+./kernel_test/unary_x2_32b_eqld.sh
+./kernel_test/unary_x2_32b_gtld.sh
+./kernel_test/unary_negate_32b_eqld.sh
+./kernel_test/unary_negate_32b_gtld.sh
+./kernel_test/unary_exp_32b_eqld.sh
+./kernel_test/unary_exp_32b_gtld.sh
+./kernel_test/unary_inc_32b_eqld.sh
+./kernel_test/unary_inc_32b_gtld.sh
+./kernel_test/unary_rcp_32b_eqld.sh
+./kernel_test/unary_rcp_32b_gtld.sh
+./kernel_test/unary_rcp_sqrt_32b_eqld.sh
+./kernel_test/unary_rcp_sqrt_32b_gtld.sh
+./kernel_test/unary_relu_32b_eqld.sh
+./kernel_test/unary_relu_32b_gtld.sh
+./kernel_test/unary_dropout_32b_eqld.sh
+./kernel_test/unary_dropout_32b_gtld.sh
+./kernel_test/binary_add_32b_eqld.sh
+./kernel_test/binary_add_32b_gtld.sh
+./kernel_test/binary_mul_32b_eqld.sh
+./kernel_test/binary_mul_32b_gtld.sh
+./kernel_test/binary_sub_32b_eqld.sh
+./kernel_test/binary_sub_32b_gtld.sh
+./kernel_test/binary_div_32b_gtld.sh
+./kernel_test/binary_div_32b_eqld.sh
+./kernel_test/binary_muladd_32b_gtld.sh
+./kernel_test/binary_muladd_32b_eqld.sh
+./kernel_test/unary_gelu_32b_eqld.sh
+./kernel_test/unary_gelu_32b_gtld.sh
+./kernel_test/unary_gelu_inv_32b_eqld.sh
+./kernel_test/unary_gelu_inv_32b_gtld.sh
+./kernel_test/unary_tanh_32b_eqld.sh
+./kernel_test/unary_tanh_32b_gtld.sh
+./kernel_test/unary_tanh_inv_32b_eqld.sh
+./kernel_test/unary_tanh_inv_32b_gtld.sh
+./kernel_test/unary_sigmoid_32b_eqld.sh
+./kernel_test/unary_sigmoid_32b_gtld.sh
+./kernel_test/unary_sigmoid_inv_32b_eqld.sh
+./kernel_test/unary_sigmoid_inv_32b_gtld.sh
+EOM
+
+rm -f tmp.??????????
+
