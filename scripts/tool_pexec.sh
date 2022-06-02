2e0f65a5a5314cc116a90399c3062ebe0afcdda2
Hans Pabst <hans.pabst@intel.com>
2022-06-02 10:50:47 +0200

    Avoid setting the number of input lines per process (although desirable to be one) as a bogus warning appears for more recent xargs in combination with replacement (-I).

--- scripts/tool_pexec.sh
+++ scripts/tool_pexec.sh
@@ -46,7 +46,7 @@ if [ "${BASENAME}" ] && [ "${XARGS}" ] && [ "${FILE}" ] && [ "${GREP}" ]; then
     export OMP_NUM_THREADS=1
     NP=0
   fi
-  ${XARGS} </dev/stdin -L1 -P${NP} -I% bash -c \
+  ${XARGS} </dev/stdin -P${NP} -I% bash -c \
     "_trap_err() { 1>&2 echo \" -> ERROR: \$(${BASENAME} %)\"; exit 1; }; trap '_trap_err' ERR; \
      if [ \"\$(${FILE} -bL --mime % | ${GREP} '^text/')\" ]; then source %; else %; fi"
   RESULT=$?
