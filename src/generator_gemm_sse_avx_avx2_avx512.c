329c23e82e689d4ba53d6f0e9ce1a16eecbe1c82
Alexander Heinecke <heinecke.alex@gmail.com>
2022-06-06 09:44:45 -0700

    attempt to fix pure BF16 AVX2 GEMM masking bug

--- src/generator_gemm_sse_avx_avx2_avx512.c
+++ src/generator_gemm_sse_avx_avx2_avx512.c
@@ -486,6 +486,8 @@ LIBXSMM_API_INTERN void libxsmm_generator_gemm_sse_avx_avx2_avx512_kernel( libxs
                (LIBXSMM_DATATYPE_I32 == LIBXSMM_GETENUM_OUT( i_xgemm_desc->datatype )) ||
                (LIBXSMM_DATATYPE_F64 == LIBXSMM_GETENUM_OUT( i_xgemm_desc->datatype ))    ) {
             libxsmm_generator_initialize_avx_mask( io_generated_code, 0, l_mask_count, (libxsmm_datatype)LIBXSMM_GETENUM_OUT( i_xgemm_desc->datatype ) );
+          } else if ( LIBXSMM_DATATYPE_BF16 == LIBXSMM_GETENUM_OUT( i_xgemm_desc->datatype ) ) {
+            libxsmm_generator_initialize_avx_mask( io_generated_code, 0, l_mask_count, LIBXSMM_DATATYPE_I32 );
           } else {
             /* shouldn't happen */
           }
