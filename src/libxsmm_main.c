62b9c42a3dbcac0e8a5637111d7e195c1764800a
Hans Pabst <hans.pabst@intel.com>
2022-06-15 15:03:33 +0200

    Improved handling errors (!NDEBUG).

--- src/libxsmm_main.c
+++ src/libxsmm_main.c
@@ -2224,8 +2224,12 @@ LIBXSMM_API_INTERN int libxsmm_build(const libxsmm_build_request* request, unsig
 # endif
   }
 
-  if  (0 == generated_code.last_error /* no error raised */
-    && 0 != generated_code.code_size /*check (tcopy issue?)*/)
+  if  (0 == generated_code.last_error
+# if !defined(NDEBUG)
+    && 0 != generated_code.code_size /*check (tcopy issue?)*/
+    && generated_code.code_size <= generated_code.buffer_size
+# endif
+    /* no error raised */)
   {
     char* code_buffer = NULL;
 # if defined(__APPLE__) && defined(__arm64__)
