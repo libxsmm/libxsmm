6985181e11a2a6e5b5016d1890d3bfce907156d7
Hans Pabst <hans.pabst@intel.com>
2022-06-23 16:21:46 +0200

    Execute cases (performance.sh).

--- samples/pyfr/performance.sh
+++ samples/pyfr/performance.sh
@@ -15,9 +15,9 @@ export PERF_R=${PERF_R:-500000}
 export PERF_N=${TEST_N:-48}
 
 for MTX in "${HERE}"/mats/p*/{pri,hex}/m{3,6}-sp.mtx; do
-  echo "${HERE}/pyfr_driver_asp_reg ${MTX} ${PERF_N} ${PERF_R} 1"
+  "${HERE}/pyfr_driver_asp_reg" "${MTX}" "${PERF_N}" "${PERF_R}" 1
 done
 
 for MTX in "${HERE}"/mats/p*/{pri,hex}/m{0,132,460}-sp.mtx; do
-  echo "${HERE}/pyfr_driver_asp_reg ${MTX} ${PERF_N} ${PERF_R} 0"
+  "${HERE}/pyfr_driver_asp_reg" "${MTX}" "${PERF_N}" "${PERF_R}" 0
 done
