64d33004d4d01cb44b6ec7a21da07b2601b2036a
Alexander Heinecke <heinecke.alex@gmail.com>
2022-07-03 19:15:33 -0700

    tighten eltwise error bound for unit-tests

--- samples/eltwise/eltwise_unary_relu.c
+++ samples/eltwise/eltwise_unary_relu.c
@@ -258,14 +258,8 @@ int test_relu_fwd( const libxsmm_blasint bitm, const libxsmm_blasint M, const li
   printf("Linf rel.error: %.24f\n", norms_out.linf_rel);
   printf("Check-norm    : %.24f\n\n", norms_out.normf_rel);
 
-  if ( (dtype_in == LIBXSMM_DATATYPE_F32) && (dtype_out == LIBXSMM_DATATYPE_F32) && (dtype_comp == LIBXSMM_DATATYPE_F32) ) {
-    if ( norms_out.normf_rel > 0.00001 ) {
-      ret = EXIT_FAILURE;
-    }
-  } else {
-    if ( norms_out.normf_rel > 0.007 ) {
-      ret = EXIT_FAILURE;
-    }
+  if ( norms_out.normf_rel > 0.00001 ) {
+    ret = EXIT_FAILURE;
   }
 
   if ( type != 2 ) {
