2f0c297b6411144792a26a8b89ae449e93f6a274
Alexander Heinecke <heinecke.alex@gmail.com>
2022-07-09 16:26:42 -0700

    added bf8 fused test scripts

--- samples/xgemm/kernel_test/create_new_fused_gemm_test.sh
+++ samples/xgemm/kernel_test/create_new_fused_gemm_test.sh
@@ -2,7 +2,7 @@
 
 for BINARY_POSTOP in 0 1; do
   for UNARY_POSTOP in 0 1 2 3; do
-    for PREC in 'SP' 'BF16' 'BF16F32' 'BF16_FLAT' 'BF16F32_FLAT'; do
+    for PREC in 'SP' 'BF16' 'BF16F32' 'BF16_FLAT' 'BF16F32_FLAT' 'BF8' 'BF8F32'; do
       for LD in 'eqld' 'gtld'; do
         for CVNNI in 0 1; do
           if [[ ("$BINARY_POSTOP" == '0'  &&  "$UNARY_POSTOP" == '0') && ( "$CVNNI" == '0') ]]; then
@@ -26,7 +26,7 @@ for BINARY_POSTOP in 0 1; do
 
           for TRA in 0 1; do
             for TRB in 0 1; do
-              if [[ (( "$PREC" == 'BF16_FLAT'  ||  "$PREC" == 'BF16' ) || ( "$PREC" == 'BF16F32'  ||  "$PREC" == 'BF16F32_FLAT')) && ( "$TRA" == '1'  ||  "$TRB" == '1') ]]; then
+              if [[ (( "$PREC" == 'BF16_FLAT'  ||  "$PREC" == 'BF16' ) || ( "$PREC" == 'BF16F32'  ||  "$PREC" == 'BF16F32_FLAT') || "$PREC" == 'BF8'  ||  "$PREC" == 'BF8F32' ) && ( "$TRA" == '1'  ||  "$TRB" == '1') ]]; then
                 continue
               fi
 
@@ -42,8 +42,14 @@ for BINARY_POSTOP in 0 1; do
                 TPLNAME="bf16f32gemm_"
               elif [ "$PREC" == 'BF16_FLAT' ] ; then
                 TPLNAME="bf16_flatgemm_"
-              else
+              elif [ "$PREC" == 'BF16F32_FLAT' ] ; then
                 TPLNAME="bf16f32_flatgemm_"
+              elif [ "$PREC" == 'BF8' ] ; then
+                TPLNAME="bf8gemm_"
+              elif [ "$PREC" == 'BF8F32' ] ; then
+                TPLNAME="bf8f32gemm_"
+              else
+                continue
               fi
 
               if [ "$TRA" == '0' ] ; then
