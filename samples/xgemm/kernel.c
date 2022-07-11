ed0e7d7116a761b1a96c005fa4d5232209582484
Hans Pabst <hans.pabst@intel.com>
2022-07-11 12:16:21 +0200

    Fixed some warnings.

--- samples/xgemm/kernel.c
+++ samples/xgemm/kernel.c
@@ -70,7 +70,7 @@ float fsigmoid(float x) {
 void relu_f32_f32_gold(libxsmm_blasint M, libxsmm_blasint N, libxsmm_blasint ldi, libxsmm_blasint ldo, libxsmm_blasint ldo_mask, float *in, float *out, float alpha, unsigned char *out_mask, unsigned char type, libxsmm_blasint use_bitmask) {
   libxsmm_blasint i, j;
   if ( (type != 2) && (use_bitmask > 0)) {
-    memset(out_mask, 0, ldo_mask*N);
+    memset(out_mask, 0, (size_t)ldo_mask*N);
     for ( j = 0; j < N; ++j ) {
       for ( i = 0; i < M; ++i ) {
         out_mask[(j*ldo_mask) + i/8] |= (unsigned char)(( in[(j*ldi) + i] < 0.0f ) ? 0x0 : (1 << (i%8)) );
@@ -93,7 +93,7 @@ void relu_f32_f32_gold(libxsmm_blasint M, libxsmm_blasint N, libxsmm_blasint ldi
 void relu_bf16_bf16_gold(libxsmm_blasint M, libxsmm_blasint N, libxsmm_blasint ldi, libxsmm_blasint ldo, libxsmm_blasint ldo_mask, libxsmm_bfloat16 *in, libxsmm_bfloat16 *out, float alpha, unsigned char *out_mask, unsigned char type, libxsmm_blasint use_bitmask) {
   libxsmm_blasint i, j;
   if ( (type != 2) && (use_bitmask > 0)) {
-    memset(out_mask, 0, ldo_mask*N);
+    memset(out_mask, 0, (size_t)ldo_mask*N);
     for ( j = 0; j < N; ++j ) {
       for ( i = 0; i < M; ++i ) {
         out_mask[(j*ldo_mask) + i/8] |= (unsigned char)(( (in[(j*ldi) + i] & 0x8000) == 0x8000 ) ? 0x0 : (1 << (i%8)) );
@@ -1602,7 +1602,7 @@ int main(int argc, char* argv []) {
         l_relu_bitmask      = (char*)libxsmm_aligned_malloc(((size_t)mask_ld) * (size_t)l_n, 64);
         l_relu_bitmask_gold = (char*)libxsmm_aligned_malloc(((size_t)mask_ld) * (size_t)l_n, 64);
         init_random_matrix( LIBXSMM_DATATYPE_I8, l_relu_bitmask, 1, mask_ld, l_n, 0 );
-        memcpy(l_relu_bitmask_gold, l_relu_bitmask, mask_ld * l_n * sizeof(char));
+        memcpy(l_relu_bitmask_gold, l_relu_bitmask, sizeof(char) * mask_ld * l_n);
         fusion_arguments.relu_bitmask = l_relu_bitmask;
         ref_fusion_arguments.relu_bitmask = l_relu_bitmask_gold;
       }
