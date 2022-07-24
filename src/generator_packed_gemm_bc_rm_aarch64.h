0c21ead28c04355a29e4a82ac9f227fb5d781419
Alexander Heinecke <heinecke.alex@gmail.com>
2022-07-23 23:12:13 -0700

    added aarch64 SVE variant for packed GEMM BC kernel

--- src/generator_packed_gemm_bc_rm_aarch64.h
+++ src/generator_packed_gemm_bc_rm_aarch64.h
@@ -27,14 +27,24 @@ LIBXSMM_API_INTERN void libxsmm_generator_packed_gemm_bc_rm_aarch64_kloop( libxs
                                                                            const unsigned int                 i_packed_width,
                                                                            const unsigned int                 i_n_blocking );
 
-LIBXSMM_API_INTERN void libxsmm_generator_packed_gemm_bc_rm_aarch64_kloop_simd_packed( libxsmm_generated_code*            io_generated_code,
-                                                                                       libxsmm_loop_label_tracker*        io_loop_label_tracker,
-                                                                                       const libxsmm_gp_reg_mapping*      i_gp_reg_mapping,
-                                                                                       const libxsmm_micro_kernel_config* i_micro_kernel_config,
-                                                                                       const libxsmm_gemm_descriptor*     i_xgemm_desc,
-                                                                                       const unsigned int                 i_packed_width,
-                                                                                       const unsigned int                 i_simd_packed_width,
-                                                                                       const unsigned int                 i_simd_packed_valid,
-                                                                                       const unsigned int                 i_n_blocking );
+LIBXSMM_API_INTERN void libxsmm_generator_packed_gemm_bc_rm_aarch64_kloop_simd_packed_asimd( libxsmm_generated_code*            io_generated_code,
+                                                                                             libxsmm_loop_label_tracker*        io_loop_label_tracker,
+                                                                                             const libxsmm_gp_reg_mapping*      i_gp_reg_mapping,
+                                                                                             const libxsmm_micro_kernel_config* i_micro_kernel_config,
+                                                                                             const libxsmm_gemm_descriptor*     i_xgemm_desc,
+                                                                                             const unsigned int                 i_packed_width,
+                                                                                             const unsigned int                 i_simd_packed_width,
+                                                                                             const unsigned int                 i_simd_packed_valid,
+                                                                                             const unsigned int                 i_n_blocking );
+
+LIBXSMM_API_INTERN void libxsmm_generator_packed_gemm_bc_rm_aarch64_kloop_simd_packed_sve( libxsmm_generated_code*            io_generated_code,
+                                                                                           libxsmm_loop_label_tracker*        io_loop_label_tracker,
+                                                                                           const libxsmm_gp_reg_mapping*      i_gp_reg_mapping,
+                                                                                           const libxsmm_micro_kernel_config* i_micro_kernel_config,
+                                                                                           const libxsmm_gemm_descriptor*     i_xgemm_desc,
+                                                                                           const unsigned int                 i_packed_width,
+                                                                                           const unsigned int                 i_simd_packed_width,
+                                                                                           const unsigned int                 i_simd_packed_valid,
+                                                                                           const unsigned int                 i_n_blocking );
 
 #endif /* GENERATOR_PACKED_GEMM_BC_RM_AARCH64_H */
