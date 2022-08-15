fc9e122f5cbb9499d5b21492c2ab20e09288cf11
Hans Pabst <hans.pabst@intel.com>
2022-08-15 10:40:51 +0200

    Updated wrapper (sample code).

--- samples/utilities/wrap/dgemv-wrap.sh
+++ samples/utilities/wrap/dgemv-wrap.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 ###############################################################################
 # Copyright (c) Intel Corporation - All rights reserved.                      #
 # This file is part of the LIBXSMM library.                                   #
