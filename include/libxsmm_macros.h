96806053cbc9a19c25318965498553f3a935ede2
Hans Pabst <hans.pabst@intel.com>
2022-09-14 22:30:17 +0200

    Refined 6d8a77b534ea965365c9704c773065bc6bb48687.

--- include/libxsmm_macros.h
+++ include/libxsmm_macros.h
@@ -127,8 +127,8 @@
 
 /** Evaluates to true if the value falls into the interval [LO, HI]. */
 #define LIBXSMM_IS_INTEGER(TYPE, VALUE, LO, HI) ( \
-  ((LO) == (TYPE)(VALUE) || (LO) < (TYPE)(VALUE)) && (VALUE) <= (HI) && \
-  ((0 <= (double)(VALUE) || 0 > (LO))))
+  ((LO) == (TYPE)(VALUE) || (LO) < (TYPE)(VALUE)) && (unsigned long long)(VALUE) <= (HI) && \
+  ((0 <= (double)(VALUE) || (0 > (LO) && 0 < (HI)))))
 /** LIBXSMM_IS_TYPE: check value against type-range of TYPE. */
 #define LIBXSMM_IS_ULLONG(VALUE) LIBXSMM_IS_INTEGER(unsigned long long, VALUE, 0, ULLONG_MAX)
 #define LIBXSMM_IS_LLONG(VALUE) LIBXSMM_IS_INTEGER(/*signed*/long long, VALUE, LLONG_MIN, LLONG_MAX)
