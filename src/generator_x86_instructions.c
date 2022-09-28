43beb1f5b3fc28588c605b124762a282b5d71602
Hans Pabst <hans.pabst@intel.com>
2022-09-28 12:06:33 +0200

    Fixed issues pointed out by SA (logically dead code).

--- src/generator_x86_instructions.c
+++ src/generator_x86_instructions.c
@@ -1429,14 +1429,15 @@ void libxsmm_x86_instruction_evex_compute_2reg_mem( libxsmm_generated_code*     
 
   /* 2 B) filling the missing prefix bits based on table look ups */
   /* R and R' */
-  code[p0   ] |= (unsigned char)((i_vec_reg_number_dst < 32) ? tbl_evex_RRp[i_vec_reg_number_dst] : tbl_evex_RRp[0]);
+  assert(i_vec_reg_number_src < 32 && i_vec_reg_number_dst < 32);
+  code[p0   ] |= (unsigned char)tbl_evex_RRp[i_vec_reg_number_dst];
   /* vvvv and V' */
-  code[p1   ] |= (unsigned char)((i_vec_reg_number_src < 32) ? tbl_evex_vvvv[i_vec_reg_number_src] : tbl_evex_vvvv[0]);
+  code[p1   ] |= (unsigned char)tbl_evex_vvvv[i_vec_reg_number_src];
   /* incase of gather scatter the V' field is used to extend the idx field for SIB to 32 registers */
   if ( (((i_vec_instr >> 24) & 0x2) == 0x2) ) {
     code[p2   ] |= (unsigned char)((l_reg_idx < 16 ) ? tbl_evex_vp[l_reg_idx] : tbl_evex_vp[0]);
   } else {
-    code[p2   ] |= (unsigned char)((i_vec_reg_number_src < 32) ? tbl_evex_vp[i_vec_reg_number_src] : tbl_evex_vp[0]);
+    code[p2   ] |= (unsigned char)tbl_evex_vp[i_vec_reg_number_src];
   }
   /* VL: 128bit,256bit,512bit */
   code[p2   ] |= (unsigned char)tbl_vl[l_vl_idx];
@@ -1562,12 +1563,13 @@ void libxsmm_x86_instruction_evex_compute_3reg( libxsmm_generated_code*     io_g
 
   /* B) filling the missing prefix bits based on table look ups */
   /* R and R' */
-  code[p0   ] |= (unsigned char)((i_vec_reg_number_2 < 32) ? tbl_evex_RRp[i_vec_reg_number_2] : tbl_evex_RRp[0]);
+  assert(i_vec_reg_number_0 < 32 && i_vec_reg_number_1 < 32 && i_vec_reg_number_2 < 32);
+  code[p0   ] |= (unsigned char)tbl_evex_RRp[i_vec_reg_number_2];
   /* B and X */
-  code[p0   ] |= (unsigned char)((i_vec_reg_number_0 < 32) ?  tbl_evex_BX[i_vec_reg_number_0] :  tbl_evex_BX[0]);
+  code[p0   ] |= (unsigned char)tbl_evex_BX[i_vec_reg_number_0];
   /* vvvv and V' */
-  code[p1   ] |= (unsigned char)((i_vec_reg_number_1 < 32) ? tbl_evex_vvvv[i_vec_reg_number_1] : tbl_evex_vvvv[0]);
-  code[p2   ] |= (unsigned char)((i_vec_reg_number_1 < 32) ?   tbl_evex_vp[i_vec_reg_number_1] :   tbl_evex_vp[0]);
+  code[p1   ] |= (unsigned char)tbl_evex_vvvv[i_vec_reg_number_1];
+  code[p2   ] |= (unsigned char)tbl_evex_vp[i_vec_reg_number_1];
   /* VL: 128bit,256bit,512bit or sae control */
   code[p2   ] |= (unsigned char)((i_sae_cntl == 0) ? tbl_vl[l_vl_idx] : (0x60 & (i_sae_cntl << 4)));
   /* masking */
