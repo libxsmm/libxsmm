ccba78c27e3c04f581a4a51602c4db9fbe2483a2
Alexander Heinecke <heinecke.alex@gmail.com>
2022-11-23 06:58:31 +0000

    minor fixes

--- src/generator_mateltwise_transform_aarch64_sve.c
+++ src/generator_mateltwise_transform_aarch64_sve.c
@@ -524,26 +524,9 @@ void libxsmm_generator_transform_vnni2_to_vnni2t_16bit_aarch64_sve_microkernel( 
                                                                                      i_gp_reg_in, i_gp_reg_out, i_gp_reg_m_loop, i_gp_reg_n_loop,
                                                                                      i_gp_reg_scratch, i_micro_kernel_config, i_mateltwise_desc );
   } else {
-    libxsmm_meltw_descriptor l_new_desc = *i_mateltwise_desc;
-    if ( i_mateltwise_desc->n > 16 ) {
-      l_new_desc.n = i_mateltwise_desc->n - (i_mateltwise_desc->n % 16);
-      libxsmm_generator_transform_vnni2_to_vnni2t_Nmod8_16bit_aarch64_sve_microkernel( io_generated_code, io_loop_label_tracker,
-                                                                                        i_gp_reg_in, i_gp_reg_out, i_gp_reg_m_loop, i_gp_reg_n_loop,
-                                                                                        i_gp_reg_scratch, i_micro_kernel_config, &l_new_desc  );
-      libxsmm_aarch64_instruction_alu_move( io_generated_code, LIBXSMM_AARCH64_INSTR_GP_LDR_I_OFF, LIBXSMM_AARCH64_GP_REG_X0, LIBXSMM_AARCH64_GP_REG_UNDEF, 32,
-                                            i_gp_reg_in );
-      libxsmm_aarch64_instruction_alu_move( io_generated_code, LIBXSMM_AARCH64_INSTR_GP_LDR_I_OFF, LIBXSMM_AARCH64_GP_REG_X0, LIBXSMM_AARCH64_GP_REG_UNDEF, 64,
-                                            i_gp_reg_out );
-      libxsmm_aarch64_instruction_alu_compute_imm64( io_generated_code, LIBXSMM_AARCH64_INSTR_GP_META_ADD,
-                                                 i_gp_reg_in, i_gp_reg_scratch, i_gp_reg_in, (long long)l_new_desc.n * l_new_desc.ldi * i_micro_kernel_config->datatype_size_in );
-      libxsmm_aarch64_instruction_alu_compute_imm64( io_generated_code, LIBXSMM_AARCH64_INSTR_GP_META_ADD,
-                                                 i_gp_reg_out, i_gp_reg_scratch, i_gp_reg_out, (long long)l_new_desc.n * 4 * i_micro_kernel_config->datatype_size_out );
-      l_new_desc.n = i_mateltwise_desc->n % 16;
-    }
-
     libxsmm_generator_transform_vnni2_to_vnni2t_mbit_scalar_aarch64_asimd_microkernel( io_generated_code, io_loop_label_tracker,
-                                                                                     i_gp_reg_in, i_gp_reg_out, i_gp_reg_m_loop, i_gp_reg_n_loop,
-                                                                                     i_gp_reg_scratch, i_micro_kernel_config, &l_new_desc );
+                                                                                       i_gp_reg_in, i_gp_reg_out, i_gp_reg_m_loop, i_gp_reg_n_loop,
+                                                                                       i_gp_reg_scratch, i_micro_kernel_config, i_mateltwise_desc );
   }
 }
 
