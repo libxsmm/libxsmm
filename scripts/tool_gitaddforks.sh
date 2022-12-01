23ea696262bbaf611e712f4f4e1001970a12f027
Hans Pabst <hans.pabst@intel.com>
2022-12-01 20:00:25 +0100

    Improved console output/breakdown.

--- scripts/tool_gitaddforks.sh
+++ scripts/tool_gitaddforks.sh
@@ -24,14 +24,15 @@ if [ "${CURL}" ] && [ "${GIT}" ] && [ "${SED}" ]; then
   if [ "${FORKS}" ]; then
     for FORK in ${FORKS}; do
       USER=$(echo "${FORK}" | ${SED} "s/\/..*//")
-      echo -n "Fork ${USER} "
+      echo -n "Fork ${USER}"
       if ${GIT} remote add "${USER}" "https://github.com/${FORK}.git" 2>/dev/null; then
-        echo -n "added and "
+        echo -n " added"
       fi
       if ${GIT} fetch "${USER}" 2>/dev/null; then
-        echo "updated."
+        echo -n " and updated"
         N=$((N+1))
       fi
+      echo "."
     done
     for USER in $(${GIT} remote 2>/dev/null); do
       if [ ! "$(echo "${FORKS}" | ${SED} -n "/${USER}/p")" ]; then
