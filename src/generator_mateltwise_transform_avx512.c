b2509eeb19a720d7338775a0d2e49b82607a5782
egeorgan <evangelos.georganas@intel.com>
2022-12-01 21:21:17 -0800

    Fixed CI issue

--- src/generator_mateltwise_transform_avx512.c
+++ src/generator_mateltwise_transform_avx512.c
@@ -3089,13 +3089,13 @@ void libxsmm_generator_transform_norm_to_vnni4_16bit_avx512_microkernel( libxsmm
     /* create store masking */
     if ( l_m_remainder > l_m_entries/2 ) {
       /* load mask */
-      unsigned int l_mask_instr = (l_m_entries == 32) ? LIBXSMM_X86_INSTR_KMOVD_GPR_LD : LIBXSMM_X86_INSTR_KMOVW_GPR_LD ;
+      l_mask_instr = (l_m_entries == 32) ? LIBXSMM_X86_INSTR_KMOVD_GPR_LD : LIBXSMM_X86_INSTR_KMOVW_GPR_LD ;
       const unsigned long long l_store_mask = ( (unsigned long long)1 << ((l_m_remainder - (l_m_entries/2)) * 4) ) - 1;
       libxsmm_x86_instruction_alu_imm( io_generated_code, LIBXSMM_X86_INSTR_MOVQ, i_gp_reg_mask, l_store_mask );
       libxsmm_x86_instruction_mask_move( io_generated_code, l_mask_instr, i_gp_reg_mask, i_mask_reg_1 );
     } else {
       /* load mask */
-      unsigned int l_mask_instr = (l_m_entries == 32) ? LIBXSMM_X86_INSTR_KMOVD_GPR_LD : LIBXSMM_X86_INSTR_KMOVW_GPR_LD ;
+      l_mask_instr = (l_m_entries == 32) ? LIBXSMM_X86_INSTR_KMOVD_GPR_LD : LIBXSMM_X86_INSTR_KMOVW_GPR_LD ;
       const unsigned long long l_store_mask = ( l_m_remainder == l_m_entries/2 ) ? (unsigned long long)0xffffffff : (unsigned long long)(( (unsigned long long)1 << (l_m_remainder * 4) ) - 1);
       libxsmm_x86_instruction_alu_imm( io_generated_code, LIBXSMM_X86_INSTR_MOVQ, i_gp_reg_mask, l_store_mask );
       libxsmm_x86_instruction_mask_move( io_generated_code, l_mask_instr, i_gp_reg_mask, i_mask_reg_1 );
