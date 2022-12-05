f57c649d92be4e96e49bd5418f18277844f543fa
Hans Pabst <hans.pabst@intel.com>
2022-12-05 16:02:55 +0100

    Avoid Clang's warning about invalid Doxygen syntax (PEDANTIC=3).

--- include/libxsmm_cpuid.h
+++ include/libxsmm_cpuid.h
@@ -69,7 +69,7 @@ LIBXSMM_API int libxsmm_cpuid_arm(libxsmm_cpuid_info* info);
 #endif
 
 /**
- * @TODO this might be limited lifetime API until we have a fully-fleged
+ * TODO: this might be limited lifetime API until we have a fully-fleged
  * ARM CPU flags test
  * Still it might be needed to overwrite BFMMLA with BFDOT for performance
  * study reasons
