0f8857e51d233e7d996f66342241fa1cd4819774
Hans Pabst <hans.pabst@intel.com>
2022-12-07 10:32:32 +0100

    Added opreduce and equation quick-tests.

---
+++ tests/opreduce.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+###############################################################################
+# Copyright (c) Intel Corporation - All rights reserved.                      #
+# This file is part of the LIBXSMM library.                                   #
+#                                                                             #
+# For information on the license, see the LICENSE file.                       #
+# Further information: https://github.com/libxsmm/libxsmm/                    #
+# SPDX-License-Identifier: BSD-3-Clause                                       #
+###############################################################################
+# Hans Pabst (Intel Corp.)
+###############################################################################
+# shellcheck disable=SC2143
+set -eo pipefail
+
+HERE=$(cd "$(dirname "$0")" && pwd -P)
+
+"${HERE}/../samples/eltwise/run_test.sh" -o /dev/null -k 0 -n 80 "$@"
