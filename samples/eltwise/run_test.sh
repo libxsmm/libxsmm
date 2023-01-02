29e0ec217b24165c9a390b7a66740a5a08b7e3fa
Alexander Heinecke <heinecke.alex@gmail.com>
2023-01-01 22:21:21 -0800

    fixed run script

--- samples/eltwise/run_test.sh
+++ samples/eltwise/run_test.sh
@@ -4,7 +4,7 @@
 HERE=$(cd "$(dirname "$0")" && pwd -P)
 EXEC=${HERE}/../../scripts/tool_pexec.sh
 
-cd "${HERE}" && ( ls -1 ./kernel_test/binary_*.sh; ls -1 ./kernel_test/reduce_*.sh; ls -1 ./kernel_test/init_acc_reduce_*.sh;  ls -1 ./kernel_test/unary_*.sh; ) | ${EXEC} -c 3- "$@"
+cd "${HERE}" && ( ls -1 ./kernel_test/binary_*.sh; ls -1 ./kernel_test/unary_*.sh; ) | ${EXEC} -c 3- "$@"
 RESULT=$?
 
 rm -f tmp.??????????
