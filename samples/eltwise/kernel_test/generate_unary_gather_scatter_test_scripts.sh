fe7031502e4f3baea1c4fb4644ae4c87123c3206
Alexander Heinecke <alexander.heinecke@intel.com>
2023-01-10 12:45:13 -0800

    Refactor Eltwise Test (#724)
    * added script for generating fine granular unary tests
    
    * removed old unary simple static scripts
    
    * updated generation script for unary
    
    * updated gitignore file
    
    * update run script to use generated scripts
    
    * added unary simple script generation to Make process
    
    * added generated test scripts for unary transform
    
    * adjust naming of opreduce script to match unary scripts
    
    * also migrated relu and dropout to generated scripts
    
    * fixed typo
    
    * added scripting support for unary gather scatter
    
    * switch binary test to generated scripts as well
    
    * minor clean-ups
    
    * switched unary reduce to generated scripts
    
    * fixed run script
    
    * fixed some LD script issues
    
    * attempt to update allow list
    
    * script removal of a script no longer needed
    
    * Fixed replicate col var issue masking for avx2
    
    * Complement previous commit
    
    Co-authored-by: egeorgan <evangelos.georganas@intel.com>

---
+++ samples/eltwise/kernel_test/generate_unary_gather_scatter_test_scripts.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+if [[ -z "${SSIZE}" ]]; then
+  SAMPLESIZE=18
+else
+  SAMPLESIZE=${SSIZE}
+fi
+
+for PREC in 'I8' 'I16' 'I32' 'I64' 'BF8' 'HF8' 'BF16' 'F16' 'F32' 'F64'; do
+  for TYPE in 0 1; do
+    for LD in 'eqld' 'gtld'; do
+      TPPNAME="none"
+      OUTNAME="unary_"
+      NUMPREC=10
+      PRECLC=`echo "$PREC" | awk '{print tolower($0)}'`
+
+      # only 16 and 32bit support
+      if [[ (("$PREC" == 'I8') || ("$PREC" == 'BF8') || ("$PREC" == 'HF8') || ("$PREC" == 'F64') || ("$PREC" == 'I64')) ]]; then
+        continue
+      fi
+
+      # get TPP name
+      if [ "$TYPE" == '0' ] ; then
+        TPPNAME="gather"
+      elif [ "$TYPE" == '1' ] ; then
+        TPPNAME="scatter"
+      else
+        continue
+      fi
+
+      if [[ (("$PREC" == 'I32') || ("$PREC" == 'F32')) ]]; then
+        NUMPREC=0
+      fi
+
+      if [[ (("$PREC" == 'I16') || ("$PREC" == 'F16') || ("$PREC" == 'BF16')) ]]; then
+        NUMPREC=1
+      fi
+
+      OUTNAME=${OUTNAME}${TPPNAME}_${PRECLC}_${LD}.sh
+
+      # generate script by sed
+      sed "s/NUMPREC=X/NUMPREC=\"${NUMPREC}\"/g" unary_gather_scatter.tpl \
+      | sed "s/GS_OP=X/GS_OP=${TYPE}/g" \
+      | sed "s/SAMPLESIZE/${SAMPLESIZE}/g" \
+      >${OUTNAME}
+
+      # for gt we need to touch up the script
+      if [ "$LD" == 'gtld' ] ; then
+        sed -i "s/+ str(m) + '_' + str(m)/+ '100_100'/g" ${OUTNAME}
+      fi
+
+      chmod 755 ${OUTNAME}
+    done
+  done
+done
