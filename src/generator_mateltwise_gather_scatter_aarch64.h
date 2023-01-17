6dc21a1b1bcbe4c1924e8b18cd3422118d5cfbb0
egeorgan <evangelos.georganas@intel.com>
2023-01-17 10:07:34 -0800

    Identation issues

--- src/generator_mateltwise_gather_scatter_aarch64.h
+++ src/generator_mateltwise_gather_scatter_aarch64.h
@@ -16,74 +16,73 @@
 
 LIBXSMM_API_INTERN
 void libxsmm_generator_gather_scatter_cols_aarch64_microkernel( libxsmm_generated_code*                        io_generated_code,
-    libxsmm_loop_label_tracker*                    io_loop_label_tracker,
-    libxsmm_mateltwise_gp_reg_mapping*             i_gp_reg_mapping,
-    const libxsmm_mateltwise_kernel_config*        i_micro_kernel_config,
-    const libxsmm_meltw_descriptor*                i_mateltwise_desc );
+                                                                libxsmm_loop_label_tracker*                    io_loop_label_tracker,
+                                                                libxsmm_mateltwise_gp_reg_mapping*             i_gp_reg_mapping,
+                                                                const libxsmm_mateltwise_kernel_config*        i_micro_kernel_config,
+                                                                const libxsmm_meltw_descriptor*                i_mateltwise_desc );
 
 LIBXSMM_API_INTERN
-void libxsmm_generator_gather_scatter_rows_aarch64_mn_loop_unrolled( libxsmm_generated_code*                        io_generated_code,
-    libxsmm_mateltwise_gp_reg_mapping*             i_gp_reg_mapping,
-    unsigned int                                   m_unroll_factor,
-    unsigned int                                   n_unroll_factor,
-    unsigned int                                   idx_vreg_start,
-    unsigned int                                   idx_tsize,
-    unsigned int                                   idx_mask_reg,
-    unsigned int                                   ld_idx_mat,
-    unsigned int                                   gather_instr,
-    unsigned int                                   scatter_instr,
-    unsigned int                                   vlen,
-    unsigned int                                   m_remainder_elements,
-    unsigned int                                   mask_reg,
-    unsigned int                                   mask_reg_full_frac_vlen,
-    unsigned int                                   is_gather,
-    unsigned int                                   gp_idx_mat_reg,
-    unsigned int                                   gp_reg_mat_reg,
-    unsigned int                                   dtype_size_reg_mat,
-    unsigned int                                   ld_reg_mat );
+void libxsmm_generator_gather_scatter_rows_aarch64_mn_loop_unrolled( libxsmm_generated_code*                              io_generated_code,
+                                                                     libxsmm_mateltwise_gp_reg_mapping*                   i_gp_reg_mapping,
+                                                                     const unsigned int                                   i_m_unroll_factor,
+                                                                     const unsigned int                                   i_n_unroll_factor,
+                                                                     const unsigned int                                   i_idx_vreg_start,
+                                                                     const unsigned int                                   i_idx_tsize,
+                                                                     const unsigned int                                   i_idx_mask_reg,
+                                                                     const unsigned int                                   i_ld_idx_mat,
+                                                                     const unsigned int                                   i_gather_instr,
+                                                                     const unsigned int                                   i_scatter_instr,
+                                                                     const unsigned int                                   i_vlen,
+                                                                     const unsigned int                                   i_m_remainder_elements,
+                                                                     const unsigned int                                   i_mask_reg,
+                                                                     const unsigned int                                   i_mask_reg_full_frac_vlen,
+                                                                     const unsigned int                                   i_is_gather,
+                                                                     const unsigned int                                   i_gp_idx_mat_reg,
+                                                                     const unsigned int                                   i_gp_reg_mat_reg,
+                                                                     const unsigned int                                   i_dtype_size_reg_mat,
+                                                                     const unsigned int                                   i_ld_reg_mat );
 
 LIBXSMM_API_INTERN
 void libxsmm_generator_gather_scatter_rows_aarch64_microkernel( libxsmm_generated_code*                        io_generated_code,
-    libxsmm_loop_label_tracker*                    io_loop_label_tracker,
-    libxsmm_mateltwise_gp_reg_mapping*             i_gp_reg_mapping,
-    const libxsmm_mateltwise_kernel_config*        i_micro_kernel_config,
-    const libxsmm_meltw_descriptor*                i_mateltwise_desc );
+                                                                libxsmm_loop_label_tracker*                    io_loop_label_tracker,
+                                                                libxsmm_mateltwise_gp_reg_mapping*             i_gp_reg_mapping,
+                                                                const libxsmm_mateltwise_kernel_config*        i_micro_kernel_config,
+                                                                const libxsmm_meltw_descriptor*                i_mateltwise_desc );
 
 LIBXSMM_API_INTERN
-void libxsmm_generator_gather_scatter_offs_aarch64_mn_loop_unrolled( libxsmm_generated_code*                        io_generated_code,
-    libxsmm_mateltwise_gp_reg_mapping*             i_gp_reg_mapping,
-    unsigned int                                   m_unroll_factor,
-    unsigned int                                   n_unroll_factor,
-    unsigned int                                   gp_idx_base_reg,
-    unsigned int                                   idx_vlen,
-    unsigned int                                   idx_tsize,
-    unsigned int                                   idx_mask_reg,
-    unsigned int                                   ld_idx,
-    unsigned int                                   gather_instr,
-    unsigned int                                   scatter_instr,
-    unsigned int                                   vlen,
-    unsigned int                                   m_remainder_elements,
-    unsigned int                                   mask_reg,
-    unsigned int                                   mask_reg_full_frac_vlen,
-    unsigned int                                   is_gather,
-    unsigned int                                   gp_idx_mat_reg,
-    unsigned int                                   gp_reg_mat_reg,
-    unsigned int                                   dtype_size_reg_mat,
-    unsigned int                                   ld_reg_mat );
+void libxsmm_generator_gather_scatter_offs_aarch64_mn_loop_unrolled( libxsmm_generated_code*                              io_generated_code,
+                                                                     libxsmm_mateltwise_gp_reg_mapping*                   i_gp_reg_mapping,
+                                                                     const unsigned int                                   i_m_unroll_factor,
+                                                                     const unsigned int                                   i_n_unroll_factor,
+                                                                     const unsigned int                                   i_gp_idx_base_reg,
+                                                                     const unsigned int                                   i_idx_vlen,
+                                                                     const unsigned int                                   i_idx_tsize,
+                                                                     const unsigned int                                   i_idx_mask_reg,
+                                                                     const unsigned int                                   i_ld_idx,
+                                                                     const unsigned int                                   i_gather_instr,
+                                                                     const unsigned int                                   i_scatter_instr,
+                                                                     const unsigned int                                   i_vlen,
+                                                                     const unsigned int                                   i_m_remainder_elements,
+                                                                     const unsigned int                                   i_mask_reg,
+                                                                     const unsigned int                                   i_mask_reg_full_frac_vlen,
+                                                                     const unsigned int                                   i_is_gather,
+                                                                     const unsigned int                                   i_gp_idx_mat_reg,
+                                                                     const unsigned int                                   i_gp_reg_mat_reg,
+                                                                     const unsigned int                                   i_dtype_size_reg_mat,
+                                                                     const unsigned int                                   i_ld_reg_mat );
 
 LIBXSMM_API_INTERN
 void libxsmm_generator_gather_scatter_offs_aarch64_microkernel( libxsmm_generated_code*                        io_generated_code,
-    libxsmm_loop_label_tracker*                    io_loop_label_tracker,
-    libxsmm_mateltwise_gp_reg_mapping*             i_gp_reg_mapping,
-    const libxsmm_mateltwise_kernel_config*        i_micro_kernel_config,
-    const libxsmm_meltw_descriptor*                i_mateltwise_desc );
+                                                                libxsmm_loop_label_tracker*                    io_loop_label_tracker,
+                                                                libxsmm_mateltwise_gp_reg_mapping*             i_gp_reg_mapping,
+                                                                const libxsmm_mateltwise_kernel_config*        i_micro_kernel_config,
+                                                                const libxsmm_meltw_descriptor*                i_mateltwise_desc );
 
 LIBXSMM_API_INTERN
 void libxsmm_generator_gather_scatter_aarch64_microkernel( libxsmm_generated_code*                        io_generated_code,
-    libxsmm_loop_label_tracker*                    io_loop_label_tracker,
-    libxsmm_mateltwise_gp_reg_mapping*             i_gp_reg_mapping,
-    const libxsmm_mateltwise_kernel_config*        i_micro_kernel_config,
-    const libxsmm_meltw_descriptor*                i_mateltwise_desc );
-
+                                                           libxsmm_loop_label_tracker*                    io_loop_label_tracker,
+                                                           libxsmm_mateltwise_gp_reg_mapping*             i_gp_reg_mapping,
+                                                           const libxsmm_mateltwise_kernel_config*        i_micro_kernel_config,
+                                                           const libxsmm_meltw_descriptor*                i_mateltwise_desc );
 #endif /* GENERATOR_MATELTWISE_GATHER_SCATTER_AARCH64_H */
 
