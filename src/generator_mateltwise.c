f47f9408bd74bae32f78d407cb208476e5e60afa
egeorgan <evangelos.georganas@intel.com>
2023-01-19 10:30:30 -0800

    Fixed redundant if case

--- src/generator_mateltwise.c
+++ src/generator_mateltwise.c
@@ -20,11 +20,8 @@ void libxsmm_generator_mateltwise_kernel( libxsmm_generated_code*          io_ge
   /* generate kernel */
   if ( (io_generated_code->arch >= LIBXSMM_X86_GENERIC) && (io_generated_code->arch <= LIBXSMM_X86_ALLFEAT) ) {
     libxsmm_generator_mateltwise_sse_avx_avx512_kernel( io_generated_code, i_mateltw_desc );
-  } else if ( ((io_generated_code->arch >= LIBXSMM_AARCH64_V81) && (io_generated_code->arch <= LIBXSMM_AARCH64_V82)) ||
-              ((io_generated_code->arch >= LIBXSMM_AARCH64_SVE256) && (io_generated_code->arch <= LIBXSMM_AARCH64_A64FX)) ) {
+  } else if ( (io_generated_code->arch >= LIBXSMM_AARCH64_V81) && (io_generated_code->arch <= LIBXSMM_AARCH64_ALLFEAT) ) {
     libxsmm_generator_mateltwise_aarch64_kernel( io_generated_code, i_mateltw_desc );
-  } else if ( (io_generated_code->arch >= LIBXSMM_AARCH64_APPL_M1 && io_generated_code->arch <= LIBXSMM_AARCH64_ALLFEAT) ) {
-    libxsmm_generator_mateltwise_aarch64_kernel(io_generated_code, i_mateltw_desc);
   } else {
     /* TODO fix this error and support for more architectures */
     LIBXSMM_HANDLE_ERROR( io_generated_code, LIBXSMM_ERR_ARCH );
