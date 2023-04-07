177f5816d44fb27674f9203f553f5b8cc234bc80
Hans Pabst <hans.pabst@intel.com>
2023-04-07 14:38:28 +0200

    Additional, example-like test (libxsmm_strimatch).

--- tests/memory.c
+++ tests/memory.c
@@ -54,6 +54,31 @@ int main(int argc, char* argv[])
   if (EXIT_SUCCESS == result && 3 != libxsmm_strimatch("Cor Prod AA", "C Product A", NULL)) result = EXIT_FAILURE;
   if (EXIT_SUCCESS == result && 1 != libxsmm_strimatch("aaaa", "A A A A", NULL)) result = EXIT_FAILURE;
   if (EXIT_SUCCESS == result && 1 != libxsmm_strimatch("A A A A", "aaaa", NULL)) result = EXIT_FAILURE;
+  if (EXIT_SUCCESS == result) {
+    const char *const sample[] = {
+      "The quick red squirrel jumps over the low fence",
+      "The slow green frog jumps over the lazy dog",
+      "The lazy brown dog jumps over the quick fox", /* match */
+      "The hazy fog crawls over the lazy crocodile"
+    };
+    int match = 0, i = 0, j = 0;
+    for (; i < (sizeof(sample) / sizeof(*sample)); ++i) {
+      const int score = libxsmm_strimatch(init, sample[i], NULL);
+      if (match < score) {
+        match = score; j = i;
+      }
+      else if (0 > score) result = EXIT_FAILURE;
+    }
+    if (EXIT_SUCCESS == result) {
+      int self = 0;
+      if (0 < match) {
+        self = libxsmm_strimatch(init, init, NULL);
+        FPRINTF(stdout, "orig (%i): %s\n", self, init);
+        FPRINTF(stdout, "best (%i): %s\n", match, sample[j]);
+      }
+      if (9 != self || 8 != match) result = EXIT_FAILURE; /* test */
+    }
+  }
 
   /* check LIBXSMM_MEMCPY127 and libxsmm_diff_n */
   if (EXIT_SUCCESS == result) {
