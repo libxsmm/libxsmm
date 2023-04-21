2d7c8ea90305c9f031f296814c90f0b1d48cd47c
Hans Pabst <hans.pabst@intel.com>
2023-04-21 20:41:36 +0200

    Improved LIBXSMM_MATINIT.

--- include/libxsmm_math.h
+++ include/libxsmm_math.h
@@ -15,15 +15,16 @@
 
 /** Helper macro to setup a matrix with some initial values. */
 #define LIBXSMM_MATINIT_AUX(OMP, TYPE, SEED, DST, NROWS, NCOLS, LD, SCALE) do { \
-  /*const*/ libxsmm_blasint libxsmm_matinit_seed_ = SEED; /* avoid constant conditional */ \
-  const double libxsmm_matinit_scale_ = (SCALE) * libxsmm_matinit_seed_ + (SCALE); \
-  const libxsmm_blasint libxsmm_matinit_nrows_ = (libxsmm_blasint)NROWS; \
-  const libxsmm_blasint libxsmm_matinit_ld_ = (libxsmm_blasint)LD; \
+  /*const*/ double libxsmm_matinit_seed_ = SEED; /* avoid constant conditional */ \
+  const double libxsmm_matinit_scale_ = libxsmm_matinit_seed_ * (SCALE) + (SCALE); \
+  const libxsmm_blasint libxsmm_matinit_nrows_ = (libxsmm_blasint)(NROWS); \
+  const libxsmm_blasint libxsmm_matinit_ncols_ = (libxsmm_blasint)(NCOLS); \
+  const libxsmm_blasint libxsmm_matinit_ld_ = (libxsmm_blasint)(LD); \
   libxsmm_blasint libxsmm_matinit_i_ = 0, libxsmm_matinit_j_ = 0; \
   LIBXSMM_OMP_VAR(libxsmm_matinit_i_); LIBXSMM_OMP_VAR(libxsmm_matinit_j_); \
   if (0 != libxsmm_matinit_seed_) { \
     OMP(parallel for private(libxsmm_matinit_i_, libxsmm_matinit_j_)) \
-    for (libxsmm_matinit_i_ = 0; libxsmm_matinit_i_ < ((libxsmm_blasint)NCOLS); ++libxsmm_matinit_i_) { \
+    for (libxsmm_matinit_i_ = 0; libxsmm_matinit_i_ < libxsmm_matinit_ncols_; ++libxsmm_matinit_i_) { \
       for (libxsmm_matinit_j_ = 0; libxsmm_matinit_j_ < libxsmm_matinit_nrows_; ++libxsmm_matinit_j_) { \
         const libxsmm_blasint libxsmm_matinit_k_ = libxsmm_matinit_i_ * libxsmm_matinit_ld_ + libxsmm_matinit_j_; \
         ((TYPE*)(DST))[libxsmm_matinit_k_] = (TYPE)(libxsmm_matinit_scale_ * (1.0 + \
@@ -36,12 +37,12 @@
     } \
   } \
   else { /* shuffle based initialization */ \
-    const unsigned int libxsmm_matinit_maxval_ = ((unsigned int)NCOLS) * ((unsigned int)libxsmm_matinit_ld_); \
-    const TYPE libxsmm_matinit_maxval2_ = (TYPE)((unsigned int)LIBXSMM_UPDIV(libxsmm_matinit_maxval_, 2)); /* non-zero */ \
+    const libxsmm_blasint libxsmm_matinit_maxval_ = libxsmm_matinit_ncols_ * libxsmm_matinit_ld_; \
+    const TYPE libxsmm_matinit_maxval2_ = (TYPE)((libxsmm_blasint)LIBXSMM_UPDIV(libxsmm_matinit_maxval_, 2)); /* non-zero */ \
     const TYPE libxsmm_matinit_inv_ = ((TYPE)(SCALE)) / libxsmm_matinit_maxval2_; \
-    const size_t libxsmm_matinit_shuffle_ = libxsmm_coprime2(libxsmm_matinit_maxval_); \
+    const size_t libxsmm_matinit_shuffle_ = libxsmm_coprime2((size_t)libxsmm_matinit_maxval_); \
     OMP(parallel for private(libxsmm_matinit_i_, libxsmm_matinit_j_)) \
-    for (libxsmm_matinit_i_ = 0; libxsmm_matinit_i_ < ((libxsmm_blasint)NCOLS); ++libxsmm_matinit_i_) { \
+    for (libxsmm_matinit_i_ = 0; libxsmm_matinit_i_ < libxsmm_matinit_ncols_; ++libxsmm_matinit_i_) { \
       for (libxsmm_matinit_j_ = 0; libxsmm_matinit_j_ < libxsmm_matinit_ld_; ++libxsmm_matinit_j_) { \
         const libxsmm_blasint libxsmm_matinit_k_ = libxsmm_matinit_i_ * libxsmm_matinit_ld_ + libxsmm_matinit_j_; \
         ((TYPE*)(DST))[libxsmm_matinit_k_] = libxsmm_matinit_inv_ * /* normalize values to an interval of [-1, +1] */ \
