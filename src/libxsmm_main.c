43837e710c4016beb9469c2677ce9d1b7d003768
egeor <egeor@eecs.berkeley.edu>
2023-05-20 02:17:05 -0700

    FIxed kernel string

--- src/libxsmm_main.c
+++ src/libxsmm_main.c
@@ -1781,6 +1781,12 @@ LIBXSMM_API const char* libxsmm_get_gemm_typename(const unsigned char *datatype)
       {
         return "i16i32";
       }
+      else if (LIBXSMM_DATATYPE_I8 == LIBXSMM_GEMM_GETENUM_A_PREC(datatype) &&
+               LIBXSMM_DATATYPE_F16 == LIBXSMM_GEMM_GETENUM_B_PREC(datatype) &&
+               LIBXSMM_DATATYPE_F16 == LIBXSMM_GEMM_GETENUM_C_PREC(datatype))
+      {
+        return "i8f16f16";
+      }
       else if (LIBXSMM_DATATYPE_I16 == LIBXSMM_GEMM_GETENUM_AB_COMMON_PREC(datatype) &&
                LIBXSMM_DATATYPE_F32 == LIBXSMM_GEMM_GETENUM_C_PREC(datatype))
       {
