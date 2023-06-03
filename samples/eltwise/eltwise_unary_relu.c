f9b48644dc4a7c413b20172c0fae1c9b4e8aee52
Hans Pabst <hans.pabst@intel.com>
2023-06-03 21:29:37 +0200

    Updated error margin.

--- samples/eltwise/eltwise_unary_relu.c
+++ samples/eltwise/eltwise_unary_relu.c
@@ -288,7 +288,7 @@ int test_relu_fwd( const libxsmm_blasint bitm, const libxsmm_blasint M, const li
   check_norm = libxsmm_matdiff_epsilon(&norms_out);
   printf("Check-norm    : %.24f\n\n", check_norm);
 
-  if ( check_norm > 4e-08) {
+  if ( check_norm > 4e-6) {
     ret = EXIT_FAILURE;
   }
 
