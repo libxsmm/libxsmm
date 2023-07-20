16f7bd6d473f0c7ac933b283b618dfb167dce992
Hans Pabst <hans.pabst@intel.com>
2023-07-20 16:27:24 +0200

    Improved sample code (shuffle).

--- samples/utilities/shuffle/shuffle.c
+++ samples/utilities/shuffle/shuffle.c
@@ -45,8 +45,8 @@ size_t bubble_sort_uint_ascending(void* inout, size_t elemsize, size_t count) {
   size_t result = 0; /* count number of swaps */
   if (0 != count) {
     unsigned char *const data = (unsigned char*)inout;
-    int swap;
-    do {
+    int swap = 1;
+    for (; 0 != swap; --count) {
       size_t i = 0, j, k;
       for (swap = 0; i < elemsize * (count - 1); i += elemsize) {
         for (j = i + elemsize, k = elemsize; 0 < k; --k) {
@@ -60,8 +60,7 @@ size_t bubble_sort_uint_ascending(void* inout, size_t elemsize, size_t count) {
           }
         }
       }
-      --count;
-    } while (swap);
+    }
   }
   return result;
 }
@@ -74,8 +73,8 @@ int main(int argc, char* argv[])
   const int niters = (3 < argc ? atoi(argv[3]) : 1);
   const int repeat = (4 < argc ? atoi(argv[4]) : 3);
   const int elsize = (0 >= insize ? 8 : insize);
-  const int entropy = (NULL == getenv("ENTROPY")
-    ? 0 : atoi(getenv("ENTROPY")));
+  const int random = (NULL == getenv("RANDOM")
+    ? 0 : atoi(getenv("RANDOM")));
   const size_t m = (0 <= niters ? niters : 1);
   const size_t n = (0 >= nelems
     ? (((size_t)64 << 20/*64 MB*/) / elsize)
@@ -91,6 +90,7 @@ int main(int argc, char* argv[])
   if (NULL != data1 && NULL != data2) {
     double d0, d1 = 0, d2 = 0, d3 = 0;
     size_t n1 = 0, n2 = 0, n3 = 0, j;
+    libxsmm_timer_tickint start;
     int i = 0;
 
     /* initialize the data */
@@ -104,12 +104,12 @@ int main(int argc, char* argv[])
       printf("---------------------------------------------------\n");
 
       { /* benchmark RNG-based shuffle routine */
-        const libxsmm_timer_tickint start = libxsmm_timer_tick();
-        for (j = 0; j < m; ++j) shuffle(data1, elsize, n);
+        memcpy(data2, data1, nbytes);
+        start = libxsmm_timer_tick();
+        for (j = 0; j < m; ++j) shuffle(data2, elsize, n);
         d0 = libxsmm_timer_duration(start, libxsmm_timer_tick()) / m1;
-        if (0 != entropy) {
-          j = bubble_sort_uint_ascending(data1, elsize, n);
-          if (n1 < j) n1 = j;
+        if (0 != random && i == repeat) {
+          n1 = bubble_sort_uint_ascending(data2, elsize, n);
         }
         if (0 < d0) printf("RNG-shuffle: %.8f s (%i MB/s)\n", d0,
           (int)LIBXSMM_ROUND((2.0 * nbytes) / ((1024.0 * 1024.0) * d0)));
@@ -117,12 +117,12 @@ int main(int argc, char* argv[])
       }
 
       { /* benchmark DS1-based shuffle routine */
-        const libxsmm_timer_tickint start = libxsmm_timer_tick();
-        libxsmm_shuffle(data1, elsize, n, NULL, &m);
+        memcpy(data2, data1, nbytes);
+        start = libxsmm_timer_tick();
+        libxsmm_shuffle(data2, elsize, n, NULL, &m);
         d0 = libxsmm_timer_duration(start, libxsmm_timer_tick()) / m1;
-        if (0 != entropy) {
-          j = bubble_sort_uint_ascending(data1, elsize, n);
-          if (n2 < j) n2 = j;
+        if (0 != random && i == repeat) {
+          n2 = bubble_sort_uint_ascending(data2, elsize, n);
         }
         if (0 < d0) printf("DS1-shuffle: %.8f s (%i MB/s)\n", d0,
           (int)LIBXSMM_ROUND((2.0 * nbytes) / ((1024.0 * 1024.0) * d0)));
@@ -130,12 +130,11 @@ int main(int argc, char* argv[])
       }
 
       { /* benchmark DS2-based shuffle routine */
-        const libxsmm_timer_tickint start = libxsmm_timer_tick();
+        start = libxsmm_timer_tick();
         libxsmm_shuffle2(data2, data1, elsize, n, NULL, &m);
         d0 = libxsmm_timer_duration(start, libxsmm_timer_tick()) / m1;
-        if (0 != entropy) {
-          j = bubble_sort_uint_ascending(data2, elsize, n);
-          if (n3 < j) n3 = j;
+        if (0 != random && i == repeat) {
+          n3 = bubble_sort_uint_ascending(data2, elsize, n);
         }
         if (0 < d0) printf("DS2-shuffle: %.8f s (%i MB/s)\n", d0,
           (int)LIBXSMM_ROUND((2.0 * nbytes) / ((1024.0 * 1024.0) * d0)));
@@ -153,7 +152,7 @@ int main(int argc, char* argv[])
           (int)LIBXSMM_ROUND((2.0 * nbytes) / ((1024.0 * 1024.0) * d1)));
         if (0 != n1) {
           const unsigned long long en = (n * n - n + 3) / 4, em = LIBXSMM_MIN(n1, en);
-          printf(", entropy=%llu%%)\n", (em * 100 + en - 1) / en);
+          printf(", random=%llu%%)\n", (em * 100 + en - 1) / en);
         }
         else printf(")\n");
       }
@@ -162,7 +161,7 @@ int main(int argc, char* argv[])
           (int)LIBXSMM_ROUND((2.0 * nbytes) / ((1024.0 * 1024.0) * d2)));
         if (0 != n2) {
           const unsigned long long en = (n * n - n + 3) / 4, em = LIBXSMM_MIN(n2, en);
-          printf(", entropy=%llu%%)\n", (em * 100 + en - 1) / en);
+          printf(", random=%llu%%)\n", (em * 100 + en - 1) / en);
         }
         else printf(")\n");
       }
@@ -171,7 +170,7 @@ int main(int argc, char* argv[])
           (int)LIBXSMM_ROUND((2.0 * nbytes) / ((1024.0 * 1024.0) * d3)));
         if (0 != n3) {
           const unsigned long long en = (n * n - n + 3) / 4, em = LIBXSMM_MIN(n3, en);
-          printf(", entropy=%llu%%)\n", (em * 100 + en - 1) / en);
+          printf(", random=%llu%%)\n", (em * 100 + en - 1) / en);
         }
         else printf(")\n");
       }
