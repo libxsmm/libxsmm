c12dc2f975d7954c810d539c66cf2bd2505a1c7b
Evangelos Georganas <evangelos.georganas@intel.com>
2024-01-31 04:01:04 -0800

    Fix add bcsc spmm runner (#851)
    * Added more arch checks
    
    * More cpuid ammends
    
    * Added runner tests and CI lists
    
    * Added bcsc spmm test
    
    * Fixed CI
    
    * Fixed CI

---
+++ samples/spmm/kernel_test.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2012
+
+HERE=$(cd "$(dirname "$0")" && pwd -P)
+EXEC=${HERE}/../../scripts/tool_pexec.sh
+
+cd "${HERE}" && ls -1 ./kernel_test/*.slurm | ${EXEC} -c 3- "$@"
+RESULT=$?
+
+rm -f tmp.??????????
+exit ${RESULT}
