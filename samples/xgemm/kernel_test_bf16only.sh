0a42f6527a887d532129fe6d257044740836b588
Alexander Heinecke <alexander.heinecke@intel.com>
2024-02-10 01:24:54 -0800

    write log for SRF test

--- samples/xgemm/kernel_test_bf16only.sh
+++ samples/xgemm/kernel_test_bf16only.sh
@@ -4,7 +4,7 @@
 HERE=$(cd "$(dirname "$0")" && pwd -P)
 EXEC=${HERE}/../../scripts/tool_pexec.sh
 
-cd "${HERE}" && ls -1 ./kernel_test/bf16*.slurm | grep -v spmm | ${EXEC} -c 3- "$@"
+cd "${HERE}" && ls -1 ./kernel_test/bf16*.slurm | grep -v spmm | ${EXEC} -o bf16only.out -c 3- "$@"
 RESULT=$?
 
 rm -f tmp.??????????
