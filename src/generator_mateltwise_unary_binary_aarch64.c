230b66dd493a56b867b9eab05c84cd31eee67afc
raisiddh <raisiddh@pcl-tiergarten-login.sc.intel.com>
2024-05-16 12:50:04 -0700

    Fixed ARM unary simple scalar broadcast failures

--- src/generator_mateltwise_unary_binary_aarch64.c
+++ src/generator_mateltwise_unary_binary_aarch64.c
@@ -589,8 +589,7 @@ void libxsmm_load_aarch64_2d_reg_block( libxsmm_generated_code*                 
         }
       } else {
         if ( (bcast_row == 1) || (bcast_scalar == 1) ) {
-          /* offset = (bcast_scalar == 1) ?  i_micro_kernel_config->datatype_size_in:l_ld_bytes*i_n_blocking; */
-          offset = (bcast_scalar == 1) ?  l_ld_bytes * i_n_blocking :l_ld_bytes*i_n_blocking;
+          offset = (bcast_scalar == 1) ?  i_micro_kernel_config->datatype_size_in:l_ld_bytes*i_n_blocking;
           if (im == 0) {
             if ((bcast_row == 1) || ((bcast_scalar == 1) && (in == 0))) {
               libxsmm_generator_bcastload_masked_vreg_aarch64_asimd( io_generated_code, i_gp_reg_mapping->gp_reg_in, i_gp_reg_mapping->gp_reg_scratch_1, cur_vreg,
