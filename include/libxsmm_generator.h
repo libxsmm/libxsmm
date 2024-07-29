e4b3a1c39652be0baaec9d6d68c8edcf6fc61092
Hans Pabst <hans.pabst@intel.com>
2024-07-29 15:14:00 +0200

    Embrace expressions (LIBXSMM_GEMM_NO_BYPASS).

--- include/libxsmm_generator.h
+++ include/libxsmm_generator.h
@@ -14,8 +14,8 @@
 #include "libxsmm_typedefs.h"
 
 #define LIBXSMM_GEMM_NO_BYPASS(FLAGS, ALPHA, BETA) ( \
-  (LIBXSMM_FEQ(1, ALPHA) /*|| LIBXSMM_FEQ(-1, ALPHA)*/) && \
-  (LIBXSMM_GEMM_FLAG_BETA_0 & (FLAGS) || LIBXSMM_FEQ(1, BETA)))
+  ((LIBXSMM_FEQ(1, ALPHA) /*|| LIBXSMM_FEQ(-1, ALPHA)*/)) && \
+  ((LIBXSMM_GEMM_FLAG_BETA_0 & (FLAGS)) || LIBXSMM_FEQ(1, BETA)))
 
 /** Initialize GEMM descriptor (generic). */
 LIBXSMM_API libxsmm_gemm_descriptor* libxsmm_gemm_descriptor_init(libxsmm_descriptor_blob* blob,
