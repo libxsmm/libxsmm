6a9507bc24931b12eafd90fb3c5fc1b2bc4ba922
Hans Pabst <hans.pabst@intel.com>
2024-09-17 17:02:04 +0200

    Avoid declaration without definition.

--- src/libxsmm_ext_gemm.h
+++ src/libxsmm_ext_gemm.h
@@ -13,6 +13,7 @@
 
 #include <libxsmm.h>
 
+#if defined(LIBXSMM_BUILD) && defined(LIBXSMM_BUILD_EXT) && defined(LIBXSMM_BLAS_WRAP_DYNAMIC)
 LIBXSMM_APIEXT libxsmm_dgemm_batch_strided_function libxsmmext_original_dgemm_batch_strided(void);
 LIBXSMM_APIEXT libxsmm_sgemm_batch_strided_function libxsmmext_original_sgemm_batch_strided(void);
 LIBXSMM_APIEXT libxsmm_dgemm_batch_function libxsmmext_original_dgemm_batch(void);
@@ -21,5 +22,6 @@ LIBXSMM_APIEXT libxsmm_dgemm_function libxsmmext_original_dgemm(void);
 LIBXSMM_APIEXT libxsmm_sgemm_function libxsmmext_original_sgemm(void);
 LIBXSMM_APIEXT libxsmm_dgemv_function libxsmmext_original_dgemv(void);
 LIBXSMM_APIEXT libxsmm_sgemv_function libxsmmext_original_sgemv(void);
+#endif
 
 #endif /*LIBXSMM_EXT_GEMM_H*/
