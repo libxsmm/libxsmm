df0d6c47512fc98212f57a6aa568a6757eaad555
Alexander Heinecke <alexander.heinecke@intel.com>
2024-09-27 22:56:59 -0700

    more fixes

--- samples/eltwise/eltwise_binary_simple.c
+++ samples/eltwise/eltwise_binary_simple.c
@@ -612,8 +612,12 @@ int test_binary_op( const libxsmm_blasint M, const libxsmm_blasint N, const libx
   printf("Linf rel.error: %.24f\n", norms_out.linf_rel);
   printf("Check-norm    : %.24f\n\n", norms_out.normf_rel);
 
-  if ( (op == MULADD_OP) && ((dtype_out == LIBXSMM_DATATYPE_BF16) || (dtype_out == LIBXSMM_DATATYPE_F16) || (dtype_out == LIBXSMM_DATATYPE_BF8) || (dtype_out == LIBXSMM_DATATYPE_HF8)) ) {
+  if ( (op == MULADD_OP) && ((dtype_out == LIBXSMM_DATATYPE_BF16) || (dtype_out == LIBXSMM_DATATYPE_F16)) ) {
     if ( norms_out.normf_rel > 0.001 ) {
+      ret = EXIT_FAILURE;
+    }
+  } else if ( (op == MULADD_OP) && ((dtype_out == LIBXSMM_DATATYPE_BF8) || (dtype_out == LIBXSMM_DATATYPE_HF8)) ) {
+    if ( norms_out.normf_rel > 0.01 ) {
       ret = EXIT_FAILURE;
     }
   } else {
