9a64c43a205022fd7c54132d36b428da5f17a1be
Alexander Heinecke <alexander.heinecke@intel.com>
2024-11-09 20:15:23 +0000

    attempt to fix A stride

--- src/generator_gemm_common_aarch64.c
+++ src/generator_gemm_common_aarch64.c
@@ -2152,8 +2152,13 @@ void libxsmm_generator_gemm_aarch64_setup_k_strides( libxsmm_generated_code*    
                                                 ((long long)i_xgemm_desc->lda - i_m_blocking) * i_micro_kernel_config->datatype_size_in );
       }
     } else {
-      libxsmm_aarch64_instruction_alu_set_imm64( io_generated_code, i_gp_reg_mapping->gp_reg_help_0,
-                                               ((long long)i_xgemm_desc->lda - i_m_blocking) * i_micro_kernel_config->datatype_size_in );
+      if ( i_xgemm_desc->k % 2 == 0 ) {
+        libxsmm_aarch64_instruction_alu_set_imm64( io_generated_code, i_gp_reg_mapping->gp_reg_help_0,
+                                                   ((long long)i_xgemm_desc->lda - i_m_blocking) * 2*i_micro_kernel_config->datatype_size_in );
+      } else {
+        libxsmm_aarch64_instruction_alu_set_imm64( io_generated_code, i_gp_reg_mapping->gp_reg_help_0,
+                                                   ((long long)i_xgemm_desc->lda - i_m_blocking) * i_micro_kernel_config->datatype_size_in );
+      }
     }
   }
 
