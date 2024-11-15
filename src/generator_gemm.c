bbd968ba7c1feedf52863eb21db189201eedcb90
Siddharth Rai <siddharth.rai@intel.com>
2024-11-15 04:49:13 -0800

    Added vector_length for F64

--- src/generator_gemm.c
+++ src/generator_gemm.c
@@ -733,6 +733,8 @@ void libxsmm_generator_gemm_kernel( libxsmm_generated_code*        io_generated_
 #endif
   } else if ( ( io_generated_code->arch == LIBXSMM_RV64 ) && LIBXSMM_DATATYPE_F32 == LIBXSMM_GEMM_GETENUM_AB_COMMON_PREC( l_xgemm_desc_mod.datatype ) ) {
     l_vector_length = 16;
+  } else if ( ( io_generated_code->arch == LIBXSMM_RV64 ) && LIBXSMM_DATATYPE_F64 == LIBXSMM_GEMM_GETENUM_AB_COMMON_PREC( l_xgemm_desc_mod.datatype ) ) {
+    l_vector_length = 8;
   } else {
     LIBXSMM_HANDLE_ERROR( io_generated_code, LIBXSMM_ERR_ARCH_PREC );
     return;
