f2544b86d56908a52f18208b46af80dcc5960063
Alexander Heinecke <alexander.heinecke@intel.com>
2025-08-04 18:59:08 +0530

    Revmoving BLAS wrappers (#981)
    * retired GEMM wrap, OMP GEMM parallelized frontends
    
    * removed manually introduced blas dependency
    
    * fixed bug in simple GEMM implementation
    
    * remove wrap tests
    
    * fixing allow lists of sparse tests
    
    * re-introduces simplied libxsmm_[s/s]gemm wrappers for JITed kernels only
    
    * fixing FORTRAN wrappers for libxsmm_[s/d]gemm, fixed smmbench
    
    * fix allocation
    
    * fix ABI definition
    
    * updaed asparse in reg tests
    
    * removed PP defines
    
    * remove more BLAS stuff
    
    * minor changes to test settings
    
    * attempt to fix MAC and RV64
    
    * polish rv64 fix
    
    * more rv64 fixes

--- include/libxsmm_utils.h
+++ include/libxsmm_utils.h
@@ -20,14 +20,4 @@
 #include "utils/libxsmm_timer.h"
 #include "utils/libxsmm_math.h"
 
-#if defined(__BLAS) && (1 == __BLAS)
-# if defined(__OPENBLAS)
-    LIBXSMM_EXTERN void openblas_set_num_threads(int num_threads);
-#   define LIBXSMM_BLAS_INIT openblas_set_num_threads(1);
-# endif
-#endif
-#if !defined(LIBXSMM_BLAS_INIT)
-# define LIBXSMM_BLAS_INIT
-#endif
-
 #endif /*LIBXSMM_UTILS_H*/
