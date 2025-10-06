10b7dc82b3c46157e76eb40e4e959555f895b24d
sidrai <siddharth.rai@intel.com>
2025-10-06 19:04:24 +0530

    Feature rv64 transpose (#985)
    * Fixed m=0 case for gemm
    
    * Added support for RVV transpose
    
    * Fixed 8x8 shuffle network masks
    
    * Fixed multi-block transpose case
    
    * Fixed partial m/n sizes
    
    * Fixed compilation errors and added gemm with transpose support
    
    * Fixed compilation errors
    
    * Few more compilation errors
    
    * Fixed declarations
    
    * Fixed transpose address calculation
    
    * Fixed FP64 transpose
    
    * Removed comments
    
    * Fixed precision check for unary ops
    
    * Minor changes to simplify the registers in a shuffle operation
    
    * Fixed CodeQL errors
    
    * Fixed stack setup bug
    
    * Minor fix in the comment
    
    * Minor fixe for batch reduce cases
    
    * update README
    
    * udpate README
    
    * Fixed strided brgemm
    
    * Fixed emu pointer address for offs brgemm
    
    * Fixed addrbr address caclculation for A^T
    
    * Restored setivli in unary binary kernel
    
    * Fixed register allocation and store for copy kernel
    
    * Fixed another register conflict between gemm and copy kernels
    
    * Removed dead code
    
    ---------
    
    Co-authored-by: Alexander Heinecke <alexander.heinecke@intel.com>

--- src/generator_mateltwise_transform_rv64.h
+++ src/generator_mateltwise_transform_rv64.h
@@ -26,6 +26,101 @@ void libxsmm_generator_transform_norm_to_normt_mbit_scalar_rv64_microkernel( lib
                                                                               const libxsmm_meltw_descriptor*         i_mateltwise_desc );
 
 LIBXSMM_API_INTERN
+void libxsmm_generator_transform_load_regblock_8x8_rv64( libxsmm_generated_code*  io_generated_code,
+                                                         const unsigned int       i_gp_reg_addr,
+                                                         const unsigned int       i_gp_reg_dst,
+                                                         const unsigned int       i_gp_reg_scratch,
+                                                         const unsigned int       i_reg_count,
+                                                         const libxsmm_mateltwise_kernel_config*  i_micro_kernel_config,
+                                                         const libxsmm_meltw_descriptor* i_mateltwise_desc );
+
+LIBXSMM_API_INTERN
+void libxsmm_generator_transform_store_regblock_8x8_rv64( libxsmm_generated_code* io_generated_code,
+                                                          const unsigned int      i_gp_reg_addr,
+                                                          const unsigned int      i_gp_reg_dst,
+                                                          const unsigned int      i_gp_reg_scratch,
+                                                          const unsigned int      i_reg_count,
+                                                          const libxsmm_mateltwise_kernel_config*  i_micro_kernel_config,
+                                                          const libxsmm_meltw_descriptor* i_mateltwise_desc );
+
+LIBXSMM_API_INTERN
+void libxsmm_generator_transform_load_regblock_4x4_rv64( libxsmm_generated_code*  io_generated_code,
+                                                         const unsigned int       i_gp_reg_addr,
+                                                         const unsigned int       i_gp_reg_dst,
+                                                         const unsigned int       i_gp_reg_scratch,
+                                                         const unsigned int       i_reg_count,
+                                                         const libxsmm_mateltwise_kernel_config*  i_micro_kernel_config,
+                                                         const libxsmm_meltw_descriptor* i_mateltwise_desc );
+
+LIBXSMM_API_INTERN
+void libxsmm_generator_transform_store_regblock_4x4_rv64( libxsmm_generated_code* io_generated_code,
+                                                          const unsigned int      i_gp_reg_addr,
+                                                          const unsigned int      i_gp_reg_dst,
+                                                          const unsigned int      i_gp_reg_scratch,
+                                                          const unsigned int      i_reg_count,
+                                                          const libxsmm_mateltwise_kernel_config*  i_micro_kernel_config,
+                                                          const libxsmm_meltw_descriptor* i_mateltwise_desc );
+
+LIBXSMM_API_INTERN
+void libxsmm_generator_transform_norm_to_normt_shuffle_regblock_32bit_8x8_rvv( libxsmm_generated_code* io_generated_code,
+                                                                               const unsigned int      i_gp_reg_dst_e,
+                                                                               const unsigned int      i_gp_reg_dst_o,
+                                                                               const unsigned int      i_gp_reg_scratch,
+                                                                               const unsigned int      i_mask_e,
+                                                                               const unsigned int      i_mask_o,
+                                                                               const unsigned int      i_shuffle_stride );
+
+LIBXSMM_API_INTERN
+void libxsmm_generator_transform_norm_to_normt_shuffle_regblock_64bit_4x4_rvv( libxsmm_generated_code* io_generated_code,
+                                                                               const unsigned int      i_gp_reg_dst_e,
+                                                                               const unsigned int      i_gp_reg_dst_o,
+                                                                               const unsigned int      i_gp_reg_scratch,
+                                                                               const unsigned int      i_mask_e,
+                                                                               const unsigned int      i_mask_o,
+                                                                               const unsigned int      i_shuffle_stride );
+
+LIBXSMM_API_INTERN
+void libxsmm_generator_transform_norm_to_normt_32bit_8x8_shufflenetwork_rvv( libxsmm_generated_code*     io_generated_code,
+                                                                             libxsmm_loop_label_tracker* io_loop_label_tracker,
+                                                                             const unsigned int          i_gp_reg_in,                                                                             const unsigned int          i_gp_reg_out,                                                                                                                                                                                                 const unsigned int          i_gp_reg_scratch,
+                                                                             const unsigned int          i_m,
+                                                                             const unsigned int          i_n,
+                                                                             const libxsmm_mateltwise_kernel_config*  i_micro_kernel_config,
+                                                                             const libxsmm_meltw_descriptor* i_mateltwise_desc );
+
+LIBXSMM_API_INTERN
+void libxsmm_generator_transform_norm_to_normt_64bit_4x4_shufflenetwork_rvv( libxsmm_generated_code*     io_generated_code,
+                                                                             libxsmm_loop_label_tracker* io_loop_label_tracker,
+                                                                             const unsigned int          i_gp_reg_in,                                                                             const unsigned int          i_gp_reg_out,
+                                                                             const unsigned int          i_gp_reg_scratch,
+                                                                             const unsigned int          i_m,
+                                                                             const unsigned int          i_n,
+                                                                             const libxsmm_mateltwise_kernel_config*  i_micro_kernel_config,
+                                                                             const libxsmm_meltw_descriptor* i_mateltwise_desc );
+
+LIBXSMM_API_INTERN
+void libxsmm_generator_transform_norm_to_normt_32bit_rvv_microkernel( libxsmm_generated_code*                 io_generated_code,
+                                                                      libxsmm_loop_label_tracker*             io_loop_label_tracker,
+                                                                      const unsigned int                      i_gp_reg_in,
+                                                                      const unsigned int                      i_gp_reg_out,
+                                                                      const unsigned int                      i_gp_reg_m_loop,
+                                                                      const unsigned int                      i_gp_reg_n_loop,
+                                                                      const unsigned int                      i_gp_reg_scratch,
+                                                                      const libxsmm_mateltwise_kernel_config* i_micro_kernel_config,
+                                                                      const libxsmm_meltw_descriptor*         i_mateltwise_desc );
+
+LIBXSMM_API_INTERN
+void libxsmm_generator_transform_norm_to_normt_64bit_rvv_microkernel( libxsmm_generated_code*                 io_generated_code,
+                                                                      libxsmm_loop_label_tracker*             io_loop_label_tracker,
+                                                                      const unsigned int                      i_gp_reg_in,
+                                                                      const unsigned int                      i_gp_reg_out,
+                                                                      const unsigned int                      i_gp_reg_m_loop,
+                                                                      const unsigned int                      i_gp_reg_n_loop,
+                                                                      const unsigned int                      i_gp_reg_scratch,
+                                                                      const libxsmm_mateltwise_kernel_config* i_micro_kernel_config,
+                                                                      const libxsmm_meltw_descriptor*         i_mateltwise_desc );
+
+LIBXSMM_API_INTERN
 void libxsmm_generator_transform_rv64_microkernel( libxsmm_generated_code*                        io_generated_code,
                                                    libxsmm_loop_label_tracker*                    io_loop_label_tracker,
                                                    libxsmm_mateltwise_gp_reg_mapping*             i_gp_reg_mapping,
