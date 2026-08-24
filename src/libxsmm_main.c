51871acfdb14df9b6d8e27003472364b3c93ac73
SY Wang <uwsy1059@qq.com>
2026-08-25 00:24:10 +0800

    Gate signal handler installation on verbosity (#1089)

--- src/libxsmm_main.c
+++ src/libxsmm_main.c
@@ -1356,10 +1356,17 @@ LIBXSMM_API_CTOR void libxsmm_init(void)
           libxsmm_timer_scale = libxsmm_timer_duration_rtc(s0, s1) / (t1 - t0);
         }
 #endif
-        internal_sigentries[0].signal = signal(SIGABRT, internal_libxsmm_signal);
-        internal_sigentries[0].signum = SIGABRT;
-        internal_sigentries[1].signal = signal(SIGSEGV, internal_libxsmm_signal);
-        internal_sigentries[1].signum = SIGSEGV;
+        { /* intercept fatal signals only when diagnostics were requested */
+          const char *const env_signal = getenv("LIBXSMM_SIGNAL");
+          const int enable_signal = (NULL != env_signal && '\0' != *env_signal)
+            ? atoi(env_signal) : (0 != libxsmm_verbosity);
+          if (0 != enable_signal) {
+            internal_sigentries[0].signal = signal(SIGABRT, internal_libxsmm_signal);
+            internal_sigentries[0].signum = SIGABRT;
+            internal_sigentries[1].signal = signal(SIGSEGV, internal_libxsmm_signal);
+            internal_sigentries[1].signum = SIGSEGV;
+          }
+        }
         result_atexit = atexit(internal_finalize);
         s1 = libxsmm_timer_tick_rtc(); t1 = libxsmm_timer_tick_tsc(); /* final timing */
         /* set timer-scale and determine start of the "uptime" (shown at termination) */
